46625ebe7b9d930526b71f369b0eb0deecad83e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:39:38 +0000

    Add package taskwiz/2.1.15/taskwiz-2.1.15.tar.gz

---
+++ packages/taskwiz/taskwiz-2.1.15.tar.gz/taskwiz-2.1.15/taskwiz/plugins/ask gemini pro.py
@@ -0,0 +1,37 @@
+"""
+LetMeDoIt AI Plugin - ask gemini pro
+
+Ask Google Gemini Pro for information
+
+[FUNCTION_CALL]
+"""
+
+
+from taskwiz import config
+from taskwiz.geminipro import GeminiPro
+
+def ask_gemini_pro(function_args):
+    config.stopSpinning()
+    query = function_args.get("query") # required
+    GeminiPro(temperature=config.llmTemperature).run(query)
+    return ""
+
+functionSignature = {
+    "name": "ask_gemini_pro",
+    "description": "Ask Gemini Pro to chat or provide information",
+    "parameters": {
+        "type": "object",
+        "properties": {
+            "query": {
+                "type": "string",
+                "description": "The original request in detail, including any supplementary information",
+            },
+        },
+        "required": ["query"],
+    },
+}
+
+config.pluginsWithFunctionCall.append("ask_gemini_pro")
+config.chatGPTApiFunctionSignatures.append(functionSignature)
+config.chatGPTApiAvailableFunctions["ask_gemini_pro"] = ask_gemini_pro
+config.inputSuggestions.append("Ask Gemini Pro: ")
