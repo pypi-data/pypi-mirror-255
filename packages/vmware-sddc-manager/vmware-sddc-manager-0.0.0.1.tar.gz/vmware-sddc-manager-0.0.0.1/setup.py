564a942711a8900584c9e8854020378c609e869e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:29:19 +0000

    Add package vmware-sddc-manager/0.0.0.1/vmware-sddc-manager-0.0.0.1.tar.gz

---
+++ packages/vmware-sddc-manager/vmware-sddc-manager-0.0.0.1.tar.gz/vmware-sddc-manager-0.0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="vmware-sddc-manager",
+    version="0.0.0.1",
+    author="VMware Inc.",
+    description="Python bindings for sddc manager",
+    packages=find_packages(),
+    install_requires=[
+        # List your package dependencies here
+    ],
+)
+
