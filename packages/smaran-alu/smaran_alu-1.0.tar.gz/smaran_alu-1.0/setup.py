267a3e94a1e374f5b17b9eac31de81b3b978140b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:38:23 +0000

    Add package smaran-alu/1.0/smaran_alu-1.0.tar.gz

---
+++ packages/smaran-alu/smaran_alu-1.0.tar.gz/smaran_alu-1.0/setup.py
@@ -0,0 +1,11 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name='smaran_alu',
+    version='1.0',
+    packages=find_packages(),
+    install_requires=[
+        # Add any dependencies here
+    ],
+)
