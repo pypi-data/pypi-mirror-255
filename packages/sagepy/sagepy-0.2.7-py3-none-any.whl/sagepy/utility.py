ec2e3e16d553e0cc992e4e896db36419c5c7f40a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:29:48 +0000

    Add package sagepy/0.2.7/sagepy-0.2.7-py3-none-any.whl

---
+++ packages/sagepy/sagepy-0.2.7-py3-none-any.whl/sagepy/utility.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+
+def mass_to_mod(mass: float) -> str:
+    """ Convert a mass to a UNIMOD modification annotation.
+
+    Args:
+        mass: a mass in Da
+
+    Returns:
+        a UNIMOD modification annotation
+    """
+    maybe_key = int(np.round(mass))
+    # TODO: find a better way to do the map-back
+    mod_dict = {
+        42: '[UNIMOD:1]',
+        57: '[UNIMOD:4]',
+        80: '[UNIMOD:21]',
+        16: '[UNIMOD:35]',
+        119: '[UNIMOD:312]',
+    }
+    # try to translate to UNIMOD annotation
+    try:
+        return mod_dict[maybe_key]
+    except KeyError:
+        raise KeyError(f"Rounded mass not in dict: {maybe_key}")
