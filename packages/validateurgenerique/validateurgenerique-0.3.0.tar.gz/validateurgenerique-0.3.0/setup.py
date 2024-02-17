8ed4d1f3be5b4b7950294528e7c0df9d42f3b5eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:34:34 +0000

    Add package validateurgenerique/0.3.0/validateurgenerique-0.3.0.tar.gz

---
+++ packages/validateurgenerique/validateurgenerique-0.3.0.tar.gz/validateurgenerique-0.3.0/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+with open ('README.txt','r') as f:
+    ldescription=f.read()
+
+setup(
+    name='validateurgenerique',
+    version='0.3.0', 
+    author='Gharbi Youssef',
+    author_email='gharbiyoussef884@gmail.com',
+    description="A tool for validating, transforming, and controlling data.",
+    long_description=ldescription,
+    long_description_content_type='text/markdown',
+    url="https://gitlab.com/gharbiyoussef884/pythonvalidatorpackage", 
+    license="MIT",
+    packages=find_packages(),
+    python_requires='>=3.6',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "jsonschema>=3.0.0", 
+        "flask>=1.0.0",  
+    ],
+    extras_require={
+        "dev": [
+            "pytest>=6.0.0",
+        ]
+    },
+    project_urls={
+        "Source": "https://gitlab.com/gharbiyoussef884/pythonvalidatorpackage",
+        "Bug Reports": "https://gitlab.com/gharbiyoussef884/pythonvalidatorpackage/-/issues",
+    },
+)
