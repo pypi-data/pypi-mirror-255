4fa3ac3fc4339968acdc8d98714231e550050c2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:52:43 +0000

    Add package magplots/0.0.1/magplots-0.0.1.tar.gz

---
+++ packages/magplots/magplots-0.0.1.tar.gz/magplots-0.0.1/README.md
@@ -0,0 +1,2 @@
+# magplots
+A python library to pull data and make plots from ground magnetometers. Emphasis on plots for comparing magnetometers at conjugate points.
---
+++ packages/magplots/magplots-0.0.1.tar.gz/magplots-0.0.1/magFunctions.py
@@ -0,0 +1,40 @@
+# functions for visualization of magnetometer data. 
+
+# Importing packages:
+# For fill_nan:
+from scipy import interpolate
+
+############################################################################################################################### 
+
+# #  FILL_NAN: Function to eliminate NaN values from a 1D numpy array.
+
+def fill_nan(y):
+    """
+        Fit a linear regression to the non-nan y values
+
+        Arguments:
+            y      : 1D numpy array with NaNs in it
+
+        Returns:
+            Same thing; no NaNs.
+    """
+    
+    # Fit a linear regression to the non-nan y values
+
+    # Create X matrix for linreg with an intercept and an index
+    X = np.vstack((np.ones(len(y)), np.arange(len(y))))
+
+    # Get the non-NaN values of X and y
+    X_fit = X[:, ~np.isnan(y)]
+    y_fit = y[~np.isnan(y)].reshape(-1, 1)
+
+    # Estimate the coefficients of the linear regression
+    # beta = np.linalg.lstsq(X_fit.T, y_fit)[0]
+    beta = np.linalg.lstsq(X_fit.T, y_fit, rcond=-1)[0]
+
+
+    # Fill in all the nan values using the predicted coefficients
+    y.flat[np.isnan(y)] = np.dot(X[:, np.isnan(y)].T, beta)
+    return y
+
+############################################################################################################################### 
