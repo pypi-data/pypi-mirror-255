1e40e76a780b4cd05238c420f1cf98cd99d88c0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:53:09 +0000

    Add package astronomer-telescope/3.2.1/astronomer_telescope-3.2.1-py3-none-any.whl

---
+++ packages/astronomer-telescope/astronomer_telescope-3.2.1-py3-none-any.whl/astronomer_telescope/__init__.py
@@ -0,0 +1,5 @@
+# type: ignore[attr-defined]
+"""A tool to observe distant (or local!) Airflow installations, and gather metadata or other required data."""
+
+__version__: str = "3.2.1"
+version: str = __version__
