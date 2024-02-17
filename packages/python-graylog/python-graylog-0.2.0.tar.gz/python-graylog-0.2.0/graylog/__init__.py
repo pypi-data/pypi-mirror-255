d66b4b6ef468ccd41d9d734d919a8390209885e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:57:01 +0000

    Add package python-graylog/0.2.0/python-graylog-0.2.0.tar.gz

---
+++ packages/python-graylog/python-graylog-0.2.0.tar.gz/python-graylog-0.2.0/graylog/__init__.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""graypy
+Python logging handlers that send messages in the
+Graylog Extended Log Format (GELF).
+Modules:
+ + :mod:`.http` - HTTP GELF Logging Handlers
+ + :mod:`.tcp` - TCP GELF Logging Handler
+ + :mod:`.tls` - TLS GELF Logging Handler
+ + :mod:`.udp` - UDP GELF Logging Handler
+"""
+
+from graylog.handlers import (
+    WAN_CHUNK,
+    LAN_CHUNK,
+)
+from graylog.http import GELFHTTPHandler
+from graylog.tcp import GELFTCPHandler
+from graylog.tls import GELFTLSHandler
+from graylog.udp import GELFUDPHandler
+
+__version__ = (0, 2, 0)
