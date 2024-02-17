eae16905d5c8ddfb6e824d04169c31f81def2cfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:35:44 +0000

    Add package do-nothing/1.2/do_nothing-1.2.tar.gz

---
+++ packages/do-nothing/do_nothing-1.2.tar.gz/do_nothing-1.2/do_nothing/__init__.py
@@ -0,0 +1,2 @@
+from senthil.helper import sleep, help_other
+from interns.utils import ask_for_help
---
+++ packages/do-nothing/do_nothing-1.2.tar.gz/do_nothing-1.2/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="do_nothing",
+    version="1.2",
+    packages=find_packages(),
+)
