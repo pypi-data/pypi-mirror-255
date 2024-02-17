8590d3ddec75c90e248b7a0ba0b5178acb982a3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:29:55 +0000

    Add package matool-dj/0.1.1/matool_dj-0.1.1.tar.gz

---
+++ packages/matool-dj/matool_dj-0.1.1.tar.gz/matool_dj-0.1.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='matool_dj',
+    version='0.1.1',
+    author='Darshit Joshi',
+    author_email='darshitj@dosepack.com',
+    description='this is a migration automation tool to ease the process of migration for multiple environments',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
