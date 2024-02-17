9b1e369397d1bb7c181f0d19cfff28b0e5c23a0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:43:50 +0000

    Add package ga-bi-distribution-classes/0.1/ga_bi_distribution_classes-0.1.tar.gz

---
+++ packages/ga-bi-distribution-classes/ga_bi_distribution_classes-0.1.tar.gz/ga_bi_distribution_classes-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name="ga_bi_distribution_classes",
+      version="0.1",
+      description="Gaussian and Binomial distribution classes",
+      packages=["ga_bi_distribution_classes"],
+      zip_safe=False)
