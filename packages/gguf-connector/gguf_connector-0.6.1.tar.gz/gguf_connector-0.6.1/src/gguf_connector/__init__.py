5643e2c4094b2eaf14ec543cac8ca2a5d924028b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:52:49 +0000

    Add package gguf-connector/0.6.1/gguf_connector-0.6.1.tar.gz

---
+++ packages/gguf-connector/gguf_connector-0.6.1.tar.gz/gguf_connector-0.6.1/src/gguf_connector/__init__.py
@@ -0,0 +1,27 @@
+# !/usr/bin/env python3
+
+__version__="0.6.1"
+
+def __init__():
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-v', '--version', action='version', version='%(prog)s ' + __version__)
+    subparsers = parser.add_subparsers(title="subcommands", dest="subcommand", help="choose a subcommand:")
+    subparsers.add_parser('cpp', help='[cpp] connector cpp')
+    subparsers.add_parser('gpp', help='[gpp] connector gpp')
+    subparsers.add_parser('g', help='[g] cli connector g')
+    subparsers.add_parser('c', help='[c] gui connector c')
+    subparsers.add_parser('m', help='[m] menu')
+
+    args = parser.parse_args()
+
+    if args.subcommand == 'm':
+        from gguf_connector import m
+    elif args.subcommand=="c":
+        from gguf_connector import c
+    elif args.subcommand=="cpp":
+        from gguf_connector import cpp
+    elif args.subcommand=="g":
+        from gguf_connector import g
+    elif args.subcommand=="gpp":
+        from gguf_connector import gpp
