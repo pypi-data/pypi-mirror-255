a0960351d697a07a8cf2fd054c30c9d67a41cb50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:39:20 +0000

    Add package miml-package/0.0.2/miml_package-0.0.2-py3-none-any.whl

---
+++ packages/miml-package/miml_package-0.0.2-py3-none-any.whl/miml/tutorial/show_data.py
@@ -0,0 +1,10 @@
+from datasets.load_dataset import load_dataset
+
+dataset = load_dataset("../datasets/miml_birds.csv")
+
+dataset.show_dataset()
+
+#print(dataset.get_instance("4", 1))
+
+dataset.describe()
+
