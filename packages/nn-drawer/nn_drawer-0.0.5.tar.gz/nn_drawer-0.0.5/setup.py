96f869f4c29c8b692e3aef37afd3d08e4f147c09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:44:40 +0000

    Add package nn-drawer/0.0.5/nn_drawer-0.0.5.tar.gz

---
+++ packages/nn-drawer/nn_drawer-0.0.5.tar.gz/nn_drawer-0.0.5/setup.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+
+from io import open
+from setuptools import setup
+
+"""
+:authors: EvgeniBondarev
+:license: OSI Approved :: Apache Software License
+:copyright: (c) 2024 EvgeniBondarev
+"""
+
+version = '0.0.5'
+
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='nn_drawer',
+    version=version,
+
+    author='nn_drawer',
+    author_email='peoplesdreamer@gmail.ru',
+
+    description=(
+        u'Иструмент для визуализации структуры нейронных сетей.'
+    ),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+
+    url='https://github.com/EvgeniBondarev/Neural-Network-Drawer.git',
+    download_url='https://github.com/EvgeniBondarev/Neural-Network-Drawer/archive/refs/heads/master.zip',
+
+    license='Apache License, Version 2.0, see LICENSE file',
+
+    packages=['nn_drawer'],
+    install_requires=['matplotlib'],
+
+    classifiers=[
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Programming Language :: Python :: Implementation :: CPython',
+    ]
+)
+
