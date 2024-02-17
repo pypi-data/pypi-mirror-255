6f411203a9e9c476d579393862bab930e5d92387
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:34:41 +0000

    Add package shapelets-native/2.2.5/shapelets_native-2.2.5-cp38-cp38-macosx_10_14_x86_64.whl

---
+++ packages/shapelets-native/shapelets_native-2.2.5-cp38-cp38-macosx_10_14_x86_64.whl/shapelets_native/_version.pyi
@@ -0,0 +1,9 @@
+# Copyright (c) 2022 Shapelets.io
+#
+# This software is released under the MIT License.
+# https://opensource.org/licenses/MIT
+
+from __future__ import annotations
+
+version: str
+version_tuple: tuple[int, int, int] | tuple[int, int, int, str, str]
