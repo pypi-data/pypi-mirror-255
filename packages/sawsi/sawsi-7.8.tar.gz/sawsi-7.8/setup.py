5fca8b212c5c826c98f721b0f415419c4913d831
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:57:58 +0000

    Add package sawsi/7.8/sawsi-7.8.tar.gz

---
+++ packages/sawsi/sawsi-7.8.tar.gz/sawsi-7.8/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="sawsi",
+    version="7.8",
+    packages=find_packages(),
+    include_package_data=True,
+    py_modules=['make'],
+    install_requires=[
+        'requests==2.31.0',
+        'click~=8.1.7',
+    ],
+    entry_points='''
+        [console_scripts]
+        sawsi=make:cli
+    ''',
+)
