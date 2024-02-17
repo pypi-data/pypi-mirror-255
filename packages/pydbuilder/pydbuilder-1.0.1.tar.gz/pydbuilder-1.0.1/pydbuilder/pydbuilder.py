b4e3b7f5eb7d3d77e6264d08a0efaa31fac19901
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:35:32 +0000

    Add package pydbuilder/1.0.1/pydbuilder-1.0.1.tar.gz

---
+++ packages/pydbuilder/pydbuilder-1.0.1.tar.gz/pydbuilder-1.0.1/pydbuilder/pydbuilder.py
@@ -0,0 +1,87 @@
+import subprocess
+import random
+import string
+import shutil
+import sys
+import os
+
+def get_dir_path():
+    executable = os.path.abspath(os.sys.argv[0])
+    return os.path.dirname(executable)
+
+def random_name():
+    upper_letters = string.ascii_uppercase
+    lower_letters = string.ascii_lowercase
+    numbers = string.digits
+    
+    password = ''
+    password += random.choice(upper_letters)
+    password += random.choice(lower_letters)
+    password += random.choice(upper_letters)
+    password += random.choice(lower_letters)
+    password += random.choice(numbers)
+    password += random.choice(upper_letters)
+    password += random.choice(lower_letters)
+    
+    return password
+
+def find_and_move_pyd(directory, target_dir, source_name):
+    for filename in os.listdir(directory):
+        if filename.startswith('code.') and filename.endswith('.pyd'):
+            source_path = os.path.join(directory, filename)
+            current_path = os.path.join(target_dir, 'code.pyd')
+
+            try:
+                os.rename(source_path, current_path)
+                os.replace(current_path, os.path.join(target_dir, f'{source_name}.pyd'))
+            except Exception as er:
+                print(f'Ошибка при перемещении файла: {er}')
+
+
+if __name__ == '__main__':
+    os.system('cls || clear')
+
+    if len(sys.argv) != 2:
+        print('Используйте: auto_compile.py <путь_к_файлу>')
+    else:
+        file_path = sys.argv[1]
+        if os.path.isfile(file_path):
+            try:
+                with open(file_path, 'r', encoding='UTF-8') as file:
+                    main_code = file.read()
+            except Exception as er:
+                print(f'Ошибка при чтении файла: {er}')
+                raise SystemExit(0)
+
+            os.system(f'title Cythonizing "{os.path.basename(file_path)}"...')
+            source_name = os.path.basename(file_path.split('.')[0])
+            source_path = os.path.dirname(file_path)
+            dir_path = os.path.join('C:/Windows/Temp', random_name())
+            os.mkdir(dir_path)
+
+            with open(os.path.join(dir_path, 'code.pyx'), 'w', encoding='UTF-8') as file:
+                file.write(main_code)
+
+            setup_code = 'from setuptools import setup\nfrom Cython.Build import cythonize\n\nsetup(\n    ext_modules=cythonize(\'code.pyx\', compiler_directives={\'language_level\': \'3\'})\n)'
+
+            with open(os.path.join(dir_path, 'setup.py'), 'w', encoding='UTF-8') as file:
+                file.write(setup_code)
+
+            try:
+                subprocess.run('python setup.py build_ext --inplace', cwd=dir_path, check=True, stdout=subprocess.PIPE)
+            except subprocess.CalledProcessError as e:
+                os.system('cls || clear')
+                print(f'Ошибка при выполнении команды: {e}')
+                raise SystemExit(0)
+
+            try:
+                find_and_move_pyd(dir_path, source_path, source_name)
+                print(f'Файл "{source_name}.pyd" успешно создан!')
+            except Exception as er:
+                os.system('cls || clear')
+                print(f'Ошибка при перемещении файла: {er}')
+                raise SystemExit(0)
+
+            shutil.rmtree(dir_path)
+        else:
+            print(f'Ошибка: Путь не найден')
