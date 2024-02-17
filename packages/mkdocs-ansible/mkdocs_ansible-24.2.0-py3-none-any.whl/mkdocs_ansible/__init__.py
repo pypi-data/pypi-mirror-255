e2e44f27be4f8c258b779a972bc5e049cb0c6561
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:40:43 +0000

    Add package mkdocs-ansible/24.2.0/mkdocs_ansible-24.2.0-py3-none-any.whl

---
+++ packages/mkdocs-ansible/mkdocs_ansible-24.2.0-py3-none-any.whl/mkdocs_ansible/__init__.py
@@ -0,0 +1,8 @@
+"""mkdocs-ansible theme."""
+try:
+    from ._version import version as __version__  # type: ignore
+
+except ImportError:  # pragma: no cover
+    __version__ = "0.1.dev1"
+
+__all__ = ("__version__",)
