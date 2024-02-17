5f350ef2a861c49862a36f5d365de3543b7f38be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:39:32 +0000

    Add package udacity-binomial-gaussian-package/0.0.1/udacity_binomial_gaussian_package-0.0.1.tar.gz

---
+++ packages/udacity-binomial-gaussian-package/udacity_binomial_gaussian_package-0.0.1.tar.gz/udacity_binomial_gaussian_package-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='udacity_binomial_gaussian_package',
+      version='0.0.1',
+      description='Binomial and Gaussian distributions',
+      packages=['udacity_binomial_gaussian_package'],
+      author = "Saburi Yusuf",
+      author_email = "sarboldipo80@gmail.com",
+      zip_safe=False)
