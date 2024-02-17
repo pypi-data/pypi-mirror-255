ec28f0bdb7e9c2067212361e6266d9184e7ee45a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:29:13 +0000

    Add package actinet/0.0.dev3/actinet-0.0.dev3.tar.gz

---
+++ packages/actinet/actinet-0.0.dev3.tar.gz/actinet-0.0.dev3/src/actinet/__init__.py
@@ -0,0 +1,12 @@
+name = "actinet"
+__author__ = "Shing Chan, Aidan Acquah, Charilaos Zisou, Aiden Doherty"
+__maintainer__ = "Shing Chan"
+__maintainer_email__ = "shing.chan@ndph.ox.ac.uk"
+__license__ = "See LICENSE file."
+
+__model_version__ = "ssl_ukb_c24_rw_20240204"
+__model_md5__ = "c13390ff024e4714eb027aa66160e2eb"
+
+from . import _version
+
+__version__ = _version.get_versions()["version"]
