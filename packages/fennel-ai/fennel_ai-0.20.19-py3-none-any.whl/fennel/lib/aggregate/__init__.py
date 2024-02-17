0b8569c5374982bed1c7b042b1959606e52991c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:48:56 +0000

    Add package fennel-ai/0.20.19/fennel_ai-0.20.19-py3-none-any.whl

---
+++ packages/fennel-ai/fennel_ai-0.20.19-py3-none-any.whl/fennel/lib/aggregate/__init__.py
@@ -0,0 +1,13 @@
+from fennel.lib.aggregate.aggregate import (
+    Count,
+    Distinct,
+    Sum,
+    LastK,
+    Min,
+    Max,
+    Average,
+    AggregateType,
+    TopK,
+    CF,
+    Stddev,
+)
