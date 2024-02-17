9df35470abc3b1af744e53a8f5b02ac4788e1bdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:52:48 +0000

    Add package imputetspy/0.1.3/imputetspy-0.1.3-py3-none-any.whl

---
+++ packages/imputetspy/imputetspy-0.1.3-py3-none-any.whl/imputetspy/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding:utf-8 -*- 
+from imputetspy.main import na_ma, na_mean, na_random, na_interpolate, locf, nocb
+
+package_data={'imputetspy' :['imputetspy/data/*']}
+
+
+__version__ = '0.1.3'
+
+# __all__ = (
+#     ts_airgap, ts_nh4, ts_heating
+# )
