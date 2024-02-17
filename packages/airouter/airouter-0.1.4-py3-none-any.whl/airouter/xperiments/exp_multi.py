ce4df37c7c5532140a821af596e9441ac73af568
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:39:23 +0000

    Add package airouter/0.1.4/airouter-0.1.4-py3-none-any.whl

---
+++ packages/airouter/airouter-0.1.4-py3-none-any.whl/airouter/xperiments/exp_multi.py
@@ -0,0 +1,13 @@
+import airouter
+
+if __name__ == '__main__':
+  outputs = airouter.StreamedCompletion.generate(
+    model='gpt-3.5-turbo-1106',
+    temperature=0.0,
+    max_tokens=100,
+    messages=[{'role': 'user', 'content': 'write a story about the big Large Language Models convention'}],
+  )
+  print(outputs)
+  for o in outputs:
+    print(o)
+
