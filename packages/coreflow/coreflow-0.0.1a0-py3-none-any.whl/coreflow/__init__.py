ec4d85c74f3e19d7e854f42b6ec300e50c1cbecd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:58:47 +0000

    Add package coreflow/0.0.1a0/coreflow-0.0.1a0-py3-none-any.whl

---
+++ packages/coreflow/coreflow-0.0.1a0-py3-none-any.whl/coreflow/__init__.py
@@ -0,0 +1,16 @@
+__version__ = "0.0.1a0"
+
+from coreflow.simulator import ScalSimulator
+from coreflow.simulators.docker_client import DockerClient
+from coreflow.io.plotting import Plot
+from coreflow.analytics.interpret import Analytics
+from coreflow.analytics.buckley_leverett import BuckleyLeverett
+
+# Use __all__ to let type checkers know what is part of the public API.
+__all__ = [
+    "DockerClient",
+    "ScalSimulator",
+    "Analytics",
+    "BuckleyLeverett",
+    "Plot",
+]
