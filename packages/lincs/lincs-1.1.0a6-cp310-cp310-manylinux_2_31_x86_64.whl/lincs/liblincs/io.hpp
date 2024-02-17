af2a7434e4ff028842acb09d4c80d2c2f17e3302
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:57:00 +0000

    Add package lincs/1.1.0a6/lincs-1.1.0a6-cp310-cp310-manylinux_2_31_x86_64.whl

---
+++ packages/lincs/lincs-1.1.0a6-cp310-cp310-manylinux_2_31_x86_64.whl/lincs/liblincs/io.hpp
@@ -0,0 +1,11 @@
+// Copyright 2023-2024 Vincent Jacques
+
+#ifndef LINCS__IO_HPP
+#define LINCS__IO_HPP
+
+#include "io/alternatives.hpp"
+#include "io/model.hpp"
+#include "io/problem.hpp"
+#include "io/validation.hpp"
+
+#endif  // LINCS__IO_HPP
