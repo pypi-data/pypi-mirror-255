1f4571e79325ed3cf717aea3a8ecebf72cd52270
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:39:11 +0000

    Add package neonize/0.0.7/neonize-0.0.7-py3-none-any.whl

---
+++ packages/neonize/neonize-0.0.7-py3-none-any.whl/neonize/types.py
@@ -0,0 +1,46 @@
+import typing
+
+from neonize.proto.def_pb2 import (
+    AudioMessage,
+    ButtonsMessage,
+    ContactsArrayMessage,
+    DocumentMessage,
+    EventMessage,
+    ExtendedTextMessage,
+    GroupInviteMessage,
+    ImageMessage,
+    ListMessage,
+    ListResponseMessage,
+    LiveLocationMessage,
+    Message,
+    MessageHistoryBundle,
+    PollCreationMessage,
+    ProductMessage,
+    StickerMessage,
+    VideoMessage,
+)
+
+MessageServerID = typing.NewType("MessageServerID", int)
+MessageWithContextInfo = typing.TypeVar(
+    "MessageWithContextInfo",
+    ImageMessage,
+    ContactsArrayMessage,
+    ExtendedTextMessage,
+    DocumentMessage,
+    AudioMessage,
+    VideoMessage,
+    LiveLocationMessage,
+    StickerMessage,
+    GroupInviteMessage,
+    GroupInviteMessage,
+    ProductMessage,
+    ListMessage,
+    ListMessage,
+    ListResponseMessage,
+    ButtonsMessage,
+    ButtonsMessage,
+    PollCreationMessage,
+    MessageHistoryBundle,
+    EventMessage,
+    ContactsArrayMessage,
+)
