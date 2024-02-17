b014aa58cd01f55901f47c1f8cec353b319404ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:34:36 +0000

    Add package aminimalreconciler/0.1.0/aminimalreconciler-0.1.0.tar.gz

---
+++ packages/aminimalreconciler/aminimalreconciler-0.1.0.tar.gz/aminimalreconciler-0.1.0/setup.py
@@ -0,0 +1,20 @@
+# Copyright (c) 2019, <NAME>
+# All rights reserved.
+from setuptools import setup, find_packages
+
+setup(
+    name='aminimalreconciler',
+    version='0.1.0',
+    description='reconciler',
+    url='https://github.com/muathendirangu/reconcile',
+    author='muathendirangu',
+    author_email='muathe.ndirangu@gmail.com',
+    license='MIT',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'reconciler=reconciler.reconciler:main'
+        ],
+    },
+    long_description_content_type='text/markdown'
+)
