c7ed560778a9a5f5306f5df6c1982fdf6575b65c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:34:49 +0000

    Add package sqladmin-litestar/0.16.1/sqladmin_litestar-0.16.1-py3-none-any.whl

---
+++ packages/sqladmin-litestar/sqladmin_litestar-0.16.1-py3-none-any.whl/sqladmin/__init__.py
@@ -0,0 +1,13 @@
+from sqladmin.application import Admin, action, expose
+from sqladmin.models import BaseView, ModelView
+
+__version__ = "0.16.1"
+__original_version__ = "0.16.0"
+
+__all__ = [
+    "Admin",
+    "expose",
+    "action",
+    "BaseView",
+    "ModelView",
+]
