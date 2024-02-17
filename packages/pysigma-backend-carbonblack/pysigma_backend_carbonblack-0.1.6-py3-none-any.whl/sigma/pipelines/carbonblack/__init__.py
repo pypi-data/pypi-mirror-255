b3433657b3d4d4a97cc3beb79972517fceaec50a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:44:55 +0000

    Add package pysigma-backend-carbonblack/0.1.6/pysigma_backend_carbonblack-0.1.6-py3-none-any.whl

---
+++ packages/pysigma-backend-carbonblack/pysigma_backend_carbonblack-0.1.6-py3-none-any.whl/sigma/pipelines/carbonblack/__init__.py
@@ -0,0 +1,7 @@
+from .carbonblack import CarbonBlack_pipeline, CarbonBlackResponse_pipeline
+# TODO: add all pipelines that should be exposed to the user of your backend in the import statement above.
+
+pipelines = {
+    "carbonblack_pipeline": CarbonBlack_pipeline,
+    "carbonblackresponse_pipeline": CarbonBlackResponse_pipeline
+}
