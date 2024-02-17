eb1af4a380bc4643c0e6249fa3022af3be42bca2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:28:58 +0000

    Add package webloc2html/0.0.3/webloc2html-0.0.3.tar.gz

---
+++ packages/webloc2html/webloc2html-0.0.3.tar.gz/webloc2html-0.0.3/upload.sh
@@ -0,0 +1,7 @@
+pytest
+api_token=$(op item get "pypi api key" --field "Anmeldedaten")
+python3 -m pip install --upgrade twine
+python3 -m pip install --upgrade build
+rm -rf dist/*
+python3 -m build
+python3 -m twine upload --repository pypi dist/* --user __token__ --password $api_token
