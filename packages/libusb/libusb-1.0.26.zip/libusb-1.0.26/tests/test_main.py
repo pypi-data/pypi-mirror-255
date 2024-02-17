7b7686e7d101979341948985f309b2b572be7fa5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:45:50 +0000

    Add package libusb/1.0.26/libusb-1.0.26.zip

---
+++ packages/libusb/libusb-1.0.26.zip/libusb-1.0.26/tests/test_main.py
@@ -0,0 +1,16 @@
+# Copyright (c) 2016 Adam Karpierz
+# Licensed under the zlib/libpng License
+# https://opensource.org/license/zlib
+
+import unittest
+
+import libusb
+
+
+class MainTestCase(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_main(self):
+        pass
