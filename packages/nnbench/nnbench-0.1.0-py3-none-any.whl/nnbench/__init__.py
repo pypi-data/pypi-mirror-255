0fe79187fdcca0dbbf5a24958ab5b218e254d232
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:30:39 +0000

    Add package nnbench/0.1.0/nnbench-0.1.0-py3-none-any.whl

---
+++ packages/nnbench/nnbench-0.1.0-py3-none-any.whl/nnbench/__init__.py
@@ -0,0 +1,31 @@
+"""A framework for organizing and running benchmark workloads on machine learning models."""
+
+from importlib.metadata import PackageNotFoundError, entry_points, version
+
+try:
+    __version__ = version("nnbench")
+except PackageNotFoundError:
+    # package is not installed
+    pass
+
+# TODO: This naming is unfortunate
+from .core import benchmark, parametrize, product
+from .reporter import BenchmarkReporter, register_reporter
+from .runner import BenchmarkRunner
+from .types import Parameters
+
+
+def add_reporters():
+    eps = entry_points()
+
+    if hasattr(eps, "select"):  # Python 3.10+ / importlib.metadata >= 3.9.0
+        reporters = eps.select(group="nnbench.reporters")
+    else:
+        reporters = eps.get("nnbench.reporters", [])  # type: ignore
+
+    for rep in reporters:
+        key, clsname = rep.name.split("=", 1)
+        register_reporter(key, clsname)
+
+
+add_reporters()
