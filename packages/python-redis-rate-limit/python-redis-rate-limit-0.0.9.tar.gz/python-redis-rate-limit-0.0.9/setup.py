f2f139b713ab1424050845ec9b74b3ea8bbd0f02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:49:12 +0000

    Add package python-redis-rate-limit/0.0.9/python-redis-rate-limit-0.0.9.tar.gz

---
+++ packages/python-redis-rate-limit/python-redis-rate-limit-0.0.9.tar.gz/python-redis-rate-limit-0.0.9/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('requirements.txt') as f:
+    requires = f.readlines()
+
+setup(
+    name='python-redis-rate-limit',
+    version='0.0.9',
+    description=u'Python Rate Limiter based on Redis.',
+    long_description=readme,
+    long_description_content_type='text/x-rst',
+    author=u'Victor Torres',
+    author_email=u'vpaivatorres@gmail.com',
+    url=u'https://github.com/evoluxbr/python-redis-rate-limit',
+    license=u'MIT',
+    packages=find_packages(exclude=('tests', 'docs')),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'License :: OSI Approved :: MIT License'
+    ],
+    install_requires=requires
+)
