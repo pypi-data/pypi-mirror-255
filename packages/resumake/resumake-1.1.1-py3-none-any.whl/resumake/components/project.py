fd2d2c5b490d4a63c04304a5e16828534feb52cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:57:19 +0000

    Add package resumake/1.1.1/resumake-1.1.1-py3-none-any.whl

---
+++ packages/resumake/resumake-1.1.1-py3-none-any.whl/resumake/components/project.py
@@ -0,0 +1,10 @@
+from .experience import Experience
+
+
+class Project(Experience):
+    """Alias for Experience class
+
+    Main difference is that addresses are links instead of locations
+    """
+
+    pass
