d131eb8beacd72ab099df295415eb548efc256a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:39:31 +0000

    Add package roche-datachapter-lib/0.0.26/roche_datachapter_lib-0.0.26.tar.gz

---
+++ packages/roche-datachapter-lib/roche_datachapter_lib-0.0.26.tar.gz/roche_datachapter_lib-0.0.26/src/roche_datachapter_lib/value_input_option.py
@@ -0,0 +1,9 @@
+"""ValueInputOption Class"""
+from enum import Enum
+
+
+class ValueInputOption(Enum):
+    """Google Spreadahseet update ValueInputOption Class"""
+    RAW = 'RAW'
+    USER_ENTERED = 'USER_ENTERED'
+    UNSPECIFIED = 'INPUT_VALUE_OPTION_UNSPECIFIED'
