3630e1d2403ffbb14ebd4fba09e86899c87129a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:43:58 +0000

    Add package determined/0.28.0rc4/determined-0.28.0rc4-py3-none-any.whl

---
+++ packages/determined/determined-0.28.0rc4-py3-none-any.whl/determined/cli/task.py
@@ -0,0 +1,222 @@
+from argparse import Namespace
+from functools import partial
+from typing import Any, Dict, List, Union, cast
+
+from termcolor import colored
+
+from determined import cli
+from determined.cli import ntsc, render
+from determined.common import api
+from determined.common.api import authentication, bindings
+from determined.common.api.bindings import v1AllocationSummary
+from determined.common.declarative_argparse import Arg, Cmd, Group
+
+
+def render_tasks(args: Namespace, tasks: Dict[str, v1AllocationSummary]) -> None:
+    """Render tasks for JSON, tabulate or csv output.
+
+    The tasks parameter requires a map from allocation IDs to v1AllocationSummary
+    describing individual tasks.
+    """
+
+    def agent_info(t: v1AllocationSummary) -> Union[str, List[str]]:
+        if t.resources is None:
+            return "unassigned"
+        agents = [a for r in t.resources for a in (r.agentDevices or {})]
+        if len(agents) == 1:
+            agent = agents[0]  # type: str
+            return agent
+        return agents
+
+    if args.json:
+        render.print_json({a: t.to_json() for (a, t) in tasks.items()})
+        return
+
+    headers = [
+        "Task ID",
+        "Allocation ID",
+        "Name",
+        "Slots Needed",
+        "Registered Time",
+        "Agent",
+        "Priority",
+        "Resource Pool",
+        "Ports",
+    ]
+    values = [
+        [
+            task.taskId,
+            task.allocationId,
+            task.name,
+            task.slotsNeeded,
+            render.format_time(task.registeredTime),
+            agent_info(task),
+            task.priority if task.schedulerType == "priority" else "N/A",
+            task.resourcePool,
+            ",".join(
+                map(
+                    str,
+                    sorted([pp.port for pp in (task.proxyPorts or []) if pp.port is not None]),
+                )
+            ),
+        ]
+        for task_id, task in sorted(
+            tasks.items(),
+            key=lambda tup: (render.format_time(tup[1].registeredTime),),
+        )
+    ]
+
+    render.tabulate_or_csv(headers, values, args.csv)
+
+
+@authentication.required
+def list_tasks(args: Namespace) -> None:
+    r = bindings.get_GetTasks(cli.setup_session(args))
+    tasks = r.allocationIdToSummary or {}
+    render_tasks(args, tasks)
+
+
+@authentication.required
+def logs(args: Namespace) -> None:
+    task_id = cast(str, ntsc.expand_uuid_prefixes(args, args.task_id))
+    try:
+        logs = api.task_logs(
+            cli.setup_session(args),
+            task_id,
+            head=args.head,
+            tail=args.tail,
+            follow=args.follow,
+            agent_ids=args.agent_ids,
+            container_ids=args.container_ids,
+            rank_ids=args.rank_ids,
+            sources=args.sources,
+            stdtypes=args.stdtypes,
+            min_level=args.level,
+            timestamp_before=args.timestamp_before,
+            timestamp_after=args.timestamp_after,
+        )
+        if "json" in args and args.json:
+            for log in logs:
+                render.print_json(log.to_json())
+        else:
+            api.pprint_logs(logs)
+    finally:
+        print(
+            colored(
+                "Task log stream ended. To reopen log stream, run: "
+                "det task logs -f {}".format(task_id),
+                "green",
+            )
+        )
+
+
+common_log_options: List[Any] = [
+    Arg(
+        "-f",
+        "--follow",
+        action="store_true",
+        help="follow the logs of a running task, similar to tail -f",
+    ),
+    Group(
+        cli.output_format_args["json"],
+    ),
+    Group(
+        Arg(
+            "--head",
+            type=int,
+            help="number of lines to show, counting from the beginning of the log",
+        ),
+        Arg(
+            "--tail",
+            type=int,
+            help="number of lines to show, counting from the end of the log",
+        ),
+    ),
+    Arg(
+        "--allocation-id",
+        dest="allocation_ids",
+        action="append",
+        help="allocations to show logs from (repeat for multiple values)",
+    ),
+    Arg(
+        "--agent-id",
+        dest="agent_ids",
+        action="append",
+        help="agents to show logs from (repeat for multiple values)",
+    ),
+    Arg(
+        "--container-id",
+        dest="container_ids",
+        action="append",
+        help="containers to show logs from (repeat for multiple values)",
+    ),
+    Arg(
+        "--rank-id",
+        dest="rank_ids",
+        type=int,
+        action="append",
+        help="containers to show logs from (repeat for multiple values)",
+    ),
+    Arg(
+        "--timestamp-before",
+        help="show logs only from before (RFC 3339 format), e.g. '2021-10-26T23:17:12Z'",
+    ),
+    Arg(
+        "--timestamp-after",
+        help="show logs only from after (RFC 3339 format), e.g. '2021-10-26T23:17:12Z'",
+    ),
+    Arg(
+        "--level",
+        dest="level",
+        help="show logs with this level or higher "
+        + "(TRACE, DEBUG, INFO, WARNING, ERROR, CRITICAL)",
+        choices=["TRACE", "DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"],
+    ),
+    Arg(
+        "--source",
+        dest="sources",
+        action="append",
+        help="sources to show logs from (repeat for multiple values)",
+    ),
+    Arg(
+        "--stdtype",
+        dest="stdtypes",
+        action="append",
+        help="output stream to show logs from (repeat for multiple values)",
+    ),
+]
+
+
+args_description: List[Any] = [
+    Cmd(
+        "task",
+        None,
+        "manage tasks (commands, experiments, notebooks, shells, tensorboards)",
+        [
+            Cmd(
+                "list ls",
+                list_tasks,
+                "list tasks in cluster",
+                [
+                    Group(
+                        cli.output_format_args["csv"],
+                        cli.output_format_args["json"],
+                    )
+                ],
+                is_default=True,
+            ),
+            Cmd(
+                "logs",
+                # Since declarative argparse tries to attach the help_str to the func itself:
+                # ./harness/determined/common/declarative_argparse.py#L57
+                # Each func must be unique.
+                partial(logs),
+                "fetch task logs",
+                [
+                    Arg("task_id", help="task ID"),
+                    *common_log_options,
+                ],
+            ),
+        ],
+    ),
+]
---
+++ packages/determined/determined-0.28.0rc4-py3-none-any.whl/determined/tensorboard/build.py
@@ -0,0 +1,170 @@
+import os
+import pathlib
+import urllib
+from typing import Any, Dict, Optional, Union
+
+from determined.common.storage.shared import _full_storage_path
+from determined.tensorboard import azure, base, directory, gcs, s3, shared
+
+
+def get_sync_path(cluster_id: str, experiment_id: str, trial_id: str) -> pathlib.Path:
+    return pathlib.Path(
+        get_experiment_sync_path(cluster_id, experiment_id),
+        "trial",
+        trial_id,
+    )
+
+
+def get_experiment_sync_path(cluster_id: str, experiment_id: str) -> pathlib.Path:
+    return pathlib.Path(
+        cluster_id,
+        "tensorboard",
+        "experiment",
+        experiment_id,
+    )
+
+
+def get_rank_if_horovod_process_else_return_zero() -> Optional[int]:
+    return int(os.getenv("HOROVOD_RANK", 0))
+
+
+def get_base_path(checkpoint_config: Dict[str, Any]) -> pathlib.Path:
+    allocation_id = os.environ.get("DET_ALLOCATION_ID", "")
+    rank = get_rank_if_horovod_process_else_return_zero()
+
+    if checkpoint_config.get("base_path"):
+        base_path = pathlib.Path(checkpoint_config["base_path"])
+    else:
+        base_path = pathlib.Path("/", "tmp")
+
+    return base_path.joinpath(f"tensorboard-{allocation_id}-{rank}")
+
+
+def _shortcut_to_config(shortcut: str) -> Dict[str, Any]:
+    p: urllib.parse.ParseResult = urllib.parse.urlparse(shortcut)
+    if any((p.params, p.query, p.fragment)):
+        raise ValueError(f'Malformed checkpoint_storage string "{shortcut}"')
+
+    scheme = p.scheme.lower()
+
+    if scheme in ["", "file"]:
+        return {
+            "type": "shared_fs",
+            "host_path": p.path,
+        }
+    elif scheme in ["s3", "gs"]:
+        bucket = p.netloc
+        prefix = p.path.lstrip("/")
+        storage_type = {
+            "s3": "s3",
+            "gs": "gcs",
+        }[scheme]
+
+        return {
+            "type": storage_type,
+            "bucket": bucket,
+            "prefix": prefix,
+        }
+    else:
+        raise NotImplementedError(
+            "tensorboard only supports shared_fs, s3, and gs " "shortcuts at the moment"
+        )
+
+
+def build(
+    cluster_id: str,
+    experiment_id: str,
+    trial_id: Optional[str],
+    checkpoint_config: Union[Dict[str, Any], str],
+    container_path: Optional[str] = None,
+    async_upload: bool = True,
+    sync_on_close: bool = True,
+) -> base.TensorboardManager:
+    """
+    Return a tensorboard manager defined by the value of the `type` key in
+    the configuration dictionary. Throws a `TypeError` if no tensorboard manager
+    with `type` is defined.
+
+    container_path, if set, will replace the host_path when determining the storage_path for the
+    SharedFSTensorboardManager.
+    """
+    if isinstance(checkpoint_config, str):
+        checkpoint_config = _shortcut_to_config(checkpoint_config)
+
+    type_name = checkpoint_config.get("type")
+
+    if not type_name:
+        raise TypeError("Missing 'type' parameter of storage configuration")
+
+    if not isinstance(type_name, str):
+        raise TypeError("`type` parameter of storage configuration must be a string")
+
+    base_path = get_base_path(checkpoint_config)
+
+    if trial_id:
+        sync_path = get_sync_path(cluster_id, experiment_id, trial_id)
+    else:
+        sync_path = get_experiment_sync_path(cluster_id, experiment_id)
+
+    if type_name == "shared_fs":
+        host_path = checkpoint_config["host_path"]
+        storage_path = checkpoint_config.get("storage_path")
+        return shared.SharedFSTensorboardManager(
+            _full_storage_path(host_path, storage_path, container_path),
+            base_path,
+            sync_path,
+            async_upload=async_upload,
+            sync_on_close=sync_on_close,
+        )
+
+    elif type_name == "directory":
+        return directory.DirectoryTensorboardManager(
+            checkpoint_config["container_path"],
+            base_path,
+            sync_path,
+            async_upload=async_upload,
+            sync_on_close=sync_on_close,
+        )
+
+    elif type_name == "gcs":
+        return gcs.GCSTensorboardManager(
+            checkpoint_config["bucket"],
+            checkpoint_config.get("prefix", None),
+            base_path,
+            sync_path,
+            async_upload=async_upload,
+            sync_on_close=sync_on_close,
+        )
+
+    elif type_name == "s3":
+        return s3.S3TensorboardManager(
+            checkpoint_config["bucket"],
+            checkpoint_config.get("access_key", None),
+            checkpoint_config.get("secret_key", None),
+            checkpoint_config.get("endpoint_url", None),
+            checkpoint_config.get("prefix", None),
+            base_path,
+            sync_path,
+            async_upload=async_upload,
+            sync_on_close=sync_on_close,
+        )
+
+    elif type_name == "azure":
+        if not checkpoint_config.get("connection_string") and checkpoint_config.get("access_url"):
+            raise ValueError(
+                """At least one of [connection_string, account_url] must be specified for Azure
+                 Tensorboard Manager, but none were."""
+            )
+        return azure.AzureTensorboardManager(
+            checkpoint_config["container"],
+            checkpoint_config.get("connection_string", None),
+            checkpoint_config.get("access_url", None),
+            checkpoint_config.get("credential", None),
+            base_path,
+            sync_path,
+            async_upload=async_upload,
+            sync_on_close=sync_on_close,
+        )
+
+    else:
+        raise TypeError(f"Unknown storage type: {type_name}")
