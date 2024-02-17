bf4c4bc5733ec66ca8f081a53137bbcbd2d76031
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:46:24 +0000

    Add package vardxg/1.1.0/vardxg-1.1.0-py3-none-any.whl

---
+++ packages/vardxg/vardxg-1.1.0-py3-none-any.whl/vardxg/__init__.py
@@ -0,0 +1,13 @@
+from vardxg import ByteBuf
+from vardxg.tiktok import device_register
+from vardxg import magic
+from vardxg import pkcs7_padding
+from vardxg import protobuf
+from vardxg import Simon
+from vardxg import Sm3
+from vardxg.tiktok import ttencrypt
+from vardxg import xor
+
+# Credits goes to xTekky for all the tiktok Stuff this.
+
+__version__: 1.0
