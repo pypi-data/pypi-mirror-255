55eda6ec919d0e54955cb3955be4a11e47f43af1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:58:24 +0000

    Add package signe/0.3.1/signe-0.3.1.tar.gz

---
+++ packages/signe/signe-0.3.1.tar.gz/signe-0.3.1/signe/__init__.py
@@ -0,0 +1,14 @@
+from signe.utils import (
+    effect,
+    # effect_with_opts,
+    createSignal,
+    computed,
+    batch,
+    cleanup,
+    on,
+    scope,
+)
+from signe.reactive.proxy import createReactive, isReactive
+
+
+__version__ = "0.3.1"
