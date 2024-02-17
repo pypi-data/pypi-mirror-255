29d88b18f650eef9fa7f4abb262e17e280d1b475
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:57:58 +0000

    Add package fake-bge-module-latest/20240206/fake_bge_module_latest-20240206-py3-none-any.whl

---
+++ packages/fake-bge-module-latest/fake_bge_module_latest-20240206-py3-none-any.whl/gpu/__init__.py
@@ -0,0 +1,12 @@
+import sys
+import typing
+from . import state
+from . import shader
+from . import select
+from . import matrix
+from . import platform
+from . import capabilities
+from . import types
+from . import texture
+
+GenericType = typing.TypeVar("GenericType")
