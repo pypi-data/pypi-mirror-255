fae43b614a4ffff098f5587bd1aa54e04f2c9ca6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:57:44 +0000

    Add package flamethrower/0.2.19/flamethrower-0.2.19-py3-none-any.whl

---
+++ packages/flamethrower/flamethrower-0.2.19-py3-none-any.whl/flamethrower/agents/drivers/done_driver.py
@@ -0,0 +1,23 @@
+from flamethrower.agents.drivers.driver_interface import Driver
+from flamethrower.models.llm import LLM
+from flamethrower.models.models import OPENAI_GPT_3_TURBO
+from typing import Any, Dict, List, Iterator, Optional
+
+system_message = f"""
+You are part of a while-loop in a program used as an effective multi-agent build & debug system.
+In particular, you are a specialized agent that is only called when a job is about to be completed.
+In this case, do not **provide any further suggestions, optimizations, or tests**.
+Instead, just thank the user for their support, ideally with some matching emojis.
+"""
+
+class DoneDriver(Driver):
+    def __init__(self, **kwargs: Any) -> None:
+        super().__init__(**kwargs)
+        self._llm: LLM = LLM(system_message=system_message, model=OPENAI_GPT_3_TURBO)
+    
+    @property
+    def llm(self) -> LLM:
+        return self._llm
+    
+    def respond_to(self, messages: List[Dict[str, str]]) -> Optional[Iterator[str]]:
+        return self.llm.new_streaming_chat_request(messages)
