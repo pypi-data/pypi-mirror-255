3db0352af9e79f58a3d1f0f127991796946842e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:34:39 +0000

    Add package polylith-cli/1.2.1/polylith_cli-1.2.1-py3-none-any.whl

---
+++ packages/polylith-cli/polylith_cli-1.2.1-py3-none-any.whl/polylith_cli/polylith/commands/__init__.py
@@ -0,0 +1,2 @@
+from polylith_cli.polylith.commands import check, create, diff, info, libs, sync
+__all__ = ['check', 'create', 'diff', 'info', 'libs', 'sync']
---
+++ packages/polylith-cli/polylith_cli-1.2.1-py3-none-any.whl/polylith_cli/polylith/sync/collect.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+from polylith_cli.polylith import check, info, workspace
+
+def get_brick_imports(root: Path, ns: str, project_data: dict) -> dict:
+    bases = {b for b in project_data.get('bases', [])}
+    components = {c for c in project_data.get('components', [])}
+    bases_paths = workspace.paths.collect_bases_paths(root, ns, bases)
+    components_paths = workspace.paths.collect_components_paths(root, ns, components)
+    brick_imports_in_bases = check.collect.extract_bricks(bases_paths, ns)
+    brick_imports_in_components = check.collect.extract_bricks(components_paths, ns)
+    return {'bases': check.collect.with_unknown_components(root, ns, brick_imports_in_bases), 'components': check.collect.with_unknown_components(root, ns, brick_imports_in_components)}
+
+def calculate_diff(root: Path, namespace: str, project_data: dict, workspace_data: dict) -> dict:
+    brick_imports = get_brick_imports(root, namespace, project_data)
+    all_bases = workspace_data['bases']
+    all_components = workspace_data['components']
+    bases = project_data['bases']
+    components = project_data['components']
+    is_project = info.is_project(project_data)
+    if is_project:
+        brick_diff = check.collect.imports_diff(brick_imports, bases, components)
+    else:
+        all_bricks = set().union(all_bases, all_components)
+        brick_diff = check.collect.diff(all_bricks, bases, components)
+    bases_diff = {b for b in brick_diff if b in all_bases}
+    components_diff = {b for b in brick_diff if b in all_components}
+    return {'name': project_data['name'], 'path': project_data['path'], 'is_project': is_project, 'bases': bases_diff, 'components': components_diff, 'brick_imports': brick_imports}
