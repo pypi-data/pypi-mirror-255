4049842b3b4d31b0d004eda4d16c47528007e4ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:57:02 +0000

    Add package ezapi-tmdb/0.8.4/ezapi_tmdb-0.8.4-py3-none-any.whl

---
+++ packages/ezapi-tmdb/ezapi_tmdb-0.8.4-py3-none-any.whl/tmdb/four/list.py
@@ -0,0 +1,22 @@
+from .base import ENDPOINT, any_required_kwargs, process_response
+
+
+class ListMixin:
+    @process_response
+    def get_list(self, list_id, **kwargs):
+        """
+        GET /list/{list_id}
+        """
+
+        url = f"{ENDPOINT}/4/list/{list_id}"
+        return self.make_request("GET", url, kwargs)
+
+    @any_required_kwargs(["media_type", "media_id"])
+    @process_response
+    def get_list_item_status(self, list_id, **kwargs):
+        """
+        GET /list/{list_id}/item_status
+        """
+
+        url = f"{ENDPOINT}/4/list/{list_id}/item_status"
+        return self.make_request("GET", url, kwargs)
