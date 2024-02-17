fd5d5f0076572302d56e06cf0a0c68eeac717b28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:35:32 +0000

    Add package maskingPackage/1.0.22/maskingPackage-1.0.22.tar.gz

---
+++ packages/maskingPackage/maskingPackage-1.0.22.tar.gz/maskingPackage-1.0.22/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='maskingPackage',
+    version='1.0.22',
+    packages=find_packages(),
+    install_requires=[
+        'pandas',  # Add any dependencies here
+        'cryptography',
+        'pyspark'
+    ],
+)
+
