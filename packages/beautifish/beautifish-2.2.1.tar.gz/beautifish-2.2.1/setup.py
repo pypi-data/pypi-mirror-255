aa1cacd4446553f252a87c2252dbe529bc1b406e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:45:29 +0000

    Add package beautifish/2.2.1/beautifish-2.2.1.tar.gz

---
+++ packages/beautifish/beautifish-2.2.1.tar.gz/beautifish-2.2.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='beautifish',
+    version='2.2.1',
+    packages=find_packages(),
+    install_requires=[
+        'pyfiglet>=1.0.2'
+    ],
+)
