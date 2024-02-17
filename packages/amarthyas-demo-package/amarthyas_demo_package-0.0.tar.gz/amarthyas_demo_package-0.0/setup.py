10a7821adf409bdf5f03faef41da93e7ae6a2594
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:57:22 +0000

    Add package amarthyas-demo-package/0.0/amarthyas_demo_package-0.0.tar.gz

---
+++ packages/amarthyas-demo-package/amarthyas_demo_package-0.0.tar.gz/amarthyas_demo_package-0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='amarthyas_demo_package',
+      version='0.0',
+      description='Gaussian and Binomial distributions',
+      packages=['amarthyas_demo_package'],
+      author_email='amarthya10@gmail.com',
+      zip_safe=False)
