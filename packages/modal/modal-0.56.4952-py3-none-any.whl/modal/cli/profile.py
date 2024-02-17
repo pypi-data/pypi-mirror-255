3569745f101ea8de6c8d95024d552b3a0c3e0e7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:35:53 +0000

    Add package modal/0.56.4952/modal-0.56.4952-py3-none-any.whl

---
+++ packages/modal/modal-0.56.4952-py3-none-any.whl/modal/cli/profile.py
@@ -0,0 +1,66 @@
+# Copyright Modal Labs 2022
+
+import asyncio
+from typing import Optional
+
+import typer
+from rich.console import Console
+from rich.json import JSON
+from rich.table import Table
+
+from modal.config import Config, _lookup_workspace, _profile, config_profiles, config_set_active_profile
+from modal.exception import AuthError
+from modal_utils.async_utils import synchronizer
+
+profile_cli = typer.Typer(name="profile", help="Switch between Modal profiles.", no_args_is_help=True)
+
+
+@profile_cli.command(help="Change the active Modal profile.")
+def activate(profile: str = typer.Argument(..., help="Modal profile to activate.")):
+    config_set_active_profile(profile)
+
+
+@profile_cli.command(help="Print the currently active Modal profile.")
+def current():
+    typer.echo(_profile)
+
+
+@profile_cli.command(name="list", help="Show all Modal profiles and highlight the active one.")
+@synchronizer.create_blocking
+async def list(json: Optional[bool] = False):
+    config = Config()
+    profiles = config_profiles()
+    lookup_coros = [
+        _lookup_workspace(
+            config.get("server_url", profile),
+            config.get("token_id", profile),
+            config.get("token_secret", profile),
+        )
+        for profile in profiles
+    ]
+    responses = await asyncio.gather(*lookup_coros, return_exceptions=True)
+
+    rows = []
+    for profile, resp in zip(profiles, responses):
+        active = profile == _profile
+        if isinstance(resp, AuthError):
+            workspace = "Unknown (authentication failure)"
+        elif isinstance(resp, Exception):
+            # Catch-all for other exceptions, like incorrect server url
+            workspace = "Unknown (profile misconfigured)"
+        else:
+            workspace = resp.username
+        content = ["•" if active else "", profile, workspace]
+        rows.append((active, content))
+
+    console = Console()
+    if json:
+        json_data = []
+        for active, content in rows:
+            json_data.append({"name": content[1], "workspace": content[2], "active": active})
+        console.print(JSON.from_data(json_data))
+    else:
+        table = Table(" ", "Profile", "Workspace")
+        for active, content in rows:
+            table.add_row(*content, style="bold green" if active else "dim")
+        console.print(table)
