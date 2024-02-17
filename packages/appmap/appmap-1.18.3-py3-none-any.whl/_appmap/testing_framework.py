d635748d50042d881813e618a2ce143fc343d755
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:44:12 +0000

    Add package appmap/1.18.3/appmap-1.18.3-py3-none-any.whl

---
+++ packages/appmap/appmap-1.18.3-py3-none-any.whl/_appmap/testing_framework.py
@@ -0,0 +1,161 @@
+"""Shared infrastructure for testing framework integration."""
+
+import os
+from pathlib import PurePath
+import re
+from contextlib import contextmanager
+import traceback
+
+import inflection
+
+from _appmap import configuration, env, generation, web_framework
+from _appmap.env import Env
+from _appmap.recording import Recording
+from _appmap.utils import fqname, root_relative_path
+
+from .metadata import Metadata
+
+try:
+    # Make sure we have hooks installed if we're testing a django app.
+    # pylint: disable=unused-import
+    import appmap.django  # noqa: F401
+except ImportError:
+    pass  # probably no django
+
+
+class FuncItem:
+    def __init__(self, cls, name, method_id=None, location=None):
+        self.cls = cls
+        self.name = name
+        self.method_id = method_id or name
+        self.location = location
+
+    @property
+    def class_name(self):
+        return self.cls.__name__
+
+    @property
+    def defined_class(self):
+        return fqname(self.cls)
+
+    def is_in_class(self):
+        return self.cls is not None
+
+    has_feature_group = is_in_class
+
+    @property
+    def feature_group(self):
+        test_name = self.class_name
+        if test_name.startswith("Test"):
+            test_name = test_name[4:]
+        return inflection.humanize(inflection.titleize(test_name))
+
+    @property
+    def feature(self):
+        return inflection.humanize(self.test_name)
+
+    @property
+    def scenario_name(self):
+        name = "%s%s" % (self.feature[0].lower(), self.feature[1:])
+        if self.has_feature_group():
+            name = " ".join([self.feature_group, name])
+        return name
+
+    @property
+    def test_name(self):
+        ret = self.name
+        return re.sub("^test.?_", "", ret)
+
+    @property
+    def filename(self):
+        fname = self.name
+        if self.cls:
+            fname = "%s_%s" % (self.defined_class, fname)
+        fname = re.sub("[^a-zA-Z0-9-_]", "_", fname)
+        if fname.endswith("_"):
+            fname = fname[:-1]
+        return fname
+
+    @property
+    def metadata(self):
+        ret = {}
+        if self.is_in_class():
+            ret["feature_group"] = self.feature_group
+            ret["recording"] = {
+                "defined_class": self.defined_class,
+                "method_id": self.method_id,
+            }
+        if self.location:
+            ret.update({"source_location": "%s:%d" % self.location[0:2]})
+        ret.update({"name": self.scenario_name, "feature": self.feature})
+        return ret
+
+
+class session:
+    def __init__(self, name, recorder_type, version=None):
+        self.name = name
+        self.recorder_type = recorder_type
+        self.version = version
+        self.metadata = None
+
+    @contextmanager
+    def record(self, klass, method, **kwds):
+        Metadata.add_framework(self.name, self.version)
+
+        item = FuncItem(klass, method, **kwds)
+
+        metadata = item.metadata
+        metadata.update(
+            {
+                "app": configuration.Config().name,
+                "recorder": {
+                    "name": self.name,
+                    "type": self.recorder_type,
+                },
+            }
+        )
+
+        rec = Recording()
+        environ = env.Env.current
+        try:
+            with rec, environ.disabled("requests"):
+                yield metadata
+        finally:
+            basedir = environ.output_dir / self.name
+            web_framework.write_appmap(basedir, item.filename, generation.dump(rec, metadata))
+
+
+@contextmanager
+def collect_result_metadata(metadata):
+    """Collect test case result metadata.
+
+    Sets test_status and exception information.
+    """
+    try:
+        yield
+        metadata["test_status"] = "succeeded"
+    except Exception as exn:
+        metadata["test_failure"] = {"message": failure_message(exn), "location": failure_location(exn)}
+        metadata["test_status"] = "failed"
+        metadata["exception"] = {"class": exn.__class__.__name__, "message": str(exn)}
+        raise
+
+
+def file_delete(filename):
+    try:
+        os.remove(filename)
+    except FileNotFoundError:
+        pass
+
+
+def failure_message(exn: Exception) -> str:
+    return f'{exn.__class__.__name__}: {exn}'
+
+
+def failure_location(exn: Exception) -> str:
+    # Exception could have been raised inside the test framework,
+    # but we want the location in the user code that caused it.
+    for frame in traceback.extract_tb(exn.__traceback__):
+        path = root_relative_path(frame.filename)
+        if not PurePath(path).is_absolute():
+            return f"{path}:{frame.lineno}"
