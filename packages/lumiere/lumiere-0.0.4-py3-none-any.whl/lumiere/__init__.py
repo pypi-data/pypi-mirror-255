f1b2ff96f91168e71510b84e6577f20c500a2ef4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:54:23 +0000

    Add package lumiere/0.0.4/lumiere-0.0.4-py3-none-any.whl

---
+++ packages/lumiere/lumiere-0.0.4-py3-none-any.whl/lumiere/__init__.py
@@ -0,0 +1,10 @@
+from lumiere.model import (
+    ConvolutionBasedInflationBlock,
+    AttentionBasedInflationBlock,
+)
+
+
+__all__ = [
+    "ConvolutionBasedInflationBlock",
+    "AttentionBasedInflationBlock",
+]
