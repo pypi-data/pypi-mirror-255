5a2a6898314862b82a940f0a93db9c0d33c68bf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:50:09 +0000

    Add package etcd-client-py-test/0.2.14b1/etcd_client_py_test-0.2.14b1.tar.gz

---
+++ packages/etcd-client-py-test/etcd_client_py_test-0.2.14b1.tar.gz/etcd_client_py_test-0.2.14b1/etcd-client-py/src/txn_response.rs
@@ -0,0 +1,23 @@
+use etcd_client::TxnResponse;
+use pyo3::prelude::*;
+
+#[derive(Clone)]
+#[pyclass(name = "TxnResponse")]
+pub struct PyTxnResponse(pub TxnResponse);
+
+// TODO: Add ResponseHeader, TxnOpResponse
+#[pymethods]
+impl PyTxnResponse {
+    pub fn succeeded(&self) -> PyResult<bool> {
+        Ok(self.0.succeeded())
+    }
+
+    // pub fn op_responses(&self) -> PyResult<()> {
+    //     let _ = self.0.op_responses();
+    //     Ok(())
+    // }
+
+    pub fn __repr__(&self) -> String {
+        format!("{:?}", self.0)
+    }
+}
