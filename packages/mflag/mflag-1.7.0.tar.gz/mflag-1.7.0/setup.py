7b647d8fbb9b3df1ab7ccb1dba4d32fdc9a7bdcf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:28:57 +0000

    Add package mflag/1.7.0/mflag-1.7.0.tar.gz

---
+++ packages/mflag/mflag-1.7.0.tar.gz/mflag-1.7.0/setup.py
@@ -0,0 +1,17 @@
+# read the contents of your README file
+from pathlib import Path
+from mflag import __version__
+from setuptools import setup
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name="mflag",
+    version=__version__,
+    author="Moses Dastmard",
+    description="put/remove flags for files and folders",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=["pandas", "numpy", "psutil", "joblib", "pathlib", "sqlite3"],
+)
