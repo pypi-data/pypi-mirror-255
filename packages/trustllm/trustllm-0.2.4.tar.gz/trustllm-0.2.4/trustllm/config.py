57eb637c1c8282189621598d08e7b2af28e62d14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:29:11 +0000

    Add package trustllm/0.2.4/trustllm-0.2.4.tar.gz

---
+++ packages/trustllm/trustllm-0.2.4.tar.gz/trustllm-0.2.4/trustllm/config.py
@@ -0,0 +1,19 @@
+# evaluation api setting
+openai_key = None
+perspective_key = None
+
+
+# generation api setting
+ernie_client_id = None
+ernie_client_secret = None
+deepinfra_api = None
+ernie_api = None
+claude_api = None
+palm_api = None
+replicate_api = None
+zhipu_api = None
+
+azure_openai = False
+azure_engine = None
+azure_api_version = "2023-08-01-preview"
+azure_api_base = None
