6dd4e133b81e7e889fb6a1cf54c2b8c0d741e29c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:46:27 +0000

    Add package pyupgw/0.8/pyupgw-0.8.tar.gz

---
+++ packages/pyupgw/pyupgw-0.8.tar.gz/pyupgw-0.8/pyupgw/cli/_logging.py
@@ -0,0 +1,49 @@
+"""CLI logging"""
+
+import json
+import logging
+import logging.config
+
+import yaml
+from rich.logging import RichHandler
+
+from pyupgw._helpers import LazyEncode
+
+# This is not pretty, but it's just the most foolproof way to ensure
+# `LazyEncode` objects always get serialized properly
+
+
+class _MyJSONEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, LazyEncode):
+            return o.encode()
+        return super().default(o)
+
+
+_original_json_dumps = json.dumps
+
+
+def _new_json_dumps(*args, **kwargs):
+    kwargs["cls"] = _MyJSONEncoder
+    return _original_json_dumps(*args, **kwargs)
+
+
+json.dumps = _new_json_dumps
+
+
+def _setup_logging_basic():
+    logging.basicConfig(level=logging.INFO, handlers=[RichHandler()])
+
+
+def _setup_logging_file(config_file: str):
+    with open(config_file, encoding="utf-8") as f:
+        config = yaml.load(f, Loader=yaml.Loader)
+    logging.config.dictConfig(config)
+
+
+def setup_logging(config_file: str | None = None):
+    """Setup logging"""
+    if config_file:
+        _setup_logging_file(config_file)
+    else:
+        _setup_logging_basic()
