143b94a8b3d82a82a86555a962354eb3a36042d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:57:37 +0000

    Add package intel-extension-for-openxla/0.2.1/intel_extension_for_openxla-0.2.1-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/intel-extension-for-openxla/intel_extension_for_openxla-0.2.1-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/jax_plugins/intel_extension_for_openxla/__init__.py
@@ -0,0 +1,35 @@
+# Copyright (c) 2023 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+'''Init file for register XPU backend'''
+
+import logging
+from pathlib import Path
+import platform
+import sys
+
+import jax._src.xla_bridge as xb
+
+logger = logging.getLogger(__name__)
+
+def initialize():
+  path = Path(__file__).resolve().parent / "pjrt_plugin_xpu.so"
+  if not path.exists():
+    logger.warning(
+        f"WARNING: Native library {path} does not exist. "
+        f"This most likely indicates an issue with how {__package__} "
+        f"was built or installed.")
+  xb.register_plugin("xpu",
+                     priority=500,
+                     library_path=str(path))
