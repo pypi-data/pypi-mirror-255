89efc08d04c9bfee9647d806af7dad0ea383ca67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:38:57 +0000

    Add package rng-rava-diag/1.0.0/rng_rava_diag-1.0.0-py3-none-any.whl

---
+++ packages/rng-rava-diag/rng_rava_diag-1.0.0-py3-none-any.whl/rng_rava_diag/__init__.py
@@ -0,0 +1,8 @@
+"""
+Copyright (c) 2024 Gabriel Guerrer
+
+Distributed under the MIT license - See LICENSE for details
+"""
+
+from .rava_subapp_quick_tests import RAVA_SUBAPP_QUICK_TESTS, rava_subapp_quick_tests
+from .rava_subapp_detailed_tests import RAVA_SUBAPP_DETAILED_TESTS, rava_subapp_detailed_tests
