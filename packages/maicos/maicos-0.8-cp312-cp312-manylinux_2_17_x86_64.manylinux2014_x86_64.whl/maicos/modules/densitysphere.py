044436285e39e1528f8d58eb5f62782752276207
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:46:20 +0000

    Add package maicos/0.8/maicos-0.8-cp312-cp312-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/maicos/maicos-0.8-cp312-cp312-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/maicos/modules/densitysphere.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# -*- Mode: python; tab-width: 4; indent-tabs-mode:nil; coding:utf-8 -*-
+#
+# Copyright (c) 2023 Authors and contributors
+# (see the AUTHORS.rst file for the full list of names)
+#
+# Released under the GNU Public Licence, v3 or any higher version
+# SPDX-License-Identifier: GPL-3.0-or-later
+"""Module for computing spherical density profiles."""
+import logging
+from typing import List, Optional, Union
+
+import MDAnalysis as mda
+
+from ..core import ProfileSphereBase
+from ..lib.util import render_docs
+from ..lib.weights import density_weights
+
+
+logger = logging.getLogger(__name__)
+
+
+@render_docs
+class DensitySphere(ProfileSphereBase):
+    r"""Spherical partial density profiles.
+
+    ${DENSITY_DESCRIPTION}
+
+    ${CORRELATION_INFO_RADIAL}
+
+    Parameters
+    ----------
+    ${PROFILE_SPHERE_CLASS_PARAMETERS}
+    ${DENS_PARAMETER}
+
+    Attributes
+    ----------
+    ${PROFILE_SPHERE_CLASS_ATTRIBUTES}
+    """
+
+    def __init__(
+        self,
+        atomgroups: Union[mda.AtomGroup, List[mda.AtomGroup]],
+        dens: str = "mass",
+        bin_width: float = 1,
+        rmin: float = 0,
+        rmax: Optional[float] = None,
+        refgroup: Optional[mda.AtomGroup] = None,
+        grouping: str = "atoms",
+        unwrap: bool = True,
+        bin_method: str = "com",
+        output: str = "density.dat",
+        concfreq: int = 0,
+        jitter: float = 0.0,
+    ):
+        self._locals = locals()
+        super().__init__(
+            weighting_function=density_weights,
+            f_kwargs={"dens": dens},
+            normalization="volume",
+            atomgroups=atomgroups,
+            bin_width=bin_width,
+            rmin=rmin,
+            rmax=rmax,
+            refgroup=refgroup,
+            grouping=grouping,
+            unwrap=unwrap,
+            bin_method=bin_method,
+            output=output,
+            concfreq=concfreq,
+            jitter=jitter,
+        )
