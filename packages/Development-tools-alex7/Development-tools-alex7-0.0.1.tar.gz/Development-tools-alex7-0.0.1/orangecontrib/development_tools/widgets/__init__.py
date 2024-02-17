19ab99c1d5fc9d489c0635e663cefae1de5fc7f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:58:27 +0000

    Add package Development-tools-alex7/0.0.1/Development-tools-alex7-0.0.1.tar.gz

---
+++ packages/Development-tools-alex7/Development-tools-alex7-0.0.1.tar.gz/Development-tools-alex7-0.0.1/orangecontrib/development_tools/widgets/__init__.py
@@ -0,0 +1,16 @@
+"""
+Widgets from Development workflow category
+
+"""
+ID = ""
+
+NAME = "6"
+
+# Category icon show in the menu
+ICON = "icons/tool.png"
+
+BACKGROUND = "light-blue"
+
+DESCRIPTION = "Development tools. Managing data, input, output, and calling workflows from other one."
+
+PRIORITY = 6
