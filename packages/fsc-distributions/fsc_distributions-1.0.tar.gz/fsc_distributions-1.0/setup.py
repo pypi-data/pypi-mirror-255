e56277aa8124c7017321ae6a7910e95232fa329a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:45:22 +0000

    Add package fsc-distributions/1.0/fsc_distributions-1.0.tar.gz

---
+++ packages/fsc-distributions/fsc_distributions-1.0.tar.gz/fsc_distributions-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='fsc_distributions',
+      version='1.0',
+      author='FarahS',
+      description='Gaussian and binomial distribution classes',
+      packages=['fsc_distributions'],
+      zip_safe=False)
