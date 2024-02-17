30424f63b4952db40337fbf02e7cf0f7a10b0851
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:53:50 +0000

    Add package pace-calc/1.0.1/pace_calc-1.0.1.tar.gz

---
+++ packages/pace-calc/pace_calc-1.0.1.tar.gz/pace_calc-1.0.1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+
+def readme():
+  with open('README.md', 'r', encoding="utf-8") as f:
+    content = f.read()
+  return content
+
+
+setup(
+  name='pace_calc',
+  version='1.0.1',
+  author='ntymko',
+  author_email='ndilkova28@gmail.com',
+  description='Библиотека для расчета темпа бега, скорости и времени',
+  long_description=readme(),
+  long_description_content_type='text/markdown',
+  url='https://github.com/ntymko/pace_calc',
+  packages=find_packages(),
+  install_requires=[],
+  classifiers=[
+    'Programming Language :: Python :: 3.6',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent'
+  ],
+  keywords='pace calc ',
+  project_urls={
+    'GitHub': 'https://github.com/ntymko/pace_calc'
+  },
+  python_requires='>=3.6'
+)
