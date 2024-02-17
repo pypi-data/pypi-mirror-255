f31b0b82a19aa8a5d3e8b5107de2b59f8d47f60a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:39:35 +0000

    Add package py-portfolio-index/0.0.36/py-portfolio-index-0.0.36.tar.gz

---
+++ packages/py-portfolio-index/py-portfolio-index-0.0.36.tar.gz/py-portfolio-index-0.0.36/py_portfolio_index/bin/__init__.py
@@ -0,0 +1,18 @@
+from .indexes import INDEXES
+from .lists import STOCK_LISTS
+from py_portfolio_index.models import StockInfo
+from pathlib import Path
+from json import loads
+
+STOCK_INFO: dict[str, StockInfo] = {}
+with open(Path(__file__).parent / "stock_info.json", "r", encoding="utf-8") as f:
+    content = f.read()
+    if content:
+        all = loads(content)
+        for row in all:
+            STOCK_INFO[row["ticker"]] = StockInfo.model_validate(row)
+
+with open(Path(__file__).parent / "cached_ticker_list.csv", "r") as f:
+    VALID_STOCKS = set([v for v in f.read().split("\n") if v])
+
+__all__ = ["INDEXES", "STOCK_LISTS", "STOCK_INFO", "VALID_STOCKS"]
