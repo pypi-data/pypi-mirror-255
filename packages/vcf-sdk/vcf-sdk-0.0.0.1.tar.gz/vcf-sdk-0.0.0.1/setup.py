21df4bdaac81a72eb5a61a54fdcf3691eb23339a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:30:11 +0000

    Add package vcf-sdk/0.0.0.1/vcf-sdk-0.0.0.1.tar.gz

---
+++ packages/vcf-sdk/vcf-sdk-0.0.0.1.tar.gz/vcf-sdk-0.0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="vcf-sdk",
+    version="0.0.0.1",
+    author="VMware Inc.",
+    description="Python bindings for vmwarecloud foundation sdk",
+    packages=find_packages(),
+    install_requires=[
+        # List your package dependencies here
+    ],
+)
+
