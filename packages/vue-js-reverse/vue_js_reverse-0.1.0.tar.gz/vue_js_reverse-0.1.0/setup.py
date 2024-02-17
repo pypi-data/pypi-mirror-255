70e105249c8beef8faa2fd694d71234391e183b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:29:19 +0000

    Add package vue-js-reverse/0.1.0/vue_js_reverse-0.1.0.tar.gz

---
+++ packages/vue-js-reverse/vue_js_reverse-0.1.0.tar.gz/vue_js_reverse-0.1.0/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="vue_js_reverse",
+    version="0.1.0",
+    author="Cem Yildiz",
+    author_email="cem.yildiz@ya.ru",
+    description="Vue JS Reverse",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/miklagard/vue-js-reverse",
+    packages=setuptools.find_packages(),
+    package_data={
+        'vue_js_reverse': [
+            'templates/vue_js_reverse/*',
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
