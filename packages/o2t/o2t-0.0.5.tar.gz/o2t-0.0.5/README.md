9af5241a7d4facec80d5e8dafa652103e11e431d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:29:54 +0000

    Add package o2t/0.0.5/o2t-0.0.5.tar.gz

---
+++ packages/o2t/o2t-0.0.5.tar.gz/o2t-0.0.5/README.md
@@ -0,0 +1,18 @@
+# o2t
+
+
+# Installation
+## Using Prebuilt
+```bash
+pip install o2t
+```
+## Build From Source
+```
+pip install .
+```
+
+
+# How to use
+```
+o2t your_onnx_model saved_pytorch_graph_module
+```
---
+++ packages/o2t/o2t-0.0.5.tar.gz/o2t-0.0.5/o2t/core/pytorch_layers/__init__.py
@@ -0,0 +1,10 @@
+from .arithmetic import Arithmetic
+from .batchnorm import BatchNorm
+from .conv import Conv
+from .embedding_layer import Embedding
+from .layernorm import LayerNorm
+from .linear import Linear
+from .observer import Observer
+from .pool import Pool
+from .slice_layer import Slice
+from .torch_layer import Arange, Full, Ones, Reshape, Size, Tensor
