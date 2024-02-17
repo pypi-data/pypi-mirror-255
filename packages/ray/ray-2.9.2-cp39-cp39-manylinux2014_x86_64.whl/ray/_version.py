7aac1ce42ccf6f68a94f34661b3d10c926155341
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:48:44 +0000

    Add package ray/2.9.2/ray-2.9.2-cp39-cp39-manylinux2014_x86_64.whl

---
+++ packages/ray/ray-2.9.2-cp39-cp39-manylinux2014_x86_64.whl/ray/_version.py
@@ -0,0 +1,6 @@
+# Replaced with the current commit when building the wheels.
+commit = "fce7a361807580953364e2da964f9498f3123bf9"
+version = "2.9.2"
+
+if __name__ == "__main__":
+    print("%s %s" % (version, commit))
