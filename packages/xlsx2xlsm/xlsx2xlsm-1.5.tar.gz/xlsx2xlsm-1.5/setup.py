86084facafb8cbe8cba0c34e824d124ca0446076
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:53:06 +0000

    Add package xlsx2xlsm/1.5/xlsx2xlsm-1.5.tar.gz

---
+++ packages/xlsx2xlsm/xlsx2xlsm-1.5.tar.gz/xlsx2xlsm-1.5/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='xlsx2xlsm',
+    version='1.5',
+    packages=find_packages(),
+    install_requires=[
+        # 
+    ],
+    author='Zhui Cao',
+    author_email='857330385@qq.com',
+    description='This package is used to convert xlsx files to xlsm files and supports custom macros',
+    readme = "README.md",
+    license='',
+    keywords = ["xlsx", "xlsx2xlsm", "xlsm", "xlsxtoxlsm", "xlsx-to-xlsm", "xlsx-xlsm"],
+    url='https://github.com/caozhui/xlsx2xlsm'
+)
