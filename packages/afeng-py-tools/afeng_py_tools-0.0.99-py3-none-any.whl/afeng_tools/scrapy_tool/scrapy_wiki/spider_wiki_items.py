8d1cca8554cca2c3c96236cc4494d4adf205295d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:39:34 +0000

    Add package afeng-py-tools/0.0.99/afeng_py_tools-0.0.99-py3-none-any.whl

---
+++ packages/afeng-py-tools/afeng_py_tools-0.0.99-py3-none-any.whl/afeng_tools/scrapy_tool/scrapy_wiki/spider_wiki_items.py
@@ -0,0 +1,39 @@
+"""
+爬虫item
+"""
+from typing import Optional, Any
+
+from pydantic import BaseModel
+
+
+class WikiInfoRuleItem(BaseModel):
+    """Wiki信息规则项"""
+    # 文本规则
+    title_rule: Optional[str] = None
+    # 链接规则
+    url_rule: Optional[str] = None
+    # url请求的页面内容规则
+    page_content_rule: Optional[str] = None
+    # 子项列表规则
+    child_item_list_rule: Optional[str] = None
+    # 子项信息规则项
+    child_item_info_rule: Optional['WikiInfoRuleItem'] = None
+    # 是否递归
+    is_recursion: bool = False
+
+
+class HandleWikiPageResult(BaseModel):
+    """处理Wiki页面结果"""
+    # request参数
+    request_kwargs: Optional[dict[str, Any]] = None
+    # 下一次callback参数
+    next_callback_kwargs: Optional[dict[str, Any]] = None
+    pass
+
+
+class HandleWikiRequestItem(BaseModel):
+    """处理wiki的request项"""
+    # 链接
+    url: Optional[str] = None
+    # request的参数
+    cb_kwargs: Optional[dict[str, Any]] = None
