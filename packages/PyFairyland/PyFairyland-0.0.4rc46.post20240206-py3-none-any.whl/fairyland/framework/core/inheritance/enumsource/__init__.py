d50c6a8ee5ab6e108901ab8fd1315fc7da9acbc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:48:46 +0000

    Add package PyFairyland/0.0.4rc46.post20240206/PyFairyland-0.0.4rc46.post20240206-py3-none-any.whl

---
+++ packages/PyFairyland/PyFairyland-0.0.4rc46.post20240206-py3-none-any.whl/fairyland/framework/core/inheritance/enumsource/__init__.py
@@ -0,0 +1,31 @@
+# coding: utf8
+""" 
+@File: __init__.py
+@Editor: PyCharm
+@Author: Austin (From Chengdu.China) https://fairy.host
+@HomePage: https://github.com/AustinFairyland
+@OperatingSystem: Windows 11 Professional Workstation 23H2 Canary Channel
+@CreatedTime: 2024-02-05
+"""
+from __future__ import annotations
+
+import os
+import sys
+import warnings
+import platform
+import asyncio
+
+sys.dont_write_bytecode = True
+warnings.filterwarnings("ignore")
+if platform.system() == "Windows":
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
+from .EnumSourceInheritance import BaseEnum
+from .EnumSourceInheritance import StringEnum
+from .EnumSourceInheritance import IntegerEnum
+
+__all__: list = [
+    "BaseEnum",
+    "StringEnum",
+    "IntegerEnum",
+]
