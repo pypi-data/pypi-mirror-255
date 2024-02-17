6364928abbde0fc8cd7565c1c2d45b62520c098a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:29:37 +0000

    Add package temporary-projects-brainfo/0.1/temporary_projects_brainfo-0.1.tar.gz

---
+++ packages/temporary-projects-brainfo/temporary_projects_brainfo-0.1.tar.gz/temporary_projects_brainfo-0.1/README.md
@@ -0,0 +1,6 @@
+# omics_utils
+<<<<<<< HEAD
+Temporary deposite utils src for omics analysis
+=======
+Temporary deposite utils packages for omics analysis
+>>>>>>> 06e1af77090624603de33005ff411c72ab7cf5ba
---
+++ packages/temporary-projects-brainfo/temporary_projects_brainfo-0.1.tar.gz/temporary_projects_brainfo-0.1/setup.py
@@ -0,0 +1,11 @@
+
+from setuptools import setup
+  
+setup(
+    name='temporary_projects_brainfo',
+    version='0.1',
+    description='A small collective utils for 2nd generation sequencing analysis',
+    author='Brainfo',
+    author_email='scilavisher@gmail.com',
+    packages=['gene_level', 'sc_utils'],
+)
