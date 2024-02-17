37975d2a3fec6c554bd490ec3f8b8ad0efe0b053
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:58:44 +0000

    Add package splinepy/0.0.51/splinepy-0.0.51.tar.gz

---
+++ packages/splinepy/splinepy-0.0.51.tar.gz/splinepy-0.0.51/splinepy/io/__init__.py
@@ -0,0 +1,13 @@
+from splinepy.io import cats, gismo, iges, ioutils, irit, json, mfem, npz, vtk
+
+__all__ = [
+    "cats",
+    "gismo",
+    "iges",
+    "ioutils",
+    "irit",
+    "json",
+    "mfem",
+    "npz",
+    "vtk",
+]
