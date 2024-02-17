cfffcd9ae7dd1a0674e04ad7f043f744bb732c3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:30:01 +0000

    Add package vvf-sdk/0.0.0.1/vvf-sdk-0.0.0.1.tar.gz

---
+++ packages/vvf-sdk/vvf-sdk-0.0.0.1.tar.gz/vvf-sdk-0.0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="vvf-sdk",
+    version="0.0.0.1",
+    author="VMware Inc.",
+    description="Python bindings for vsphere foundation sdk",
+    packages=find_packages(),
+    install_requires=[
+        # List your package dependencies here
+    ],
+)
+
