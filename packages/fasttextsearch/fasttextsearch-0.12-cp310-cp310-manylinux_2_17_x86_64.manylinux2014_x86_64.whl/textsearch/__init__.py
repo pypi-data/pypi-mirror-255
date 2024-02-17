b0c77b7d96644cc4db9f5995710bfd133bf10641
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:54:34 +0000

    Add package fasttextsearch/0.12/fasttextsearch-0.12-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/fasttextsearch/fasttextsearch-0.12-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/textsearch/__init__.py
@@ -0,0 +1,24 @@
+from _textsearch import find_close_matches
+from _textsearch import levenshtein_distance
+
+from .datatypes import SourcedText
+from .datatypes import TextSource
+from .datatypes import TextSources
+from .datatypes import Transcript
+from .datatypes import append_texts
+from .datatypes import filter_texts
+from .datatypes import texts_to_sourced_texts
+
+from .levenshtein import get_nice_alignments
+
+from .match import align_queries
+from .match import get_longest_increasing_pairs
+from .match import split_aligned_queries
+
+from .suffix_array import create_suffix_array
+
+from .utils import AttributeDict
+from .utils import is_punctuation
+from .utils import row_ids_to_row_splits
+from .utils import str2bool
+__version__ = '0.12'
