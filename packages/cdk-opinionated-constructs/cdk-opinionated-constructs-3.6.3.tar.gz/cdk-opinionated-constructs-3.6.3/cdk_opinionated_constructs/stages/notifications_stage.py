2de0d61326596dfb9afbf66163484ccde68d72bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:38:58 +0000

    Add package cdk-opinionated-constructs/3.6.3/cdk-opinionated-constructs-3.6.3.tar.gz

---
+++ packages/cdk-opinionated-constructs/cdk-opinionated-constructs-3.6.3.tar.gz/cdk-opinionated-constructs-3.6.3/cdk_opinionated_constructs/stages/notifications_stage.py
@@ -0,0 +1,33 @@
+import aws_cdk as cdk
+
+from constructs import Construct
+
+from cdk_opinionated_constructs.stacks.governance_stack import GovernanceStack
+from cdk_opinionated_constructs.stacks.notifications_stack import NotificationsStack
+
+
+class NotificationsStage(cdk.Stage):
+    def __init__(self, scope: Construct, construct_id: str, env: cdk.Environment, props: dict, **kwargs) -> None:
+        """Constructs the Notifications and Governance stacks.
+
+        The NotificationsStack constructs resources related to
+        notifications, such as SNS topics and subscriptions.
+
+        The GovernanceStack constructs resources related to governance,
+        such as Config and GuardDuty.
+        """
+        super().__init__(scope, construct_id, env=env, **kwargs)
+
+        NotificationsStack(
+            self,
+            construct_id="notifications-stack",
+            env=env,
+            props=props,
+        )
+
+        GovernanceStack(
+            self,
+            construct_id="governance-stack",
+            env=env,
+            props=props,
+        )
