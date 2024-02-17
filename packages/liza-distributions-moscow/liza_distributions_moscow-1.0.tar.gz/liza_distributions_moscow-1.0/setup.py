02b9ec7f2746b67a9c8c9f77856a6f20d1a330b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:30:30 +0000

    Add package liza-distributions-moscow/1.0/liza_distributions_moscow-1.0.tar.gz

---
+++ packages/liza-distributions-moscow/liza_distributions_moscow-1.0.tar.gz/liza_distributions_moscow-1.0/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='liza_distributions_moscow',
+      version='1.0',
+      description='Gaussian distributions',
+      packages=['liza_distributions_moscow'],
+      zip_safe=False)
