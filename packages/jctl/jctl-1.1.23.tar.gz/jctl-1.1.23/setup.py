4abf3b98819ff28a6f0dc3b15c11a87d82dadc1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:44:47 +0000

    Add package jctl/1.1.23/jctl-1.1.23.tar.gz

---
+++ packages/jctl/jctl-1.1.23.tar.gz/jctl-1.1.23/setup.py
@@ -0,0 +1,36 @@
+import re
+
+import setuptools
+
+
+def get_property(prop, file):
+    result = re.search(r'{}\s*=\s*[\'"]([^\'"]*)[\'"]'.format(prop), open(file).read())
+    return result.group(1)
+
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="jctl",
+    version=get_property("__version__", "jctl/jctl"),
+    author="The University of Utah",
+    author_email="mlib-its-mac@lists.utah.edu",
+    description="Command line tool that utilizes Python-Jamf",
+    license="MIT",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/univ-of-utah-marriott-library-apple/jctl",
+    scripts=["jctl/jctl", "jctl/pkgctl", "jctl/patch.py"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        # 4 - Beta
+        # 5 - Production/Stable
+        "Development Status :: 4 - Beta",
+    ],
+    python_requires=">=3.7",
+    install_requires=["python-jamf>=0.9.0"],
+    py_modules=["jctl"],
+)
