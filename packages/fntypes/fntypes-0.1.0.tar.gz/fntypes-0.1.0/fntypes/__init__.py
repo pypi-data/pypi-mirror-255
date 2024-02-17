24c5aca09d4b90eadc6047c1479c4c6dfbad9c02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:49:33 +0000

    Add package fntypes/0.1.0/fntypes-0.1.0.tar.gz

---
+++ packages/fntypes/fntypes-0.1.0.tar.gz/fntypes-0.1.0/fntypes/__init__.py
@@ -0,0 +1,6 @@
+from .option import Option, Nothing, Some
+from .union import Union
+from .result import Result, Error, Ok, Wrapped
+from .tools import unwrapping
+
+__all__ = ("Option", "Nothing", "Some", "Union", "Result", "Error", "Ok", "Wrapped", "unwrapping")
