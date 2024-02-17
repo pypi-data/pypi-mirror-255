fd3bcd5cda3e1d70a6bc302387532844e6917493
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:44:58 +0000

    Add package transport-data/24.2.5rc1/transport_data-24.2.5rc1-py3-none-any.whl

---
+++ packages/transport-data/transport_data-24.2.5rc1-py3-none-any.whl/transport_data/org/cli.py
@@ -0,0 +1,19 @@
+"""CLI for :mod:`.org`.
+
+.. runblock:: console
+
+   $ tdc org --help
+
+"""
+import click
+
+from transport_data import STORE
+from transport_data.util.click import common_params
+
+
+@click.command("org", params=common_params("version"))
+def main(version):
+    """Information about the TDCI per se."""
+    from . import get_agencyscheme
+
+    STORE.write(get_agencyscheme(version=version), force=True)
