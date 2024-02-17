8cf1c99d256ead404ecc076ce83e1f260ae14d00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:43:24 +0000

    Add package paddle-billing-python-sdk/0.0.1a91/paddle-billing-python-sdk-0.0.1a91.tar.gz

---
+++ packages/paddle-billing-python-sdk/paddle-billing-python-sdk-0.0.1a91.tar.gz/paddle-billing-python-sdk-0.0.1a91/paddle_billing/Entities/Reports/ReportName.py
@@ -0,0 +1,9 @@
+from enum import StrEnum
+
+
+class ReportName(StrEnum):
+    CollectionMode = 'collection_mode'
+    CurrencyCode   = 'currency_code'
+    Origin         = 'origin'
+    Status         = 'status'
+    UpdatedAt      = 'updated_at'
---
+++ packages/paddle-billing-python-sdk/paddle-billing-python-sdk-0.0.1a91.tar.gz/paddle-billing-python-sdk-0.0.1a91/paddle_billing/Entities/Reports/ReportOperator.py
@@ -0,0 +1,6 @@
+from enum import StrEnum
+
+
+class ReportOperator(StrEnum):
+    Lt  = 'lt'
+    Gte = 'gte'
