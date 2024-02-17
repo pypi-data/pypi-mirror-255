1df8f36497a46fb4d50dad373f884dd3b1f699db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:30:12 +0000

    Add package ezyli-utils/1.0.0/ezyli_utils-1.0.0.tar.gz

---
+++ packages/ezyli-utils/ezyli_utils-1.0.0.tar.gz/ezyli_utils-1.0.0/ezyli_utils/amqp_consumer.py
@@ -0,0 +1,69 @@
+import json
+import time
+import pika
+from pika.exceptions import ChannelClosed, AMQPConnectionError
+from .validator import validate_data
+
+
+class AMQPConsumer:
+    def __init__(self, amqp_url, callback):
+        self.amqp_url = amqp_url
+        self.callback = callback
+        self.params = pika.URLParameters(self.amqp_url)
+
+    def validate_data(self, data, schema):
+        return validate_data(data, schema)
+
+    def internal_callback(self, ch, method, properties, body):
+        # Convert body from bytes to string and then to a dictionary
+        try:
+            content = json.loads(body.decode())
+        except json.JSONDecodeError:
+            print(f"Invalid JSON received :: {body.decode()}")
+            return
+
+        # Define your schema here
+        schema = {
+            "$schema": "http://json-schema.org/draft-07/schema#",
+            "type": "object",
+            "properties": {
+                "data": {"type": "object"},
+                "routing_id": {"type": "string"},
+                "user_id": {"type": "integer"},
+                "app": {"type": "string"},
+                "event": {"type": "string"},
+                "meta": {"type": "object"},
+            },
+            "required": ["data", "routing_id", "user_id", "app", "event", "meta"],
+        }
+        if self.validate_data(content, schema):
+            self.callback(ch, method, properties, body)
+
+    def consume(self, queue_name, durability):
+        while True:
+            print("Setting up consumer 1:- ...")
+            try:
+                connection = pika.BlockingConnection(self.params)
+                channel = connection.channel()
+                channel.queue_declare(queue=queue_name, durable=durability)
+                channel.basic_consume(
+                    queue=queue_name,
+                    on_message_callback=self.internal_callback,
+                    auto_ack=True,
+                )
+
+                print("Started Consuming")
+
+                channel.start_consuming()
+            except (ChannelClosed, AMQPConnectionError) as e:
+                print("Reconnecting ...")
+                try:
+                    channel.close()
+                    connection.close()
+                except:
+                    pass
+                time.sleep(5)
+            except Exception as e:
+                print("Reconnecting ...")
+                print(e)
+                pass
