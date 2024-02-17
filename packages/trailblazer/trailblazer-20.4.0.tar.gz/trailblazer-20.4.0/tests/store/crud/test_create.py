9e36dffdc56cd7b4947ed03a3ecea81f5321e577
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:29:06 +0000

    Add package trailblazer/20.4.0/trailblazer-20.4.0.tar.gz

---
+++ packages/trailblazer/trailblazer-20.4.0.tar.gz/trailblazer-20.4.0/tests/store/crud/test_create.py
@@ -0,0 +1,49 @@
+from tests.mocks.store_mock import MockStore
+from trailblazer.store.filters.user_filters import UserFilter, apply_user_filter
+from trailblazer.store.models import Analysis, User
+
+
+def test_add_pending_analysis(raw_analyses: list[dict], store: MockStore, user_email: str):
+    """Test adding a new analysis to the database."""
+    # GIVEN an empty database
+    assert not store.get_query(table=Analysis).first()
+
+    # GIVEN an unprocessed raw analysis
+    analysis: dict = raw_analyses[0]
+
+    # WHEN adding a new analysis
+    new_analysis: Analysis = store.add_pending_analysis(
+        case_id=analysis.get("case_id"),
+        config_path=analysis.get("config_path"),
+        email=user_email,
+        out_dir=analysis.get("out_dir"),
+        priority=analysis.get("priority"),
+        type=analysis.get("type"),
+    )
+
+    # THEN it should be stored in the database
+    stored_analysis: Analysis = store.get_analysis(
+        case_id=analysis.get("case_id"),
+        started_at=new_analysis.started_at,
+        status=analysis.get("status"),
+    )
+    assert new_analysis and stored_analysis
+    assert stored_analysis == new_analysis
+
+
+def test_add_user(store: MockStore, user_email: str, username: str):
+    """Test adding a user to the database."""
+    # GIVEN an empty database
+    assert not store.get_query(table=User).first()
+
+    # WHEN adding a new user
+    new_user: User = store.add_user(name=username, email=user_email)
+
+    # THEN it should be stored in the database
+    user: User = apply_user_filter(
+        filter_functions=[UserFilter.FILTER_BY_EMAIL],
+        users=store.get_query(table=User),
+        email=user_email,
+    ).first()
+    assert new_user
+    assert user == new_user
---
+++ packages/trailblazer/trailblazer-20.4.0.tar.gz/trailblazer-20.4.0/trailblazer/store/crud/create.py
@@ -0,0 +1,65 @@
+from datetime import datetime
+
+from sqlalchemy.orm import Session
+
+from trailblazer.constants import SlurmJobStatus, TrailblazerStatus
+from trailblazer.dto.create_job_request import CreateJobRequest
+from trailblazer.store.base import BaseHandler
+from trailblazer.store.database import get_session
+from trailblazer.store.models import Analysis, Job, User
+
+
+class CreateHandler(BaseHandler):
+    """Class for creating items in the database."""
+
+    def add_pending_analysis(
+        self,
+        case_id: str,
+        config_path: str,
+        out_dir: str,
+        priority: str,
+        type: str,
+        email: str = None,
+        data_analysis: str = None,
+        ticket_id: str = None,
+        workflow_manager: str = None,
+    ) -> Analysis:
+        """Add pending analysis with user if supplied with email."""
+        session: Session = get_session()
+        new_analysis = Analysis(
+            config_path=config_path,
+            data_analysis=data_analysis,
+            case_id=case_id,
+            out_dir=out_dir,
+            priority=priority,
+            started_at=datetime.now(),
+            status=TrailblazerStatus.PENDING,
+            ticket_id=ticket_id,
+            type=type,
+            workflow_manager=workflow_manager,
+        )
+        new_analysis.user = self.get_user(email=email) if email else None
+        session.add(new_analysis)
+        session.commit()
+        return new_analysis
+
+    def add_user(self, name: str, email: str) -> User:
+        """Add a new user to the database."""
+        session: Session = get_session()
+        new_user = User(email=email, name=name)
+        session.add(new_user)
+        session.commit()
+        return new_user
+
+    def add_job(self, analysis_id: int, job_request: CreateJobRequest) -> Job:
+        """Add a new job to the database."""
+        analysis: Analysis = self.get_analysis_with_id(analysis_id)
+        job = Job(
+            status=SlurmJobStatus.PENDING,
+            slurm_id=job_request.slurm_id,
+            job_type=job_request.job_type,
+        )
+        analysis.jobs.append(job)
+        session: Session = get_session()
+        session.commit()
+        return job
