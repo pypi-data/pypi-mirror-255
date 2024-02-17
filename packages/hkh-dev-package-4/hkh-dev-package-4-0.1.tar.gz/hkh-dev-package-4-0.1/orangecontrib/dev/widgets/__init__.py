d5c425320d0b48ec68f652a804b6cbd6b052d379
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:35:55 +0000

    Add package hkh-dev-package-4/0.1/hkh-dev-package-4-0.1.tar.gz

---
+++ packages/hkh-dev-package-4/hkh-dev-package-4-0.1.tar.gz/hkh-dev-package-4-0.1/orangecontrib/dev/widgets/__init__.py
@@ -0,0 +1,17 @@
+"""
+Widgets from Development workflow category
+
+"""
+ID = "Orange.widgets.dev_workflow"
+
+NAME = "dev_worklow"
+
+# Category icon show in the menu
+ICON = "icons/Logo_HKH.svg"
+
+BACKGROUND = "light-blue"
+
+DESCRIPTION = "Dev widget."
+
+
+PRIORITY = 6
