94a0871bb65386155a9be0a2f445fbfcc27fa447
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:29:36 +0000

    Add package common-util-py/0.0.18/common_util_py-0.0.18.tar.gz

---
+++ packages/common-util-py/common_util_py-0.0.18.tar.gz/common_util_py-0.0.18/mailer/__init__.py
@@ -0,0 +1,4 @@
+from .gmail import send_text as gmail_send_text
+from .gmail import send_html as gmail_send_html
+from .mailer import send_text as mail_send_text
+from .mailer import send_html as mail_send_html
