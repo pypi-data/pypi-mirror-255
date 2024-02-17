dee78cbb3a9e4b7b4faebfe332113a0d3c6a6940
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:58:40 +0000

    Add package aryantdistributions/0.1/aryantdistributions-0.1.tar.gz

---
+++ packages/aryantdistributions/aryantdistributions-0.1.tar.gz/aryantdistributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='aryantdistributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['aryantdistributions'],
+      author='Aryan Tanwar',
+      author_email='tanwararyan11@gmail.com',
+    zip_safe=False)
