47040777ed6bcf31ed0c81b7c48e57dc8e5b3de9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:35:33 +0000

    Add package PBD-Gaussian-Binomial/0.1/PBD_Gaussian_Binomial-0.1.tar.gz

---
+++ packages/PBD-Gaussian-Binomial/PBD_Gaussian_Binomial-0.1.tar.gz/PBD_Gaussian_Binomial-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='PBD_Gaussian_Binomial',
+      version='0.1',
+      description='Gaussian and Binomial Distributions',
+      author='Andrew Paster',
+      packages=['distributions'],
+      zip_safe=False)
