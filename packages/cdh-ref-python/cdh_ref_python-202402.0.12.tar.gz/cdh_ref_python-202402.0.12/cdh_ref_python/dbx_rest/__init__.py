0660a26a80c380315d56fe0ce05524d6477e624b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:46:04 +0000

    Add package cdh-ref-python/202402.0.12/cdh_ref_python-202402.0.12.tar.gz

---
+++ packages/cdh-ref-python/cdh_ref_python-202402.0.12.tar.gz/cdh_ref_python-202402.0.12/cdh_ref_python/dbx_rest/__init__.py
@@ -0,0 +1,16 @@
+"""
+Modules shared between dbrest and dougrest.  Forms the basis of a shared unified rest client.
+"""
+
+__all__ = [
+    "cluster_policies",
+    "clusters",
+    "instance_pools",
+    "jobs",
+    "pipelines",
+    "repos",
+    "runs",
+    "token_management",
+    "tokens",
+    "workspace",
+]
