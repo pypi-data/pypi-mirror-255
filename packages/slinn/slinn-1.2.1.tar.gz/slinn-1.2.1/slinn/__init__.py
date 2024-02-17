6a64163d122432f01ec94c97b56a118effa07eb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:35:50 +0000

    Add package slinn/1.2.1/slinn-1.2.1.tar.gz

---
+++ packages/slinn/slinn-1.2.1.tar.gz/slinn-1.2.1/slinn/__init__.py
@@ -0,0 +1,10 @@
+from slinn.address import Address
+from slinn.filter import Filter
+from slinn.request import Request
+from slinn.http_response import HttpResponse
+from slinn.http_redirect import HttpRedirect
+from slinn.http_api_response import HttpAPIResponse
+from slinn.server import Server
+from slinn.dispatcher import Dispatcher
+
+version = 'Slinn v1.2.1 050224b'
