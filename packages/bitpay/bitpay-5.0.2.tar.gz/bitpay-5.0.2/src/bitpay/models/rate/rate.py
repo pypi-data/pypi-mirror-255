334eca64bc6a27dd09c2546355264150a3990b9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:39:09 +0000

    Add package bitpay/5.0.2/bitpay-5.0.2.tar.gz

---
+++ packages/bitpay/bitpay-5.0.2.tar.gz/bitpay-5.0.2/src/bitpay/models/rate/rate.py
@@ -0,0 +1,15 @@
+"""
+Rate
+"""
+from typing import Union
+from bitpay.models.bitpay_model import BitPayModel
+
+
+class Rate(BitPayModel):
+    """
+    Rate
+    """
+
+    name: Union[str, None] = None
+    code: Union[str, None] = None
+    rate: Union[float, None] = None
