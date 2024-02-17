e886439cd590bba60462c78600cae5ad483b0658
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:43:32 +0000

    Add package distclassipy/0.0.2/distclassipy-0.0.2.tar.gz

---
+++ packages/distclassipy/distclassipy-0.0.2.tar.gz/distclassipy-0.0.2/distclassipy/__init__.py
@@ -0,0 +1,2 @@
+from .classifier import DistanceMetricClassifier  # Importing the DistanceMetricClassifier from the classifier module
+from .distances import Distance  # Importing the Distance class from the distances module
