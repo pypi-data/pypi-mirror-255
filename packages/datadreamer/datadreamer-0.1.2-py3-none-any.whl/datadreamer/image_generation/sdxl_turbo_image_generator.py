ab75053d1630e8fed288b807f3b5b4d178ffb01d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:34:46 +0000

    Add package datadreamer/0.1.2/datadreamer-0.1.2-py3-none-any.whl

---
+++ packages/datadreamer/datadreamer-0.1.2-py3-none-any.whl/datadreamer/image_generation/sdxl_turbo_image_generator.py
@@ -0,0 +1,113 @@
+from typing import List, Optional
+
+import torch
+from diffusers import AutoPipelineForText2Image
+from PIL import Image
+
+from datadreamer.image_generation.image_generator import ImageGenerator
+
+
+class StableDiffusionTurboImageGenerator(ImageGenerator):
+    """A subclass of ImageGenerator specifically designed to use the Stable Diffusion
+    Turbo model for faster image generation.
+
+    Attributes:
+        base (AutoPipelineForText2Image): The Stable Diffusion Turbo model for image generation.
+
+    Methods:
+        _init_gen_model(): Initializes the Stable Diffusion Turbo model.
+        generate_image(prompt, negative_prompt, prompt_objects): Generates an image based on the provided prompt.
+        release(empty_cuda_cache): Releases resources and optionally empties the CUDA cache.
+    """
+
+    def __init__(self, *args, **kwargs):
+        """Initializes the StableDiffusionTurboImageGenerator with the given
+        arguments."""
+        super().__init__(*args, **kwargs)
+        self.base = self._init_gen_model()
+
+    def _init_gen_model(self):
+        """Initializes the Stable Diffusion Turbo model for image generation.
+
+        Returns:
+            AutoPipelineForText2Image: The initialized Stable Diffusion Turbo model.
+        """
+        if self.device == "cpu":
+            base = AutoPipelineForText2Image.from_pretrained(
+                "stabilityai/sdxl-turbo",
+                # variant="fp16",
+                torch_dtype=torch.float32,
+                use_safetensors=True,
+            )
+            base.to("cpu")
+        else:
+            base = AutoPipelineForText2Image.from_pretrained(
+                "stabilityai/sdxl-turbo",
+                torch_dtype=torch.float16,
+                variant="fp16",
+                use_safetensors=True,
+            )
+            base.enable_model_cpu_offload()
+
+        return base
+
+    def generate_image(
+        self,
+        prompt: str,
+        negative_prompt: str,
+        prompt_objects: Optional[List[str]] = None,
+    ) -> Image.Image:
+        """Generates an image using the Stable Diffusion Turbo model based on the
+        provided prompt.
+
+        Args:
+            prompt (str): The positive prompt to guide image generation.
+            negative_prompt (str): The negative prompt to avoid certain features in the image.
+            prompt_objects (Optional[List[str]]): Optional list of objects to be used in CLIP model testing.
+
+        Returns:
+            Image.Image: The generated image.
+        """
+        image = self.base(
+            prompt=prompt,
+            negative_prompt=negative_prompt,
+            guidance_scale=0.0,
+            num_inference_steps=4,
+        ).images[0]
+
+        return image
+
+    def release(self, empty_cuda_cache=False) -> None:
+        """Releases the model and optionally empties the CUDA cache."""
+        self.base = self.base.to("cpu")
+        if self.use_clip_image_tester:
+            self.clip_image_tester.release()
+        if empty_cuda_cache:
+            with torch.no_grad():
+                torch.cuda.empty_cache()
+
+
+if __name__ == "__main__":
+    import os
+
+    # Create the generator
+    image_generator = StableDiffusionTurboImageGenerator(
+        seed=42,
+        use_clip_image_tester=False,
+        image_tester_patience=1,
+        device="cpu",
+    )
+    prompts = [
+        "A photo of a bicycle pedaling alongside an aeroplane taking off, showcasing the harmony between human-powered and mechanical transportation.",
+    ]
+    prompt_objects = [["aeroplane", "boat", "bicycle"]]
+
+    image_paths = []
+    for i, generated_image in enumerate(
+        image_generator.generate_images(prompts, prompt_objects)
+    ):
+        image_path = os.path.join("./", f"image_turbo_{i}.jpg")
+        generated_image.save(image_path)
+        image_paths.append(image_path)
+
+    image_generator.release(empty_cuda_cache=True)
---
+++ packages/datadreamer/datadreamer-0.1.2-py3-none-any.whl/datadreamer/prompt_generation/tinyllama_lm_prompt_generator.py
@@ -0,0 +1,136 @@
+import re
+from typing import List, Optional
+
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+from datadreamer.prompt_generation.lm_prompt_generator import LMPromptGenerator
+
+
+class TinyLlamaLMPromptGenerator(LMPromptGenerator):
+    """A language model-based prompt generator class, extending PromptGenerator.
+
+    Attributes:
+        device (str): Device to run the language model on ('cuda' for GPU, 'cpu' for CPU).
+        model (AutoModelForCausalLM): The pre-trained causal language model for generating prompts.
+        tokenizer (AutoTokenizer): The tokenizer for the pre-trained language model.
+
+    Methods:
+        _init_lang_model(): Initializes the language model and tokenizer.
+        generate_prompts(): Generates a list of prompts based on the class names.
+        _create_lm_prompt_text(selected_objects): Creates a text prompt for the language model.
+        generate_prompt(prompt_text): Generates a single prompt using the language model.
+        _test_prompt(prompt, selected_objects): Tests if the generated prompt is valid.
+        release(empty_cuda_cache): Releases resources and optionally empties the CUDA cache.
+    """
+
+    def __init__(
+        self,
+        class_names: List[str],
+        prompts_number: int = 10,
+        num_objects_range: Optional[List[int]] = None,
+        seed: Optional[float] = 42,
+        device: str = "cuda",
+    ) -> None:
+        """Initializes the LMPromptGenerator with class names and other settings."""
+        super().__init__(class_names, prompts_number, num_objects_range, seed, device)
+
+    def _init_lang_model(self):
+        """Initializes the language model and tokenizer for prompt generation.
+
+        Returns:
+            tuple: The initialized language model and tokenizer.
+        """
+        if self.device == "cpu":
+            print("Loading language model on CPU...")
+            model = AutoModelForCausalLM.from_pretrained(
+                "TinyLlama/TinyLlama-1.1B-Chat-v1.0",
+                torch_dtype="auto",
+                device_map="cpu",
+                low_cpu_mem_usage=True,
+            )
+        else:
+            print("Loading language model on GPU...")
+            model = AutoModelForCausalLM.from_pretrained(
+                "TinyLlama/TinyLlama-1.1B-Chat-v1.0", torch_dtype=torch.float16
+            )
+
+        tokenizer = AutoTokenizer.from_pretrained(
+            "TinyLlama/TinyLlama-1.1B-Chat-v1.0", trust_remote_code=True
+        )
+        print("Done!")
+        return model.to(self.device), tokenizer
+
+    def _remove_incomplete_sentence(self, text):
+        # Define the regex pattern to capture up to the last sentence-ending punctuation
+        pattern = r"^(.*[.!?])"
+        match = re.search(pattern, text)
+        return match.group(0) if match else text
+
+    def _remove_caption_sentences(self, text):
+        # Pattern to find sentences that start with "Caption reads: "
+        # \s* matches any whitespace characters at the beginning of the string (including none)
+        # re.IGNORECASE makes the search case-insensitive
+        # [^\.!?]* matches any sequence of characters that are not a period, exclamation mark, or question mark
+        # [\.\!?] matches a period, exclamation mark, or question mark, indicating the end of a sentence
+        pattern = re.compile(r"\s*Caption reads: [^\.!?]*[\.\!?]", re.IGNORECASE)
+        # Replace the matched sentences with an empty string
+        cleaned_text = re.sub(pattern, "", text)
+        return cleaned_text
+
+    def _create_lm_prompt_text(self, selected_objects: List[str]) -> str:
+        """Creates a language model text prompt based on selected objects.
+
+        Args:
+            selected_objects (List[str]): Objects to include in the prompt.
+
+        Returns:
+            str: A text prompt for the language model.
+        """
+        return f"<|system|>\nYou are a chatbot who describes content of images!</s>\n<|user|>\nGenerate a short and concise caption for an image. Follow this template: 'A photo of {', '.join(selected_objects)}', where the objects interact in a meaningful way within a scene, complete with a short scene description. The caption must be short in length and start with the words: 'A photo of '! Do not use the phrase 'Caption reads'.</s>\n<|assistant|>\n"
+
+    def generate_prompt(self, prompt_text: str) -> str:
+        """Generates a single prompt using the language model.
+
+        Args:
+            prompt_text (str): The text prompt for the language model.
+
+        Returns:
+            str: The generated prompt.
+        """
+        encoded_input = self.tokenizer(prompt_text, return_tensors="pt").to(self.device)
+        generated_ids = self.model.generate(
+            **encoded_input,
+            max_new_tokens=70,
+            do_sample=True,
+            top_p=0.95,
+            top_k=50,
+            temperature=0.7,
+            num_beams=1,
+            pad_token_id=self.tokenizer.eos_token_id,
+        )
+        decoded_prompt = self.tokenizer.decode(
+            generated_ids[0], skip_special_tokens=True
+        )
+        instructional_pattern = r"<\|system\|>\n.*?\n<\|user\|>\n.*?\n<\|assistant\|>\n"
+        # Remove the instructional text to isolate the caption
+        decoded_prompt = (
+            re.sub(instructional_pattern, "", decoded_prompt)
+            .replace('"', "")
+            .replace("'", "")
+        )
+
+        return self._remove_caption_sentences(
+            self._remove_incomplete_sentence(decoded_prompt)
+        )
+
+
+if __name__ == "__main__":
+    # Example usage of the class
+    object_names = ["aeroplane", "bicycle", "bird", "boat", "city"]
+    prompt_generator = TinyLlamaLMPromptGenerator(
+        class_names=object_names, prompts_number=5, device="cpu"
+    )
+    generated_prompts = prompt_generator.generate_prompts()
+    for prompt in generated_prompts:
+        print(prompt)
