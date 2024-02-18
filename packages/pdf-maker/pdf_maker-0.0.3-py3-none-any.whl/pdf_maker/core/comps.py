7d6d81811726be98d7245842f91e92536575e9a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 06:04:10 +0000

    Add package pdf-maker/0.0.3/pdf_maker-0.0.3-py3-none-any.whl

---
+++ packages/pdf-maker/pdf_maker-0.0.3-py3-none-any.whl/pdf_maker/core/comps.py
@@ -0,0 +1,314 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+"""
+# ==========================================
+# Copyright 2024 Yang 
+# pdf-tool - comps
+# ==========================================
+#
+#
+# 
+"""
+import re
+from fontTools.ttLib import TTFont
+from pdf_maker.constants._global import FONT_LIB, COLOR_PALETTE, LINE_STYLE, WIND
+from typing import Tuple, Union, List
+from xml.etree import ElementTree
+
+
+class BaseContent:
+    def __init__(self, name: str = "", z_index: int = 0, **options):
+        self._name = name
+        self._z_index = z_index
+        for key, value in options.items():
+            names = [key, f"_{key.lower()}"]
+            for name in names:
+                if hasattr(self, name) and not callable(getattr(self, name)):
+                    setattr(self, name, value)
+
+        self._code: str = ""
+
+    def code(self):
+        return ""
+
+    def name(self):
+        return self._name
+
+    def z_index(self):
+        return self._z_index
+
+
+class Text(BaseContent):
+    def __init__(self, font_name, size, x, y, text, font, **options):
+        self._font_name = font_name  # font name defined in page resources
+        self._font = font
+        self._size = size
+        self._x = x
+        self._y = y
+        self._text = text
+        self._sub_size = int(self._size * 2. / 3.)
+        self._sup_size = int(self._size * 2. / 3.)
+        self._sub_Ts = -int(self._size * 1. / 4.)
+        self._sup_Ts = int(self._size * 1. / 3.)
+        self._color = COLOR_PALETTE.get('black', [0, 0, 0])
+        self._line_space = 1.3
+        self._line_height = 0
+
+        super().__init__(**options)
+
+        if isinstance(self._color, str):
+            self._color = COLOR_PALETTE.get(self._color, [0, 0, 0])
+
+        self.code()
+
+    def code(self):
+        self._line_height = ((abs(self._sub_Ts)) + max(
+            (abs(self._sup_Ts) + self.get_text_height(self._text, self._font, self._sup_size)),
+            self.get_text_height(self._text, self._font, self._size))) * self._line_space
+        text_list = self.read_rich_text(self._text)
+        self._code = f"BT\n{self._x} {self._y} Td\n" + "\n".join([f"/{self._font_name} {self.size(script)} Tf\n{color} rg\n{color} RG\n{self.Ts(script)} Ts\n{f'0 -{self._line_height} Td' if r == 'r' else ''}\n({item}) Tj" for (item, script, color, r) in text_list]) + "\nET"
+        return self._code
+
+    def size(self, flag: str):
+        if flag == "sub": return self._sub_size
+        if flag == "sup": return self._sup_size
+        return self._size
+
+    def Ts(self, flag: str):
+        if flag == "sub": return self._sub_Ts
+        if flag == "sup": return self._sup_Ts
+        return 0
+
+    def read_rich_text(self, text):
+        rich_text_list = self.read_script(text)
+        rich_text_list = [(color[0], script[1], color[1]) for script in rich_text_list for color in self.read_color(script[0])]
+        rich_text_list = [(r[0], script, color, r[1]) for (item, script, color) in rich_text_list for r in self.read_break(item)]
+        return rich_text_list
+
+    def read_color(self, rich_text):
+        for name, value in COLOR_PALETTE.items():
+            if rich_text.startswith(f"<{name}>") and rich_text.endswith(f"</{name}>"):
+                return [(rich_text.removeprefix(f"<{name}>").removesuffix(f"</{name}>"), ' '.join([str(i) for i in value]))]
+
+            if f"<{name}>" in rich_text and f"</{name}>" in rich_text:
+                reg = f"(<{name}>.*?</{name}>)"
+                text_list = re.split(reg, rich_text)
+                return [i for item in text_list for i in self.read_color(item)]
+
+        return [(rich_text, ' '.join([str(i) for i in self._color]))]
+
+    def read_script(self, rich_text):
+        if rich_text.startswith("<sub>") and rich_text.endswith("</sub>"):
+            return [(rich_text.removeprefix("<sub>").removesuffix("</sub>"), "sub")]
+        if rich_text.startswith("<sup>") and rich_text.endswith("</sup>"):
+            return [(rich_text.removeprefix("<sup>").removesuffix("</sup>"), "sup")]
+        if "<sub>" in rich_text and "</sub>" in rich_text:
+            text_list = re.split(r"(<sub>.*?</sub>)", rich_text)
+            return [i for item in text_list for i in self.read_script(item)]
+        if "<sup>" in rich_text and "</sup>" in rich_text:
+            text_list = re.split(r"(<sup>.*?</sup>)", rich_text)
+            return [i for item in text_list for i in self.read_script(item)]
+        return [(rich_text, "normal")]
+
+    def read_break(self, rich_text: str):
+        if "<r>" in rich_text:
+            text = rich_text.split("<r>")
+            return [(text[0], ""), (text[1], "r")]
+        return [(rich_text, "")]
+
+    @staticmethod
+    def to_chr(code: str):
+        return "".join([chr(int(f"0x{each}", 16)) for each in code.lstrip("FEFF").split(" ")])
+
+    @staticmethod
+    def get_text_width(text: str, font: str, size: int):
+        font = TTFont(FONT_LIB.get(str(font).lower()))
+        cmap = font['cmap']
+        t = cmap.getcmap(3, 1).cmap
+        s = font.getGlyphSet()
+        pixels_per_em = font['head'].unitsPerEm
+        total = 0
+        for c in text:
+            if ord(c) in t and t[ord(c)] in s:
+                total += s[t[ord(c)]].width
+            else:
+                total += s['.notdef'].width
+        total = total * float(size) / pixels_per_em
+        return int(total)
+
+    @staticmethod
+    def get_text_height(text: str, font: str, size: int):
+        # font = TTFont(FONT_LIB.get(str(font).lower()))
+        # pixels_per_em = font['head'].unitsPerEm
+        # ascender = font['OS/2'].sTypoAscender
+        # descender = font['OS/2'].sTypoDescender
+        # line_gap = font['OS/2'].sTypoLineGap
+
+        tree = ElementTree.parse(FONT_LIB.get(font.lower()))
+        root = tree.getroot()
+        head_obj = root.find('head')
+        OS_2_obj = root.find('OS_2')
+        pixels_per_em = head_obj.find("unitsPerEm").attrib['value']
+        ascender = OS_2_obj.find("sTypoAscender").attrib['value']
+        descender = OS_2_obj.find("sTypoDescender").attrib['value']
+        line_gap = OS_2_obj.find("sTypoLineGap").attrib['value']
+        line_height = (float(ascender) + float(descender) + float(line_gap)) / float(pixels_per_em) * size
+        return int(line_height)
+
+
+class Line(BaseContent):
+    def __init__(self, start, end, line_style: str = "solid", **options):
+        self._start = start
+        self._end = end
+        self._width = 1
+        self._line_style = line_style
+        self._color = COLOR_PALETTE.get('black', [0, 0, 0])
+
+        super().__init__(**options)
+
+        if isinstance(self._color, str):
+            self._color = COLOR_PALETTE.get(self._color, [0, 0, 0])
+
+        self.code()
+
+    def code(self, start=None, end=None, lt: str = None):
+        if start is None or end is None:
+            start, end = self._start, self._end
+        if lt is None:
+            lt = self._line_style
+        distance = ((end[0] - start[0]) ** 2 + (end[1] - start[1]) ** 2) ** .5
+        if distance == 0:
+            return ""
+        if lt == "solid":
+            color = " ".join([str(i) for i in self._color])
+            start = " ".join([str(i) for i in start])
+            end = " ".join([str(i) for i in end])
+            code = f"{str(self._width)} w\n{color} RG\n{start} m\n{end} l\nS"
+        elif lt in ["dashed", "dotted", "densely_dotted", "densely_dashed", "loosely_dashed", "loosely_dotted"]:
+            dash_length = LINE_STYLE[lt][0]
+            blank_length = LINE_STYLE[lt][1]
+            step_x = (dash_length + blank_length) / distance * (end[0] - start[0])
+            step_y = (dash_length + blank_length) / distance * (end[1] - start[1])
+            add_x = dash_length / distance * (end[0] - start[0])
+            add_y = dash_length / distance * (end[1] - start[1])
+            code = "\n".join([self.code(
+                start=[start[0] + i * step_x, start[1] + i * step_y],
+                end=[start[0] + i * step_x + add_x, start[1] + i * step_y + add_y],
+                lt="solid") for i in range(int(distance // (dash_length + blank_length) + 1))]
+            )
+        elif lt == "dashdot":
+            step_x = sum(LINE_STYLE[lt]) / distance * (end[0] - start[0])
+            step_y = sum(LINE_STYLE[lt]) / distance * (end[1] - start[1])
+            add_x_1 = LINE_STYLE[lt][0] / distance * (end[0] - start[0])
+            add_y_1 = LINE_STYLE[lt][0] / distance * (end[1] - start[1])
+            add_x_2 = sum(LINE_STYLE[lt][:2]) / distance * (end[0] - start[0])
+            add_y_2 = sum(LINE_STYLE[lt][:2]) / distance * (end[1] - start[1])
+            add_x_3 = sum(LINE_STYLE[lt][:3]) / distance * (end[0] - start[0])
+            add_y_3 = sum(LINE_STYLE[lt][:3]) / distance * (end[1] - start[1])
+            code = "\n".join(
+                ["\n".join([
+                    self.code(
+                        start=[start[0] + i * step_x, start[1] + i * step_y],
+                        end=[start[0] + i * step_x + add_x_1, start[1] + i * step_y + add_y_1],
+                        lt="solid"),
+                    self.code(
+                        start=[start[0] + i * step_x + add_x_2, start[1] + i * step_y + add_y_2],
+                        end=[start[0] + i * step_x + add_x_3, start[1] + i * step_y + add_y_3],
+                        lt="solid"),
+                ]) for i in range(int(distance // sum(LINE_STYLE[lt]) + 1))]
+            )
+        else:
+            raise ValueError(f"line style must be one of f{', '.join(LINE_STYLE.keys())}, but got {lt} instead.")
+        return code
+
+
+class Rect(BaseContent):
+    def __init__(self, x, y, width, height, **options):
+        self._x = x
+        self._y = y
+        self._width = width
+        self._height = height
+        self._line_width = 1
+        self._color = COLOR_PALETTE.get('black', [0, 0, 0])
+        self._fill: bool = False
+        self._wind: bool = False
+        self._wind_color = COLOR_PALETTE.get('white', [1, 1, 1])
+        self._wind_style = "WIND_NON_ZERO"
+        self._wind_inside_rects: List[Tuple[Union[float, int], ...]] = ...
+
+        super().__init__(**options)
+
+        if isinstance(self._color, str):
+            self._color = COLOR_PALETTE.get(self._color, [0, 0, 0])
+
+        self.code()
+
+    def code(self):
+        wind = ""
+        if self._wind and len(self._wind_inside_rects) > 0:
+            rects = '\n'.join([' '.join([str(i) for i in rect]) + ' re' for rect in self._wind_inside_rects])
+            wind = f"\n{rects} f{'*' if self._wind_style == 'WIND_EVEN_ODD' else ''}"
+        self._code = f"{str(self._line_width)} w\n" \
+                     f"{' '.join([str(i) for i in self._color])} RG\n" \
+                     f"{(' '.join([str(i) for i in self._wind_color]) + ' rg') if self._wind else ''}\n" \
+                     f"{self._x} {self._y} {self._width} {self._height} re {wind}\nS"
+        return self._code
+
+
+class Scatter(BaseContent):
+    def __init__(self, x, y, **options):
+        """
+        Args:
+            x: center of the point
+            y: center of the point
+            **options: type: circle, rectangle
+        """
+        self._x = x
+        self._y = y
+        self._size = 5
+        self._line_width = 1
+        self._fill_color = COLOR_PALETTE.get('black', [0, 0, 0])
+        self._stroke_color = COLOR_PALETTE.get('black', [0, 0, 0])
+        self._scale_factor = 1
+        self._type = "circle"
+
+        super().__init__(**options)
+
+        if isinstance(self._fill_color, str):
+            self._fill_color = COLOR_PALETTE.get(self._fill_color, [0, 0, 0])
+        if isinstance(self._stroke_color, str):
+            self._stroke_color = COLOR_PALETTE.get(self._stroke_color, [0, 0, 0])
+
+        self.code()
+
+    def code(self):
+        stroke_color = f"{' '.join([str(i) for i in self._stroke_color])} RG"
+        fill_color = f"{' '.join([str(i) for i in self._fill_color])} rg"
+        line_width = f"{self._line_width} w"
+        line = ""
+        if self._type.lower()[:3] == "rec":
+            x = self._x - self._size
+            y = self._y - self._size * self._scale_factor
+            line = f"{x} {y} {self._size * 2} {self._size * self._scale_factor * 2} re b"
+        if self._type.lower()[:3] == "cir":
+            r = 0.55228 * self._size
+            s = (str(self._x - self._size), str(self._y))
+            k1 = (str(self._x - self._size), str(r + self._y))
+            k2 = (str(self._x - r), str(self._y + self._size))
+            e1 = (str(self._x), str(self._y + self._size))
+            k3 = (str(self._x + r), str(self._y + self._size))
+            k4 = (str(self._x + self._size), str(r + self._y))
+            e2 = (str(self._x + self._size), str(self._y))
+            k5 = (str(self._x + self._size), str(self._y - r))
+            k6 = (str(self._x + r), str(self._y - self._size))
+            e3 = (str(self._x), str(self._y - self._size))
+            k7 = (str(self._x - r), str(self._y - self._size))
+            k8 = (str(self._x - self._size), str(self._y - r))
+            e4 = s
+            # line = f"{self._x} {self._y} {self._size} 0 360 arc"
+            line = f"{' '.join(s)} m\n{' '.join(k1)} {' '.join(k2)} {' '.join(e1)} c\n" \
+                   f"{' '.join(k3)} {' '.join(k4)} {' '.join(e2)} c\n" \
+                   f"{' '.join(k5)} {' '.join(k6)} {' '.join(e3)} c\n" \
+                   f"{' '.join(k7)} {' '.join(k8)} {' '.join(e4)} c\nh B"
+        self._code = f"{line_width}\n{stroke_color}\n{fill_color}\n{line}"
+        return self._code
---
+++ packages/pdf-maker/pdf_maker-0.0.3-py3-none-any.whl/pdf_maker/core/main.py
@@ -0,0 +1,323 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+"""
+# ==========================================
+# Copyright 2024 Yang 
+# pdf-tool - main
+# ==========================================
+#
+#
+# 
+"""
+from .objs import Obj, Resources, Text, Line, Rect, Scatter
+from pdf_maker.canvas import PlotArea, Canvas
+from .crf import Crf
+from pdf_maker.constants._global import PAGE_SIZE, FONT_LIB
+from typing import List, Union
+
+
+COLOR_PALETTE: dict = {
+    "black": [0, 0, 0],
+    "red": [1, 0, 0],
+    "green": [0, 1, 0],
+    "blue": [0, 0, 1],
+}
+
+
+class NewPDF:
+    def __init__(self, filepath: str = "", **options):
+
+        self._objs: List[Obj] = []
+        self._pages = []
+        self._crf: Crf = ...
+        self._header = "%PDF-1.7\n"
+        self._body = ""
+
+        self.title = "NewPDF"
+        self.author = "Yang"
+        self.producer = "https://pypi.org/project/pdf-maker/"
+        self.creator = "PDF-Maker"
+        self.page_size = (595, 842)
+        self.filepath = filepath
+
+        self.content_bytes = b""
+        self.content_str = ""
+        self.component = []
+        self.frame = []
+        self.axis_area = [138, 400, 360, 270]  # x0, y0, w, h
+
+        for key, value in options.items():
+            setattr(self, key, value)
+
+        if isinstance(self.page_size, str):
+            self.page_size = PAGE_SIZE.get(self.page_size.lower(), (595, 842))
+
+        font = "Arial"
+        # default obj: catalog
+        self.add_obj(obj=Obj(type="Catalog", index="1", pages="2"))
+        # default obj: pages
+        self.add_obj(obj=Obj(type="Pages", index="2", kids=[]))
+        # default obj: font
+        self.add_obj(obj=Obj(type="Font", index="3", subtype="Type1", name="F1", basefont=font,
+                             encoding="4", font_descriptor="5", horizontal_scale=0.5))
+        # Encoding
+        self.add_obj(obj=Obj(type="Encoding", index="4", font_name=font))
+        # fontDescriptor
+        self.add_obj(obj=Obj(type="FontDescriptor", index="5", font_name=font))
+        # default obj: info
+        self.add_obj(obj=Obj(type="Info", index="6", title=self.title, author=self.author,
+                             producer=self.producer, creator=self.creator))
+
+    def del_obj(self, index: int):
+        found = False
+        for obj in self._objs:
+            if obj.index() == str(index):
+                self._objs.remove(obj)
+                found = True
+                break
+        if not found:
+            return
+        if obj.get_type() == "Page":
+            pages = self.get_obj(type="Pages")[0]
+            pages.remove_kid(obj.index())
+
+    def add_obj(self, index: Union[str, int] = None, type: str = None, obj: Obj = None, **options):
+        if index is None:
+            index = (lambda objs: max([int(obj.index()) for obj in objs]) if len(objs) > 0 else 0)(self._objs) + 1
+        if obj is None:
+            obj = Obj(index=str(index), type=type, **options)
+        if self.check_obj(obj, self._objs):
+            self._objs.append(obj)
+        if obj.get_type() == "Page":
+            pages = self.get_obj(index=obj._parent)
+            pages.kids(obj.index())
+        return obj
+
+    def add_page(self, contents_index: Union[int, str] = None, size: Union[tuple, list, str] = None,
+                 resources: Resources = None):
+        if size is None:
+            size = "a4"
+        if isinstance(size, str):
+            size = PAGE_SIZE.get(size, (595, 842))
+        if resources is None:
+            font = self.get_obj(type="Font")[0]
+            resources = Resources(font=font._name, font_index=font.index(), procset="[/PDF /Text]")
+        if contents_index is None:
+            stream = self.add_stream()
+        else:
+            stream = self.get_obj(index=str(contents_index))
+        pages_index = self.get_obj(type="Pages")[0].index()
+        return self.add_obj(type="Page", parent=f"{pages_index}", contents=f"{stream.index()}",
+                            mediabox=[0, 0, *size], resources=resources)
+
+    def add_stream(self):
+        stream = self.add_obj(type="Stream", text=[])  # stream must be created firstly
+        length = self.add_obj(type="StreamLength", prefix="")
+        stream._length = length.index()
+        return stream
+
+    def get_obj(self, type: str = None, index: Union[str, int] = None):
+        """
+        Args:
+            type: If type is given will return a list of objects with this type.
+                  Note that the type should be totally correct. Can be empty.
+            index: If index is given will return the first object matched.
+                   Will raise Index error if no object with such index.
+        Returns:
+
+        """
+        if type is not None:
+            return [obj for obj in self._objs if obj.get_type() == type]
+        if index is not None:
+            return [obj for obj in self._objs if obj.index() == str(index)][0]
+        raise ValueError("Object not found.")
+
+    def get_page(self, index: int, base: int = 1):
+        return self.get_obj(type="Page")[int(int(index) - int(base))]
+
+    def get_page_indexes(self):
+        return [obj.index() for obj in self.get_obj("Page")]
+
+    def check_obj(self, obj, objs):
+        if obj is None:
+            return False
+        indexes = [obj.index() for obj in objs]
+        types = [obj.type() for obj in objs]
+        if obj.index() in indexes:
+            raise ValueError(f"Index {obj.index()} already exists")
+        if obj.type() in types and obj.type() in ["Catalog", "Pages"]:
+            raise ValueError(f"Type {obj.type()} already exists")
+        return True
+
+    def save(self, filepath: str = ""):
+        """
+        Args:
+            filepath:
+
+        Returns:
+
+        """
+        if filepath == "":
+            filepath = self.filepath
+        with open(filepath, 'wb') as f:
+            f.write(self.get_byte())
+
+    def get_content(self):
+        """
+        Returns:
+
+        """
+        self.content_str = self.header() + self.body() + self.crf() + self.trailer()
+        return self.content_str
+
+    def get_byte(self):
+        self.content_bytes = self.get_content().encode('utf-8')
+        return self.content_bytes
+
+    def set_page_size(self, index: int, width: int, height: int):
+        """
+        Args:
+            index:
+            height:
+            width:
+
+        Returns:
+
+        """
+        page = [obj for obj in self._objs if obj.get_type() == "Page"][index]
+        page.page_size((width, height))
+
+    def text(self, page: int, x: int, y: int, text: str, size: int = 12, base: int = 1, **options):
+        page = self.get_page(index=page, base=base)
+        font_name = page.get_font_name()
+        font = self.get_obj(type="Font")[0].get_basefont()
+        contents_page = self.get_obj(index=page.get_contents_index())
+        text = Text(font_name=font_name, size=size, x=x, y=y, text=text, font=font, **options)
+        contents_page.text(text)
+        return text
+
+    def line(self, page: int, start: List[int], end: List[int], width: Union[float, int] = None,
+             color: Union[tuple, list, str] = None, **options):
+        if width is None:
+            width = 0.5
+        if color is None:
+            color = "black"
+        if isinstance(color, str):
+            color = COLOR_PALETTE.get(color.lower(), [0, 0, 0])
+        page = self.get_page(index=page)
+        contents_page = self.get_obj(index=page.get_contents_index())
+        line = Line(start=start, end=end, color=color, width=width, **options)
+        contents_page.line(line)
+        return line
+
+    def rect(self, page: int, left_bottom: Union[list, tuple], width: int, height: int,
+             line_width: Union[float, int] = None, color: Union[tuple, list, str] = None,
+             **options):
+        if line_width is None:
+            line_width = 0.5
+        if color is None:
+            color = "black"
+        if isinstance(color, str):
+            color = COLOR_PALETTE.get(color.lower(), [0, 0, 0])
+        page = self.get_page(index=page)
+        contents_page = self.get_obj(index=page.get_contents_index())
+        rect = Rect(*left_bottom, width=width, height=height, line_width=line_width, color=color, **options)
+        contents_page.rect(rect)
+        return rect
+
+    def scatter(self, page: int, x: int, y: int, size: int = 5, fill_color: Union[tuple, list, str] = None,
+                stroke_color: Union[tuple, list, str] = None, **options):
+        if stroke_color is None:
+            stroke_color = "black"
+        if fill_color is None:
+            fill_color = "grey"
+        page = self.get_page(index=page)
+        contents_page = self.get_obj(index=page.get_contents_index())
+        scatter = Scatter(x=x, y=y, size=size, fill_color=fill_color, stroke_color=stroke_color, **options)
+        contents_page.scatter(scatter)
+        return scatter
+
+    def header(self, header: str = None):
+        if header is not None:
+            self._header = header
+        return self._header
+
+    def objs(self):
+        return self._objs
+
+    def body(self):
+        body = ""
+        for obj in self._objs:
+            body = body + obj.data()
+            obj._offset = (self.header() + body).encode('utf-8').find(f"{obj.index()} 0 obj".encode('utf-8'))
+            obj._number = "00000"
+            obj._state = "n"
+            if obj.get_type() == "Stream" and obj.length() != "":
+                self.get_obj(index=obj._length)._prefix = len(obj._stream.encode('utf-8'))
+        self._body = body
+        return self._body
+
+    def crf(self):
+        self._crf = Crf(objs=[
+            {"offset": obj.offset(), "number": obj.number(), "state": obj.state()} for obj in self._objs])
+        data = self._crf.data()
+        self._crf._startoffset = (self._header + self._body + data).encode('utf-8').find(b"xref")
+        return data
+
+    def trailer(self):
+        def _get_obj(type):
+            try:
+                return f"{self.get_obj(type=type)[0].index()} 0 R"
+            except (ValueError, IndexError):
+                return ""
+        root = _get_obj(type="Catalog")
+        info = _get_obj(type="Info")
+        self._trailer = f"trailer\n<<\n" \
+                        f"/Size {self._crf.size()}\n" \
+                        f"/Root {root}\n" \
+                        f"/Info {info}\n" \
+                        f">>\nstartxref\n{self._crf.startoffset()}\n%%EOF\n"
+        return self._trailer
+
+    def move_page(self, from_index: int, to_index: int, base: int = 1):
+        pages = self.get_obj(type="Pages")[0]
+        page = pages._kids.pop(from_index - base)
+        pages._kids.insert(to_index - base, page)
+
+    def canvas(self, page: int, margin_left: Union[int, float], margin_top: Union[int, float], unit: str,
+               canvas: Canvas, base: int = 1):
+        """
+        Args:
+            unit:
+            margin_top:
+            margin_left:
+            page:
+            canvas:
+            base: from which indexing page number starts
+
+        Returns:
+
+        """
+        page = self.get_page(index=page, base=base)
+        left, top = canvas.unit_to_points(margin_left, margin_top, unit=unit)
+        canvas.left_bottom((left, page.page_size()[1] - top - canvas.height()))
+        font_name = page.get_font_name()
+        font = self.get_obj(type="Font")[0].get_basefont()
+        contents_page = self.get_obj(index=page.get_contents_index())
+        for comp in canvas.all_components():
+            # print(comp.name())
+            # print(type(comp))
+            if isinstance(comp, Text):
+                comp._font_name = font_name
+                comp._font = font
+                contents_page.text(comp)
+            if isinstance(comp, Rect):
+                contents_page.rect(comp)
+            if isinstance(comp, Scatter):
+                contents_page.scatter(comp)
+            if isinstance(comp, Line):
+                contents_page.line(comp)
+        pass
+
+
+
