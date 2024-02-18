a4616d9d21f21b7236ddf5862ff5a764912d5175
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 19:04:46 +0000

    Add package test-tele/0.0.4.8.7/test_tele-0.0.4.8.7.tar.gz

---
+++ packages/test-tele/test_tele-0.0.4.8.7.tar.gz/test_tele-0.0.4.8.7/test_tele/bot/bot_header.py
@@ -0,0 +1,48 @@
+
+from typing import TYPE_CHECKING
+from telethon.tl import types
+
+from test_tele.utils import start_sending
+
+if TYPE_CHECKING:
+    from test_tele.plugins import TgcfMessage
+
+
+## Helper function for get_message
+    
+async def get_entity(event, entity):
+    """Get chat entity from entity parameter"""
+    if entity.isdigit() or entity.startswith("-"):
+        chat = types.PeerChannel(int(entity))
+    else:
+        try:
+            chat = await event.client.get_entity(entity)
+        except Exception as e:
+            chat = await event.client.get_entity(types.PeerChat(int(entity)))
+
+    return chat
+
+
+async def loop_message(event, chat, ids: int, next=True):
+    """Loop channel posts to get message"""
+    skip = 20
+    tries = 0
+    while True:
+        if ids > 0 and tries <= skip:
+            message = await event.client.get_messages(chat, ids=ids)
+            tries += 1
+            if not message:
+                if next:
+                    ids += 1
+                    continue
+                else:
+                    ids -= 1
+                    continue
+            else:
+                if hasattr(message, 'message'):
+                    if message.media and not (message.sticker or message.voice or message.web_preview):
+                        return message
+                ids = ids + 1 if next else ids - 1
+        else:
+            return
+
