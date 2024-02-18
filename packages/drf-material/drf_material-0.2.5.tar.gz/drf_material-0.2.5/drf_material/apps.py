53dba8443997ffad54ede1470c0be9c5653dc6bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 13:04:47 +0000

    Add package drf-material/0.2.5/drf_material-0.2.5.tar.gz

---
+++ packages/drf-material/drf_material-0.2.5.tar.gz/drf_material-0.2.5/drf_material/apps.py
@@ -0,0 +1,10 @@
+""" AppConf for DRF Material """
+
+from django.apps import AppConfig
+
+
+class DRFMaterialConfig(AppConfig):
+    """App configuration for drf_material"""
+
+    name = "drf_material"
+    default_auto_field = "django.db.models.BigAutoField"
