9849c1c0e2487535af0bcb2228dc9a2175b4fa0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 07:05:32 +0000

    Add package echristie-test/0.0.5/echristie_test-0.0.5-py3-none-any.whl

---
+++ packages/echristie-test/echristie_test-0.0.5-py3-none-any.whl/print_numbers/__init__.py
@@ -0,0 +1,1 @@
+from print_number import MyPackage
---
+++ packages/echristie-test/echristie_test-0.0.5-py3-none-any.whl/print_numbers/print_number.py
@@ -0,0 +1,5 @@
+class MyPackage:
+    """ Print numbers in a rnage Bye hi"""
+    def print_numbers(self, range_number):
+        for i in range(1, range_number):
+            print(i)
