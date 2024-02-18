898958f8f7893816e39145c0ade0df227b05dfe3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 01:28:08 +0000

    Add package loicmouquet-picsou/0.1.0/loicmouquet_picsou-0.1.0.tar.gz

---
+++ packages/loicmouquet-picsou/loicmouquet_picsou-0.1.0.tar.gz/loicmouquet_picsou-0.1.0/loicmouquet_picsou/picsou.py
@@ -0,0 +1,9 @@
+account = 10000
+
+def delete_money_from_bank_account(a:int, b:int) -> int:
+    global account
+    if a + b > 200:
+        account -= (a+b+100)
+        return account
+    account -= (a + b)
+    return account
