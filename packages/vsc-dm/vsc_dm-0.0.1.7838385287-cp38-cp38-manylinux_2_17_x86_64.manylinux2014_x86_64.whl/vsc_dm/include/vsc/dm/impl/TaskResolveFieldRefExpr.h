f68c981e93a2639c88a7ca4dabc38718f666382f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 18:04:45 +0000

    Add package vsc-dm/0.0.1.7838385287/vsc_dm-0.0.1.7838385287-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/vsc-dm/vsc_dm-0.0.1.7838385287-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/vsc_dm/include/vsc/dm/impl/TaskResolveFieldRefExpr.h
@@ -0,0 +1,60 @@
+/*
+ * TaskResolveFieldRefExpr.h
+ *
+ *  Created on: Jun 19, 2022
+ *      Author: mballance
+ */
+
+#pragma once
+#include "vsc/dm/impl/VisitorBase.h"
+#include "vsc/dm/IModelBuildContext.h"
+#include "vsc/dm/IModelField.h"
+#include "vsc/dm/ITypeExprFieldRef.h"
+
+namespace vsc {
+namespace dm {
+
+class TaskResolveFieldRefExpr : public VisitorBase {
+public:
+
+	TaskResolveFieldRefExpr(IModelBuildContext *ctxt, IVisitor *this_p=0) :
+		VisitorBase(this_p), m_ctxt(ctxt), m_field(0) { }
+
+	virtual ~TaskResolveFieldRefExpr() { }
+
+	IModelField *resolve(ITypeExpr *target) {
+		m_field = 0;
+
+		target->accept(m_this);
+
+		return m_field;
+	}
+
+	template <class T> T *resolveT(ITypeExpr *target) {
+		return dynamic_cast<T *>(resolve(target));
+	}
+
+	virtual void visitTypeExprFieldRef(ITypeExprFieldRef *e) override {
+        switch (e->getRootRefKind()) {
+            case ITypeExprFieldRef::RootRefKind::TopDownScope:
+				m_field = m_ctxt->getTopDownScope();
+                break;
+            case ITypeExprFieldRef::RootRefKind::BottomUpScope:
+				m_field = m_ctxt->getScope();
+                break;
+        }
+		for (std::vector<int32_t>::const_iterator
+			it=e->getPath().begin(); 
+            it!=e->getPath().end(); it++) {
+			m_field = m_field->getField(*it);
+		}
+	}
+
+protected:
+	IModelBuildContext					*m_ctxt;
+	IModelField							*m_field;
+
+};
+
+}
+}
