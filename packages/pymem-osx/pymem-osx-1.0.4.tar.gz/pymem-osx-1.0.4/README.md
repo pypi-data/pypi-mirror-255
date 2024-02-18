4ce31feecd138b652ab8401018fc1d99b5bbd1eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 21:04:54 +0000

    Add package pymem-osx/1.0.4/pymem-osx-1.0.4.tar.gz

---
+++ packages/pymem-osx/pymem-osx-1.0.4.tar.gz/pymem-osx-1.0.4/README.md
@@ -0,0 +1,28 @@
+# pymem-osx
+A python library for MacOS, providing the base functions required for memory manipulation (read/write).
+
+# Features
+- Memory read/write helpers.
+- Base address searcher
+- Modules name and address dumper
+- Live in-memory modules macho-o address parser/dumper (64-bit implementation).
+  - Useful for finding address of different sections (__DATA, __TEXT) and even their respective segments.
+
+# Installation
+You can install pymem from source.
+```
+git clone https://github.com/qtkite/pymem-osx.git
+cd pymem-osx
+python3 -m pip install -e .
+```
+
+Alternatively you can do:
+```
+pip install pymem-osx
+```
+# Note
+When running your python script, it must be elevated (in sudo mode) for the api calls to work.
+
+# Donate
+Buy me a coffee to support some of the work I put out  
+[![ko-fi](https://ko-fi.com/img/githubbutton_sm.svg)](https://ko-fi.com/B0B65F2OS)
---
+++ packages/pymem-osx/pymem-osx-1.0.4.tar.gz/pymem-osx-1.0.4/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+from pathlib import Path
+from setuptools import setup
+
+directory = Path(__file__).resolve().parent
+
+with open(directory / 'README.md', encoding='utf-8') as f:
+  long_description = f.read()
+
+setup(name="pymem-osx",
+      version = "1.0.4",
+      description="A MacOS python library for reading and writing memory of other processes",
+      author="qtkite",
+      license="MIT",
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      packages=["pymem", "pymem.resources"],
+      python_requires='>=3.8',
+      classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: MacOS",
+        "Programming Language :: Python :: 3",
+      ],
+      url="https://github.com/qtkite/pymem-osx",
+      project_urls={
+        "Source Code": "https://github.com/qtkite/pymem-osx"
+      },
+      include_package_data=True)
