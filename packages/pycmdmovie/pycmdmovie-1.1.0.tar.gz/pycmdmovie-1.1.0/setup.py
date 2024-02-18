156abc88bd00456a995e098473686bf2e33a6a16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 07:04:28 +0000

    Add package pycmdmovie/1.1.0/pycmdmovie-1.1.0.tar.gz

---
+++ packages/pycmdmovie/pycmdmovie-1.1.0.tar.gz/pycmdmovie-1.1.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as f:
+    description = f.read()
+
+
+setup(
+    name='pycmdmovie',
+    version='1.1.0',
+    packages=find_packages(),
+    install_requires=[
+        'pillow',
+        'opencv-python',
+        'colorama'
+    ],
+    long_description=description,
+    long_description_content_type='text/markdown'
+)
