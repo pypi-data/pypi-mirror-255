0c39e47b9ff5353cd60badcdbb726486f881939e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 22:06:03 +0000

    Add package adsmiff/0.0.1/adsmiff-0.0.1.tar.gz

---
+++ packages/adsmiff/adsmiff-0.0.1.tar.gz/adsmiff-0.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='adsmiff',
+    version='0.0.1',
+    packages=find_packages(),
+    author='Jai Madera',
+    author_email='onesthathumbled@gmail.com',
+    description='adsmiff is a Python library tailored for business mathematics.',
+    install_requires = [
+        # Add dependencies here.
+        # e.g. 'numpy>=1.11.1'
+    ],
+)
