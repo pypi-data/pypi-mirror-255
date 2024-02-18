f649ee61fcac87a05bdbb22183b2148970c51583
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 15:05:00 +0000

    Add package tutorialtestpackage3/0.1.0/tutorialtestpackage3-0.1.0.tar.gz

---
+++ packages/tutorialtestpackage3/tutorialtestpackage3-0.1.0.tar.gz/tutorialtestpackage3-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tutorialtestpackage3',  # Replace with your actual package name
+    url = 'https://gitlab.com/AnoopANair/pypitest.git', # Replace with your github project link
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        # Your project dependencies
+    ],
+)
