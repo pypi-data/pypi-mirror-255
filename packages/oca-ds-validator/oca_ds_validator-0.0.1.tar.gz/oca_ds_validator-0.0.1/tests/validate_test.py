ce8755d6c0806d8bb7a774f8ad545243f0c299a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 15:05:15 +0000

    Add package oca-ds-validator/0.0.1/oca_ds_validator-0.0.1.tar.gz

---
+++ packages/oca-ds-validator/oca_ds_validator-0.0.1.tar.gz/oca_ds_validator-0.0.1/tests/validate_test.py
@@ -0,0 +1,21 @@
+from oca_ds_validator import OCABundle, OCADataSet
+
+test_bd = OCABundle("test_bundle.json")
+valid_ds = OCADataSet.from_path("test_data_entry_example.xlsx")
+
+valid_rslt = test_bd.validate(valid_ds)
+valid_rslt.overview()
+# Found 5 problematic row(s) in the following attribute(s): {'Breed'}
+
+# valid_rslt.first_err_col()
+# Format error(s) would occur in the following row(s):
+# row 0 :  Entry code format mismatch (manually fix the attribute format). Supported format for entry code is: [A-Z]{15}.
+# row 1 :  Entry code format mismatch (manually fix the attribute format). Supported format for entry code is: [A-Z]{15}.
+# row 2 :  Entry code format mismatch (manually fix the attribute format). Supported format for entry code is: [A-Z]{15}.
+# row 3 :  Entry code format mismatch (manually fix the attribute format). Supported format for entry code is: [A-Z]{15}.
+# row 4 :  Entry code format mismatch (manually fix the attribute format). Supported format for entry code is: [A-Z]{15}.
+
+# print(valid_rslt.get_attr_err())
+# print(valid_rslt.get_format_err())
+# print(valid_rslt.get_ecode_err())
+# print(valid_rslt.get_char_encode_err())
