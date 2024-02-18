05e0e644316ce51012bd782b0d8d5055caff6ade
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 14:05:42 +0000

    Add package youwol/0.1.7/youwol-0.1.7-py3-none-any.whl

---
+++ packages/youwol/youwol-0.1.7-py3-none-any.whl/youwol/app/environment/models/__init__.py
@@ -0,0 +1,10 @@
+"""
+The models used in YouWolEnvironment and its Configuration.
+"""
+
+# relative
+from .defaults import *
+from .flow_switches import *
+from .models import *
+from .models_config import *
+from .predefined_configs import *
