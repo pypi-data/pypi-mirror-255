1c8986d383a16196cc5887ffbfd7a146164c9f95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 15:05:47 +0000

    Add package pplog/0.1.32/pplog-0.1.32-py3-none-any.whl

---
+++ packages/pplog/pplog-0.1.32-py3-none-any.whl/pplog/errors/__init__.py
@@ -0,0 +1,27 @@
+"""Exceptions thrown by pplog."""
+
+
+class FailedToGetClusterInfoException(Exception):
+    """Could not get ClusterInfo for DBU Estimation."""
+
+
+class FailedToEstimateDBU(Exception):
+    """Failed to estimate DBU"""
+
+
+class DatabricksEnvironmentNotAvailable(Exception):
+    """Tried to access Databricks resources inside an environment
+    that is not Databricks."""
+
+
+class GreatExpectationsNotAvailable(Exception):
+    """Tried to access GreatExpectations resources inside an environment
+    that does not have the library installed."""
+
+
+class ShouldAbortException(Exception):
+    """A check returned 'Failed' and was configured to abort the pipeline."""
+
+
+class ClusterEventsNotSupported(Exception):
+    """An error raised when Databricks Runtime does not support fetching ClusterEvents."""
