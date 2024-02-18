b89408ea7aefd43792a471a9facb22bec32c5ebb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 08:04:30 +0000

    Add package tomoscan/2.0.0/tomoscan-2.0.0-py3-none-any.whl

---
+++ packages/tomoscan/tomoscan-2.0.0-py3-none-any.whl/tomoscan/scanfactory.py
@@ -0,0 +1,13 @@
+"""deprecated module. Use 'factory' instead"""
+
+from tomoscan.utils.io import deprecated_warning
+
+
+deprecated_warning(
+    "Class",
+    name="tomoscan.scanfactory.ScanFactory",
+    reason="Has been moved",
+    replacement="tomoscan.factory.TomoObjectFactory",
+    only_once=True,
+)
+from tomoscan.factory import Factory as ScanFactory  # noqa F401
