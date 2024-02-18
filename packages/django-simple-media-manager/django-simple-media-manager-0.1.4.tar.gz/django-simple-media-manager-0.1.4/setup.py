3582dda35bb00c2635308dec729762ede93b1166
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 08:07:37 +0000

    Add package django-simple-media-manager/0.1.4/django-simple-media-manager-0.1.4.tar.gz

---
+++ packages/django-simple-media-manager/django-simple-media-manager-0.1.4.tar.gz/django-simple-media-manager-0.1.4/setup.py
@@ -0,0 +1,57 @@
+import codecs
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.1.4'
+DESCRIPTION = 'Simple django rest media manager'
+LONG_DESCRIPTION = ('This library is created based on CQRS and domain driven design in order to understand the underlying structure of this '
+                    'architecture')
+with open(file='django_simple_media_manager/requirements.txt') as file:
+    REQUIREMENTS = [requirement.rsplit() for requirement in file]
+
+# Setting up
+setup(
+    name="django-simple-media-manager",
+    version=VERSION,
+    author="Ali Seylaneh",
+    author_email="aliseylaneh@gmail.com",
+    url="https://hamgit.ir/aliseylaneh/media-manager",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=["Django==4.0.7",
+                      "django-environ==0.9.0",
+                      "psycopg2-binary==2.9.5",
+                      "djangorestframework==3.13.1",
+                      "whitenoise==6.2.0",
+                      "django-filter==22.1",
+                      "django-extensions==3.2.1",
+                      "django-cors-headers==3.13.0",
+                      "django-storages==1.13.1",
+                      "drf-jwt==1.19.2",
+                      "boto3==1.24.71",
+                      "attrs==22.1.0",
+                      "djangorestframework-simplejwt==5.2.2",
+                      "drf-spectacular==0.24.2",
+                      "django-redis==5.2.0",
+                      "django-categories~=1.9.2",
+                      "djangorestframework-camel-case==1.4.2",
+                      "setuptools==69.0.3"],
+    keywords=['python', 'video', 'media', 'django', 'domain-driven-design', 'CQRS'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "Framework :: Django",
+
+    ],
+
+)
