8630ede9f9b8fb785f0039c2804378e1c8c4a679
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 01:28:56 +0000

    Add package symusic/0.3.5/symusic-0.3.5-cp312-cp312-win32.whl

---
+++ packages/symusic/symusic-0.3.5-cp312-cp312-win32.whl/symusic/__init__.py
@@ -0,0 +1,29 @@
+from .factory import (
+    ControlChange,
+    KeySignature,
+    Note,
+    Pedal,
+    PitchBend,
+    Score,
+    Tempo,
+    TextMeta,
+    TimeSignature,
+    TimeUnit,
+    Track,
+)
+
+__version__ = "0.3.5"
+
+__all__ = [
+    "TimeUnit",
+    "Note",
+    "KeySignature",
+    "TimeSignature",
+    "ControlChange",
+    "Tempo",
+    "Pedal",
+    "PitchBend",
+    "TextMeta",
+    "Track",
+    "Score",
+]
