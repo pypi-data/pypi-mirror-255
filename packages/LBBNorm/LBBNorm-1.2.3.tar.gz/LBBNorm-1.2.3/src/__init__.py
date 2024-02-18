63adfdd4dd69b888d9eca40357ad7c0b31a9f529
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 08:08:27 +0000

    Add package LBBNorm/1.2.3/LBBNorm-1.2.3.tar.gz

---
+++ packages/LBBNorm/LBBNorm-1.2.3.tar.gz/LBBNorm-1.2.3/src/__init__.py
@@ -0,0 +1,8 @@
+from .normalization.macenko import Macenko
+from .normalization.vahadane import Vahadane
+from .normalization.adaptive_color_deconvolution import AdaptiveColorDeconvolution
+from .normalization.reinhard import Reinhard
+from .normalization.modified_reinhard import ModifiedReinhard
+
+
+from .utility.slide_patcher import WholeSlidePatcher
