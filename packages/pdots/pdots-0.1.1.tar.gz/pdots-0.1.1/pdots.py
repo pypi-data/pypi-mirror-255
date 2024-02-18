1b8f2471566ec68ebc120632b243f065ca82bde7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 06:04:24 +0000

    Add package pdots/0.1.1/pdots-0.1.1.tar.gz

---
+++ packages/pdots/pdots-0.1.1.tar.gz/pdots-0.1.1/pdots.py
@@ -0,0 +1,28 @@
+import sys
+from time import sleep
+from contextlib import contextmanager
+
+@contextmanager
+def dots():
+    def _dots():
+        while True:
+            for i, char in enumerate("⠁⠃⠇⡇⣇⣧⣷⣿"):
+                if i:
+                    yield f"\x08{char}"
+                else:
+                    yield char
+    d = _dots()
+    def step():
+        print(next(d), end="", flush=True, file=sys.stderr)
+    try:
+        print("\x1b[?25l", end="", flush=True, file=sys.stderr)
+        yield step
+    finally:
+        print("\x1b[?25h", end="", flush=True, file=sys.stderr)
+
+
+if __name__ == "__main__":
+    with dots() as dot:
+        for _ in range(48):
+            dot()
+            sleep(0.05)
