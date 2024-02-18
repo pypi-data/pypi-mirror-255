d8ed5eb99cb995c2f87424021c6439771596a579
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 22:04:36 +0000

    Add package AziizAM/0.0.4/AziizAM-0.0.4.tar.gz

---
+++ packages/AziizAM/AziizAM-0.0.4.tar.gz/AziizAM-0.0.4/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+setuptools.setup(
+    name='AziizAM',
+    version='0.0.4',
+    author="A.Alminqah",
+    author_email="A.Alminqah@hotmail.com",
+    description="Fast way to access my custom python function.",
+    url="https://github.com/AziizMq/AziizAM-Pypi",
+    project_urls={
+        'Tracker': 'https://github.com/AziizMq/AziizAM-Pypi/issues',
+    },
+    packages=setuptools.find_packages(),
+    classifiers=[
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+    ],
+)
