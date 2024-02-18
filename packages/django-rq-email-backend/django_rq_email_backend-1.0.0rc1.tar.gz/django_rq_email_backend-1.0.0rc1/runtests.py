6119789c231a65e79dadcc3c06d070361634d5db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 15:04:55 +0000

    Add package django-rq-email-backend/1.0.0rc1/django_rq_email_backend-1.0.0rc1.tar.gz

---
+++ packages/django-rq-email-backend/django_rq_email_backend-1.0.0rc1.tar.gz/django_rq_email_backend-1.0.0rc1/runtests.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import os
+import sys
+
+import django
+from django.test.runner import DiscoverRunner
+
+
+def runtests():
+    os.environ["DJANGO_SETTINGS_MODULE"] = "tests.settings"
+    django.setup()
+    test_runner = DiscoverRunner(verbosity=3)
+    failures = test_runner.run_tests([])
+    sys.exit(failures)
+
+
+if __name__ == "__main__":
+    runtests()
