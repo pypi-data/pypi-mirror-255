b6ec5437eade7d208b79f93382c61e4179c6f9e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 20:05:12 +0000

    Add package upkie/3.2.0/upkie-3.2.0.tar.gz

---
+++ packages/upkie/upkie-3.2.0.tar.gz/upkie-3.2.0/tools/workspace/vulp/repository.bzl
@@ -0,0 +1,17 @@
+# -*- python -*-
+#
+# Copyright 2022 Stéphane Caron
+# Copyright 2024 Inria
+
+load("@bazel_tools//tools/build_defs/repo:git.bzl", "git_repository")
+
+def vulp_repository():
+    """
+    Clone repository from GitHub and make its targets available for binding.
+    """
+    git_repository(
+        name = "vulp",
+        remote = "https://github.com/upkie/vulp.git",
+        commit = "b179d224da0468a93b6a9a051f6137b70f86498c",
+        shallow_since = "1705597050 +0100",
+    )
---
+++ packages/upkie/upkie-3.2.0.tar.gz/upkie-3.2.0/upkie/envs/tests/upkie_ground_velocity_test.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# SPDX-License-Identifier: Apache-2.0
+# Copyright 2023 Inria
+
+"""Test UpkieGroundVelocity."""
+
+import unittest
+
+import numpy as np
+import posix_ipc
+
+from upkie.envs import UpkieGroundVelocity
+from upkie.envs.tests.mock_spine import MockSpine
+
+
+class TestUpkieGroundVelocity(unittest.TestCase):
+    def setUp(self):
+        shm_name = "/vroum"
+        shared_memory = posix_ipc.SharedMemory(
+            shm_name, posix_ipc.O_RDWR | posix_ipc.O_CREAT, size=42
+        )
+        self.env = UpkieGroundVelocity(
+            fall_pitch=1.0,
+            frequency=100.0,
+            max_ground_velocity=1.0,
+            shm_name=shm_name,
+        )
+        shared_memory.close_fd()
+        self.env._spine = MockSpine()
+
+    def test_reset(self):
+        observation, info = self.env.reset()
+        spine_observation = info["spine_observation"]
+        self.assertAlmostEqual(
+            observation[1], spine_observation["wheel_odometry"]["position"]
+        )
+        self.assertGreaterEqual(spine_observation["number"], 1)
+
+    def test_reward(self):
+        observation, info = self.env.reset()
+        action = np.zeros(self.env.action_space.shape)
+        observation, reward, terminated, truncated, _ = self.env.step(action)
+        self.assertAlmostEqual(reward, 1.0)  # survival reward
+
+    def test_check_env(self):
+        try:
+            from stable_baselines3.common.env_checker import check_env
+
+            check_env(self.env)
+        except ImportError:
+            pass
+
+
+if __name__ == "__main__":
+    unittest.main()
