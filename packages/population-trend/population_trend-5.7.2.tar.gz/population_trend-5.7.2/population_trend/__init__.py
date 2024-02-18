aa66fea91ea2e63c1c71482e4c333f8f1ba2ef7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 17:03:57 +0000

    Add package population-trend/5.7.2/population_trend-5.7.2.tar.gz

---
+++ packages/population-trend/population_trend-5.7.2.tar.gz/population_trend-5.7.2/population_trend/__init__.py
@@ -0,0 +1,10 @@
+"""A population growth model package"""
+
+__version__ = "5.7.2"
+from .cli import *  # noqa
+from .filter_data import *  # noqa
+from .population_growth_model import *  # noqa
+from .plotter_growth_rate import *  # noqa
+from .calculate_growth_rates import *  # noqa
+from .regional_lambdas import *  # noqa
+from bootstrapping_tools import *  # noqa
