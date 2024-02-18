db1a7afc71beef34d09c6c2133ca47c476b89942
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 23:07:49 +0000

    Add package NetStrcuter/0.4/NetStrcuter-0.4.tar.gz

---
+++ packages/NetStrcuter/NetStrcuter-0.4.tar.gz/NetStrcuter-0.4/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+	name = 'NetStrcuter',
+	version = '0.4',
+	author='Haytam-Zakaria',
+	description='Simple library from low level socket api',
+	packages=setuptools.find_packages(),
+	classifiers=[
+	'Programming Language :: Python :: 3',
+	'Operating System :: OS Independent',
+	'License :: OSI Approved :: MIT License'
+	]
+)
