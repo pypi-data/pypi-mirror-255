3ee438780d18f6260682bd9f74963f17a7843656
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 22:05:09 +0000

    Add package polars-ds/0.3.1/polars_ds-0.3.1-cp312-none-win32.whl

---
+++ packages/polars-ds/polars_ds-0.3.1-cp312-none-win32.whl/polars_ds/type_alias.py
@@ -0,0 +1,13 @@
+from __future__ import annotations
+from typing import Literal
+import sys
+
+if sys.version_info >= (3, 10):
+    from typing import TypeAlias
+else:  # 3.9, 3.8
+    from typing_extensions import TypeAlias
+
+
+DetrendMethod: TypeAlias = Literal["linear", "mean"]
+Alternative: TypeAlias = Literal["two-sided", "less", "greater"]
+Distance = Literal["l1", "l2", "inf", "h", "haversine"]
