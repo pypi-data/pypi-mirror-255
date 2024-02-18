9471810a4762a36782a166c7a722d4a8f5cc8d88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 16:08:33 +0000

    Add package JanexUltimate/0.1.7/JanexUltimate-0.1.7.tar.gz

---
+++ packages/JanexUltimate/JanexUltimate-0.1.7.tar.gz/JanexUltimate-0.1.7/JanexUltimate/main.py
@@ -0,0 +1,7 @@
+import json
+import os
+
+from JanexUltimate.janexnlg import *
+from JanexUltimate.janexpytorch import *
+from JanexUltimate.janexpython import *
+from JanexUltimate.janexspacy import *
---
+++ packages/JanexUltimate/JanexUltimate-0.1.7.tar.gz/JanexUltimate-0.1.7/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="JanexUltimate",
+    version="0.1.7",
+    author="ChronoByte404",
+    author_email="cipher58public@gmail.com",
+    description="A description of your package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ChronoByte404/Janex-Ultimate",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "CipherProgram",  # Specify the minimum version if known
+        "torch",
+        "nltk",
+        "spacy",
+    ],
+    license="Lily 3.0",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
