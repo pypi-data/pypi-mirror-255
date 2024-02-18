0d5e153cd6ba36e866f28fb8dbbf02a18260f7f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 08:05:28 +0000

    Add package func-live/1.2.18/func_live-1.2.18-py3-none-any.whl

---
+++ packages/func-live/func_live-1.2.18-py3-none-any.whl/func_live/upload_file.py
@@ -0,0 +1,12 @@
+from aiohttp import FormData
+from pathlib import Path
+from .config import API_URL
+
+async def upload_file(input_path, session):
+    filename = Path(input_path).name
+    data = FormData()
+    data.add_field('file', open(input_path, 'rb'), filename=filename)
+
+    async with session.post(f"{API_URL}/upload-file", data=data) as response:
+        response_data = await response.json()
+        return response_data['url']
