f5f3051c831fce12a6dcac1fcea9dec3e755673f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 06:04:35 +0000

    Add package castor-extractor/0.14.6/castor_extractor-0.14.6-py3-none-any.whl

---
+++ packages/castor-extractor/castor_extractor-0.14.6-py3-none-any.whl/castor_extractor/visualization/salesforce_reporting/assets.py
@@ -0,0 +1,9 @@
+from ...types import ExternalAsset
+
+
+class SalesforceReportingAsset(ExternalAsset):
+    """Salesforce Reporting assets"""
+
+    DASHBOARDS = "dashboards"
+    REPORTS = "reports"
+    USERS = "users"
