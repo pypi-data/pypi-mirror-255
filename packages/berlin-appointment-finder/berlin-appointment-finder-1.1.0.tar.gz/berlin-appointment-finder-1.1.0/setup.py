fb527cba65603dea3bbce1363d990b59881d8636
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 19:04:09 +0000

    Add package berlin-appointment-finder/1.1.0/berlin-appointment-finder-1.1.0.tar.gz

---
+++ packages/berlin-appointment-finder/berlin-appointment-finder-1.1.0.tar.gz/berlin-appointment-finder-1.1.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+long_description = (Path(__file__).parent / "README.md").read_text()
+
+
+setup(
+    name='berlin-appointment-finder',
+    version='1.1.0',
+    description='Finds appointments at the Berlin Bürgeramt, broadcasts them via websockets',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/nicbou/burgeramt-appointments-websockets',
+    author='Nicolas Bouliane',
+    author_email='contact@nicolasbouliane.com',
+    license='MIT',
+    packages=find_packages(),
+    scripts=['bin/appointments'],
+    python_requires='>=3.10',
+    install_requires=[
+        'aiohttp==3.8.4',
+        'beautifulsoup4==4.10.0',
+        'chime==0.7.0',
+        'pytz',
+        'websockets==10.1',
+    ],
+    zip_safe=False,
+)
