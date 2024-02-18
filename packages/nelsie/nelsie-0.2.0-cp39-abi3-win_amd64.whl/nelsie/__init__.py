21921aed0b9b6e24df52ab3c3110051c2a115f5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 22:05:43 +0000

    Add package nelsie/0.2.0/nelsie-0.2.0-cp39-abi3-win_amd64.whl

---
+++ packages/nelsie/nelsie-0.2.0-cp39-abi3-win_amd64.whl/nelsie/__init__.py
@@ -0,0 +1,23 @@
+from .basictypes import Align, FlexWrap, Stroke, TextAlign
+from .insteps import InSteps
+from .shapes import Arrow, Path
+from .slidedeck import Resources, SlideDeck, Slide
+from .textstyle import FontStretch, TextStyle
+from .box import Box, BoxBuilder
+
+__all__ = [
+    "SlideDeck",
+    "InSteps",
+    "Path",
+    "Stroke",
+    "TextStyle",
+    "Resources",
+    "FontStretch",
+    "TextAlign",
+    "FlexWrap",
+    "Align",
+    "Arrow",
+    "BoxBuilder",
+    "Box",
+    "Slide",
+]
---
+++ packages/nelsie/nelsie-0.2.0-cp39-abi3-win_amd64.whl/nelsie/basictypes.py
@@ -0,0 +1,58 @@
+import enum
+import typing
+from dataclasses import dataclass
+from typing import Literal
+
+from .layoutexpr import LayoutExpr
+
+Length = int | float | str
+LengthAuto = Length | Literal["auto"]
+Size = Length | None
+Position = Length | None | LayoutExpr
+
+
+@dataclass
+class Stroke:
+    color: str
+    width: float = 1.0
+    dash_array: list[float] | None = None
+    dash_offset: float = 0.0
+
+
+@enum.unique
+class TextAlign(enum.IntEnum):
+    Start = 0
+    Center = 1
+    End = 2
+
+
+@enum.unique
+class Align(enum.IntEnum):
+    Start = 0
+    End = 1
+    FlexStart = 2
+    FlexEnd = 3
+    Center = 4
+    Stretch = 5
+
+    Baseline = 10  # Allowed in align_items, align_self, justify_self
+
+    # Allowed in justify_content, align_content
+    SpaceBetween = 20
+    SpaceEvenly = 21
+    SpaceAround = 22
+
+
+@enum.unique
+class FlexWrap(enum.IntEnum):
+    NoWrap = 0
+    Wrap = 1
+    WrapReverse = 2
+
+
+def parse_debug_layout(value: bool | str) -> str | None:
+    if value is True:
+        return "#ff00ff"
+    elif not value:
+        return None
+    return typing.cast(str, value)
