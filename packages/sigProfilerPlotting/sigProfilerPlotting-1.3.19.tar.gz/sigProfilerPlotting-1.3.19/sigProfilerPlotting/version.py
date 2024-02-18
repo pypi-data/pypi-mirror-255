7597857d3a294441cf6f7529027b5846c19515b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 23:05:08 +0000

    Add package sigProfilerPlotting/1.3.19/sigProfilerPlotting-1.3.19.tar.gz

---
+++ packages/sigProfilerPlotting/sigProfilerPlotting-1.3.19.tar.gz/sigProfilerPlotting-1.3.19/sigProfilerPlotting/version.py
@@ -0,0 +1,7 @@
+
+# THIS FILE IS GENERATED FROM SIGPROFILERPLOTTING SETUP.PY
+short_version = '1.3.19'
+version = '1.3.19'
+update = 'Upgrade v1.3.19: Fix plotSV input handling and refactor tests.'
+    
+    
