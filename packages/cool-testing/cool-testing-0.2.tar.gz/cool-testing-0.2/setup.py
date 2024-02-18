d227c3c087d28b68ebd794084b8f194a75e4cd1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 11:05:04 +0000

    Add package cool-testing/0.2/cool-testing-0.2.tar.gz

---
+++ packages/cool-testing/cool-testing-0.2.tar.gz/cool-testing-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="cool-testing",
+    version="0.2",
+    packages=find_packages(),
+        long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+)
