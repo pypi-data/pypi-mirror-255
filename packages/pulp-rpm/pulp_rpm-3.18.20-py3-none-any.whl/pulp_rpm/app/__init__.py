39b1d1078b79d9be213110a01adacee18c140900
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 22:04:43 +0000

    Add package pulp-rpm/3.18.20/pulp_rpm-3.18.20-py3-none-any.whl

---
+++ packages/pulp-rpm/pulp_rpm-3.18.20-py3-none-any.whl/pulp_rpm/app/__init__.py
@@ -0,0 +1,12 @@
+from pulpcore.plugin import PulpPluginAppConfig
+
+
+class PulpRpmPluginAppConfig(PulpPluginAppConfig):
+    """
+    Entry point for pulp_rpm plugin.
+    """
+
+    name = "pulp_rpm.app"
+    label = "rpm"
+    version = "3.18.20"
+    python_package_name = "pulp-rpm"
