c8938a27519b5a01e2f2618fe68f3b7afafa534c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 05:05:30 +0000

    Add package django42-datatable/0.3.2/django42-datatable-0.3.2.tar.gz

---
+++ packages/django42-datatable/django42-datatable-0.3.2.tar.gz/django42-datatable-0.3.2/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+from setuptools import setup, find_packages
+
+
+setup(
+    name='django42-datatable',
+    version='0.3.2',
+    author='shymonk',
+    author_email='hellojohn201@gmail.com',
+    url='https://github.com/kamilanindita/django42-datatable',
+    description='A simple Django app to origanize data in tabular form.',
+    long_description=open('README.rst').read(),
+    packages=find_packages(exclude=['test*', 'example*']),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=["django>=4.2"],
+    license='MIT License',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Libraries',
+    ],
+)
