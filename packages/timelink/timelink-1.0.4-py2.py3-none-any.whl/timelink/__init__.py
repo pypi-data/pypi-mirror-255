7eed80a93d880bb1491495e98fe3d9456fc37c14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 00:06:41 +0000

    Add package timelink/1.0.4/timelink-1.0.4-py2.py3-none-any.whl

---
+++ packages/timelink/timelink-1.0.4-py2.py3-none-any.whl/timelink/__init__.py
@@ -0,0 +1,14 @@
+"""Timelink Python package.
+
+Timelink, formerly known as MHK (Micro History with Kleio) is an information system
+designed for processing person related information collected from historical sources.
+
+.. moduleauthor:: Joaquim Ramos de Carvalho
+
+
+
+"""
+
+__author__ = """Joaquim Ramos de Carvalho"""
+__email__ = 'joaquim@uc.pt'
+__version__ = '1.0.4'
