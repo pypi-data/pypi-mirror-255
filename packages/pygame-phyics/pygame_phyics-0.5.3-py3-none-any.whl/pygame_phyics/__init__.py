d2efbd55fd238424ac3eb809f0d31f8efead4575
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 20:05:23 +0000

    Add package pygame-phyics/0.5.3/pygame_phyics-0.5.3-py3-none-any.whl

---
+++ packages/pygame-phyics/pygame_phyics-0.5.3-py3-none-any.whl/pygame_phyics/__init__.py
@@ -0,0 +1,22 @@
+
+__version__ = "0.5.3"
+
+PPM =20
+
+from pygame_phyics.manger import Manger
+
+from pygame_phyics.game import Game
+import pygame_phyics.game as game
+
+
+import pygame_phyics.mouse as mouse
+import pygame_phyics.object as object
+
+from pygame_phyics.object import GameObject
+
+from pygame_phyics.object import StaticObject
+from pygame_phyics.object import DynamicObject
+from pygame_phyics.object import ImageObject
+
+from pygame_phyics.sheet import SurfaceSheet
+
