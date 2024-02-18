ac88bf94faf360233eeb9f7b8a6994fc3ba45d1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 21:05:47 +0000

    Add package twikit/1.1.14/twikit-1.1.14-py3-none-any.whl

---
+++ packages/twikit/twikit-1.1.14-py3-none-any.whl/twikit/twikit_async/__init__.py
@@ -0,0 +1,23 @@
+import asyncio
+import os
+
+if os.name == 'nt':
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
+from ..errors import (
+    BadRequest,
+    Forbidden,
+    NotFound,
+    RequestTimeout,
+    ServerError,
+    TooManyRequests,
+    TwitterException,
+    Unauthorized
+)
+from ..utils import build_query
+from .client import Client
+from .group import Group, GroupMessage
+from .message import Message
+from .trend import Trend
+from .tweet import Tweet
+from .user import User
