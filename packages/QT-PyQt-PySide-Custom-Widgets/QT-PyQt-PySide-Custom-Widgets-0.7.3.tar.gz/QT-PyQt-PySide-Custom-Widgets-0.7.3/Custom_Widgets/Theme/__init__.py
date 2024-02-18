f610b4f9862de2fa5525932cb48fd3fa15c1e949
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 07:05:54 +0000

    Add package QT-PyQt-PySide-Custom-Widgets/0.7.3/QT-PyQt-PySide-Custom-Widgets-0.7.3.tar.gz

---
+++ packages/QT-PyQt-PySide-Custom-Widgets/QT-PyQt-PySide-Custom-Widgets-0.7.3.tar.gz/QT-PyQt-PySide-Custom-Widgets-0.7.3/Custom_Widgets/Theme/__init__.py
@@ -0,0 +1,30 @@
+from qtpy.QtGui import *
+from qtpy.QtWidgets import *
+
+class QPushButtonThemed(QPushButton):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        # store icon url 
+        self.iconUrl = None
+
+    def setNewIcon(self, url):
+        icon = QIcon(url)
+        self.setIcon(icon)
+
+        self.iconUrl = url
+
+class QLabelThemed(QLabel):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        # store icon url 
+        self.piximapUrl = None
+
+    def setNewPixmap(self, url):
+        piximap = QPixmap(url)
+        self.setPixmap(piximap)
+
+        self.piximapUrl = url
+
+    
