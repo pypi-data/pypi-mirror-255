10808c87207a36eb960d1f059c15ead3411b193d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 20:05:01 +0000

    Add package pyspedfiscal/0.2.10/pyspedfiscal-0.2.10.tar.gz

---
+++ packages/pyspedfiscal/pyspedfiscal-0.2.10.tar.gz/pyspedfiscal-0.2.10/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    readme = fh.read()
+
+setup(
+    name="pyspedfiscal",
+    version="0.2.10",
+    url="https://github.com/rodolfoscarp-jr/pyspedfiscal",
+    license="MIT License",
+    author="Rodolfo Scarp",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author_email="rodolfoscarp@gmail.com",
+    keywords="spedfiscal",
+    description="Serializa um arquivo do tipo SPED Fiscal",
+    packages=find_packages(exclude=["test*"]),
+    install_requires=["pydantic"],
+)
