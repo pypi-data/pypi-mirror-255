a36da87d39a32188d7842b204cdaf122f659b9fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 22:05:46 +0000

    Add package aiosu/2.2.0/aiosu-2.2.0-py3-none-any.whl

---
+++ packages/aiosu/aiosu-2.2.0-py3-none-any.whl/aiosu/__init__.py
@@ -0,0 +1,34 @@
+from __future__ import annotations
+
+from datetime import date
+from importlib import metadata
+
+__title__ = "aiosu"
+__author__ = "Nice Aesthetics"
+__license__ = "GPLv3+"
+__copyright__ = f"Copyright {date.today().year} {__author__}"
+
+from . import events
+from . import exceptions
+from . import helpers
+from . import utils
+from . import models
+from . import v1
+from . import v2
+
+__all__ = (
+    "events",
+    "exceptions",
+    "helpers",
+    "models",
+    "utils",
+    "v1",
+    "v2",
+)
+
+try:
+    __version__ = metadata.version(__package__)
+except metadata.PackageNotFoundError:
+    import toml
+
+    __version__ = toml.load("pyproject.toml")["tool"]["poetry"]["version"] + "dev"
---
+++ packages/aiosu/aiosu-2.2.0-py3-none-any.whl/aiosu/v2/__init__.py
@@ -0,0 +1,7 @@
+"""
+This module contains the v2 API client and client storage.
+"""
+from __future__ import annotations
+
+from .client import *
+from .clientstorage import *
