e277059e8905d5b6ac0d1ed7b5fa9eb555da2fae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 18:05:03 +0000

    Add package rez/3.0.0/rez-3.0.0-py3-none-any.whl

---
+++ packages/rez/rez-3.0.0-py3-none-any.whl/rez/bind/os.py
@@ -0,0 +1,26 @@
+# SPDX-License-Identifier: Apache-2.0
+# Copyright Contributors to the Rez Project
+
+
+"""
+Creates the operating system package.
+"""
+from __future__ import absolute_import
+from rez.package_maker import make_package
+from rez.version import Version
+from rez.bind._utils import check_version
+from rez.system import system
+
+
+def bind(path, version_range=None, opts=None, parser=None):
+    version = Version(system.os)
+    check_version(version, version_range)
+
+    with make_package("os", path) as pkg:
+        pkg.version = version
+        pkg.requires = [
+            "platform-%s" % system.platform,
+            "arch-%s" % system.arch
+        ]
+
+    return pkg.installed_variants
