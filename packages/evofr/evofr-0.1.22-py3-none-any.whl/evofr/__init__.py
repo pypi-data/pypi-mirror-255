bc15830aedcecabf416fea1606060d6c4dae4c0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 23:07:05 +0000

    Add package evofr/0.1.22/evofr-0.1.22-py3-none-any.whl

---
+++ packages/evofr/evofr-0.1.22-py3-none-any.whl/evofr/__init__.py
@@ -0,0 +1,32 @@
+from .data import DataSpec
+from .data import CaseCounts, VariantFrequencies, CaseFrequencyData
+from .data import HierCases, HierFrequencies, HierarchicalCFData
+from .data.data_helpers import *
+
+from .models import ModelSpec
+from .models import MultinomialLogisticRegression, HierMLR
+from .models import PianthamModel
+from .models import InnovationMLR, InnovationSequenceCounts
+from .models.renewal_model import *
+
+from .infer import SVIHandler, MCMCHandler #, BlackJaxHandler
+from .infer import (
+    InferSVI,
+    InferMCMC,
+    InferFullRank,
+    InferMAP,
+    InferNUTS,
+    #InferBlackJax,
+    init_to_MAP,
+)
+
+from .posterior import PosteriorHandler, MultiPosterior
+from .posterior import get_median, get_quantile, get_quantiles
+from .posterior import (
+    get_site_by_variant,
+    get_freq,
+    get_sites_quantiles_json,
+    get_sites_variants_json,
+    EvofrEncoder,
+    save_json,
+)
