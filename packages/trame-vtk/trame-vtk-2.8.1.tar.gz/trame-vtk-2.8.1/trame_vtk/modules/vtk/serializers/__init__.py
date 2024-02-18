029cbbf9009627df7113f0d6e795a1bd178082fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 23:06:05 +0000

    Add package trame-vtk/2.8.1/trame-vtk-2.8.1.tar.gz

---
+++ packages/trame-vtk/trame-vtk-2.8.1.tar.gz/trame-vtk-2.8.1/trame_vtk/modules/vtk/serializers/__init__.py
@@ -0,0 +1,29 @@
+import logging
+import os
+
+from .mesh import mesh
+from .initialize import initialize_serializers
+from .serialize import serialize, serialize_widget
+from .export import extract_array_hash
+from .synchronization_context import SynchronizationContext
+from .utils import reference_id
+from .initialize import encode_lut
+
+logger = logging.getLogger(__name__)
+# By default, only show critical messages for serializers
+logger.setLevel(logging.CRITICAL)
+
+if "TRAME_SERIALIZE_DEBUG" in os.environ:
+    # If this environment variable is set, print out all messages
+    logger.setLevel(logging.DEBUG)
+
+__all__ = [
+    "encode_lut",
+    "reference_id",
+    "initialize_serializers",
+    "mesh",
+    "serialize",
+    "SynchronizationContext",
+    "serialize_widget",
+    "extract_array_hash",
+]
