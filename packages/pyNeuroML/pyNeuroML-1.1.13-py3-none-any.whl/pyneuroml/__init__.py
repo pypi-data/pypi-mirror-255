ec05959ff3161a8dba74a0d8beee1788fe1101da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 06:05:12 +0000

    Add package pyNeuroML/1.1.13/pyNeuroML-1.1.13-py3-none-any.whl

---
+++ packages/pyNeuroML/pyNeuroML-1.1.13-py3-none-any.whl/pyneuroml/__init__.py
@@ -0,0 +1,31 @@
+import logging
+
+try:
+    import importlib.metadata
+
+    __version__ = importlib.metadata.version("pyNeuroML")
+except ImportError:
+    import importlib_metadata
+
+    __version__ = importlib_metadata.version("pyNeuroML")
+
+
+JNEUROML_VERSION = "0.13.0"
+
+# Define a logger for the package
+logging.basicConfig(
+    format="pyNeuroML >>> %(levelname)s - %(message)s",
+    level=logging.WARN,
+)
+
+
+def print_v(msg):
+    """Print message to console.
+
+    This is to be used for printing out messages during ordinary usage of the
+    tool. For status monitoring, fault investigation etc., use the logger.
+
+    :param msg: string to print
+    :type msg: str
+    """
+    print("pyNeuroML >>> " + msg)
