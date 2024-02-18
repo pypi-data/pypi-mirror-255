9a1c5c04bf44dfa536517a39d98fe0deb991a9cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 07:05:24 +0000

    Add package semirings/0.3.1/semirings-0.3.1.tar.gz

---
+++ packages/semirings/semirings-0.3.1.tar.gz/semirings-0.3.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(name='semirings',
+      version='0.3.1',
+      description='Semirings are a powerful abstraction for dynamic programming.',
+      author='Tim Vieira',
+      project_url = 'https://github.com/timvieira/semirings',
+      install_requires = [
+          'numpy',
+          'scipy',       # for convex hull support
+          'graphviz',
+          'pytest',
+          'arsenal',     # https://github.com/timvieira/arsenal
+      ],
+      long_description=open('README.md').read(),
+      packages = ['semirings'],
+)
