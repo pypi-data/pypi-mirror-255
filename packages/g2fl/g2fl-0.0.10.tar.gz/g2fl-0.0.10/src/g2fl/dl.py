ad3e0d7a4c8cc3a4ffb437dcdbb309cba7167cc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 20:05:42 +0000

    Add package g2fl/0.0.10/g2fl-0.0.10.tar.gz

---
+++ packages/g2fl/g2fl-0.0.10.tar.gz/g2fl-0.0.10/src/g2fl/dl.py
@@ -0,0 +1,97 @@
+import time
+
+import numpy as np
+import torch
+import torchvision
+from matplotlib import pyplot as plt
+from matplotlib_inline import backend_inline
+from torch.utils import data
+from torchvision import transforms
+
+
+def use_svg_display():
+    """Use the svf format to display a plot in Jupyter."""
+    backend_inline.set_matplotlib_formats("svg")
+
+
+def set_figsize(figsize=(3.5, 2.5)):
+    """Set the figure size for matplotlib."""
+    use_svg_display()
+    plt.rcParams["figure.figsize"] = figsize
+
+
+def synthetic_data(w, b, num_examples):
+    """生成 y = Wx + b + 噪声。"""
+    X = torch.normal(0, 1, (num_examples, len(w)))
+    y = torch.matmul(X, w) + b
+    y += torch.normal(0, 0.01, y.shape)
+    return X, y.reshape((-1, 1))
+
+
+def load_array(data_arrays, batch_size, is_train=True):
+    """Construct a PyTorch data iterator."""
+    dataset = torch.utils.data.TensorDataset(*data_arrays)
+    return torch.utils.data.DataLoader(dataset, batch_size, shuffle=is_train)
+
+
+class Timer:
+    """Record multiple running times."""
+
+    def __init__(self):
+        """Defined in :numref:`sec_minibatch_sgd`"""
+        self.times = []
+        self.start()
+
+    def start(self):
+        """Start the timer."""
+        self.tik = time.time()
+
+    def stop(self):
+        """Stop the timer and record the time in a list."""
+        self.times.append(time.time() - self.tik)
+        return self.times[-1]
+
+    def avg(self):
+        """Return the average time."""
+        return sum(self.times) / len(self.times)
+
+    def sum(self):
+        """Return the sum of time."""
+        return sum(self.times)
+
+    def cumsum(self):
+        """Return the accumulated time."""
+        return np.array(self.times).cumsum().tolist()
+
+
+def get_dataloader_workers():
+    """Use 4 processes to read the data."""
+    return 4
+
+
+def load_data_fashion_mnist(batch_size, resize=None):
+    """下载Fashion-MNIST数据集，然后将其加载到内存中"""
+    trans = [transforms.ToTensor()]
+    if resize:
+        trans.insert(0, transforms.Resize(resize))
+    trans = transforms.Compose(trans)
+    mnist_train = torchvision.datasets.FashionMNIST(
+        root="../data", train=True, transform=trans, download=True
+    )
+    mnist_test = torchvision.datasets.FashionMNIST(
+        root="../data", train=False, transform=trans, download=True
+    )
+    return (
+        data.DataLoader(
+            mnist_train,
+            batch_size,
+            shuffle=True,
+            num_workers=get_dataloader_workers(),
+        ),
+        data.DataLoader(
+            mnist_test,
+            batch_size,
+            shuffle=False,
+            num_workers=get_dataloader_workers(),
+        ),
+    )
