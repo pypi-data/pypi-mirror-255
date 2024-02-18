70f889927e7217b231a3eb9b0a6d5e5670d8777f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 00:06:19 +0000

    Add package NEMO-stockroom/1.1.3/NEMO_stockroom-1.1.3-py3-none-any.whl

---
+++ packages/NEMO-stockroom/NEMO_stockroom-1.1.3-py3-none-any.whl/NEMO_stockroom/templatetags/stockroom_custom_tags_and_filters.py
@@ -0,0 +1,29 @@
+from django import template
+
+from NEMO_stockroom.models import ConsumableThumbnail, ConsumableRequest
+from NEMO.models import ConsumableWithdraw
+
+register = template.Library()
+
+@register.filter
+def get_thumbnail_url(consumable_id):
+	try:
+		t = ConsumableThumbnail.objects.get(consumable_id=consumable_id)
+	except:
+		return ""
+
+	return t.image.url
+
+@register.filter
+def get_total_price(unit_price, qty):
+	unit_price = float(unit_price)
+	qty = float(qty)
+	return str(round(unit_price / qty, 2))
+
+@register.filter
+def get_request_date(cw):
+	try:
+		return cw.consumablerequest_set.all()[0].date
+	except Exception:
+		return "N/A"
+
