9796fb43d6ef56b0d3696136ec110b65ead5d667
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 08:05:31 +0000

    Add package func-live/1.2.2/func_live-1.2.2-py3-none-any.whl

---
+++ packages/func-live/func_live-1.2.2-py3-none-any.whl/func_live/__init__.py
@@ -0,0 +1,36 @@
+import os
+import requests
+from .config import API_URL
+from .recursively_upload_filepaths import recursively_upload_filepaths
+
+class FuncLive:
+    def __init__(self):
+        self._fetch_functions()
+
+    async def _fetch_functions(self):
+        response = requests.get(f"{API_URL}/functions")
+        functions = response.json().get('functions', [])
+
+        for func in functions:
+            func_name = func['name']
+            setattr(self, func_name, await self._create_function(func_name))
+
+    async def _create_function(self, func_name):
+        async def func(input_data):
+            try:
+                modified_input = await recursively_upload_filepaths(input_data, self.session)
+                response = requests.post(f"{API_URL}/functions/{func_name}",
+                                         headers={
+                                             'Authorization': f'Bearer {os.getenv("FUNC_TOKEN")}',
+                                             'Accept': 'application/json',
+                                             'Content-Type': 'application/json'
+                                         },
+                                         json={'input': modified_input})
+                return response.json().get('output')
+            except requests.RequestException as e:
+                raise Exception(f"There was an error: {str(e)}")
+
+        return func
+
+
+func_live = FuncLive()
