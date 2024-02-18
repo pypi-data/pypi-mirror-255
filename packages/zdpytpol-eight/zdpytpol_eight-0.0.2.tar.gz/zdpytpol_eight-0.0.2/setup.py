1513b72ba17c55d3098e5c1612dd20028188489d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 02:12:31 +0000

    Add package zdpytpol-eight/0.0.2/zdpytpol_eight-0.0.2.tar.gz

---
+++ packages/zdpytpol-eight/zdpytpol_eight-0.0.2.tar.gz/zdpytpol_eight-0.0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="zdpytpol_eight",
+    version="0.0.2",
+    author="Ditto",
+    packages=find_packages(),
+    description="some description"
+)
