d2a0c384e5e712f2b21e279d28e49d3220b2d55d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 05:05:33 +0000

    Add package apngasm-python/1.2.3/apngasm_python-1.2.3.tar.gz

---
+++ packages/apngasm-python/apngasm_python-1.2.3.tar.gz/apngasm_python-1.2.3/conanfile.py
@@ -0,0 +1,34 @@
+from conan import ConanFile
+import shutil
+from scripts.get_arch import get_arch
+from conan.tools.cmake import CMake, CMakeToolchain, CMakeDeps, cmake_layout
+from conan.tools.apple import is_apple_os
+
+
+class ApngasmRecipe(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+
+    def requirements(self):
+        self.requires("zlib/1.2.13") # type: ignore
+        self.requires("libpng/1.6.40") # type: ignore
+        self.requires(
+            "boost/1.75.0" # type: ignore
+        )  # https://github.com/conan-io/conan-center-index/issues/19704
+
+    def build_requirements(self):
+        self.build_requires("b2/4.10.1") # type: ignore
+        if not shutil.which("cmake"):
+            self.tool_requires("cmake/[>=3.27]") # type: ignore
+
+    def build(self):
+        build_type = "Release"
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        cmake = CMakeDeps(self)
+        if is_apple_os(self) and get_arch() == "universal2":
+            tc.blocks["apple_system"].values[
+                "cmake_osx_architectures"
+            ] = "x86_64; arm64"
+        tc.generate()
+        cmake.generate()
