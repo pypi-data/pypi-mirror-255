826a2213086d6d86ef62ae0db816b2f1598ab6af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 04:04:53 +0000

    Add package log9/0.2/log9-0.2.tar.gz

---
+++ packages/log9/log9-0.2.tar.gz/log9-0.2/log9.py
@@ -0,0 +1,29 @@
+
+import logging
+from colorlog import ColoredFormatter
+
+def setup_logging(level=logging.INFO):
+    # 清除所有已添加的处理器，防止重复输出日志
+    for handler in logging.root.handlers[:]:
+        logging.root.removeHandler(handler)
+        
+    # 定义日志格式和颜色
+    fmt = "%(log_color)s%(asctime)s %(levelname)s [%(filename)s:%(lineno)d] %(message)s"
+    log_colors = {
+        'DEBUG': 'cyan',
+        'INFO': 'green',
+        'WARNING': 'yellow',
+        'ERROR': 'red',
+        'CRITICAL': 'red,bg_white',
+    }
+    formatter = ColoredFormatter(fmt, log_colors=log_colors, style='%')
+
+    # 创建流处理器，并添加到日志记录器
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(formatter)
+    
+    # 获取日志记录器
+    logger = logging.getLogger()
+    logger.setLevel(level)
+    logger.addHandler(stream_handler)
+    return logger
---
+++ packages/log9/log9-0.2.tar.gz/log9-0.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+import os
+
+
+setup(
+    name='log9',
+    version='0.2',
+    packages=find_packages(),
+    py_modules=['log9'],
+    install_requires=[
+        'colorlog',  # 依赖项
+    ],
+    author='Tim-Saijun',
+    author_email='c@zair.top',
+    description='简单封装的彩色日志模块',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ]
+)
