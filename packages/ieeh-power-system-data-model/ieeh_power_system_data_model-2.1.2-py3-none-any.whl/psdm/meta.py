fec5996d16dc77e929f9b2ba15d70f24326cc1aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 14:06:58 +0000

    Add package ieeh-power-system-data-model/2.1.2/ieeh_power_system_data_model-2.1.2-py3-none-any.whl

---
+++ packages/ieeh-power-system-data-model/ieeh_power_system_data_model-2.1.2-py3-none-any.whl/psdm/meta.py
@@ -0,0 +1,37 @@
+# :author: Sasan Jacob Rasti <sasan_jacob.rasti@tu-dresden.de>
+# :copyright: Copyright (c) Institute of Electrical Power Systems and High Voltage Engineering - TU Dresden, 2022-2023.
+# :license: BSD 3-Clause
+
+from __future__ import annotations
+
+import datetime as dt
+import enum
+import uuid
+
+import pydantic
+
+from psdm.base import Base
+
+VERSION = "2.1.2"
+
+
+class SignConvention(enum.Enum):
+    PASSIVE = "PASSIVE"  # consumer load centered
+    ACTIVE = "ACTIVE"  # producer load centered
+
+
+class Meta(Base):
+    """This class represents the meta data related to the grid export."""
+
+    grid: str
+    date: dt.date
+
+    id: uuid.UUID = pydantic.Field(default_factory=uuid.uuid4)
+    sign_convention: SignConvention | None = None
+    project: str | None = None
+    case: str | None = None
+
+    @pydantic.computed_field  # type: ignore[misc]
+    @property
+    def version(self) -> str:
+        return VERSION
