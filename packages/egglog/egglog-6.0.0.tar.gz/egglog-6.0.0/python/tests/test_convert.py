1ae83c694e84ab04ab813cc7cb588be1dfc1eff7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 03:05:52 +0000

    Add package egglog/6.0.0/egglog-6.0.0.tar.gz

---
+++ packages/egglog/egglog-6.0.0.tar.gz/egglog-6.0.0/python/tests/test_convert.py
@@ -0,0 +1,102 @@
+from egglog import *
+
+
+def test_conversion_custom_metaclass():
+    class MyMeta(type):
+        pass
+
+    class MyType(metaclass=MyMeta):
+        pass
+
+    egraph = EGraph()
+
+
+    class MyTypeExpr(Expr):
+        def __init__(self):
+            ...
+
+    converter(MyMeta, MyTypeExpr, lambda x: MyTypeExpr())
+    assert expr_parts(convert(MyType(), MyTypeExpr)) == expr_parts(MyTypeExpr())
+
+
+def test_conversion():
+    egraph = EGraph()
+
+    class MyType:
+        pass
+
+
+    class MyTypeExpr(Expr):
+        def __init__(self):
+            ...
+
+    converter(MyType, MyTypeExpr, lambda x: MyTypeExpr())
+
+    assert expr_parts(convert(MyType(), MyTypeExpr)) == expr_parts(MyTypeExpr())
+
+
+def test_conversion_transitive_forward():
+    egraph = EGraph()
+
+    class MyType:
+        pass
+
+
+    class MyTypeExpr(Expr):
+        def __init__(self):
+            ...
+
+
+    class MyTypeExpr2(Expr):
+        def __init__(self):
+            ...
+
+    converter(MyType, MyTypeExpr, lambda x: MyTypeExpr())
+    converter(MyTypeExpr, MyTypeExpr2, lambda x: MyTypeExpr2())
+
+    assert expr_parts(convert(MyType(), MyTypeExpr2)) == expr_parts(MyTypeExpr2())
+
+
+def test_conversion_transitive_backward():
+    egraph = EGraph()
+
+    class MyType:
+        pass
+
+
+    class MyTypeExpr(Expr):
+        def __init__(self):
+            ...
+
+
+    class MyTypeExpr2(Expr):
+        def __init__(self):
+            ...
+
+    converter(MyTypeExpr, MyTypeExpr2, lambda x: MyTypeExpr2())
+    converter(MyType, MyTypeExpr, lambda x: MyTypeExpr())
+    assert expr_parts(convert(MyType(), MyTypeExpr2)) == expr_parts(MyTypeExpr2())
+
+
+def test_conversion_transitive_cycle():
+    egraph = EGraph()
+
+    class MyType:
+        pass
+
+
+    class MyTypeExpr(Expr):
+        def __init__(self):
+            ...
+
+
+    class MyTypeExpr2(Expr):
+        def __init__(self):
+            ...
+
+    converter(MyType, MyTypeExpr, lambda x: MyTypeExpr())
+    converter(MyTypeExpr, MyTypeExpr2, lambda x: MyTypeExpr2())
+    converter(MyTypeExpr2, MyTypeExpr, lambda x: MyTypeExpr())
+
+    assert expr_parts(convert(MyType(), MyTypeExpr2)) == expr_parts(MyTypeExpr2())
+    assert expr_parts(convert(MyType(), MyTypeExpr)) == expr_parts(MyTypeExpr())
