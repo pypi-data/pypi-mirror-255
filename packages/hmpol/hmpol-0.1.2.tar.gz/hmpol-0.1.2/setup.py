39de71f4eb4cc91af67c2ab8d5d33ae0c7fd7f95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 15:06:00 +0000

    Add package hmpol/0.1.2/hmpol-0.1.2.tar.gz

---
+++ packages/hmpol/hmpol-0.1.2.tar.gz/hmpol-0.1.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='hmpol',  # Replace with your actual package name
+    url = 'https://gitlab.com/AnoopANair/the-hmpol-project.git', # Replace with your github project link
+    version='0.1.2',
+    packages=find_packages(),
+    install_requires=[
+        # Your project dependencies
+    ],
+)
