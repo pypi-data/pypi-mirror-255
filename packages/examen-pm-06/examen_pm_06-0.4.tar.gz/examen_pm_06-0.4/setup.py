a446ce6ab2a9defc9bbbe2fc761b360ce024a49d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 11:05:35 +0000

    Add package examen-pm-06/0.4/examen_pm_06-0.4.tar.gz

---
+++ packages/examen-pm-06/examen_pm_06-0.4.tar.gz/examen_pm_06-0.4/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='examen_pm_06',
+      version='0.4',
+      description='Gaussian and Binomial distributions',
+      packages={'examen_pm_06': ['govno.pdf', 'arch.zip']},
+      author_email='manasow.iw@yandex.ru',
+      zip_safe=False)
