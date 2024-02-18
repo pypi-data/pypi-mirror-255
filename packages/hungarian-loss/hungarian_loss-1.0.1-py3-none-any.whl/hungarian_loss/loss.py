94461fa43aadf56573c95ad7de9024182b1b7687
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 09:04:28 +0000

    Add package hungarian-loss/1.0.1/hungarian_loss-1.0.1-py3-none-any.whl

---
+++ packages/hungarian-loss/hungarian_loss-1.0.1-py3-none-any.whl/hungarian_loss/loss.py
@@ -0,0 +1,238 @@
+"""The module implementing the Hungarian loss function."""
+
+import tensorflow as tf
+
+from .steps import (
+    compute_euclidean_distance,
+    reduce_matrix,
+    select_optimal_assignment_mask,
+)
+
+
+class HungarianLoss(tf.keras.losses.Loss):
+    """
+    Computes the Hungarian loss between `y_true` and `y_pred`.
+
+    For example, if we are detecting 10  bounding boxes on a batch
+    of 32 images, the  `y_true` and `y_pred` will represent 32 images
+    where each image is represented by 10 bounding boxes and each
+    bounding box is represented by 4  (x,y,w,h) coordinates. This
+    gives us the final shape of  `y_true` and `y_pred` `(32, 10, 4)`.
+
+    Example:
+        >>> def rmse(y_true, y_pred):
+        >>>     return tf.sqrt(tf.keras.losses.mse(y_pred, y_true))
+        >>> y_true = tf.constant(
+        >>>     [
+        >>>         [[1.0, 2.0, 3.0, 4.0], [5.0, 6.0, 7.0, 8.0]],
+        >>>         [[1.0, 2.0, 3.0, 4.0], [5.0, 6.0, 7.0, 8.0]],
+        >>>     ]
+        >>> )
+        >>> y_pred = tf.constant(
+        >>>     [
+        >>>         [[1.0, 1.0, 1.0, 1.0], [2.0, 2.0, 2.0, 2.0]],
+        >>>         [[1.0, 1.0, 1.0, 1.0], [2.0, 2.0, 2.0, 2.0]],
+        >>>     ]
+        >>> )
+        >>> HungarianLoss(
+        >>>    [4], 0, compute_euclidean_distance, [rmse], [1.0]
+        >>> ).call(y_true, y_pred)
+
+        >>> tf.Tensor([3.254 3.254], shape=(2,), dtype=float32)
+
+    Args:
+        slice_sizes:
+            The list of slice sizes for fragmenting input `y_true` and
+            `y_pred`.
+        slice_index_to_compute_assignment:
+            The slice index used for computing the cost matrix and performing
+            the optimal assignment.
+        compute_cost_matrix_fn:
+            The function for computing the cost matrix.
+        slice_losses_fn:
+            The list of loss functions for computing loss in each slice.
+        slice_weights:
+            The list of weights applying to each slice loss.
+    """
+
+    def __init__(
+        self,
+        slice_sizes: list,
+        slice_index_to_compute_assignment: int = 0,
+        compute_cost_matrix_fn: object = compute_euclidean_distance,
+        slice_losses_fn: list = None,
+        slice_weights: list = None,
+    ):
+        super().__init__(
+            reduction=tf.keras.losses.Reduction.NONE, name="hungarian_loss"
+        )
+        if slice_sizes is None:
+            raise TypeError(
+                "The slice_sizes must be a <class 'list'>, but got None;"
+            )
+        if len(slice_sizes) == 0:
+            raise ValueError(
+                "The slice_sizes must be containing at least one element, "
+                "but got 0;"
+            )
+        if slice_index_to_compute_assignment >= len(slice_sizes):
+            raise ValueError(
+                "The slice_index_to_compute_assignment must be less than "
+                f"a length of the slice_sizes but got "
+                f"idx({slice_index_to_compute_assignment})>="
+                f"len({len(slice_sizes)});"
+            )
+        if slice_losses_fn is not None:
+            if len(slice_sizes) != len(slice_losses_fn):
+                raise ValueError(
+                    "A length of the slice_losses_fn must be equal to a "
+                    "length of the slice_sizes but got "
+                    f"{len(slice_sizes)}!={len(slice_losses_fn)};"
+                )
+        if slice_weights is not None:
+            if len(slice_sizes) != len(slice_weights):
+                raise ValueError(
+                    "A length of the slice_weights must be equal to a "
+                    "length of the slice_sizes but got "
+                    f"{len(slice_sizes)}!={len(slice_weights)};"
+                )
+
+        self.slice_sizes = slice_sizes
+        self.slice_index_to_compute_assignment = (
+            slice_index_to_compute_assignment
+        )
+        self.compute_cost_matrix_fn = compute_cost_matrix_fn
+        if slice_losses_fn:
+            self.slice_losses_fn = slice_losses_fn
+        else:
+            self.slice_losses_fn = [tf.keras.losses.mse for _ in slice_sizes]
+        if slice_weights:
+            self.slice_weights = slice_weights
+        else:
+            self.slice_losses_fn = [1.0 for _ in slice_sizes]
+
+    def __compute_sample_loss(self, y_true, y_pred):  # pragma: no cover
+        shift = 0
+        v_trues, v_preds = [], []
+
+        for i, size in enumerate(self.slice_sizes):
+            v_true = tf.slice(
+                tf.cast(y_true, tf.float32), [0, shift], [-1, size]
+            )
+            v_pred = tf.slice(
+                tf.cast(y_pred, tf.float32), [0, shift], [-1, size]
+            )
+
+            if i == self.slice_index_to_compute_assignment:
+                cost = self.compute_cost_matrix_fn(v_true, v_pred)
+                # We need to reshape the distance matrix by removing the
+                # `None` dimension values.
+                n = cost.shape[1]
+                cost = tf.reshape(cost, (n, n))
+
+            v_trues.append(v_true)
+            v_preds.append(v_pred)
+            shift += size
+
+        assignments = select_optimal_assignment_mask(reduce_matrix(cost))
+        y_true_order = tf.gather(tf.where(assignments), indices=[0], axis=1)
+        y_pred_order = tf.gather(tf.where(assignments), indices=[1], axis=1)
+
+        slice_losses = []
+        for loss_fn, v_true, v_pred in zip(
+            self.slice_losses_fn, v_trues, v_preds
+        ):
+            v_true_reordered = tf.gather_nd(v_true, y_true_order)
+            v_pred_reordered = tf.gather_nd(v_pred, y_pred_order)
+            slice_losses.append(
+                tf.reduce_mean(loss_fn(v_true_reordered, v_pred_reordered))
+            )
+
+        return tf.reduce_mean(tf.multiply(self.slice_weights, slice_losses))
+
+    def call(self, y_true, y_pred):
+        """
+        The function called to compute the Hungarian.
+
+        Args:
+            y_true:
+                The ground truth values, 3D `Tensor` of shape
+                `[batch_size, num_of_entities, num_of_quantifiers]`.
+            y_pred:
+                The predicted values, 3D `Tensor` with of shape
+                `[batch_size, num_of_entities, num_of_quantifiers]`.
+
+        Returns:
+            The predicted loss values 1D `tensor` with shape = `[batch_size]`.
+        """
+        return tf.reduce_mean(
+            tf.map_fn(
+                lambda x: self.__compute_sample_loss(x[0], x[1]),
+                tf.cast(tf.stack([y_true, y_pred], 1), tf.float32),
+            )
+        )
+
+
+def hungarian_loss(y_true: tf.Tensor, y_pred: tf.Tensor) -> tf.Tensor:
+    """
+    Computes the Hungarian loss between `y_true` and `y_pred`.
+
+    For example, if we are detecting 10  bounding boxes on a batch
+    of 32 images, the  `y_true` and `y_pred` will represent 32 images
+    where each image is represented by 10 bounding boxes and each
+    bounding box is represented by 4  (x,y,w,h) coordinates. This
+    gives us the final shape of  `y_true` and `y_pred` `(32, 10, 4)`.
+
+    Example:
+        >>> y_true = tf.constant(
+        >>>     [
+        >>>         [[1.0, 2.0, 3.0, 4.0], [5.0, 6.0, 7.0, 8.0]],
+        >>>         [[1.0, 2.0, 3.0, 4.0], [5.0, 6.0, 7.0, 8.0]],
+        >>>     ]
+        >>> )
+        >>> y_pred = tf.constant(
+        >>>     [
+        >>>         [[1.0, 1.0, 1.0, 1.0], [2.0, 2.0, 2.0, 2.0]],
+        >>>         [[1.0, 1.0, 1.0, 1.0], [2.0, 2.0, 2.0, 2.0]],
+        >>>     ]
+        >>> )
+        >>> hungarian_loss(y_true, y_pred)
+
+        >>> tf.Tensor([3.254 3.254], shape=(2,), dtype=float32)
+
+    Args:
+        y_true:
+            The ground truth values, 3D `Tensor` of shape
+            `[batch_size, num_of_entities, num_of_quantifiers]`.
+        y_pred:
+            The predicted values, 3D `Tensor` with of shape
+            `[batch_size, num_of_entities, num_of_quantifiers]`.
+
+    Returns:
+        The predicted loss values 1D `tensor` with shape = `[batch_size]`.
+    """
+
+    def compute_sample_loss(v_true, v_pred):  # pragma: no cover
+        cost = compute_euclidean_distance(v_true, v_pred)
+
+        # We need to reshape the distance matrix by removing the
+        # `None` dimension values.
+        n = cost.shape[1]
+        cost = tf.reshape(cost, (n, n))
+
+        return tf.reduce_mean(
+            tf.multiply(
+                cost,
+                tf.cast(
+                    select_optimal_assignment_mask(reduce_matrix(cost)),
+                    tf.float32,
+                ),
+            )
+        )
+
+    return tf.reduce_mean(
+        tf.map_fn(
+            lambda x: compute_sample_loss(x[0], x[1]),
+            tf.cast(tf.stack([y_true, y_pred], 1), tf.float32),
+        )
+    )
---
+++ packages/hungarian-loss/hungarian_loss-1.0.1-py3-none-any.whl/hungarian_loss/steps.py
@@ -0,0 +1,542 @@
+"""Steps for computing Hungarian loss."""
+
+import tensorflow as tf
+
+from .const import ZERO, ONE
+from .ops import (
+    count_zeros_in_rows,
+    count_zeros_in_cols,
+    get_row_mask_with_min_zeros,
+    get_row_mask_with_max_zeros,
+    get_col_mask_with_min_zeros,
+    get_col_mask_with_max_zeros,
+    expand_item_mask,
+)
+
+
+def compute_euclidean_distance(
+    a: tf.Tensor, b: tf.Tensor
+) -> tf.Tensor:  # pylint: disable=invalid-name
+    """
+    Computes euclidean distance between two inputs `a` and `b`.
+
+    The computation is performed using the following formula
+    `dist = sqrt((a - b)^2) = sqrt(a^2 - 2ab.T - b^2)`
+
+    Example:
+    >>> a = tf.constant(
+    >>>     [[1., 2., 3., 4.],
+    >>>      [5., 6., 7., 8.]]
+    >>> )
+    >>> b = tf.constant(
+    >>>     [[1., 1., 1., 1.],
+    >>>      [2., 2., 2., 2.]]
+    >>> )
+    >>> compute_euclidean_distance(a,b)
+
+    >>> tf.Tensor(
+    >>>     [[ 3.7416575,  2.4494898],
+    >>>      [11.224972 ,  9.273619 ]] dtype=float32)
+
+    Args:
+        a:
+            The 2D-tensor [num_entities, num_dimesions] of floats.
+        b:
+            The 2D-tensor [num_entities, num_dimesions] of floats.
+
+    Result:
+        The 2D tensor [num_entities, num_entities] with computed
+        distances between entities.
+    """
+    a2 = tf.reshape(tf.reduce_sum(tf.square(a), axis=1), [-1, 1])
+    b2 = tf.reshape(tf.reduce_sum(tf.square(b), axis=1), [1, -1])
+    dist = tf.sqrt(a2 - 2 * tf.matmul(a, tf.transpose(b, perm=[1, 0])) + b2)
+    return dist
+
+
+def reduce_rows(matrix: tf.Tensor) -> tf.Tensor:
+    """
+    Subtracts the minimum value from each row.
+
+    Example:
+    >>> matrix = tf.Variable(
+    >>>    [[ 30., 25., 10.],
+    >>>     [ 15., 10., 20.],
+    >>>     [ 25., 20., 15.]]
+    >>> )
+    >>> reduce_rows(matrix)
+
+    >>> tf.Tensor(
+    >>>     [[20. 15.  0.]
+    >>>      [ 5.  0. 10.]
+    >>>      [10.  5.  0.]], shape=(3, 3), dtype=float32)
+
+    Args:
+        matrix:
+            The 2D-tensor [rows, columns] of floats to reduce.
+
+    Returns:
+        A new tensor with reduced values of the same shape as
+        the input tensor.
+    """
+    return tf.subtract(
+        matrix, tf.reshape(tf.reduce_min(matrix, axis=1), (-1, 1))
+    )
+
+
+def reduce_cols(matrix: tf.Tensor) -> tf.Tensor:
+    """
+    Subtracts the minimum value from each column.
+
+    Example:
+    >>> matrix = tf.Variable(
+    >>>    [[ 30., 25., 10.],
+    >>>     [ 15., 10., 20.],
+    >>>     [ 25., 20., 15.]]
+    >>> )
+    >>> reduce_cols(matrix)
+
+    >>> tf.Tensor(
+    >>>     [[15. 15.  0.]
+    >>>      [ 0.  0. 10.]
+    >>>      [10. 10.  5.]], shape=(3, 3), dtype=float32)
+
+    Args:
+        matrix:
+            The 2D-tensor [rows, columns] of floats to reduce.
+
+    Returns:
+        A new tensor with reduced values of the same shape as
+        the input tensor.
+    """
+    return tf.subtract(matrix, tf.reduce_min(matrix, axis=0))
+
+
+def scratch_matrix(matrix: tf.Tensor) -> tf.Tensor:
+    """
+    Creates the mask for rows and columns which are covering all
+    zeros in the matrix.
+
+    Example:
+    >>> matrix = tf.Variable(
+    >>>    [[15., 15.,  0.],
+    >>>     [ 0.,  0., 10.],
+    >>>     [ 5.,  5.,  0.]]
+    >>> )
+    >>> scratch_matrix(matrix)
+
+    >>> (<tf.Tensor: shape=(3, 1), dtype=bool, numpy=
+    >>>     array([[False],
+    >>>            [ True],
+    >>>            [False]])>,
+    >>>  <tf.Tensor: shape=(1, 3), dtype=bool, numpy=
+    >>>     array([[False, False,  True]])>)
+
+    Args:
+        matrix:
+            The 2D-tensor [rows, columns] of floats to scrarch.
+
+    Returns:
+        scratched_rows_mask:
+            The 2D-tensor row mask, where `True` values indicates the
+            scratched rows and `False` intact rows accordingly.
+        scratched_cols_mask:
+            The 2D-tensor column mask, where `True` values indicates the
+            scratched columns and `False` intact columns accordingly.
+    """
+    NEXT_SCRATCH_ROWS = 0
+    NEXT_SCRATCH_COLS = 1
+
+    def scratch_row(zeros_mask, scratched_rows_mask, scratched_cols_mask):
+        scratched_row_mask = get_row_mask_with_max_zeros(zeros_mask)
+        new_scratched_rows_mask = tf.logical_or(
+            scratched_rows_mask, scratched_row_mask
+        )
+        new_zeros_mask = tf.logical_and(
+            zeros_mask, tf.logical_not(expand_item_mask(scratched_row_mask))
+        )
+        return (
+            new_zeros_mask,
+            new_scratched_rows_mask,
+            scratched_cols_mask,
+            NEXT_SCRATCH_COLS,
+        )
+
+    def scratch_col(zeros_mask, scratched_rows_mask, scratched_cols_mask):
+        scratched_col_mask = get_col_mask_with_max_zeros(zeros_mask)
+        new_scratched_cols_mask = tf.logical_or(
+            scratched_cols_mask, scratched_col_mask
+        )
+        new_zeros_mask = tf.logical_and(
+            zeros_mask, tf.logical_not(expand_item_mask(scratched_col_mask))
+        )
+        return (
+            new_zeros_mask,
+            scratched_rows_mask,
+            new_scratched_cols_mask,
+            NEXT_SCRATCH_ROWS,
+        )
+
+    def body(
+        zeros_mask, scratched_rows_mask, scratched_cols_mask, scratch_next
+    ):
+        max_zeros_in_rows = (tf.reduce_max(count_zeros_in_rows(zeros_mask)),)
+        max_zeros_in_cols = (tf.reduce_max(count_zeros_in_cols(zeros_mask)),)
+        return tf.cond(
+            tf.math.equal(max_zeros_in_rows, max_zeros_in_cols),
+            true_fn=lambda: tf.cond(
+                tf.math.equal(scratch_next, NEXT_SCRATCH_ROWS),
+                true_fn=lambda: scratch_row(
+                    zeros_mask, scratched_rows_mask, scratched_cols_mask
+                ),
+                false_fn=lambda: scratch_col(
+                    zeros_mask, scratched_rows_mask, scratched_cols_mask
+                ),
+            ),
+            false_fn=lambda: tf.cond(
+                tf.math.greater(max_zeros_in_rows, max_zeros_in_cols),
+                true_fn=lambda: scratch_row(
+                    zeros_mask, scratched_rows_mask, scratched_cols_mask
+                ),
+                false_fn=lambda: scratch_col(
+                    zeros_mask, scratched_rows_mask, scratched_cols_mask
+                ),
+            ),
+        )
+
+    def condition(
+        zeros_mask, scratched_rows_mask, scratched_cols_mask, scratch_next
+    ):
+        return tf.reduce_any(zeros_mask)
+
+    num_of_rows, num_of_cols = matrix.shape
+    _, scratched_rows_mask, scratched_cols_mask, _ = tf.while_loop(
+        condition,
+        body,
+        [
+            tf.math.equal(matrix, ZERO),
+            tf.zeros((num_of_rows, 1), tf.bool),
+            tf.zeros((1, num_of_cols), tf.bool),
+            NEXT_SCRATCH_ROWS,
+        ],
+    )
+    return scratched_rows_mask, scratched_cols_mask
+
+
+def is_optimal_assignment(
+    scratched_rows_mask: tf.Tensor, scratched_cols_mask: tf.Tensor
+) -> tf.Tensor:
+    """
+    Test if we can achieve the optimal assignment.
+
+    We can achieve the optimal assignment if the combined number of
+    scratched columns and rows equals to the matrix dimensions (since
+    matrix is square, dimension side does not matter.)
+
+    Example:
+
+        Optimal assignment:
+        >>> scratched_rows_mask = tf.constant(
+        >>>    [[False], [True], [False]], tf.bool)
+        >>> scratched_cols_mask = tf.constant(
+        >>>    [[True, False, True]])
+        >>> is_optimal_assignment(scratched_rows_mask, scratched_cols_mask)
+
+        >>> tf.Tensor(True, shape=(), dtype=bool)
+
+        Not optimal assignment:
+        >>> scratched_rows_mask = tf.constant(
+        >>>    [[False], [True], [False]], tf.bool)
+        >>> scratched_cols_mask = tf.constant(
+        >>>    [[True, False, True]])
+        >>> is_optimal_assignment(scratched_rows_mask, scratched_cols_mask)
+
+        >>> tf.Tensor(False, shape=(), dtype=bool)
+
+    Args:
+        scratched_rows_mask:
+            The 2D-tensor row mask, where `True` values indicates the
+            scratched rows and `False` intact rows accordingly.
+        scratched_cols_mask:
+            The 2D-tensor column mask, where `True` values indicates the
+            scratched columns and `False` intact columns accordingly.
+
+    Returns:
+        The boolean tensor, where `True` indicates the optimal assignment
+        and `False` otherwise.
+    """
+    assert scratched_rows_mask.shape[0] == scratched_cols_mask.shape[1]
+    n = scratched_rows_mask.shape[0]
+    number_of_lines_covering_zeros = tf.add(
+        tf.reduce_sum(tf.cast(scratched_rows_mask, tf.int32)),
+        tf.reduce_sum(tf.cast(scratched_cols_mask, tf.int32)),
+    )
+    return tf.less_equal(n, number_of_lines_covering_zeros)
+
+
+def shift_zeros(matrix, scratched_rows_mask, scratched_cols_mask):
+    """
+    Shifts zeros in not optimal mask.
+
+    Example:
+
+        Optimal assignment:
+        >>> matrix = tf.constant(
+        >>>    [[ 30., 25., 10.],
+        >>>     [ 15., 10., 20.],
+        >>>     [ 25., 20., 15.]], tf.float32
+        >>> )
+        >>> scratched_rows_mask = tf.constant(
+        >>>    [[False], [True], [False]], tf.bool)
+        >>> scratched_cols_mask = tf.constant(
+        >>>    [[False, False, True]])
+        >>> shift_zeros(matrix, scratched_rows_mask, scratched_cols_mask)
+
+        >>> (<tf.Tensor:
+        >>>       [[10., 10.,  0.],
+        >>>        [ 0.,  0., 15.],
+        >>>        [ 0.,  0.,  0.]], shape=(3, 3) dtype=float32)>,
+        >>> <tf.Tensor:
+        >>>       [[False],
+        >>>        [ True],
+        >>>        [False]], shape=(3, 1), dtype=bool>,
+        >>> <tf.Tensor:
+        >>>       [[False, False,  True]], shape=(1, 3), dtype=bool>)
+
+    Args:
+        matrix:
+            The 2D-tensor [rows, columns] of floats with reduced
+            values.
+        scratched_rows_mask:
+            The 2D-tensor row mask, where `True` values indicates the
+            scratched rows and `False` intact rows accordingly.
+        scratched_cols_mask:
+            The 2D-tensor column mask, where `True` values indicates the
+            scratched columns and `False` intact columns accordingly.
+
+    Returns:
+        matrix:
+            The 3D-tensor [rows, columns] of floats with shifted
+            zeros.
+        scratched_rows_mask:
+            The same as input.
+        scratched_cols_mask:
+            The same as input
+    """
+    cross_mask = tf.cast(
+        tf.logical_and(scratched_rows_mask, scratched_cols_mask),
+        tf.float32,
+    )
+    inline_mask = tf.cast(
+        tf.logical_or(
+            tf.logical_and(
+                scratched_rows_mask, tf.logical_not(scratched_cols_mask)
+            ),
+            tf.logical_and(
+                tf.logical_not(scratched_rows_mask), scratched_cols_mask
+            ),
+        ),
+        tf.float32,
+    )
+    outline_mask = tf.cast(
+        tf.logical_not(
+            tf.logical_or(scratched_rows_mask, scratched_cols_mask)
+        ),
+        tf.float32,
+    )
+
+    outline_min_value = tf.reduce_min(
+        tf.math.add(
+            tf.math.multiply(
+                tf.math.subtract(ONE, outline_mask), tf.float32.max
+            ),
+            tf.math.multiply(matrix, outline_mask),
+        )
+    )
+
+    cross_matrix = tf.add(
+        tf.multiply(matrix, cross_mask),
+        tf.multiply(outline_min_value, cross_mask),
+    )
+    inline_matrix = tf.multiply(matrix, inline_mask)
+    outline_matrix = tf.subtract(
+        tf.multiply(matrix, outline_mask),
+        tf.multiply(outline_min_value, outline_mask),
+    )
+
+    return [
+        tf.math.add(cross_matrix, tf.math.add(inline_matrix, outline_matrix)),
+        scratched_rows_mask,
+        scratched_cols_mask,
+    ]
+
+
+def reduce_matrix(matrix):
+    """
+    Reduce matrix suitable to perform the optimal assignment.
+
+    Example:
+        >>> matrix = tf.constant(
+        >>>    [[ 30., 25., 10.],
+        >>>     [ 15., 10., 20.],
+        >>>     [ 25., 20., 15.]], tf.float32
+        >>> )
+        >>> reduce_matrix(matrix)
+
+        >>> tf.Tensor(
+        >>>     [[10. 10.  0.]
+        >>>      [ 0.  0. 15.]
+        >>>      [ 0.  0.  0.]], shape=(3, 3), dtype=float32)
+
+    Args:
+        matrix:
+            The 2D-tensor [rows, columns] of floats to reduce.
+
+    Returns:
+        A new tensor representing the reduced matrix of the same
+        shape as the input tensor.
+    """
+
+    def body(matrix, scratched_rows_mask, scratched_cols_mask):
+        new_matrix = reduce_rows(matrix)
+        new_matrix = reduce_cols(new_matrix)
+        scratched_rows_mask, scratched_cols_mask = scratch_matrix(new_matrix)
+
+        return tf.cond(
+            is_optimal_assignment(scratched_rows_mask, scratched_cols_mask),
+            true_fn=lambda: [
+                new_matrix,
+                scratched_rows_mask,
+                scratched_cols_mask,
+            ],
+            false_fn=lambda: shift_zeros(
+                new_matrix, scratched_rows_mask, scratched_cols_mask
+            ),
+        )
+
+    def condition(
+        matrix, scratched_rows_mask, scratched_cols_mask
+    ):  # pylint: disable=unused-argument
+        return tf.logical_not(
+            is_optimal_assignment(scratched_rows_mask, scratched_cols_mask)
+        )
+
+    num_of_rows, num_of_cols = matrix.shape
+    reduced_matrix, _, _ = tf.while_loop(
+        condition,
+        body,
+        [
+            matrix,
+            tf.zeros((num_of_rows, 1), tf.bool),
+            tf.zeros((1, num_of_cols), tf.bool),
+        ],
+    )
+
+    return reduced_matrix
+
+
+def select_optimal_assignment_mask(reduced_matrix):
+    """
+    Selects the optimal solution based on the reduced matrix.
+
+    Example:
+        >>> reduced_matrix = tf.constant(
+        >>>     [[10. 10.  0.]
+        >>>      [ 0.  0. 15.]
+        >>>      [ 0.  0.  0.]], shape=(3, 3), dtype=float32)
+        >>> reduce_matrix(matrix)
+
+        >>> tf.Tensor(
+        >>>     [[False False  True]
+        >>>      [ True False False]
+        >>>      [False  True False]], shape=(3, 3), dtype=bool)
+
+        Args:
+            matrix:
+                The 2D-tensor [rows, columns] of floats representing
+                the reduced matrix and used for selecting the optimal
+                solution.
+
+        Returns:
+            A new tensor representing the optimal assignment mask has
+            the same dimension as the input.
+    """
+
+    def select_based_on_row(zeros_mask, selection_mask):
+        best_row_mask = expand_item_mask(
+            get_row_mask_with_min_zeros(zeros_mask)
+        )
+        best_col_mask = expand_item_mask(
+            get_col_mask_with_max_zeros(
+                tf.logical_and(best_row_mask, zeros_mask)
+            )
+        )
+        new_selection_mask = tf.logical_or(
+            selection_mask, tf.logical_and(best_row_mask, best_col_mask)
+        )
+        new_mask = tf.logical_and(
+            zeros_mask,
+            tf.logical_not(tf.logical_or(best_row_mask, best_col_mask)),
+        )
+        return new_mask, new_selection_mask
+
+    def select_based_on_col(zeros_mask, selection_mask):
+        best_col_mask = expand_item_mask(
+            get_col_mask_with_min_zeros(zeros_mask)
+        )
+        best_row_mask = expand_item_mask(
+            get_row_mask_with_max_zeros(
+                tf.logical_and(best_col_mask, zeros_mask)
+            )
+        )
+        new_selection_mask = tf.logical_or(
+            selection_mask, tf.logical_and(best_col_mask, best_row_mask)
+        )
+        new_mask = tf.logical_and(
+            zeros_mask,
+            tf.logical_not(tf.logical_or(best_col_mask, best_row_mask)),
+        )
+        return new_mask, new_selection_mask
+
+    def body(zeros_mask, selection_mask):
+        zero_count_in_rows = tf.reduce_sum(
+            tf.cast(tf.equal(zeros_mask, True), tf.float32), axis=1
+        )
+        zero_count_in_rows = tf.where(
+            tf.equal(zero_count_in_rows, ZERO),
+            tf.float32.max,
+            zero_count_in_rows,
+        )
+        min_zero_count_in_rows = tf.reduce_min(zero_count_in_rows)
+
+        zero_count_in_cols = tf.reduce_sum(
+            tf.cast(tf.equal(zeros_mask, True), tf.float32), axis=0
+        )
+        zero_count_in_cols = tf.where(
+            tf.equal(zero_count_in_cols, ZERO),
+            tf.float32.max,
+            zero_count_in_cols,
+        )
+        min_zero_count_in_cols = tf.reduce_min(zero_count_in_cols)
+
+        return tf.cond(
+            tf.math.less(min_zero_count_in_rows, min_zero_count_in_cols),
+            true_fn=lambda: select_based_on_row(zeros_mask, selection_mask),
+            false_fn=lambda: select_based_on_col(zeros_mask, selection_mask),
+        )
+
+    def condition(
+        zeros_mask, selection_mask
+    ):  # pylint: disable=unused-argument
+        return tf.reduce_any(zeros_mask)
+
+    output = tf.while_loop(
+        condition,
+        body,
+        [
+            tf.math.equal(reduced_matrix, ZERO),
+            tf.zeros(reduced_matrix.shape, tf.bool),
+        ],
+    )
+
+    return output[1]
