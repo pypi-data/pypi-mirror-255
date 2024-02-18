27b538cd7a189f39482754a91a021bdfee10a34f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 03:05:54 +0000

    Add package mass2chem/0.5.0/mass2chem-0.5.0-py3-none-any.whl

---
+++ packages/mass2chem/mass2chem-0.5.0-py3-none-any.whl/mass2chem/io.py
@@ -0,0 +1,203 @@
+from collections import namedtuple
+import os
+import json
+import numpy as np
+
+# from metDataModel.core import Feature
+
+# intensities is a list; mass_id links to MassTrace
+tFeature = namedtuple('tFeature', ['feature_id', 'mass_id', 'mz', 'rtime', 'rt_min', 'rt_max', 
+                                'peak_quality_max', 'peak_quality_median', 'number_peaks', 'perc_peaks',
+                                'selectivity_combined', 'selectivity_mz', 'intensity_mean',
+                                'intensities'])
+
+
+def read_feature_tuples(feature_table, 
+                        id_col=0, mz_col=1, rtime_col=2, 
+                        intensity_cols=(3,4), delimiter="\t"):
+    '''
+    Read a text feature table into a list of features.
+
+    Input
+    -----
+    feature_table: Tab delimited feature table. First line as header.
+                    Recommended col 0 for ID, col 1 for m/z, col 2 for rtime.
+    id_col: column for id. If feature ID is not given, row_number is used as ID.
+    mz_col: column for m/z.
+    rtime_col: column for retention time.
+    intensity_cols: range of columns for intensity values. E.g. (3,5) includes only col 3 and 4.
+
+    Return
+    ------
+    List of namedtuple 'Features': [(Feature)...], 
+    '''
+    featureLines = open(feature_table).read().splitlines()
+    header = featureLines[0].split(delimiter)
+    num_features = len(featureLines)-1
+    # sanity check
+    print("table headers ordered: ", header[mz_col], header[rtime_col])
+    print("Read %d feature lines" %num_features)
+    L = []
+    for ii in range(1, num_features+1):
+        if featureLines[ii].strip():
+            a = featureLines[ii].split(delimiter)
+            if isinstance(id_col, int):         # feature id specified in a col, which could be 0
+                iid = a[id_col]
+            else:
+                iid = 'row'+str(ii)
+            xstart, xend = intensity_cols
+            intensities = [float(x) for x in a[xstart: xend]]
+            L.append({
+                'id': iid, 'mz': float(a[mz_col]), 'rtime': float(a[rtime_col]),
+                'intensities': intensities,
+                'representative_intensity': np.mean(intensities),
+            })
+    return L
+
+
+def index_features(list_of_features, range_low=20, range_high=2000):
+    '''
+    Indexing a list of features to speed up search.
+    A filter/Exception should be added for the mass range in production.
+    Internal use.
+
+    Input
+    -----
+    List of features, [{'id': '', 'mz': 0, 'rtime': 0, ...}, ...]
+    range_low, range_high: m/z range, default [20, 2000]. 
+
+    Return
+    ------
+    Dictionary of features, indexed using integer of m/z.
+    '''
+    L = {}
+    for ii in range(range_low, range_high+1): L[ii] = []
+    for F in list_of_features: L[int(F['mz'])].append(F)
+    return L
+
+
+def read_features(feature_table, 
+                        id_col=0, mz_col=1, rtime_col=2, 
+                        intensity_cols=(3,4), delimiter="\t"):
+    '''
+    Read a text feature table into a list of features.
+
+    Input
+    -----
+    feature_table: Tab delimited feature table. First line as header.
+                    Recommended col 0 for ID, col 1 for m/z, col 2 for rtime.
+    id_col: column for id. If feature ID is not given, row_number is used as ID.
+    mz_col: column for m/z.
+    rtime_col: column for retention time.
+    intensity_cols: range of columns for intensity values. E.g. (3,5) includes only col 3 and 4.
+
+    Return
+    ------
+    List of features: [{'id': '', 'mz': 0, 'rtime': 0, 
+                        intensities: [], 'representative_intensity': 0, ...}, 
+                        ...], 
+                        where representative_intensity is mean value.
+    '''
+    featureLines = open(feature_table).read().splitlines()
+    header = featureLines[0].split(delimiter)
+    num_features = len(featureLines)-1
+    # sanity check
+    print("table headers ordered: ", header[mz_col], header[rtime_col])
+    print("Read %d feature lines" %num_features)
+    L = []
+    for ii in range(1, num_features+1):
+        if featureLines[ii].strip():
+            a = featureLines[ii].split(delimiter)
+            if isinstance(id_col, int):         # feature id specified
+                iid = a[id_col]
+            else:
+                iid = 'row'+str(ii)
+            xstart, xend = intensity_cols
+            intensities = [float(x) for x in a[xstart: xend]]
+            L.append({
+                'id': iid, 'mz': float(a[mz_col]), 'rtime': float(a[rtime_col]),
+                'intensities': intensities,
+                'representative_intensity': np.mean(intensities),
+            })
+    return L
+
+
+def get_json_peaklist_from_featuretable(feature_table, outfile='',
+                        id_col=0, mz_col=1, rtime_col=2, intensity_cols=(11,12), delimiter="\t"):
+    '''
+    Read a text feature table into a list of features and export as JSON file.
+    Will update later to expand import attributes.
+
+    Parameters
+    ----------
+    feature_table: Tab delimited feature table. First line as header.
+                    Recommended col 0 for ID, col 1 for m/z, col 2 for rtime.
+    id_col: column for id. If feature ID is not given, row_number is used as ID.
+    mz_col: column for m/z.
+    rtime_col: column for retention time.
+    intensity_cols: range of columns for intensity values. E.g. (3,5) includes only col 3 and 4.
+    '''
+    features = read_features(feature_table, 
+                        id_col=id_col, mz_col=mz_col, rtime_col=rtime_col, 
+                        intensity_cols=intensity_cols, delimiter=delimiter)
+    if not outfile:
+        outfile = os.path.basename(feature_table).replace(".", "_") + '.json'
+    with open(outfile, 'w', encoding='utf-8') as f:
+            json.dump(features, f, ensure_ascii=False, indent=2)
+
+
+def read_json_peaklist(jfile):
+    return json.load( open(jfile, 'rb') )
+
+
+def text_to_mcg_features(textValue, MASS_RANGE = (50, 2500), delimiter='\t'):
+    '''
+    Parse text input to List of features, in JSON peak formats. 
+    The older versions used instances of metDataModel.Feature, which is better for complex operations.
+    Here, simplicity is preferred.
+
+    Input
+    -----
+    Text string of feature table, mummichog format, Column order is hard coded for now, 
+    1st row as header = [mz, retention_time, p_value, statistic, CompoundID_from_user].
+    Each row is a unique feature. Redundant entries should be check prior to this function.
+
+    Return
+    -------
+    peak_list: [{''id_number': 555, 'mz': 133.09702315984987, 'apex': 654, 
+    'height': 14388.0, 
+    'left_base': 648, 'right_base': 655, },
+                 ...]
+    list of excluded lines in input text
+    '''
+    list_of_features, excluded_list = [], []
+    lines = textValue.splitlines()
+    _header_fields = lines[0].rstrip().split(delimiter)
+
+    for ii in range(len( lines )-1):
+        y = lines[ii+1].split('\t')
+        CompoundID_from_user = ''
+        if len(y) > 4: CompoundID_from_user = y[4]
+        [mz, retention_time, p_value, statistic] = [float(x) for x in y[:4]]
+        
+        if MASS_RANGE[0] < mz < MASS_RANGE[1]:
+            # row # human-friendly, numbering from 1
+            F = {
+                'id_number': 'row'+str(ii+1),
+                'mz': mz,
+                'retention_time': retention_time,
+                'apex': retention_time,
+                'statistic_score': statistic,
+                'p_value': p_value,
+                'CompoundID_from_user': CompoundID_from_user,
+            }
+            list_of_features.append( F )
+        else:
+            excluded_list.append( lines[ii+1] )
+
+    return (list_of_features, excluded_list)
+
+
+def textfile_to_mcg_features(infile):
+    return text_to_mcg_features(open(infile).read())
+
---
+++ packages/mass2chem/mass2chem-0.5.0-py3-none-any.whl/mass2chem/search.py
@@ -0,0 +1,422 @@
+'''
+Two lines of search methods:
+- A centurion_tree is an indexed dictionary of peaks/features.
+- We also use DataFrame based vector operations for some cases (less often, less useful).
+
+Emperical compounds are constructed by co-eluting isotopic and adduct patterns.
+# isotopic_signatures: example as [(182, 'anchor'), (191, 'M(13C)'), (205, 'M(18O)')]
+# consider Cl as adduct, both 35Cl and 37Cl are considered
+# 34S and 37Cl are close, but should have diff ratios and are formula dependent
+
+'15N/14N' and '33S/32S' are close for high m/z features, e.g. 0.002353/470 ~ 5 pmm.
+'18O/16O' and 'M(13C),M(15N)' are close for high m/z features, e.g. 0.003855/770 ~ 5 pmm.
+
+Comprehensive combinations of isotopes and adducts, vs seeded search:
+all adducts can be combined with isotopes in theory, greatly depending on abundance in practice.
+Controlled by coelution, it's high confidence in assigning these peaks to empCpds, 
+but not 100% correct. 
+Until we have a better strategy, step-wise searches are less prone to errors.
+
+search_patterns: Use seed_empCpd_patterns. May update after seeing experimental statistics.
+                    Avoid H2O because hard to distinguish from differences in chemical formulae.
+                    The third item in tuples is to control abundance ratio, 
+                    which is linient to leave room for high carbon numbers and measurement.
+
+'''
+
+# Mass difference and Abundance of natually occuring isotopic elements.
+# (mz difference, notion, ratio low limit, ratio high limit)
+# Ratio is loose to leave room for 1) multiple atoms, e.g. 40 C atoms lead to ~ 40*1% in abundance
+# 2) measurement errors; not all peaks are in linear dynamic range
+# 3) quantification by peak height is not same as peak area
+
+isotopic_patterns = [
+    # mass diff, isotopes, (intensity ratio constraint)
+    (1.003355, '13C/12C', (0, 0.8)),      # 13C-12C, 12C~99%, 13C ~ 1%
+    (0.997035, '15N/14N', (0, 0.2)),     # 15N-14N, 14N ~ 99.64%, 15N ~ 0.36%
+    (2.004245, '18O/16O', (0, 0.2)),      # 18O-16O, 16O ~ 99.76, 16O ~ 0.2%
+    (1.995796, '34S/32S', (0, 0.4)),      # 32S (95.02%), 33S (0.75%), 34S (4.21%)
+    (0.999388, '33S/32S', (0, 0.1)),
+    # double isotopes
+    (2.00039, 'M(13C),M(15N)', (0, 0.2)),
+    (2.999151, 'M(13C),M(34S)', (0, 0.4)),
+    # double charged
+    (0.5017, '13C/12C, double charged', (0, 0.8)),
+    (0.4985, '15N/14N, double charged', (0, 0.2)),
+]
+
+common_adducts = {
+    # mass diff, modification
+    # not using (intensity ratio constraint), but it can be documented or learned
+    'pos': [
+        (1.0078, 'H'),
+        (21.9820, 'Na/H'), # Na replacing H
+        (10.991, 'Na/H, double charged'),
+        (18.0106, '+H2O'), 
+        (18.033823, '+NH4'),
+        (37.9559, '39K/H'),
+        (39.9540, '41K/H'),
+        (41.026549, 'Acetonitrile'),
+    ],
+    'neg': [
+        (1.0078, 'H'),
+        (22.9893, 'Na'),
+        (20.97474706646, '+Na-2H'),
+        (18.0106, 'H2O'), 
+        (34.9689, '35Cl'),
+        (36.9659, '37Cl'),
+        (40.01926853323, '+ACN-H'),
+        (44.998201, 'COOH'),
+        (59.013295, 'CH3COO'),
+    ],
+}
+
+extended_adducts = {
+    'pos': [],
+    'neg': [],
+}
+
+# this does not include combinatorial values of isotopes and adducts
+# Mostly used as seeds; better to do inclusive search after matched to formulae
+seed_empCpd_patterns = {
+    'pos': [(1.003355, '13C/12C', (0, 0.8)), (1.0078, 'H'), (21.9820, 'Na/H'), ],
+    'neg': [(1.003355, '13C/12C', (0, 0.8)), (1.0078, 'H'), (20.97474706646, '+Na-2H'), (34.9689, '35Cl')],
+}
+
+
+#
+# -----------------------------------------------------------------------------
+#
+
+def build_centurion_tree(list_peaks):
+    '''
+    list_peaks: [{'parent_masstrace_id': 1670, 'mz': 133.09702315984987, 'apex': 654, 'height': 14388.0, 
+                    'left_base': 648, 'right_base': 655, 'id_number': 555}, ...]
+    Return a dictionary, indexing mzList by 100*mz bins.
+    Because most high-resolution mass spectrometers measure well under 0.01 amu, 
+    one only needs to search the corresponding 0.01 bin and two adjacent bins (to capture bordering values).
+    list_mass_tracks has similar format as list_peaks.
+    '''
+    d = {}
+    for p in list_peaks:
+        cent = int(100 * p['mz'])
+        if cent in d:
+            d[cent].append(p)
+        else:
+            d[cent] = [p]
+    return d
+
+def build_peak_id_dict(list_peaks):
+    d = {}
+    for p in list_peaks:
+        d[p['id_number']] = p
+    return d
+
+def build_centurion_tree_mzlist(mzList):
+    '''
+    Return a dictionary, indexing mzList by 100*mz bins.
+    Because most high-resolution mass spectrometers measure well under 0.01 amu, 
+    one only needs to search the corresponding 0.01 bin and two adjacent bins (to capture bordering values).
+    '''
+    d = {}
+    for ii in range(len(mzList)):
+        cent = int(100*mzList[ii])
+        if cent in d:
+            d[cent].append((mzList[ii], ii))
+        else:
+            d[cent] = [(mzList[ii], ii)]
+    return d
+
+def find_all_matches_centurion_indexed_list(query_mz, mz_centurion_tree, limit_ppm=5):
+    '''
+    Return matched peaks in mz_centurion_tree by m/z diff within limit_ppm.
+    '''
+    q = int(query_mz * 100)
+    mz_tol = query_mz * limit_ppm * 0.000001
+    results = []
+    for ii in (q-1, q, q+1):
+        L = mz_centurion_tree.get(ii, [])
+        for peak in L:
+            if abs(peak['mz']-query_mz) < mz_tol:
+                results.append(peak)
+                
+    return results
+
+def find_best_match_centurion_indexed_list(query_mz, mz_centurion_tree, limit_ppm=2):
+    '''
+    Return matched indices in mz_centurion_tree (based on peak list).
+    '''
+    q = int(query_mz * 100)
+    mz_tol = query_mz * limit_ppm * 0.000001
+    result = (None, 999)
+    for ii in (q-1, q, q+1):
+        L = mz_centurion_tree.get(ii, [])
+        for peak in L:
+            _d = abs(peak['mz']-query_mz)
+            if _d < min(result[1], mz_tol):     # enforce mz_tol here
+                result = (peak, _d)
+                
+    return result[0]
+
+def is_coeluted_by_overlap(P1, P2, rt_tolerance=20):
+    '''Check overalp in elution peaks. Used for annotation with asari, when peak parameters are detailed.
+    Coelution is defined by overlap more than half of the smaller peak, or apexes within rt_tolerance (
+    so this is computational term, not LC term). 
+    If not enough parameters are given for peaks, fallback to is_coeluted_by_distance(P1, P2, rt_tolerance=10).
+    
+    Example peak format: {'parent_masstrace_id': 1670, 'mz': 133.09702315984987, 'apex': 654, 'height': 14388.0,
+    'left_base': 648, 'right_base': 655, 'id_number': 555}
+
+    rt_tolerance: tolerance of retention time is used when peak boundaries are not given.
+                Can be scan numbers or seconds.
+                
+    return: True or False
+    '''
+    _coeluted = False
+    try:
+        len1, len2 = P1['right_base'] - P1['left_base'], P2['right_base'] - P2['left_base']
+        # overlap is the max L and min R
+        overlap = min(P1['right_base'], P2['right_base']) - max(P1['left_base'], P2['left_base'])
+        if overlap > 0.5 * min(len1, len2):
+            _coeluted = True
+        return _coeluted
+    except:
+        print("KeyError, fallback to function is_coeluted_by_distance.")
+        return is_coeluted_by_distance(P1, P2, rt_tolerance)
+
+def is_coeluted_by_distance(P1, P2, rt_tolerance=10):
+    _coeluted = False
+    if abs(P1['apex']-P2['apex']) <= rt_tolerance:
+        _coeluted = True
+    return _coeluted
+
+def get_seed_empCpd_signatures(list_peaks, 
+                    mztree, 
+                    search_patterns = seed_empCpd_patterns['pos'],
+                    mz_tolerance_ppm=5, 
+                    isotope_rt_tolerance=5, coelution_rt_tolerance=10, 
+                    is_coeluted = is_coeluted_by_overlap,
+                    check_isotope_ratio = True,
+                    ):
+    '''
+    To find a core group of ions that belong to an empirical compound (emp_cpd, or empCpd, ecd).
+    This should be a combination of 12C/13C isotopes and common adducts;
+    an empCpd can be constructed even if the 13C ion is missing.
+    Other isotopes and adducts can be searched after establishing the seed empCpds.
+
+    This is currently a heuristic approach to generate empCpds without too many errors.
+    One may research more to polish an optimal method of assigning ions to empCpds, 
+    where chemical formulae and learned patterns can be incorporated.
+
+    Input
+    =====
+    list_peaks: [{'parent_masstrace_id': 1670, 'mz': 133.09702315984987, 'apex': 654, 'height': 14388.0, 
+                    'left_base': 648, 'right_base': 655, 'id_number': 555}, ...]
+    mztree: indexed list_peaks
+    mz_tolerance_ppm: ppm tolerance in examining m/z patterns.
+    seed_search_patterns: initial ions for constructing an empCpd. 
+            This is very conservative and requires stricter isotope_rt_tolerance, because other ions can be searched later. 
+            Can use self.seed_search_patterns, e.g. for pos ions:
+            [(1.003355, '13C/12C', (0, 0.8)), (1.0078, 'H'), (21.9820, 'Na/H')]
+    isotope_rt_tolerance: tolerance threshold for deviation in retetion time, stricter version for seed ions (isotopes). 
+            Default unit is scan number, but can be arbitrary.
+    coelution_rt_tolerance: tolerance threshold for deviation in retetion time of adducts (other than seeds).
+    is_coeluted: coelution function
+
+    Return
+    ======
+    list of lists of peak numbers that match search_patterns patterns, e.g.
+    [ [(195, 'anchor'), (206, '13C/12C')],  ...]
+    '''
+    signatures = []
+    for P1 in list_peaks:
+        matched = [ (P1['id_number'], 'anchor'), ]          # Nature is nice to have lowest mass for the most abundant 
+        for _pair in search_patterns:
+            (mass_difference, relation) = _pair[:2]
+            tmp = find_all_matches_centurion_indexed_list(P1['mz'] + mass_difference, mztree, mz_tolerance_ppm)
+            for P2 in tmp:
+                if abs(P1['apex']-P2['apex']) <= isotope_rt_tolerance and is_coeluted(P1, P2, coelution_rt_tolerance):
+                    if check_isotope_ratio and len(_pair) > 2:  # checking abundance ratio
+                        (abundance_ratio_min, abundance_ratio_max) = _pair[2]
+                        if abundance_ratio_min*P1['height'] < P2['height'] < abundance_ratio_max*P1['height']:
+                            matched.append( (P2['id_number'], relation) )
+                    else:
+                        matched.append( (P2['id_number'], relation) )
+ 
+        if len(matched) > 1:
+            # if len(matched) > 2: matched = merge_confused_peak_matches(matched)
+            signatures.append(matched)
+
+    return signatures
+
+
+def extend_seed_empCpd_signature(seed_signatures, peak_dict, mztree, ext_search_patterns, 
+                    mz_tolerance_ppm=5, 
+                    coelution_rt_tolerance=10, 
+                    is_coeluted = is_coeluted_by_overlap,
+                    ):
+    '''
+    Extend a single signature by ext_search_patterns, including combinatorial values of isotopes and adducts.
+
+    Input
+    =====
+    mztree: indexed list_peaks
+    seed_signatures: e.g. [(182, 'anchor'), (191, '13C/12C'), (205, '18O/16O')]
+    search_patterns: e.g. [(10.991, 'Na/H, double charged'), (18.0106, '+H2O'), (18.033823, '+NH4'),]
+    example peak: {'parent_masstrace_id': 1670, 'mz': 133.09702315984987, 'apex': 654, 'height': 14388.0, 
+                    'left_base': 648, 'right_base': 655, 'id_number': 555}
+
+    Return
+    ======
+    One epd (empirical compound) based on seed peaks and ext_search_patterns, as a list of [(peak_id, relation), ...]
+    '''
+    matched = []
+    for seed in seed_signatures:
+        peak, relation = peak_dict[seed[0]], seed[1]
+        for adduct in ext_search_patterns:
+            tmp = find_all_matches_centurion_indexed_list( peak['mz'] + adduct[0], mztree, mz_tolerance_ppm )
+            for P2 in tmp:
+                if is_coeluted(peak, P2, coelution_rt_tolerance):
+                    matched.append( (P2['id_number'], relation +','+ adduct[1]) )
+
+    return matched
+
+
+def find_isotopic_signatures(list_peaks, mztree, isotopic_patterns, mz_tolerance_ppm=5, rt_tolerance_scans=5):
+    '''
+    See find_isotopic_pairs. This extends to all related isotopic signatures.
+    Allows ambiguous matches, which will be dealt with in empCpd statistics.
+    But the peaks need to be unique. E.g. 
+    '15N/14N' and '33S/32S', '18O/16O' and 'M(13C),M(15N)' are close for high m/z features.
+    Matches like ('F5486', '15N/14N'), ('F5486', '33S/32S') need to merge on the peak/feature.
+
+    Input
+    =====
+    isotopic_patterns = [(1.003355, '13C/12C', (0, 0.8)), ...], the third item is optional limits of abundance ratio.
+
+    Return
+    ======
+    list of lists of peak numbers that match isotopic patterns, [ [], ... ]
+
+    Example
+    =======
+    [ [(195, 'anchor'), (206, '13C/12C')], 
+      [(182, 'anchor'), (191, '13C/12C'), (205, '18O/16O')],
+      [(295, 'anchor'), (335, '13C/12C'), (368, 'M(13C),M(34S)')], ...]
+    '''
+    signatures = []
+    for P1 in list_peaks:
+        matched = [ (P1['id_number'], 'anchor'), ]          # Nature is nice to have lowest mass for the most abundant 
+        for isotopic_pair in isotopic_patterns:
+            (mass_difference, relation) = isotopic_pair[:2]
+            tmp = find_all_matches_centurion_indexed_list(P1['mz'] + mass_difference, mztree, mz_tolerance_ppm)
+            for P2 in tmp:
+                if abs(P1['apex']-P2['apex']) <= rt_tolerance_scans and is_coeluted_by_overlap(P1, P2):
+                    if len(isotopic_pair) == 2:             # not checking abundance ratio
+                        matched.append( (P2['id_number'], relation) )
+                    else:
+                        (abundance_ratio_min, abundance_ratio_max) = isotopic_pair[2]
+                        # checking abundance ratio
+                        if abundance_ratio_min*P1['height'] < P2['height'] < abundance_ratio_max*P1['height']:
+                            matched.append( (P2['id_number'], relation) )
+        if len(matched) > 1:
+            # if len(matched) > 2: matched = merge_confused_peak_matches(matched)
+            signatures.append(matched)
+
+    return signatures
+
+
+def find_adduct_signatures(list_peaks, mztree, adduct_patterns, mz_tolerance_ppm=5):
+    '''
+    Search adduct mass_diff in ceelution peaks, de novo. 
+    Not requiring matched apex as in isotopic pairs, nor abundance ratio restriction.
+    '''
+    signatures = []
+    for P1 in list_peaks:
+        matched = [ (P1['id_number'], 'anchor'), ]
+        for adduct in adduct_patterns:
+            tmp = find_all_matches_centurion_indexed_list( P1['mz'] + adduct[0], mztree, mz_tolerance_ppm )
+            for P2 in tmp:
+                if is_coeluted_by_overlap(P1, P2):
+                    matched.append( (P2['id_number'], adduct[1]) )
+
+        if len(matched) > 1:
+            signatures.append(matched)
+
+    return signatures
+
+
+def merge_confused_peak_matches(L):
+    '''
+    merge if same peak is matched to more than one isotopes or adducts.
+    L example: [(295, 'anchor'), (335, '13C/12C'), (368, 'M(13C),M(34S)')]
+    '''
+    tmp, _d = [L[0]], {}
+    for peak in L[1:]:
+        if peak[0] in _d:
+            _d[peak[0]] += ';' + peak[1]
+        else:
+            _d[peak[0]] = peak[1]
+    for k,v in _d.items():
+        tmp.append((k,v))
+    return tmp
+
+
+def find_mzdiff_pairs_from_masstracks(list_mass_tracks, list_mz_diff=[1.003355, 21.9820], mz_tolerance_ppm=5):
+    '''
+    Find all pairs in list_mass_tracks that match a pattern in list_mz_diff, and return their id_numbers as pairs.
+    This function does not use coeluction (rtime) rules. 
+
+    Input
+    =====
+    list_mass_tracks: [{ 'id_number': ii,  'mz': xic[0], 'rt_scan_numbers': xic[1],  'intensity': xic[2],  }, ...]
+    list_mz_diff: defaul 1.003355, 21.9820 are 13C/12C, Na/H. Dependent on ionization mode.
+    
+    Return
+    ======
+    list of pairs of mass tracks numbers.
+    '''
+    pairs = []
+    # list_mass_tracks has similar format as list_peaks.
+    mztree = build_centurion_tree(list_mass_tracks)
+    for mzdiff in list_mz_diff:
+        for P1 in list_mass_tracks:
+            P2 = find_best_match_centurion_indexed_list(P1['mz'] + mzdiff, mztree, mz_tolerance_ppm)
+            if P2:
+                pairs.append((P1['id_number'], P2['id_number']))
+
+    return pairs
+
+
+#
+# -----------------------------------------------------------------------------
+#
+
+def search_formula_mass_dataframe(query_mz, DFDB, limit_ppm=10):
+    '''
+    return best match formula_mass in DFDB if under ppm limit.
+    DFDB is using a Pandas DataFrame to house reference database.
+    ppm is signed to capture the direction of mass shift.
+
+    Not using idxmin,   #ii = DFDB.tmp.idxmin()
+    # in pd.DF, index not necessarily integer; can be sequence if more than one match, but they are trying to fix in pandas dev version
+    '''
+    DFDB['tmp'] = abs(DFDB.mz - query_mz)
+    ii = DFDB.tmp.values.argmin()
+    ppm = 1000000 * (query_mz - DFDB.iloc[ii].mz)/query_mz
+    if  abs(ppm) < limit_ppm:
+        return (DFDB.iloc[ii].name, ppm)            # name is formula_mass
+    else:
+        return None
+
+def is_matched_lcms_peaks(peak1, peak2, mz_tolerance_ppm=5, rt_tolerance=10):
+    '''
+    Check if two peaks are matched by mz and rtime.
+    peak format: {'id': 'F1', 'mz': 60.0808, 'rtime': 117.7, ...}
+    '''
+    matched = False
+    mz_tol = peak1['mz'] * mz_tolerance_ppm * 0.000001
+    if abs(peak2['mz']-peak1['mz']) < mz_tol and abs(peak2['rtime']-peak1['rtime']) < rt_tolerance:
+        matched = True
+    return matched
+
