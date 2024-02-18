55ea288dce3654fd9da3da10414c88be2804db18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 06:05:12 +0000

    Add package development-tool/0.0.2/development-tool-0.0.2.tar.gz

---
+++ packages/development-tool/development-tool-0.0.2.tar.gz/development-tool-0.0.2/orangecontrib/development_tool/widgets/__init__.py
@@ -0,0 +1,17 @@
+"""
+Widgets from Development workflow category
+
+"""
+ID = "Orange.widgets.dev_workflow"
+
+NAME = "dev_worklow"
+
+# Category icon show in the menu
+ICON = "icons/tool.png"
+
+BACKGROUND = "light-blue"
+
+DESCRIPTION = "Development tools. Managing data, input, output, and calling workflows from other one."
+
+
+PRIORITY = 6
