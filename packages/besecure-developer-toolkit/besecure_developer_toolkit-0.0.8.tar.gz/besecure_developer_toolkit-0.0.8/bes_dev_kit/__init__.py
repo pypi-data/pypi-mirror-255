a7e90f8898e497073e68d3bc71e959d8f64abf4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 20:04:46 +0000

    Add package besecure-developer-toolkit/0.0.8/besecure_developer_toolkit-0.0.8.tar.gz

---
+++ packages/besecure-developer-toolkit/besecure_developer_toolkit-0.0.8.tar.gz/besecure_developer_toolkit-0.0.8/bes_dev_kit/__init__.py
@@ -0,0 +1,19 @@
+"""Top-level package for BeS-dev-kit."""
+# src/__init__.py
+
+__app_name__ = "bes-dev-kit"
+__version__ = "0.0.8"
+
+(
+    SUCCESS,
+    DIR_ERROR,
+    FILE_ERROR,
+    JSON_ERROR,
+    ID_ERROR,
+) = range(5)
+
+ERRORS = {
+    DIR_ERROR: "config directory error",
+    FILE_ERROR: "config file error",
+    ID_ERROR: "to-do id error",
+}
