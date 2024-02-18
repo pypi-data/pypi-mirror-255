0830531002b300363696b1228e9ac99d3658fb75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 14:05:36 +0000

    Add package firstPackToPypI/0.1/firstPackToPypI-0.1.tar.gz

---
+++ packages/firstPackToPypI/firstPackToPypI-0.1.tar.gz/firstPackToPypI-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='firstPackToPypI',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['firstPackToPypI'],
+      author = 'Anas Abuelhaag',
+      author_email = 'anaselhaag@gmail.com',
+      zip_safe=False)
