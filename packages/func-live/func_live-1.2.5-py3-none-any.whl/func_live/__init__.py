36657fbcb508110aaf09ff216ab65bc2c0a42ee2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 08:05:29 +0000

    Add package func-live/1.2.5/func_live-1.2.5-py3-none-any.whl

---
+++ packages/func-live/func_live-1.2.5-py3-none-any.whl/func_live/__init__.py
@@ -0,0 +1,56 @@
+import asyncio
+import aiohttp
+from .config import API_URL
+from .recursively_upload_filepaths import recursively_upload_filepaths
+
+class FuncLive:
+    def __init__(self):
+        self.session = None 
+        self.functions_fetched = False
+
+    async def _ensure_initialized(self):
+        if not self.session:
+            self.session = aiohttp.ClientSession()
+        if not self.functions_fetched:
+            await self._fetch_functions()
+            self.functions_fetched = True
+
+    async def _fetch_functions(self):
+        await self._ensure_initialized()  
+        async with self.session.get(API_URL + "/functions") as response:
+            functions = (await response.json()).get('functions', [])
+            for func in functions:
+                func_name = func['name']
+                setattr(self, func_name, self._sync_wrapper(self._create_function(func_name)))
+
+    def _sync_wrapper(self, async_func):
+        def sync_func(input_data):
+            loop = asyncio.get_event_loop()
+            if loop.is_running():
+                raise Exception("Can't run synchronous wrapper when event loop is already running")
+            else:
+                return loop.run_until_complete(async_func(input_data))
+        return sync_func
+
+    async def _create_function(self, func_name):
+        await self._ensure_initialized()
+        async def func(input_data):
+            try:
+                modified_input = await recursively_upload_filepaths(input_data, self.session)
+                async with self.session.post(API_URL + "/functions/" + func_name,
+                                             headers={
+                                                 'Authorization': f'Bearer {os.getenv("FUNC_TOKEN")}',
+                                                 'Accept': 'application/json',
+                                                 'Content-Type': 'application/json'
+                                             },
+                                             json={'input': modified_input}) as response:
+                    return await response.json().get('output')
+            except aiohttp.ClientError as e:
+                raise Exception(f"There was an error: {str(e)}")
+        return func
+
+    async def close(self):
+        if self.session:
+            await self.session.close()
+
+func_live = FuncLive()
