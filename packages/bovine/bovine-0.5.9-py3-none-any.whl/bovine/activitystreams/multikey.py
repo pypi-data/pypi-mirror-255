309ef699dbb4894669466ca10ccde0f411f338b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 02:11:29 +0000

    Add package bovine/0.5.9/bovine-0.5.9-py3-none-any.whl

---
+++ packages/bovine/bovine-0.5.9-py3-none-any.whl/bovine/activitystreams/multikey.py
@@ -0,0 +1,31 @@
+# SPDX-FileCopyrightText: 2023 Helge
+# SPDX-FileCopyrightText: 2024 helge
+#
+# SPDX-License-Identifier: MIT
+
+from dataclasses import dataclass
+
+
+@dataclass
+class Multikey:
+    """Represents a Multikey"""
+
+    id: str
+    controller: str
+    multibase: str
+    type: str = "Multikey"
+
+    def build(self):
+        return {
+            "@context": "https://w3id.org/security/multikey/v1",
+            "id": self.id,
+            "type": self.type,
+            "controller": self.controller,
+            "publicKeyMultibase": self.multibase,
+        }
+
+    @staticmethod
+    def from_multibase_and_controller(controller, multibase):
+        return Multikey(
+            id=f"{controller}#{multibase}", controller=controller, multibase=multibase
+        )
