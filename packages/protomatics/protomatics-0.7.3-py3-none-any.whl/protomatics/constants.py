1aff66ee0029d649dfe14acc594caac051bd2cf3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 07:05:45 +0000

    Add package protomatics/0.7.3/protomatics-0.7.3-py3-none-any.whl

---
+++ packages/protomatics/protomatics-0.7.3-py3-none-any.whl/protomatics/constants.py
@@ -0,0 +1,10 @@
+jansky = 1e-26  # Watt /sq M / Hz
+G = 6.67e-11  # SI
+G_cgs = 6.67e-8  # cgs
+au = 149597870700.0  # m
+au_cm = 14959787070000.0  # cm
+au_pc = 206265.0  # pc
+Msol_kg = 1.989e30  # kg
+Msol_g = 1.989e33  # g
+c = 299792458.0  # m/s
+c_kms = c * 1e-3
