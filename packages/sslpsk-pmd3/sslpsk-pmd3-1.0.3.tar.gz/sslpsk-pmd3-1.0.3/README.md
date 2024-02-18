5a8b5ebfd60ea8e8d6412cd78f80a351b2b54493
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 21:05:43 +0000

    Add package sslpsk-pmd3/1.0.3/sslpsk-pmd3-1.0.3.tar.gz

---
+++ packages/sslpsk-pmd3/sslpsk-pmd3-1.0.3.tar.gz/sslpsk-pmd3-1.0.3/README.md
@@ -0,0 +1,5 @@
+# sslpsk3
+
+[![PyPI version](https://badge.fury.io/py/sslpsk-pmd3.svg)](https://badge.fury.io/py/sslpsk-pmd3)
+
+Fork of sslpsk with support for latest python versions
---
+++ packages/sslpsk-pmd3/sslpsk-pmd3-1.0.3.tar.gz/sslpsk-pmd3-1.0.3/setup.py
@@ -0,0 +1,50 @@
+# Copyright 2017 David R. Bild
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License
+
+import sys
+
+from setuptools import Extension, setup
+
+if sys.platform == 'win32':
+    libraries = ['libcrypto', 'libssl']
+else:
+    libraries = ['crypto', 'ssl']
+
+extension_openssl1 = Extension(
+    "sslpsk_pmd3._sslpsk_openssl1",
+    sources=["sslpsk_pmd3/_sslpsk.c"],
+    libraries=libraries,
+    include_dirs=["openssl1/include/"],
+    library_dirs=["openssl1/lib/VC/"],
+    define_macros=[
+        ("OPENSSL_VER", "openssl1"),
+        ("INIT_SSLPSK_OPENSSL", "init_sslpsk_openssl1"),
+        ("PYINIT_SSLPSK_OPENSSL", "PyInit__sslpsk_openssl1"),
+    ],
+)
+
+extension_openssl3 = Extension(
+    "sslpsk_pmd3._sslpsk_openssl3",
+    sources=["sslpsk_pmd3/_sslpsk.c"],
+    libraries=libraries,
+    include_dirs=["openssl3/include/"],
+    library_dirs=["openssl3/lib/VC/"],
+    define_macros=[
+        ("OPENSSL_VER", "openssl3"),
+        ("INIT_SSLPSK_OPENSSL", "init_sslpsk_openssl3"),
+        ("PYINIT_SSLPSK_OPENSSL", "PyInit__sslpsk_openssl3"),
+    ],
+)
+
+setup(name='pytun-pmd3', ext_modules=[extension_openssl1, extension_openssl3])
