0225ec080f1a2067c00a7c19ac61d42bcedefd56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 01:26:36 +0000

    Add package guardshield/1.1.5/guardshield-1.1.5.tar.gz

---
+++ packages/guardshield/guardshield-1.1.5.tar.gz/guardshield-1.1.5/guardshield/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+__version__ = '1.0.0'
+
+from guardshield.main import Security
+import guardshield.untils.cheatengine
---
+++ packages/guardshield/guardshield-1.1.5.tar.gz/guardshield-1.1.5/setup.py
@@ -0,0 +1,20 @@
+import setuptools, os
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setuptools.setup(
+    name="guardshield",
+    version="1.1.5",
+    author="Oxyn",
+    author_email="oxyn.dev@gmail.com",
+    description="Security lib",
+    keywords = "python anti debugger security exe",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    url='https://github.com/OxynDev/guardshield',
+    zip_safe=False,
+    license='MIT',
+    long_description=read('README.md'),
+    long_description_content_type='text/markdown'
+)
