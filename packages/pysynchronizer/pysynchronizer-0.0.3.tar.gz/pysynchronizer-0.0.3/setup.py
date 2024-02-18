726fe5a23d5b030a5b978755579c6eacdd689d66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 02:10:53 +0000

    Add package pysynchronizer/0.0.3/pysynchronizer-0.0.3.tar.gz

---
+++ packages/pysynchronizer/pysynchronizer-0.0.3.tar.gz/pysynchronizer-0.0.3/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pyscynchronize",
+    version="0.0.3",
+    packages=find_packages(),
+    python_requires=">=3.7",
+    install_requires=[
+
+    ],
+    extras_require={
+
+    }
+)
