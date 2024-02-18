26d0a35404ce6a3666ed741d7e4aa19e365cbec8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 03:04:18 +0000

    Add package django-content-settings/0.8/django_content_settings-0.8.tar.gz

---
+++ packages/django-content-settings/django_content_settings-0.8.tar.gz/django_content_settings-0.8/content_settings/types/__init__.py
@@ -0,0 +1,16 @@
+PREVIEW_HTML = "html"
+PREVIEW_TEXT = "text"
+PREVIEW_PYTHON = "python"
+PREVIEW_NONE = "none"
+
+PREVIEW_ALL = [
+    value for name, value in globals().items() if name.startswith("PREVIEW_")
+]
+
+
+class required:
+    pass
+
+
+class optional:
+    pass
