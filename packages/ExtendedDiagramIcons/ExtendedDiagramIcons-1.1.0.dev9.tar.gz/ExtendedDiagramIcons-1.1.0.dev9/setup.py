d399ce56eeaf236f23a585b9f4c3904160caefdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 02:12:27 +0000

    Add package ExtendedDiagramIcons/1.1.0.dev9/ExtendedDiagramIcons-1.1.0.dev9.tar.gz

---
+++ packages/ExtendedDiagramIcons/ExtendedDiagramIcons-1.1.0.dev9.tar.gz/ExtendedDiagramIcons-1.1.0.dev9/setup.py
@@ -0,0 +1,35 @@
+import os
+from setuptools import setup, find_packages
+
+def get_version():
+    basedir = os.path.dirname(__file__)
+    with open(os.path.join(basedir, "__init__.py")) as f:
+        locals = {}
+        exec(f.read(), locals)
+        return locals["__version__"]
+
+setup(
+    name="ExtendedDiagramIcons",
+    version=get_version(),
+    author="Joshua Duma",
+    author_email="joshua.duma@trader.ca",
+    description="This is intended to be used in a project that uses the diagrams (diagrams as code) python package as an extention of the icons.",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="",
+    include_package_data=True,
+    package_data={
+        'ExtendedDiagramIcons': ['resources/**/*.png'],  # Include only .png files in some_folder
+    },
+    repository="https://github.com/JoshuaDuma/ExtendedDiagramIcons",
+    packages=find_packages(),
+    install_requires=[
+        "diagrams>=0.23.4"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
