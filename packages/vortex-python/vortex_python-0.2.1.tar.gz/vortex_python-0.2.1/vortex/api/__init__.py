ddee16f84021ec506d89eab5ac1668ca16a70f0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 23:07:02 +0000

    Add package vortex-python/0.2.1/vortex_python-0.2.1.tar.gz

---
+++ packages/vortex-python/vortex_python-0.2.1.tar.gz/vortex_python-0.2.1/vortex/api/__init__.py
@@ -0,0 +1,27 @@
+from dotenv import load_dotenv
+from fastapi import Depends, FastAPI, Form, Request
+from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.orm import Session
+
+from vortex.api.data_models import get_db
+from vortex.io.twilio import handle_wapp_message
+
+load_dotenv()
+
+app = FastAPI()
+
+
+@app.get("/api/check")
+def hello_world():
+    return {"message": "Vortex is Running!"}
+
+
+@app.get("/")
+async def index():
+    return {"msg": "Vortex is Running!"}
+
+
+@app.post("/message")
+async def reply(request: Request, Body: str = Form(), db: Session = Depends(get_db)):
+    await handle_wapp_message(request, Body, db)
+    return ""
