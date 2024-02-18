50b5e7d351cf49dd8018c5d5680b7e02f97788a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 03:04:15 +0000

    Add package open-space-toolkit-astrodynamics/6.1.0/open_space_toolkit_astrodynamics-6.1.0-py39-none-any.whl

---
+++ packages/open-space-toolkit-astrodynamics/open_space_toolkit_astrodynamics-6.1.0-py39-none-any.whl/ostk/astrodynamics/test/test_trajectory.py
@@ -0,0 +1,40 @@
+# Apache License 2.0
+
+import ostk.mathematics as mathematics
+
+import ostk.physics as physics
+
+import ostk.astrodynamics as astrodynamics
+
+RealInterval = mathematics.objects.RealInterval
+Quaternion = mathematics.geometry.d3.transformation.rotation.Quaternion
+Length = physics.units.Length
+Angle = physics.units.Angle
+DateTime = physics.time.DateTime
+Scale = physics.time.Scale
+Duration = physics.time.Duration
+Instant = physics.time.Instant
+Transform = physics.coordinate.Transform
+Frame = physics.coordinate.Frame
+Axes = physics.coordinate.Axes
+DynamicProvider = physics.coordinate.frame.providers.Dynamic
+Environment = physics.Environment
+Earth = physics.environment.objects.celestial_bodies.Earth
+Trajectory = astrodynamics.Trajectory
+Profile = astrodynamics.flight.Profile
+State = astrodynamics.flight.profile.State
+SatelliteSystem = astrodynamics.flight.system.SatelliteSystem
+Orbit = astrodynamics.trajectory.Orbit
+Pass = astrodynamics.trajectory.orbit.Pass
+Kepler = astrodynamics.trajectory.orbit.models.Kepler
+COE = astrodynamics.trajectory.orbit.models.kepler.COE
+SGP4 = astrodynamics.trajectory.orbit.models.sgp4
+Access = astrodynamics.Access
+
+
+def test_trajectory_undefined():
+    trajectory: Trajectory = Trajectory.undefined()
+
+    assert trajectory is not None
+    assert isinstance(trajectory, Trajectory)
+    assert trajectory.is_defined() is False
