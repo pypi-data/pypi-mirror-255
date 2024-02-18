11c53ad08522499854f6fa63c8531a9c877e7e5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 19:04:49 +0000

    Add package teste-sql70/0.0.1/teste_sql70-0.0.1.tar.gz

---
+++ packages/teste-sql70/teste_sql70-0.0.1.tar.gz/teste_sql70-0.0.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Mi primer paquete de Python'
+LONG_DESCRIPTION = 'Mi primer paquete de Python con una descripción ligeramente más larga'
+
+# Configurando
+setup(
+    # el nombre debe coincidir con el nombre de la carpeta
+    name="teste_sql70",
+    version=VERSION,
+    author="Sql 70",
+    author_email="<sql70@outlook.es>",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],  # añade cualquier paquete adicional que debe ser
+    # instalado junto con tu paquete.
+
+    keywords=['python', 'prueba', 'paquete', 'teste'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Education",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
