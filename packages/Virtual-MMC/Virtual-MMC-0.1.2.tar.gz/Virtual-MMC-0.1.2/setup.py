d0aaae22c5462887880fd8910fd19813314a6a38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 23:06:32 +0000

    Add package Virtual-MMC/0.1.2/Virtual-MMC-0.1.2.tar.gz

---
+++ packages/Virtual-MMC/Virtual-MMC-0.1.2.tar.gz/Virtual-MMC-0.1.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Virtual-MMC',
+    version='0.1.2',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'matplotlib',
+    ],
+    author='James_Broster',
+    author_email='james.broster@st-annes.ox.ac.uk',
+    description='A brief description of your package',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/James-Broster/VMMC',
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
