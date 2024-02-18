3adae969fe4e136beb0c5fd301ec93ab2d781140
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 22:05:46 +0000

    Add package PySDM/2.41/PySDM-2.41-py3-none-any.whl

---
+++ packages/PySDM/PySDM-2.41-py3-none-any.whl/PySDM/physics/__init__.py
@@ -0,0 +1,31 @@
+"""
+Physical `PySDM.physics.constants` and simple formulae (essentially one-liners)
+  that can be automatically either njit-ted or translated to C (in contrast to
+  more complex code that resides in backends)
+"""
+
+from . import (
+    condensation_coordinate,
+    constants_defaults,
+    diffusion_kinetics,
+    diffusion_thermics,
+    drop_growth,
+    fragmentation_function,
+    freezing_temperature_spectrum,
+    heterogeneous_ice_nucleation_rate,
+    hydrostatics,
+    hygroscopicity,
+    impl,
+    isotope_equilibrium_fractionation_factors,
+    isotope_meteoric_water_line_excess,
+    isotope_ratio_evolution,
+    latent_heat,
+    particle_advection,
+    particle_shape_and_density,
+    saturation_vapour_pressure,
+    state_variable_triplet,
+    surface_tension,
+    trivia,
+    ventilation,
+)
+from .constants import convert_to, in_unit, si
