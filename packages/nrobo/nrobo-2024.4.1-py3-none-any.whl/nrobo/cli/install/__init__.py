1d6e2813566b03b6c0889c780d47cbcbfae02fb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 14:05:26 +0000

    Add package nrobo/2024.4.1/nrobo-2024.4.1-py3-none-any.whl

---
+++ packages/nrobo/nrobo-2024.4.1-py3-none-any.whl/nrobo/cli/install/__init__.py
@@ -0,0 +1,81 @@
+import os
+import shutil
+import sys
+
+from nrobo.cli import *
+from nrobo.cli.cli_constansts import *
+from nrobo.cli.nglobals import *
+from nrobo.cli.cli_args import *
+
+from nrobo.util.process import *
+from nrobo.util.python import __PIP__
+
+__REQUIREMENTS__ = "requirements.txt"
+
+global __PIP__
+
+
+def copy_dir(src, dest):
+    try:
+        shutil.copytree(src, dest)
+    except FileExistsError as e:
+        print(e)
+
+
+def copy_file(src, dest):
+    try:
+        shutil.copyfile(src, dest)
+    except FileExistsError as e:
+        print(e)
+
+
+def install_nrobo(requirements_file: Optional[str] = None):
+    """
+    Install nrobo
+
+    :return:
+    """
+
+    set_environment()
+
+    from nrobo.cli import STYLE
+    from nrobo.cli.tools import console
+
+    print(f"Welcome to NROBO install")
+    print(f"Installing requirements")
+    if requirements_file is None:
+        requirements_file = f"{os.environ[EnvKeys.DirNrobo]}{os.sep}cli{os.sep}install{os.sep}requirements.txt"
+
+        return_code = terminal(command=[__PIP__, nCLI.INSTALL, '-r', requirements_file],
+                               stdout=subprocess.DEVNULL,
+                               stderr=subprocess.STDOUT)
+
+        if return_code:
+            """non-zero return code means a success"""
+            print(f"Requirements are installed successfully.")
+        else:
+            print(f"Requirements are not installed successfully!")
+
+    if os.environ[EnvKeys.Environment] == Environment.PRODUCTION:
+        """Install framework on Production environment"""
+
+        print(f"Installing framework")
+        # create framework folders on host system
+
+        # Copy framework to current directory
+        copy_dir(f"{os.environ[EnvKeys.DirNrobo]}{os.sep}framework{os.sep}pages",
+                 f"{os.environ[EnvKeys.DirExecution]}{os.sep}pages")
+        copy_dir(f"{os.environ[EnvKeys.DirNrobo]}{os.sep}framework{os.sep}tests",
+                 f"{os.environ[EnvKeys.DirExecution]}{os.sep}tests")
+        copy_dir(f"{os.environ[EnvKeys.DirNrobo]}{os.sep}browserConfigs",
+                 f"{os.environ[EnvKeys.DirExecution]}{os.sep}browserConfigs")
+
+        # Copy conftest.py and other files to current directory
+        copy_file(f"{os.environ[EnvKeys.DirNrobo]}{os.sep}framework{os.sep}__init__.py",
+                  f"{os.environ[EnvKeys.DirExecution]}{os.sep}__init__.py")
+        copy_file(f"{os.environ[EnvKeys.DirNrobo]}{os.sep}conftest.py",
+                  f"{os.environ[EnvKeys.DirExecution]}{os.sep}conftest.py")
+        copy_file(f"{os.environ[EnvKeys.DirNrobo]}{os.sep}framework{os.sep}nrobo-config.yaml",
+                  f"{os.environ[EnvKeys.DirExecution]}{os.sep}nrobo-config.yaml")
+
+        print(f"Installation complete")
