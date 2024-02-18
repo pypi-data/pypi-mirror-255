f5ca4dfecf09880ecd60a35a81e41a03e7008162
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 23:05:08 +0000

    Add package CorrDim-By-Bisca/0.0.3/CorrDim_By_Bisca-0.0.3.tar.gz

---
+++ packages/CorrDim-By-Bisca/CorrDim_By_Bisca-0.0.3.tar.gz/CorrDim_By_Bisca-0.0.3/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.3'
+DESCRIPTION = 'Python module for dimensionality analysis using CorrDim and related algorithms'
+LONG_DESCRIPTION = (
+    'This Python module provides robust methods for estimating the Correlation Dimension '
+    'of any given dataset, leveraging the CorrDim algorithm, and includes additional '
+    'functions such as packing_numbers and local_dim for comprehensive '
+    'dimensionality analysis. Suitable for applications in fractal dimensions, '
+    'chaos theory, and complex data analysis.'
+)
+
+# Setting up
+setup(
+    name="CorrDim_By_Bisca",
+    version=VERSION,
+    author="Eng. Alberto Biscalchin",
+    author_email="biscalchin.mau.se@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['numpy', 'scipy', 'scikit-learn'],
+    keywords=[
+        "correlation dimension", "CorrDim algorithm", "fractal analysis", "chaos theory",
+        "data analysis", "NumPy", "SciPy", "multidimensional scaling",
+        "time-series analysis", "packing numbers", "local dimension"
+    ],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ]
+)
