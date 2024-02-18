87e6c07cbbd784d863f295327547d094f082cf07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 03:03:44 +0000

    Add package shellfire/0.12/shellfire-0.12.tar.gz

---
+++ packages/shellfire/shellfire-0.12.tar.gz/shellfire-0.12/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='shellfire',
+    version='0.12',
+    description=(
+        'shellfire is an exploitation shell focusing on exploiting command '
+        'injection vulnerabilities, eg., LFI, RFI, SSTI, etc.'
+    ),
+    url='https://github.com/unix-ninja/shellfire',
+    author='unix-ninja',
+    author_email='chris@unix-ninja.com',
+    license='BSD',
+    python_requires='>=3.8.0',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    include_package_data=True,
+    entry_points={
+        "console_scripts": [
+             "shellfire=shellfire:cli"
+        ]
+    },
+    install_requires=[
+        'requests'
+        ]
+    )
