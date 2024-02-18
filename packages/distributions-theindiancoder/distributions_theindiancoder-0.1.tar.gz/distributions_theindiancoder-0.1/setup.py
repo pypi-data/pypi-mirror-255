88f9a7e6961273a1e0df118cd9acbb6d499408fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 02:12:00 +0000

    Add package distributions-theindiancoder/0.1/distributions_theindiancoder-0.1.tar.gz

---
+++ packages/distributions-theindiancoder/distributions_theindiancoder-0.1.tar.gz/distributions_theindiancoder-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='distributions_theindiancoder',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions_theindiancoder'],
+      author='theindiancoder',
+      zip_safe=True)
