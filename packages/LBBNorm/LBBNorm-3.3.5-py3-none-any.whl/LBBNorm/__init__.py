c2db8c9bea450f6889849517c596b1fd1a3e7067
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 08:07:41 +0000

    Add package LBBNorm/3.3.5/LBBNorm-3.3.5-py3-none-any.whl

---
+++ packages/LBBNorm/LBBNorm-3.3.5-py3-none-any.whl/LBBNorm/__init__.py
@@ -0,0 +1,18 @@
+from .normalization.macenko import Macenko
+from .normalization.vahadane import Vahadane
+from .normalization.adaptive_color_deconvolution import AdaptiveColorDeconvolution
+from .normalization.reinhard import Reinhard
+from .normalization.modified_reinhard import ModifiedReinhard
+
+
+from .utility.slide_patcher import WholeSlidePatcher
+
+
+
+if __name__ == '__main__':
+    pass
+
+
+
+if __name__ == 'LBBNorm.Macenko':
+    Macenko()
