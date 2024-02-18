26835af8c74840ebf8b4626731705cde2c8e6a0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 01:28:31 +0000

    Add package dev-wor/0.0.1/dev_wor-0.0.1.tar.gz

---
+++ packages/dev-wor/dev_wor-0.0.1.tar.gz/dev_wor-0.0.1/orangecontrib/dev_workflow/widgets/__init__.py
@@ -0,0 +1,17 @@
+"""
+Widgets from Development workflow category
+
+"""
+ID = "Orange.widgets.dev_workflow"
+
+NAME = "dev_worklow"
+
+# Category icon show in the menu
+ICON = "icons/Logo_HKH_4_jc.svg"
+
+BACKGROUND = "light-blue"
+
+DESCRIPTION = "Dev widget."
+
+
+PRIORITY = 6
