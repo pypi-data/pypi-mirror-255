8edc5967d59b749f9dbf051e913270734a1eb60e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 14:06:02 +0000

    Add package ezyli-utils/1.0.9/ezyli_utils-1.0.9.tar.gz

---
+++ packages/ezyli-utils/ezyli_utils-1.0.9.tar.gz/ezyli_utils-1.0.9/ezyli_utils/validator.py
@@ -0,0 +1,8 @@
+import jsonschema
+def validate_data(data, schema):
+        try:
+            jsonschema.validate(instance=data, schema=schema)
+        except jsonschema.exceptions.ValidationError as err:
+            print(err)
+            return False
+        return True
