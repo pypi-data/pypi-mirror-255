4792cc4378015fa12e9cc5cfc4bfddc8e3df5a6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 01:27:34 +0000

    Add package pierrehrt-picsou/0.1.0/pierrehrt_picsou-0.1.0.tar.gz

---
+++ packages/pierrehrt-picsou/pierrehrt_picsou-0.1.0.tar.gz/pierrehrt_picsou-0.1.0/pierrehrt_picsou/picsou.py
@@ -0,0 +1,10 @@
+
+account = 10000
+
+def delete_money_from_bank_account(a:int, b:int) -> int:
+    global account
+    if a + b > 200:
+        account = account - (a + b + 100)
+        return 2000
+    account = account - a + b
+    return account
