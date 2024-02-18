35d0d76ef496f7b5056785d031fd3eab0ee7b00b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 17:04:29 +0000

    Add package acb/0.5.0/acb-0.5.0.tar.gz

---
+++ packages/acb/acb-0.5.0.tar.gz/acb-0.5.0/acb/__init__.py
@@ -0,0 +1,34 @@
+from contextvars import ContextVar
+from pathlib import Path
+
+
+from aiopath import AsyncPath
+from pydantic import BaseModel
+
+base_path: AsyncPath = AsyncPath(Path.cwd())
+tmp_path = base_path / "tmp"
+
+
+class Action(BaseModel, arbitrary_types_allowed=True):
+    name: str
+    path: AsyncPath = AsyncPath(Path.cwd())
+    pkg: str = path.parent.stem
+
+
+action_registry: ContextVar[list[Action]] = ContextVar("action_registry", default=[])
+
+
+class Adapter(BaseModel, arbitrary_types_allowed=True):
+    name: str
+    category: str
+    required: bool = False
+    enabled: bool = False
+    installed: bool = False
+    pkg: str = "acb"
+    path: AsyncPath = AsyncPath(Path(__file__) / "adapters")
+
+    def __str__(self) -> str:
+        return self.__repr__()
+
+
+adapter_registry: ContextVar[list[Adapter]] = ContextVar("adapter_registry", default=[])
