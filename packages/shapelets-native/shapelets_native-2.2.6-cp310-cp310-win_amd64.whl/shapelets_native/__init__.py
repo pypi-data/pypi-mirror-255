2035daba83b61e3d6a931e64235dae5665d41a35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 09:05:32 +0000

    Add package shapelets-native/2.2.6/shapelets_native-2.2.6-cp310-cp310-win_amd64.whl

---
+++ packages/shapelets-native/shapelets_native-2.2.6-cp310-cp310-win_amd64.whl/shapelets_native/__init__.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2022 Shapelets.io
+#
+# This software is released under the MIT License.
+# https://opensource.org/licenses/MIT
+
+from __future__ import annotations
+from ._version import version as __version__
+from ._shapelets_extension import *
