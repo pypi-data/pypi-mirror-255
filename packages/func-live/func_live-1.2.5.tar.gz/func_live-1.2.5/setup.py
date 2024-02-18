19997ef4ddddd696a102051e76f1ff6be38587ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 08:05:29 +0000

    Add package func-live/1.2.5/func_live-1.2.5.tar.gz

---
+++ packages/func-live/func_live-1.2.5.tar.gz/func_live-1.2.5/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='func_live',
+    version='1.2.5',
+    packages=find_packages(),
+    install_requires=[
+        'aiohttp',
+        'requests'
+    ],
+    description="Allows you to import and call the functions on www.func.live",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author="Aravind Vakil",
+    license="ISC",
+    keywords="func.live func functions",
+    url="https://func.live"
+)
