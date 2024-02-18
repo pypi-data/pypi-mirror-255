1a4e789cc63b1e09a68e89555ef63c645e42caed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 13:04:51 +0000

    Add package py-utls/0.2.0/py-utls-0.2.0.tar.gz

---
+++ packages/py-utls/py-utls-0.2.0.tar.gz/py-utls-0.2.0/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="py-utls",
+    version="0.2.0",
+    author="Cyril Dever",
+    author_mail="cdever@pep-s.com",
+    description="Utilities for Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/cyrildever/py-utls",
+    project_urls={
+        "Bug Tracker": "https://github.com/cyrildever/py-utls/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    keywords=[
+        "python",
+        "utils",
+        "chunks",
+        "flatten",
+        "hexadecimal",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
