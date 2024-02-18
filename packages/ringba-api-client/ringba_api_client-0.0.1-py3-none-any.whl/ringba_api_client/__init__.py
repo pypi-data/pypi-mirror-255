092b10b1ab706c9651307018934942af4e053043
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 23:06:07 +0000

    Add package ringba-api-client/0.0.1/ringba_api_client-0.0.1-py3-none-any.whl

---
+++ packages/ringba-api-client/ringba_api_client-0.0.1-py3-none-any.whl/ringba_api_client/__init__.py
@@ -0,0 +1,8 @@
+"""Top-level package for ringba-api-client."""
+
+__author__ = """Nick Tulli"""
+__email__ = "ntulli.dev@gmail.com"
+__version__ = "0.0.1"
+
+
+from .ringba_api_client import RingbaApiClient
