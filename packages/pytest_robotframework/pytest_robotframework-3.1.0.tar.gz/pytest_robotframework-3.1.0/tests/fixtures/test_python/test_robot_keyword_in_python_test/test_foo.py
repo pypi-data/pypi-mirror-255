a82080498a64d143f445d6689d90242bec3da3ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 12:06:07 +0000

    Add package pytest_robotframework/3.1.0/pytest_robotframework-3.1.0.tar.gz

---
+++ packages/pytest_robotframework/pytest_robotframework-3.1.0.tar.gz/pytest_robotframework-3.1.0/tests/fixtures/test_python/test_robot_keyword_in_python_test/test_foo.py
@@ -0,0 +1,13 @@
+# noqa: INP001
+# init file breaks it and i dont care
+from __future__ import annotations
+
+from robot.libraries.BuiltIn import BuiltIn
+
+from pytest_robotframework import import_resource
+
+import_resource("bar/bar.resource")
+
+
+def test_foo():
+    BuiltIn().run_keyword("bar")  # pyright:ignore[reportUnknownMemberType]
