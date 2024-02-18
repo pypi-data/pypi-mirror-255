327345ace4edda8deb1f5d8c90959ccdf43c917f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 14:06:15 +0000

    Add package xenon-fuse/0.3.1/xenon_fuse-0.3.1-py2.py3-none-any.whl

---
+++ packages/xenon-fuse/xenon_fuse-0.3.1-py2.py3-none-any.whl/fuse/plugins/micro_physics/microphysics_summary.py
@@ -0,0 +1,17 @@
+import strax
+
+export, __all__ = strax.exporter()
+
+@export
+class MicroPhysicsSummary(strax.MergeOnlyPlugin):
+    """
+    MergeOnlyPlugin that summarizes the fuse microphysics simulation results into a single output. 
+    """
+
+    depends_on = ['interactions_in_roi',
+                  'quanta',
+                  'electric_field_values',
+                  ]
+    save_when = strax.SaveWhen.ALWAYS
+    provides = 'microphysics_summary'
+    __version__ = '0.1.0'
