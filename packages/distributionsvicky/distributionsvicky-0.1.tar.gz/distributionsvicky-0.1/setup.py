1873c6ee84f9b349758965d44ec985d68df97d23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 23:05:23 +0000

    Add package distributionsvicky/0.1/distributionsvicky-0.1.tar.gz

---
+++ packages/distributionsvicky/distributionsvicky-0.1.tar.gz/distributionsvicky-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributionsvicky',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributionsvicky'],
+      zip_safe=False)
