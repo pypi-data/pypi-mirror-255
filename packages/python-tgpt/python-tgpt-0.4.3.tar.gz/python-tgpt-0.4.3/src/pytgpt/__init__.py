fd17cce95ab2dceafa83fe836761a7bed47dc12b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 16:06:40 +0000

    Add package python-tgpt/0.4.3/python-tgpt-0.4.3.tar.gz

---
+++ packages/python-tgpt/python-tgpt-0.4.3.tar.gz/python-tgpt-0.4.3/src/pytgpt/__init__.py
@@ -0,0 +1,28 @@
+from .utils import appdir
+import g4f
+
+__version__ = "0.4.3"
+__author__ = "Smartwa"
+__repo__ = "https://github.com/Simatwa/python-tgpt"
+
+tgpt_providers = [
+    "leo",
+    "openai",
+    "opengpt",
+    "koboldai",
+    "bard",
+    "phind",
+    "llama2",
+    "blackboxai",
+]
+
+gpt4free_providers = [
+    provider.__name__ for provider in g4f.Provider.__providers__  # if provider.working
+]
+
+available_providers = tgpt_providers + gpt4free_providers
+
+__all__ = [
+    "appdir",
+    "imager",
+] + available_providers
