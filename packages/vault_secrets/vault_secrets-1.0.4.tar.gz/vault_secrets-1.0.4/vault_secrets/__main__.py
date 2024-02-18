7443f2f47cb56e6150450446607480b3ca38b447
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 20:05:02 +0000

    Add package vault_secrets/1.0.4/vault_secrets-1.0.4.tar.gz

---
+++ packages/vault_secrets/vault_secrets-1.0.4.tar.gz/vault_secrets-1.0.4/vault_secrets/__main__.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: latin-1 -*-
+
+"""
+Make API calls to Vault Secrets to retrieve secrets.
+
+Prerequisites:
+    - Vault Secrets Organization ID
+    - Vault Secrets Project ID
+    - Vault Secrets Application Name
+    - Vault Secrets Organization Client ID
+    - Vault Secrets Organization Client Secret
+
+Optional:
+    - Secret Name (to return the latest value of a specific secret)
+
+Organization and Project IDs can be retrieved using the vlt CLI:
+https://developer.hashicorp.com/vault/tutorials/hcp-vault-secrets-get-started/hcp-vault-secrets-retrieve-secret
+"""
+import __init__
+
+if __name__ == "__main__":
+    __init__.main()
