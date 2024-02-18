7151ed8b6baddea035e923bded7f4c3d955bd95f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 16:08:41 +0000

    Add package subprocess-loop/1.0.2/subprocess_loop-1.0.2.tar.gz

---
+++ packages/subprocess-loop/subprocess_loop-1.0.2.tar.gz/subprocess_loop-1.0.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+  name='subprocess_loop',
+  # packages=['subprocess_loop'],
+  version='1.0.2',
+  author='dev mx',
+  description='subprocess exchange with stdio',
+  url='https://github.com/maxavaiable/py-subprocess-loop',
+  packages=find_packages(),
+  python_requires='>=3.0'
+)
