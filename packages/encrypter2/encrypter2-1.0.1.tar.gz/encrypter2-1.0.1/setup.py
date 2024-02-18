dde7f546220aa6ee9eff664ada304509f97b4507
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 14:04:54 +0000

    Add package encrypter2/1.0.1/encrypter2-1.0.1.tar.gz

---
+++ packages/encrypter2/encrypter2-1.0.1.tar.gz/encrypter2-1.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+setup(name="encrypter2",
+version="1.0.1",
+description="This is an encoder package",
+long_description="This is python package helps us encode a given text using some basic encryption algorithms",
+author="Kanishk",
+packages=["encoder"],
+install_requires=["nltk"]
+)
