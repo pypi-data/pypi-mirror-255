c7cbd68b7d274fe4d68bef1e855228459df71368
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 06:05:22 +0000

    Add package vmware-nsx-sdk/0.0.0.1/vmware-nsx-sdk-0.0.0.1.tar.gz

---
+++ packages/vmware-nsx-sdk/vmware-nsx-sdk-0.0.0.1.tar.gz/vmware-nsx-sdk-0.0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="vmware-nsx-sdk",
+    version="0.0.0.1",
+    author="VMware Inc.",
+    description="Python bindings for nsx",
+    packages=find_packages(),
+    install_requires=[
+        # List your package dependencies here
+    ],
+)
+
