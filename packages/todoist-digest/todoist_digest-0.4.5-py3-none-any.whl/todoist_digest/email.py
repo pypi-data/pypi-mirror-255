ce4084c8f69ee5ab563e1271987daa117a2c6185
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 05:04:25 +0000

    Add package todoist-digest/0.4.5/todoist_digest-0.4.5-py3-none-any.whl

---
+++ packages/todoist-digest/todoist_digest-0.4.5-py3-none-any.whl/todoist_digest/email.py
@@ -0,0 +1,30 @@
+import logging
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from urllib.parse import urlparse
+
+import markdown2
+
+logger = logging.getLogger(__name__)
+
+
+def send_markdown_email(auth_url, markdown_content, subject, to_address):
+    parsed_url = urlparse(auth_url)
+    html_content = markdown2.markdown(markdown_content)
+
+    msg = MIMEMultipart()
+    msg["From"] = parsed_url.username
+    msg["To"] = to_address
+    msg["Subject"] = subject
+
+    # TODO any sane styling we can setup?
+    msg.attach(MIMEText(html_content, "html"))
+
+    with smtplib.SMTP_SSL(parsed_url.hostname, parsed_url.port) as server:
+        logger.info("Sending email to %s", to_address)
+
+        server.login(parsed_url.username, parsed_url.password)
+        result = server.send_message(msg)
+        server.quit()
+        breakpoint()
