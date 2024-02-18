7c9dc88086241f47d3b1667edf97a80ede053b1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 15:05:57 +0000

    Add package parea-ai/0.2.61/parea_ai-0.2.61.tar.gz

---
+++ packages/parea-ai/parea_ai-0.2.61.tar.gz/parea_ai-0.2.61/parea/cookbook/langchain/trace_langchain_simple.py
@@ -0,0 +1,28 @@
+import os
+
+from dotenv import load_dotenv
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_openai import ChatOpenAI
+
+from parea import Parea, PareaAILangchainTracer
+
+load_dotenv()
+
+p = Parea(api_key=os.getenv("PAREA_API_KEY"))
+handler = PareaAILangchainTracer()
+
+llm = ChatOpenAI(openai_api_key=os.getenv("OPENAI_API_KEY"))
+prompt = ChatPromptTemplate.from_messages([("user", "{input}")])
+chain = prompt | llm | StrOutputParser()
+
+
+def main():
+    return chain.invoke(
+        {"input": "Write a Hello World program in Python using FastAPI."},
+        config={"callbacks": [handler]},
+    )
+
+
+if __name__ == "__main__":
+    print(main())
