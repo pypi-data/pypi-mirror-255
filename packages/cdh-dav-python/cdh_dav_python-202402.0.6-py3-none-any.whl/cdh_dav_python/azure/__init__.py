417e0c6c6eb8369d00331496d92bdcda615fe211
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 07:04:32 +0000

    Add package cdh-dav-python/202402.0.6/cdh_dav_python-202402.0.6-py3-none-any.whl

---
+++ packages/cdh-dav-python/cdh_dav_python-202402.0.6-py3-none-any.whl/cdh_dav_python/azure/__init__.py
@@ -0,0 +1,9 @@
+"""Initialize the alation_service subpackage of cdh_dav_python package"""
+# allow absolute import from the root folder
+# whatever its name is.
+# from cdh_dav_python.az_storage_service import az_storage_queue
+import sys  # don't remove required for error handling
+import os
+
+# Import from sibling directory ..\databricks_service
+OS_NAME = os.name
