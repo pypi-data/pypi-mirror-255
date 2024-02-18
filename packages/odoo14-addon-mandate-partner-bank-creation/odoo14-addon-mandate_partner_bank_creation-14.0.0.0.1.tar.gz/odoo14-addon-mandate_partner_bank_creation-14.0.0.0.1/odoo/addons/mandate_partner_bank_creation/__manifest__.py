1692fdfdf51c26e4f39e8d9ff0bb7cd2209d054e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 20:04:57 +0000

    Add package odoo14-addon-mandate-partner-bank-creation/14.0.0.0.1/odoo14-addon-mandate_partner_bank_creation-14.0.0.0.1.tar.gz

---
+++ packages/odoo14-addon-mandate-partner-bank-creation/odoo14-addon-mandate_partner_bank_creation-14.0.0.0.1.tar.gz/odoo14-addon-mandate_partner_bank_creation-14.0.0.0.1/odoo/addons/mandate_partner_bank_creation/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2021-Coopdevs Treball SCCL (<https://coopdevs.org>)
+# - César López Ramírez - <cesar.lopez@coopdevs.org>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Account Banking Mandate creation with Res Partner Bank",
+    "version": "14.0.0.0.1",
+    "depends": ["base", "component_event", "account_banking_mandate"],
+    "author": "Coopdevs Treball SCCL",
+    "category": "Accounting & Finance",
+    "website": "https://coopdevs.org",
+    "license": "AGPL-3",
+    "summary": """
+        When a Res Partner Bank is created, a Account Banking Mandate is created too
+    """,
+    "data": [],
+    "installable": True,
+}
