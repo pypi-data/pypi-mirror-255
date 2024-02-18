7b80beef4652551f9a79dda6e5e943aa17a3f1e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 07:03:55 +0000

    Add package uglychain/0.1.2/uglychain-0.1.2-py3-none-any.whl

---
+++ packages/uglychain/uglychain-0.1.2-py3-none-any.whl/uglychain/__init__.py
@@ -0,0 +1,18 @@
+from .chains import LLM, MapChain, Model, ReActChain, ReduceChain
+from .llm import BaseLanguageModel, finish, run_function
+from .retrievers import BaseRetriever, StoresRetriever
+
+__version__ = "0.1.2"
+
+__all__ = [
+    "BaseLanguageModel",
+    "Model",
+    "finish",
+    "run_function",
+    "LLM",
+    "MapChain",
+    "ReduceChain",
+    "ReActChain",
+    "BaseRetriever",
+    "StoresRetriever",
+]
