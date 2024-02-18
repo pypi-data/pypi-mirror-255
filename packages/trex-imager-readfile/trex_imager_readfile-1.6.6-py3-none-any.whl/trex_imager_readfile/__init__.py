3ebd52e07f9d8f4e32f7e0f82b20e0d88c479d6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 01:28:17 +0000

    Add package trex-imager-readfile/1.6.6/trex_imager_readfile-1.6.6-py3-none-any.whl

---
+++ packages/trex-imager-readfile/trex_imager_readfile-1.6.6-py3-none-any.whl/trex_imager_readfile/__init__.py
@@ -0,0 +1,13 @@
+__version__ = "1.6.6"
+
+# core functions for easy use
+from .blueline import read as read_blueline
+from .nir import read as read_nir
+from .rgb import read as read_rgb
+from .spectrograph import read as read_spectrograph
+
+# module imports
+from trex_imager_readfile import blueline
+from trex_imager_readfile import nir
+from trex_imager_readfile import rgb
+from trex_imager_readfile import spectrograph
