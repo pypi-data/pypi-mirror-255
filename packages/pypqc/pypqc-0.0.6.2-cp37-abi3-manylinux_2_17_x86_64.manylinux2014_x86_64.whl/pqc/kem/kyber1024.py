512f9d52dc77b57bac81d81ed9c30eb3c8bada80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 21:06:29 +0000

    Add package pypqc/0.0.6.2/pypqc-0.0.6.2-cp37-abi3-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/pypqc/pypqc-0.0.6.2-cp37-abi3-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/pqc/kem/kyber1024.py
@@ -0,0 +1,63 @@
+from .._lib.libkyber1024_clean import ffi, lib
+
+import os
+
+if os.environ.get('LICENSED_KYBER', '0') == '0':
+	# fmt: off
+	from .._util import patent_notice
+	patent_notice(['FR2956541A1/US9094189B2/EP2537284B1', 'US9246675/EP2837128B1', 'potential unknown others'],
+		      'the Kyber cryptosystem', 1, [
+		      'https://ntruprime.cr.yp.to/faq.html',
+		      'https://csrc.nist.gov/csrc/media/Projects/post-quantum-cryptography/documents/selected-algos-2022/nist-pqc-license-summary-and-excerpts.pdf',
+		      'https://groups.google.com/a/list.nist.gov/g/pqc-forum/c/G0DoD7lkGPk/m/d7Zw0qhGBwAJ',
+		      'https://datatracker.ietf.org/meeting/116/proceedings#pquip:~:text=Patents%20and%20PQC',
+		      'https://mailarchive.ietf.org/arch/msg/pqc/MS92cuZkSRCDEjpPP90s2uAcRPo/']
+	)
+	# fmt: on
+
+__all__ = ['keypair', 'encap', 'decap']
+
+_LIB_NAMESPACE = ffi.string(lib._NAMESPACE).decode('ascii')
+_T_PUBLICKEY = f'{_LIB_NAMESPACE}crypto_publickey'
+_T_SECRETKEY = f'{_LIB_NAMESPACE}crypto_secretkey'
+_T_KEM_PLAINTEXT = f'{_LIB_NAMESPACE}crypto_kem_plaintext'
+_T_KEM_CIPHERTEXT = f'{_LIB_NAMESPACE}crypto_kem_ciphertext'
+
+_crypto_kem_keypair = getattr(lib, f'{_LIB_NAMESPACE}crypto_kem_keypair')
+_crypto_kem_enc = getattr(lib, f'{_LIB_NAMESPACE}crypto_kem_enc')
+_crypto_kem_dec = getattr(lib, f'{_LIB_NAMESPACE}crypto_kem_dec')
+
+
+def keypair():
+	_pk = ffi.new(_T_PUBLICKEY)
+	_sk = ffi.new(_T_SECRETKEY)
+
+	errno = _crypto_kem_keypair(_pk, _sk)
+
+	if errno == 0:
+		return bytes(_pk), bytes(_sk)
+	raise RuntimeError(f'{_crypto_kem_keypair.__name__} returned error code {errno}')
+
+
+def encap(pk):
+	_ct = ffi.new(_T_KEM_CIPHERTEXT)
+	_ss = ffi.new(_T_KEM_PLAINTEXT)
+	_pk = ffi.cast(_T_PUBLICKEY, ffi.from_buffer(pk))
+
+	errno = _crypto_kem_enc(_ct, _ss, _pk)
+
+	if errno == 0:
+		return bytes(_ss), bytes(_ct)
+	raise RuntimeError(f'{_crypto_kem_enc.__name__} returned error code {errno}')
+
+
+def decap(ciphertext, sk):
+	_ss = ffi.new(_T_KEM_PLAINTEXT)
+	_ct = ffi.cast(_T_KEM_CIPHERTEXT, ffi.from_buffer(ciphertext))
+	_sk = ffi.cast(_T_SECRETKEY, ffi.from_buffer(sk))
+
+	errno = _crypto_kem_dec(_ss, _ct, _sk)
+
+	if errno == 0:
+		return bytes(_ss)
+	raise RuntimeError(f'{_crypto_kem_dec.__name__} returned error code {errno}')
---
+++ packages/pypqc/pypqc-0.0.6.2-cp37-abi3-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/pqc/sign/sphincs_shake_192f_simple.py
@@ -0,0 +1,51 @@
+from .._lib.libsphincs_shake_192f_simple_clean import ffi, lib
+
+__all__ = ['keypair', 'sign', 'verify']
+
+_LIB_NAMESPACE = ffi.string(lib._NAMESPACE).decode('ascii')
+_T_PUBLICKEY = f'{_LIB_NAMESPACE}crypto_publickey'
+_T_SECRETKEY = f'{_LIB_NAMESPACE}crypto_secretkey'
+_T_SIGNATURE = f'{_LIB_NAMESPACE}crypto_signature'
+
+_crypto_sign_keypair = getattr(lib, f'{_LIB_NAMESPACE}crypto_sign_keypair')
+_crypto_sign_signature = getattr(lib, f'{_LIB_NAMESPACE}crypto_sign_signature')
+_crypto_sign_verify = getattr(lib, f'{_LIB_NAMESPACE}crypto_sign_verify')
+
+
+def keypair():
+	_pk = ffi.new(_T_PUBLICKEY)
+	_sk = ffi.new(_T_SECRETKEY)
+
+	errno = _crypto_sign_keypair(_pk, _sk)
+
+	if errno == 0:
+		return bytes(_pk), bytes(_sk)
+	raise RuntimeError(f'{_crypto_sign_keypair.__name__} returned error code {errno}')
+
+
+def sign(m, sk):
+	_m = ffi.from_buffer(m)
+	_sk = ffi.cast(_T_SECRETKEY, ffi.from_buffer(sk))
+	_sig = ffi.new(_T_SIGNATURE)
+	_siglen = ffi.new('size_t*')
+
+	errno = _crypto_sign_signature(_sig, _siglen, _m, len(m), _sk)
+
+	if errno == 0:
+		assert len(_sig) == _siglen[0]  # Fixed-length signature
+		return bytes(_sig)
+	raise RuntimeError(f'{_crypto_sign_signature.__name__} returned error code {errno}')
+
+
+def verify(sig, m, pk):
+	_sig = ffi.cast(_T_SIGNATURE, ffi.from_buffer(sig))
+	_m = ffi.from_buffer(m)
+	_pk = ffi.cast(_T_PUBLICKEY, ffi.from_buffer(pk))
+
+	errno = _crypto_sign_verify(_sig, len(_sig), _m, len(_m), _pk)
+
+	if errno == 0:
+		return
+	if errno == -1:
+		raise ValueError('verification failed')
+	raise RuntimeError(f'{_crypto_sign_verify.__name__} returned error code {errno}')
