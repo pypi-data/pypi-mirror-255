a6f7d5a2c2310ae82d139ed06331d72d14ce636f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 03:05:53 +0000

    Add package dropbase/0.0.2/dropbase-0.0.2.1.tar.gz

---
+++ packages/dropbase/dropbase-0.0.2.1.tar.gz/dropbase-0.0.2/src/dropbase/database/__init__.py
@@ -0,0 +1,104 @@
+from sqlalchemy.engine import URL, create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.sql import text
+
+from server.constants import WORKSPACE_SOURCES
+
+
+class Database:
+    def __init__(self, database: str, schema: str = "public"):
+        connection_url = self._get_connection_url(
+            WORKSPACE_SOURCES.get(database)
+        )  # NOTE: May want to change this later, added as temporary solution
+        self.source = database
+        self.schema = schema
+        self.engine = create_engine(connection_url, future=True)
+        self.session_obj = scoped_session(sessionmaker(bind=self.engine))
+
+    def __enter__(self):
+        self.session = self.session_obj()
+        return self
+
+    def __exit__(self, type, value, traceback):
+        self.session.close()
+        self.engine.dispose()
+
+    def commit(self):
+        self.session.commit()
+
+    def close(self):
+        self.session.close()
+        self.engine.dispose()
+
+    def rollback(self):
+        self.session.rollback()
+
+    def _get_connection_url(self, creds: dict):
+        return URL.create(**creds)
+
+    def update(self, table: str, keys: dict, values: dict, auto_commit: bool = False):
+        value_keys = list(values.keys())
+        if len(value_keys) > 1:
+            set_claw = f"SET ({', '.join(value_keys)}) = (:{', :'.join(value_keys)})"
+        else:
+            set_claw = f"SET {value_keys[0]} = :{value_keys[0]}"
+        key_keys = list(keys.keys())
+        if len(key_keys) > 1:
+            where_claw = f"WHERE ({', '.join(key_keys)}) = (:{', :'.join(key_keys)})"
+        else:
+            where_claw = f"WHERE {key_keys[0]} = :{key_keys[0]}"
+        sql = f"""UPDATE {self.schema}.{table}\n{set_claw}\n{where_claw} RETURNING *;"""
+        values.update(keys)
+        result = self.session.execute(text(sql), values)
+        if auto_commit:
+            self.commit()
+        return [dict(x) for x in result.fetchall()]
+
+    def select(self, table: str, where_clause: str = None, values: dict = None):
+        if where_clause:
+            sql = f"""SELECT * FROM {self.schema}.{table} WHERE {where_clause};"""
+        else:
+            sql = f"""SELECT * FROM {self.schema}.{table};"""
+
+        if values is None:
+            values = {}
+
+        result = self.session.execute(text(sql), values)
+        return [dict(row) for row in result.fetchall()]
+
+    def insert(self, table: str, values: dict, auto_commit: bool = False):
+        keys = list(values.keys())
+        sql = f"""INSERT INTO {self.schema}.{table} ({', '.join(keys)})
+        VALUES (:{', :'.join(keys)})
+        RETURNING *;"""
+        row = self.session.execute(text(sql), values)
+        if auto_commit:
+            self.commit()
+        return dict(row.fetchone())
+
+    def delete(self, table: str, keys: dict, auto_commit: bool = False):
+        key_keys = list(keys.keys())
+        if len(key_keys) > 1:
+            where_claw = f"WHERE ({', '.join(key_keys)}) = (:{', :'.join(key_keys)})"
+        else:
+            where_claw = f"WHERE {key_keys[0]} = :{key_keys[0]}"
+        sql = f"""DELETE FROM {self.schema}.{table}\n{where_claw};"""
+        res = self.session.execute(text(sql), keys)
+        if auto_commit:
+            self.commit()
+        return res.rowcount
+
+    def filter_and_sort(
+        self, table: str, filter_clauses: list, sort_by: str = None, ascending: bool = True
+    ):
+        sql = f"""SELECT * FROM {self.schema}.{table}"""
+        if filter_clauses:
+            sql += " WHERE " + " AND ".join(filter_clauses)
+        if sort_by:
+            sql += f" ORDER BY {sort_by} {'ASC' if ascending else 'DESC'}"
+        result = self.session.execute(text(sql))
+        return [dict(row) for row in result.fetchall()]
+
+    def execute_custom_query(self, sql: str, values: dict = None):
+        result = self.session.execute(text(sql), values if values else {})
+        return [dict(row) for row in result.fetchall()]
