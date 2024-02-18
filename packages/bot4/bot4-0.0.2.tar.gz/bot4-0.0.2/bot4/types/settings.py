a934a63422d8be1498fc99c666387f4adae517ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 08:04:33 +0000

    Add package bot4/0.0.2/bot4-0.0.2.tar.gz

---
+++ packages/bot4/bot4-0.0.2.tar.gz/bot4-0.0.2/bot4/types/settings.py
@@ -0,0 +1,22 @@
+from dataclasses import dataclass
+from bot4.net.auth import OfflineProfile
+
+
+@dataclass
+class Settings:
+    version: str | int = 754
+    ip: str = 'mc.prostocraft.ru'
+    port: int = 25565
+    timeout: float = 30.0
+    profile: OfflineProfile = OfflineProfile()
+
+@dataclass
+class Server_Settings:
+    version: str | int = 754
+    
+    ip_source: str = '127.0.0.1'
+    port_source: int = 25565
+
+    ip_destination: str = 'mc.prostocraft.ru'
+    port_destination: int = 25565
+
