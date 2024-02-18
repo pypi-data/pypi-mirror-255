e9ef75b09845e64ef524f0d5c7dc41bb8df08839
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 22:04:44 +0000

    Add package klu/0.1.48/klu-0.1.48.tar.gz

---
+++ packages/klu/klu-0.1.48.tar.gz/klu-0.1.48/klu/api/config.py
@@ -0,0 +1,15 @@
+import os
+
+
+def get_klu_env():
+    return os.getenv("KLU_ENV", "prod")
+
+
+def get_api_url():
+    env = get_klu_env()
+    if env == "dev":
+        return "http://localhost:4000/v1"
+    elif env == "staging":
+        return "https://staging-api.klu.ai/v1"
+    else:
+        return "https://api.klu.ai/v1"
