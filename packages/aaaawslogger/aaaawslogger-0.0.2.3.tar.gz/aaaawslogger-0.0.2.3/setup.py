0b3454300c33796e74e6d5abeb3223aebc22059a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 03:05:02 +0000

    Add package aaaawslogger/0.0.2.3/aaaawslogger-0.0.2.3.tar.gz

---
+++ packages/aaaawslogger/aaaawslogger-0.0.2.3.tar.gz/aaaawslogger-0.0.2.3/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(name='aaaawslogger', 
+      version='0.0.2.3', 
+      author='ace',
+      author_email='kardaras.george@clublabs.com',
+      description='Logger',
+      packages=find_packages(),
+      install_requires=[
+          'boto3',
+                        ],
+      )
