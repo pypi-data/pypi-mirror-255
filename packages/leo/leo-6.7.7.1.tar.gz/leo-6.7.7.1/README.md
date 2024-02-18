f5c27e983692a8038213ac84e042cb3ebea22e0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 03:05:55 +0000

    Add package leo/6.7.7.1/leo-6.7.7.1.tar.gz

---
+++ packages/leo/leo-6.7.7.1.tar.gz/leo-6.7.7.1/README.md
@@ -0,0 +1,26 @@
+Leo https://leo-editor.github.io/leo-editor/ 6.7.7 is now available on [GitHub](https://github.com/leo-editor/leo-editor/releases) and [pypi](https://pypi.org/project/leo/).
+
+Leo is an [IDE, outliner and PIM](https://leo-editor.github.io/leo-editor/preface.html).
+
+**The highlights of Leo 6.7.7**
+
+- PR #3761: Improve leoserver.py to support LeoInteg.
+- Asserted minor improvements.
+
+**Links**
+
+- [Download Leo](https://leo-editor.github.io/leo-editor/download.html)
+- [Install Leo](https://leo-editor.github.io/leo-editor/installing.html)
+- [6.7.7 Issues](https://github.com/leo-editor/leo-editor/issues?q=is%3Aissue+milestone%3A6.7.7+)
+- [6.7.7 Pull Requests](https://github.com/leo-editor/leo-editor/pulls?q=is%3Apr+milestone%3A6.7.7)
+- [Documentation](https://leo-editor.github.io/leo-editor/leo_toc.html)
+- [Tutorials](https://leo-editor.github.io/leo-editor/tutorial.html)
+- [Video tutorials](https://leo-editor.github.io/leo-editor/screencasts.html)
+- [Forum](http://groups.google.com/group/leo-editor)
+- [Download](http://sourceforge.net/projects/leo/files/)
+- [Leo on GitHub](https://github.com/leo-editor/leo-editor)
+- [LeoVue](https://github.com/kaleguy/leovue#leo-vue)
+- [What people are saying about Leo](https://leo-editor.github.io/leo-editor/testimonials.html)
+- [A web page that displays .leo files](https://leo-editor.github.io/leo-editor/load-leo.html)
+- [More links](https://leo-editor.github.io/leo-editor/leoLinks.html)
+
---
+++ packages/leo/leo-6.7.7.1.tar.gz/leo-6.7.7.1/setup.py
@@ -0,0 +1,51 @@
+#@+leo-ver=5-thin
+#@+node:maphew.20180224170853.1: * @file ../../setup.py
+"""
+setup.py for a dummy distribution at PyPi.
+
+Run with `python -m build` from the leo-editor directory.
+"""
+import setuptools
+#@+<< define classifiers >>
+#@+node:maphew.20141126230535.4: ** << define classifiers >>
+classifiers = [
+    'Development Status :: 6 - Mature',
+    'Intended Audience :: End Users/Desktop',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Natural Language :: English',
+    'Operating System :: MacOS',
+    'Operating System :: Microsoft :: Windows',
+    'Operating System :: POSIX :: Linux',
+    'Programming Language :: Python :: 3 :: Only',
+    'Topic :: Software Development',
+    'Topic :: Text Editors',
+    'Topic :: Text Processing',
+]
+#@-<< define classifiers >>
+version = '6.7.7.1'  # Dashes are not allowed.
+description = 'An IDE, PIM and Outliner. Distributed only at GitHub: https://github.com/leo-editor/leo-editor/releases'
+setuptools.setup(
+    name='leo',
+    version=version,
+    author='Edward K. Ream',
+    author_email='edreamleo@gmail.com',
+    url='https://leo-editor.github.io/leo-editor',
+    license='MIT License',
+    description=description,  # becomes 'Summary' in pkg-info
+    long_description=description,
+    long_description_content_type="text/plain",
+    platforms=['Linux', 'Windows', 'MacOS'],
+    download_url='https://github.com/leo-editor/leo-editor/releases',
+    classifiers=classifiers,
+    packages=[],
+    include_package_data=False,
+    setup_requires=[],
+    install_requires=[],
+    entry_points = {},
+    python_requires='>=3.9',
+)
+print('setup.py: done')
+#@@language python
+#@@tabwidth -4
+#@-leo
