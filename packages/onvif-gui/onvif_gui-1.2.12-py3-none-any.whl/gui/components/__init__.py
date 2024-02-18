c3a2b5f132f60b6f2217a4aaa8660be65dce0751
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 16:08:49 +0000

    Add package onvif-gui/1.2.12/onvif_gui-1.2.12-py3-none-any.whl

---
+++ packages/onvif-gui/onvif_gui-1.2.12-py3-none-any.whl/gui/components/__init__.py
@@ -0,0 +1,7 @@
+from .fileselector import FileSelector
+from .directoryselector import DirectorySelector
+from .labelselector import LabelSelector
+from .thresholdslider import ThresholdSlider
+from .comboselector import ComboSelector
+from .progress import Progress
+from .waitdialog import WaitDialog
