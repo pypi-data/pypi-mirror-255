239fbfb26c9414f8546eca8e9764b77c0557ceee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 21:06:29 +0000

    Add package nrobo/2024.4.4/nrobo-2024.4.4-py3-none-any.whl

---
+++ packages/nrobo/nrobo-2024.4.4-py3-none-any.whl/nrobo/util/process/__init__.py
@@ -0,0 +1,38 @@
+"""
+=====================CAUTION=======================
+DO NOT DELETE THIS FILE SINCE IT IS PART OF NROBO
+FRAMEWORK AND IT MAY CHANGE IN THE FUTURE UPGRADES
+OF NROBO FRAMEWORK. THUS, TO BE ABLE TO SAFELY UPGRADE
+TO LATEST NROBO VERSION, PLEASE DO NOT DELETE THIS
+FILE OR ALTER ITS LOCATION OR ALTER ITS CONTENT!!!
+===================================================
+
+
+"""
+import subprocess
+
+
+def terminal(command=[], stdin=None, input=None, stdout=None, stderr=None, capture_output=False, shell=False,
+             cwd=None, timeout=None, check=False, encoding=None, errors=None, text=None, env=None,
+             universal_newlines=None):
+    """
+    Execute given command, command
+
+    :param capture_output:
+    :param command: command
+    :return: status code
+    """
+    try:
+        if stdout and stderr:
+            return subprocess.check_call(command, stdout=stdout, stderr=stderr)
+        else:
+            return subprocess.check_call(command)
+    except subprocess.CalledProcessError as e:
+        print(f"Command failed with return code {e.returncode}")
+    except FileNotFoundError as e:
+        print(f"Command failed with FileNotFoundError!")
+        print(e)
+        return 1
+
+
+
