420c4d8f1b56f4635be62f5282017b3614fd044d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 13:05:31 +0000

    Add package swh.scheduler/2.1.0/swh.scheduler-2.1.0.tar.gz

---
+++ packages/swh.scheduler/swh.scheduler-2.1.0.tar.gz/swh.scheduler-2.1.0/swh/scheduler/cli/origin.py
@@ -0,0 +1,430 @@
+# Copyright (C) 2021-2024  The Software Heritage developers
+# See the AUTHORS file at the top-level directory of this distribution
+# License: GNU General Public License version 3, or any later version
+# See top-level LICENSE file for more information
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+import click
+
+from . import cli
+from ..utils import create_origin_task_dicts
+
+if TYPE_CHECKING:
+    from typing import Dict, Iterable, List, Optional
+    from uuid import UUID
+
+    from ..interface import SchedulerInterface
+    from ..model import ListedOrigin
+
+
+@cli.group("origin")
+@click.pass_context
+def origin(ctx):
+    """Manipulate listed origins."""
+    if not ctx.obj["scheduler"]:
+        ctx.fail("Scheduler class (local/remote) must be instantiated")
+
+
+def format_origins(
+    ctx: click.Context,
+    origins: List[ListedOrigin],
+    fields: Optional[List[str]] = None,
+    with_header: bool = True,
+) -> Iterable[str]:
+    """Format a list of origins as CSV.
+
+    Arguments:
+       origins: list of origins to output
+       fields: optional list of fields to output (defaults to all fields)
+       with_header: if True, output a CSV header.
+    """
+    import csv
+    from io import StringIO
+
+    import attr
+
+    from ..model import ListedOrigin
+
+    expected_fields = [field.name for field in attr.fields(ListedOrigin)]
+    if not fields:
+        fields = expected_fields
+
+    unknown_fields = set(fields) - set(expected_fields)
+    if unknown_fields:
+        ctx.fail("Unknown ListedOrigin field(s): %s" % ", ".join(unknown_fields))
+
+    output = StringIO()
+    writer = csv.writer(output)
+
+    def csv_row(data):
+        """Return a single CSV-formatted row. We clear the output buffer after we're
+        done to keep it reasonably sized."""
+        writer.writerow(data)
+        output.seek(0)
+        ret = output.read().rstrip()
+        output.seek(0)
+        output.truncate()
+        return ret
+
+    if with_header:
+        yield csv_row(fields)
+
+    for origin in origins:
+        yield csv_row(str(getattr(origin, field)) for field in fields)
+
+
+@origin.command("grab-next")
+@click.option(
+    "--policy", "-p", default="oldest_scheduled_first", help="Scheduling policy"
+)
+@click.option(
+    "--fields", "-f", default=None, help="Listed origin fields to print on output"
+)
+@click.option(
+    "--with-header/--without-header",
+    is_flag=True,
+    default=True,
+    help="Print the CSV header?",
+)
+@click.argument("type", type=str)
+@click.argument("count", type=int)
+@click.pass_context
+def grab_next(
+    ctx, policy: str, fields: Optional[str], with_header: bool, type: str, count: int
+):
+    """Grab the next COUNT origins to visit using the TYPE loader from the
+    listed origins table."""
+
+    if fields:
+        parsed_fields: Optional[List[str]] = fields.split(",")
+    else:
+        parsed_fields = None
+
+    scheduler = ctx.obj["scheduler"]
+
+    origins = scheduler.grab_next_visits(type, count, policy=policy)
+    for line in format_origins(
+        ctx, origins, fields=parsed_fields, with_header=with_header
+    ):
+        click.echo(line)
+
+
+@origin.command("schedule-next")
+@click.option(
+    "--policy", "-p", default="oldest_scheduled_first", help="Scheduling policy"
+)
+@click.argument("type", type=str)
+@click.argument("count", type=int)
+@click.pass_context
+def schedule_next(ctx, policy: str, type: str, count: int):
+    """Send the next COUNT origin visits of the TYPE loader to the scheduler as
+    one-shot tasks."""
+    from ..utils import utcnow
+    from .utils import pretty_print_task
+
+    scheduler = ctx.obj["scheduler"]
+
+    origins = scheduler.grab_next_visits(type, count, policy=policy)
+
+    created = scheduler.create_tasks(
+        [
+            {
+                **task_dict,
+                "policy": "oneshot",
+                "next_run": utcnow(),
+                "retries_left": 1,
+            }
+            for task_dict in create_origin_task_dicts(origins, scheduler)
+        ]
+    )
+
+    output = ["Created %d tasks\n" % len(created)]
+    for task in created:
+        output.append(pretty_print_task(task))
+
+    click.echo_via_pager("\n".join(output))
+
+
+@origin.command("send-to-celery")
+@click.option(
+    "--policy", "-p", default="oldest_scheduled_first", help="Scheduling policy"
+)
+@click.option(
+    "--queue",
+    "-q",
+    help="Target celery queue",
+    type=str,
+)
+@click.option(
+    "--tablesample",
+    help="Table sampling percentage",
+    type=float,
+)
+@click.option(
+    "--only-enabled/--only-disabled",
+    "enabled",
+    is_flag=True,
+    default=True,
+    help="""Determine whether we want to scheduled enabled or disabled origins. As default, we
+            want to reasonably deal with enabled origins. For some edge case though, we
+            might want the disabled ones.""",
+)
+@click.option(
+    "--lister-name",
+    default=None,
+    help="Limit origins to those listed from lister with provided name",
+)
+@click.option(
+    "--lister-instance-name",
+    default=None,
+    help="Limit origins to those listed from lister with instance name",
+)
+@click.option(
+    "--absolute-cooldown",
+    "absolute_cooldown_str",
+    default="12 hours",
+    help="Minimal interval between two visits of the same origin",
+)
+@click.option(
+    "--scheduled-cooldown",
+    "scheduled_cooldown_str",
+    default="7 days",
+    help="Minimal interval to wait before scheduling the same origins again",
+)
+@click.option(
+    "--not-found-cooldown",
+    "not_found_cooldown_str",
+    default="14 days",
+    help="The minimal interval to wait before rescheduling not_found origins",
+)
+@click.option(
+    "--failed-cooldown",
+    "failed_cooldown_str",
+    default="31 days",
+    help="Minimal interval to wait before rescheduling failed origins",
+)
+@click.argument("visit_type_name", type=str)
+@click.pass_context
+def send_to_celery_cli(
+    ctx,
+    policy: str,
+    queue: Optional[str],
+    tablesample: Optional[float],
+    visit_type_name: str,
+    enabled: bool,
+    lister_name: Optional[str] = None,
+    lister_instance_name: Optional[str] = None,
+    absolute_cooldown_str: Optional[str] = None,
+    scheduled_cooldown_str: Optional[str] = None,
+    failed_cooldown_str: Optional[str] = None,
+    not_found_cooldown_str: Optional[str] = None,
+):
+    """Send next origin visits of VISIT_TYPE_NAME to celery, filling the queue."""
+
+    from .utils import get_task_type, parse_time_interval, send_to_celery
+
+    absolute_cooldown = (
+        parse_time_interval(absolute_cooldown_str) if absolute_cooldown_str else None
+    )
+    scheduled_cooldown = (
+        parse_time_interval(scheduled_cooldown_str) if scheduled_cooldown_str else None
+    )
+    failed_cooldown = (
+        parse_time_interval(failed_cooldown_str) if failed_cooldown_str else None
+    )
+    not_found_cooldown = (
+        parse_time_interval(not_found_cooldown_str) if not_found_cooldown_str else None
+    )
+
+    scheduler = ctx.obj["scheduler"]
+
+    task_type = get_task_type(scheduler, visit_type_name)
+    if not task_type:
+        ctx.fail(f"Unknown task type {task_type}.")
+        assert False  # for mypy
+
+    queue_name = queue or task_type["backend_name"]
+
+    send_to_celery(
+        scheduler,
+        visit_type_to_queue={visit_type_name: queue_name},
+        policy=policy,
+        tablesample=tablesample,
+        enabled=enabled,
+        lister_name=lister_name,
+        lister_instance_name=lister_instance_name,
+        absolute_cooldown=absolute_cooldown,
+        scheduled_cooldown=scheduled_cooldown,
+        failed_cooldown=failed_cooldown,
+        not_found_cooldown=not_found_cooldown,
+    )
+
+
+@origin.command("update-metrics")
+@click.option("--lister", default=None, help="Only update metrics for this lister")
+@click.option(
+    "--instance", default=None, help="Only update metrics for this lister instance"
+)
+@click.pass_context
+def update_metrics(ctx, lister: Optional[str], instance: Optional[str]):
+    """Update the scheduler metrics on listed origins.
+
+    Examples:
+       swh scheduler origin update-metrics
+       swh scheduler origin update-metrics --lister github
+       swh scheduler origin update-metrics --lister phabricator --instance llvm
+    """
+    import json
+
+    import attr
+
+    scheduler: SchedulerInterface = ctx.obj["scheduler"]
+
+    lister_id: Optional[UUID] = None
+    if lister is not None:
+        lister_instance = scheduler.get_lister(name=lister, instance_name=instance)
+        if not lister_instance:
+            click.echo(f"Lister not found: {lister} instance={instance}")
+            ctx.exit(2)
+            assert False  # for mypy
+
+        lister_id = lister_instance.id
+
+    def dictify_metrics(d):
+        return {k: str(v) for (k, v) in attr.asdict(d).items()}
+
+    ret = scheduler.update_metrics(lister_id=lister_id)
+    click.echo(json.dumps(list(map(dictify_metrics, ret)), indent=4, sort_keys=True))
+
+
+@origin.command("check-listed-origins")
+@click.option(
+    "--list",
+    "-l",
+    is_flag=True,
+    help="Display listed origins (disabled by default).",
+    required=False,
+)
+@click.argument("lister_name", nargs=1, required=True)
+@click.argument("instance_name", nargs=1, required=True)
+@click.pass_context
+def check_listed_origins_cli(ctx, list, lister_name, instance_name):
+    """
+    Check listed origins registered in the scheduler database.
+    """
+
+    from tabulate import tabulate
+
+    from .utils import check_listed_origins
+
+    scheduler = ctx.obj["scheduler"]
+    listed_origins = check_listed_origins(
+        scheduler=scheduler,
+        lister_name=lister_name,
+        instance_name=instance_name,
+    )
+    listed_origins_table = [
+        (origin.url, str(origin.last_seen), str(origin.last_update))
+        for origin in listed_origins
+    ]
+    headers = ["url", "last_seen", "last_update"]
+
+    if list:
+        print(tabulate(listed_origins_table, headers))
+    print(
+        f"\nForge {instance_name} ({lister_name}) has {len(listed_origins)} \
+listed origins in the scheduler database."
+    )
+
+
+def _print_status_summary(
+    lister_name: str, instance_name: str, status_counters: Dict, watch: bool = False
+) -> None:
+    """Print a status of the ingestion and a small success rate summary."""
+    if watch:
+        suffix_str = " ingestion is still in progress..."
+    else:
+        suffix_str = f" ({lister_name}) has {status_counters['total']} \
+scheduled ingests in the scheduler."
+
+    print(f"\nForge {instance_name}{suffix_str}")
+    for status, counter in status_counters.items():
+        print(f"{status:<12}: {counter}")
+
+    success_rate = status_counters["successful"] / status_counters["total"] * 100
+    print(f"{'success rate':<12}: {success_rate:.2f}%")
+
+
+@origin.command("check-ingested-origins")
+@click.option(
+    "--list",
+    "-l",
+    is_flag=True,
+    help="Display listed origins (disabled by default).",
+    required=False,
+)
+@click.option(
+    "--watch",
+    "-w",
+    is_flag=True,
+    required=False,
+    help="Watch periodically for ingestion status.",
+)
+@click.option(
+    "--watch-period",
+    required=False,
+    default="30 minutes",
+    help="Watch period ingestion.",
+)
+@click.argument("lister_name", nargs=1, required=True)
+@click.argument("instance_name", nargs=1, required=True)
+@click.pass_context
+def check_ingested_origins_cli(
+    ctx, list, watch, watch_period, lister_name, instance_name
+):
+    """
+    Check the origins marked as ingested in the scheduler database.
+    """
+
+    from time import sleep
+
+    from .utils import check_listed_origins, count_ingested_origins, parse_time_interval
+
+    scheduler = ctx.obj["scheduler"]
+    listed_origins = check_listed_origins(
+        scheduler=scheduler,
+        lister_name=lister_name,
+        instance_name=instance_name,
+    )
+    ids = [(origin.url, origin.visit_type) for origin in listed_origins]
+    status_counters = count_ingested_origins(
+        scheduler=scheduler,
+        ids=ids,
+        instance_name=instance_name,
+    )
+
+    if watch:
+        watch_period_seconds = parse_time_interval(watch_period).total_seconds()
+        while status_counters["None"] != 0:
+            status_counters = count_ingested_origins(
+                scheduler=scheduler,
+                ids=ids,
+                instance_name=instance_name,
+            )
+            if status_counters["None"] > 0:
+                _print_status_summary(
+                    lister_name, instance_name, status_counters, watch
+                )
+
+            sleep(watch_period_seconds)
+
+    status_counters = count_ingested_origins(
+        scheduler=scheduler,
+        ids=ids,
+        instance_name=instance_name,
+        displayed=list,
+    )
+    _print_status_summary(lister_name, instance_name, status_counters)
---
+++ packages/swh.scheduler/swh.scheduler-2.1.0.tar.gz/swh.scheduler-2.1.0/swh/scheduler/tests/test_cli_origin.py
@@ -0,0 +1,204 @@
+# Copyright (C) 2021  The Software Heritage developers
+# See the AUTHORS file at the top-level directory of this distribution
+# License: GNU General Public License version 3, or any later version
+# See top-level LICENSE file for more information
+
+from typing import Tuple
+from unittest.mock import MagicMock
+
+import pytest
+
+from swh.scheduler.cli.origin import format_origins
+from swh.scheduler.tests.common import TASK_TYPES
+from swh.scheduler.tests.test_cli import invoke as basic_invoke
+
+
+def invoke(scheduler, args: Tuple[str, ...] = (), catch_exceptions: bool = False):
+    return basic_invoke(
+        scheduler, args=["origin", *args], catch_exceptions=catch_exceptions
+    )
+
+
+def test_cli_origin(swh_scheduler):
+    """Check that swh scheduler origin returns its help text"""
+
+    result = invoke(swh_scheduler)
+
+    assert "Commands:" in result.stdout
+
+
+def test_format_origins_basic(listed_origins):
+    listed_origins = listed_origins[:100]
+
+    ctx = MagicMock()
+
+    basic_output = list(format_origins(ctx, listed_origins))
+    assert ctx.method_calls == []
+    # 1 header line + all origins
+    assert len(basic_output) == len(listed_origins) + 1
+
+    no_header_output = list(format_origins(ctx, listed_origins, with_header=False))
+    assert ctx.method_calls == []
+    assert basic_output[1:] == no_header_output
+
+
+def test_format_origins_fields_unknown(listed_origins):
+    listed_origins = listed_origins[:10]
+
+    ctx = MagicMock()
+    it = format_origins(ctx, listed_origins, fields=["unknown_field"])
+    assert ctx.method_calls == []
+
+    next(it)
+    ctx.fail.assert_called()
+
+
+def test_format_origins_fields(listed_origins):
+    listed_origins = listed_origins[:10]
+    fields = ["lister_id", "url", "visit_type"]
+
+    ctx = MagicMock()
+    output = list(format_origins(ctx, listed_origins, fields=fields))
+    assert ctx.method_calls == []
+    assert output[0] == ",".join(fields)
+    for i, origin in enumerate(listed_origins):
+        assert output[i + 1] == f"{origin.lister_id},{origin.url},{origin.visit_type}"
+
+
+def test_grab_next(swh_scheduler, listed_origins_by_type):
+    NUM_RESULTS = 10
+    # Strict inequality to check that grab_next_visits doesn't return more
+    # results than requested
+
+    # XXX: should test all of 'listed_origins_by_type' here...
+    visit_type = next(iter(listed_origins_by_type))
+    assert len(listed_origins_by_type[visit_type]) > NUM_RESULTS
+
+    for origins in listed_origins_by_type.values():
+        swh_scheduler.record_listed_origins(origins)
+
+    result = invoke(swh_scheduler, args=("grab-next", visit_type, str(NUM_RESULTS)))
+    assert result.exit_code == 0
+
+    out_lines = result.stdout.splitlines()
+    assert len(out_lines) == NUM_RESULTS + 1
+
+    fields = out_lines[0].split(",")
+    returned_origins = [dict(zip(fields, line.split(","))) for line in out_lines[1:]]
+
+    # Check that we've received origins we had listed in the first place
+    assert set(origin["url"] for origin in returned_origins) <= set(
+        origin.url for origin in listed_origins_by_type[visit_type]
+    )
+
+
+def test_schedule_next(swh_scheduler, listed_origins_by_type):
+    for task_type in TASK_TYPES.values():
+        swh_scheduler.create_task_type(task_type)
+
+    NUM_RESULTS = 10
+    # Strict inequality to check that grab_next_visits doesn't return more
+    # results than requested
+    visit_type = next(iter(listed_origins_by_type))
+    assert len(listed_origins_by_type[visit_type]) > NUM_RESULTS
+
+    for origins in listed_origins_by_type.values():
+        swh_scheduler.record_listed_origins(origins)
+
+    result = invoke(swh_scheduler, args=("schedule-next", visit_type, str(NUM_RESULTS)))
+    assert result.exit_code == 0
+
+    # pull all tasks out of the scheduler
+    tasks = swh_scheduler.search_tasks()
+    assert len(tasks) == NUM_RESULTS
+
+    scheduled_tasks = {
+        (task["type"], task["arguments"]["kwargs"]["url"]) for task in tasks
+    }
+    all_possible_tasks = {
+        (f"load-{origin.visit_type}", origin.url)
+        for origin in listed_origins_by_type[visit_type]
+    }
+
+    assert scheduled_tasks <= all_possible_tasks
+
+
+def test_send_to_celery_unknown_visit_type(
+    swh_scheduler,
+):
+    "Calling cli without a known visit type should raise"
+    result = invoke(
+        swh_scheduler,
+        args=("send-to-celery", "unknown-visit-type"),
+        catch_exceptions=True,
+    )
+    assert "Unknown" in result.output
+    assert result.exit_code != 0
+
+
+@pytest.mark.parametrize(
+    "extra_cmd_args",
+    [
+        [],
+        ["--lister-name", "github", "--lister-instance-name", "github"],
+        [
+            "--absolute-cooldown",
+            "24 days",
+            "--scheduled-cooldown",
+            "24 hours",
+            "--failed-cooldown",
+            "1 day",
+            "--not-found-cooldown",
+            "60 days",
+        ],
+    ],
+)
+def test_send_to_celery(
+    mocker,
+    swh_scheduler,
+    swh_scheduler_celery_app,
+    listed_origins_by_type,
+    extra_cmd_args,
+):
+    for task_type in TASK_TYPES.values():
+        swh_scheduler.create_task_type(task_type)
+
+    visit_type = next(iter(listed_origins_by_type))
+
+    for origins in listed_origins_by_type.values():
+        swh_scheduler.record_listed_origins(origins)
+
+    get_queue_length = mocker.patch(
+        "swh.scheduler.celery_backend.config.get_queue_length"
+    )
+    get_queue_length.return_value = None
+
+    send_task = mocker.patch.object(swh_scheduler_celery_app, "send_task")
+    send_task.return_value = None
+
+    cmd_args = ["send-to-celery", visit_type] + extra_cmd_args
+
+    result = invoke(swh_scheduler, args=tuple(cmd_args))
+    assert result.exit_code == 0
+
+    scheduled_tasks = {
+        (call[0][0], call[1]["kwargs"]["url"]) for call in send_task.call_args_list
+    }
+
+    expected_tasks = {
+        (TASK_TYPES[origin.visit_type]["backend_name"], origin.url)
+        for origin in listed_origins_by_type[visit_type]
+    }
+
+    assert expected_tasks == scheduled_tasks
+
+
+def test_update_metrics(swh_scheduler, listed_origins):
+    swh_scheduler.record_listed_origins(listed_origins)
+
+    assert swh_scheduler.get_metrics() == []
+
+    result = invoke(swh_scheduler, args=("update-metrics",))
+
+    assert result.exit_code == 0
+    assert swh_scheduler.get_metrics() != []
