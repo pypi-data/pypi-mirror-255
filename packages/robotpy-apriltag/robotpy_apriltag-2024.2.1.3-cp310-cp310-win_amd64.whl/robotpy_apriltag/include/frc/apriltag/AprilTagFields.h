0d50c1895197bcbd223e09a3777896d954039349
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 05:05:06 +0000

    Add package robotpy-apriltag/2024.2.1.3/robotpy_apriltag-2024.2.1.3-cp310-cp310-win_amd64.whl

---
+++ packages/robotpy-apriltag/robotpy_apriltag-2024.2.1.3-cp310-cp310-win_amd64.whl/robotpy_apriltag/include/frc/apriltag/AprilTagFields.h
@@ -0,0 +1,39 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#pragma once
+
+#include <string_view>
+
+#include <wpi/SymbolExports.h>
+
+#include "frc/apriltag/AprilTagFieldLayout.h"
+
+namespace frc {
+
+/**
+ * Loadable AprilTag field layouts.
+ */
+enum class AprilTagField {
+  /// 2022 Rapid React.
+  k2022RapidReact,
+  /// 2023 Charged Up.
+  k2023ChargedUp,
+  /// 2024 Crescendo.
+  k2024Crescendo,
+
+  // This is a placeholder for denoting the last supported field. This should
+  // always be the last entry in the enum and should not be used by users
+  kNumFields,
+};
+
+/**
+ * Loads an AprilTagFieldLayout from a predefined field
+ *
+ * @param field The predefined field
+ */
+WPILIB_DLLEXPORT AprilTagFieldLayout
+LoadAprilTagLayoutField(AprilTagField field);
+
+}  // namespace frc
