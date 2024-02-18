26203dbaea7b37617d23b33303c41567046d2cc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 18:04:43 +0000

    Add package vsc-dm/0.0.1.7838385287/vsc_dm-0.0.1.7838385287-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/vsc-dm/vsc_dm-0.0.1.7838385287-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/vsc_dm/include/vsc/dm/ITypeExprFieldRef.h
@@ -0,0 +1,51 @@
+/*
+ * ITypeExprFieldRef.h
+ *
+ *  Created on: Apr 7, 2022
+ *      Author: mballance
+ */
+
+#pragma once
+#include <vector>
+#include "vsc/dm/ITypeExpr.h"
+
+namespace vsc {
+namespace dm {
+
+
+class ITypeExprFieldRef;
+using ITypeExprFieldRefUP=UP<ITypeExprFieldRef>;
+class ITypeExprFieldRef : public ITypeExpr {
+public:
+enum class RootRefKind {
+	TopDownScope,
+	BottomUpScope,
+    RootExpr
+};
+
+public:
+
+	virtual ~ITypeExprFieldRef() { }
+
+    virtual ITypeExpr *getRootExpr() const = 0;
+
+	virtual RootRefKind getRootRefKind() const = 0;
+
+    virtual int32_t getRootRefOffset() const = 0;
+
+    virtual void setRootRefOffset(int32_t off) = 0;
+
+	virtual void addPathElem(int32_t idx) = 0;
+
+	virtual uint32_t size() const = 0;
+
+	virtual int32_t at(int32_t idx) const = 0;
+
+	virtual const std::vector<int32_t> &getPath() const = 0;
+
+	virtual std::vector<int32_t> &getPath() = 0;
+
+};
+
+}
+}
