fb0a7103ec846c0ab5bf4e58a474607738a1e7b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 22:05:15 +0000

    Add package blacs/3.2.1/blacs-3.2.1.tar.gz

---
+++ packages/blacs/blacs-3.2.1.tar.gz/blacs-3.2.1/setup.py
@@ -0,0 +1,9 @@
+import os
+from setuptools import setup
+
+setup(
+    use_scm_version={
+        "version_scheme": os.getenv("SCM_VERSION_SCHEME", "release-branch-semver"),
+        "local_scheme": os.getenv("SCM_LOCAL_SCHEME", "node-and-date"),
+    }
+)
