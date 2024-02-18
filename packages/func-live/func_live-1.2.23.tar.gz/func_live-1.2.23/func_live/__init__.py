3ff7898248e62491f5780806838da299c035c4a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 15:06:15 +0000

    Add package func-live/1.2.23/func_live-1.2.23.tar.gz

---
+++ packages/func-live/func_live-1.2.23.tar.gz/func_live-1.2.23/func_live/__init__.py
@@ -0,0 +1,37 @@
+import os
+import requests
+from .config import API_URL
+from .recursively_upload_filepaths import recursively_upload_filepaths
+
+class FuncLive:
+    def __init__(self):
+        self._fetch_functions()
+
+    def _fetch_functions(self):
+        response = requests.get(f"{API_URL}/functions")
+        functions = response.json().get('functions', [])
+
+        for func in functions:
+            func_name = func['name']
+            setattr(self, func_name, self._create_function(func_name))
+
+    def _create_function(self, func_name):
+        def func(input_data=None):
+            try:
+                modified_input = None if input_data is None else recursively_upload_filepaths(input_data)           
+                payload = {'input': modified_input} if modified_input is not None else {}
+                
+                response = requests.post(f"{API_URL}/functions/{func_name}",
+                                         headers={
+                                             'Authorization': f'Bearer {os.getenv("FUNC_TOKEN")}',
+                                             'Accept': 'application/json',
+                                             'Content-Type': 'application/json'
+                                         },
+                                         json=payload)
+                return response.json().get('output')
+            except requests.RequestException as e:
+                raise Exception(f"There was an error: {str(e)}")
+
+        return func
+
+func = FuncLive()
