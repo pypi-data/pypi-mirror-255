59d61fba28654feaa03ae7365102d4213b0cfac8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 21:05:50 +0000

    Add package feza/0.0.1/feza-0.0.1.tar.gz

---
+++ packages/feza/feza-0.0.1.tar.gz/feza-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='feza',
+    packages=["feza"],
+    version='0.0.1',
+    long_description="""feza karanlığa boğulsa da benimlesin""",
+    long_description_content_type='text/markdown',
+    author='fswair',
+)
