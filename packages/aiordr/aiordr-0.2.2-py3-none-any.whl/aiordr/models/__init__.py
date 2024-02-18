67acf4a0363ee76f686b6df6a9e9ef61b7e24ab9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 22:04:35 +0000

    Add package aiordr/0.2.2/aiordr-0.2.2-py3-none-any.whl

---
+++ packages/aiordr/aiordr-0.2.2-py3-none-any.whl/aiordr/models/__init__.py
@@ -0,0 +1,9 @@
+"""This module contains models for the o!rdr API."""
+from __future__ import annotations
+
+from .base import *
+from .errorcode import *
+from .events import *
+from .render import *
+from .renderserver import *
+from .skin import *
