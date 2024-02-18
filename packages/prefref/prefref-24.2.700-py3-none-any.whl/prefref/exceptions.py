3bbc7b5da35e7598027378c872e846dd3ce749ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 11:05:32 +0000

    Add package prefref/24.2.700/prefref-24.2.700-py3-none-any.whl

---
+++ packages/prefref/prefref-24.2.700-py3-none-any.whl/prefref/exceptions.py
@@ -0,0 +1,22 @@
+# custom exception types for this project
+class PrefRefError(Exception):
+  # base class
+  pass
+
+class PrefRefOptionNotFound(PrefRefError):
+  def __init__(self, message, code: int = 569) -> None:
+    self.message = message
+    self.code = code
+    super().__init__(f'{code}: {message}')
+
+class PrefRefUnknownArgument(PrefRefError):
+  def __init__(self, message, code: int = 569) -> None:
+    self.message = message
+    self.code = code
+    super().__init__(f'{code}: {message}')
+
+class PrefRefMissingRequiredOption(PrefRefError):
+  def __init__(self, message, code: int = 569) -> None:
+    self.message = message
+    self.code = code
+    super().__init__(f'{code}: {message}')
