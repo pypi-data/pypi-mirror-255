223df1400c3fd261ca5df1b2ea36815c44372c8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 07:04:20 +0000

    Add package scaleway/1.5.0/scaleway-1.5.0.tar.gz

---
+++ packages/scaleway/scaleway-1.5.0.tar.gz/scaleway-1.5.0/scaleway/jobs/v1alpha1/__init__.py
@@ -0,0 +1,29 @@
+# This file was automatically generated. DO NOT EDIT.
+# If you have any remark or suggestion do not hesitate to open an issue.
+from .types import JobRunState
+from .types import ListJobDefinitionsRequestOrderBy
+from .types import ListJobRunsRequestOrderBy
+from .types import CreateJobDefinitionRequestCronScheduleConfig
+from .types import CronSchedule
+from .types import JobDefinition
+from .types import JobRun
+from .types import ListJobDefinitionsResponse
+from .types import ListJobRunsResponse
+from .types import UpdateJobDefinitionRequestCronScheduleConfig
+from .content import JOB_RUN_TRANSIENT_STATUSES
+from .api import JobsV1Alpha1API
+
+__all__ = [
+    "JobRunState",
+    "ListJobDefinitionsRequestOrderBy",
+    "ListJobRunsRequestOrderBy",
+    "CreateJobDefinitionRequestCronScheduleConfig",
+    "CronSchedule",
+    "JobDefinition",
+    "JobRun",
+    "ListJobDefinitionsResponse",
+    "ListJobRunsResponse",
+    "UpdateJobDefinitionRequestCronScheduleConfig",
+    "JOB_RUN_TRANSIENT_STATUSES",
+    "JobsV1Alpha1API",
+]
