5b3090b682067b4a2a513ee702ae1e6ad8ac15d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 16:08:18 +0000

    Add package stockdex/0.1.3/stockdex-0.1.3.tar.gz

---
+++ packages/stockdex/stockdex-0.1.3.tar.gz/stockdex-0.1.3/setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages, setup
+
+from __version__ import version
+
+setup(
+    name="stockdex",
+    version=version,
+    packages=find_packages(),
+    install_requires=["pandas==2.0.3", "beautifulsoup4==4.12.2"],
+    python_requires=">=3.8",
+    author="Amir Nazary",
+    description="A package to get stock data from Yahoo Finance",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/ahnazary/stockdex",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
