edb3ac9ecb6e803f43337ade6d2320c118ab3c10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 23:05:10 +0000

    Add package vickyspython/1.2/vickyspython-1.2.tar.gz

---
+++ packages/vickyspython/vickyspython-1.2.tar.gz/vickyspython-1.2/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='vickyspython',
+     version='1.2',
+     packages=['vickyspython'],
+     author='Vikram singh', 
+     author_email='Vikramkumrsingh.kpt@gmail.com',
+     zip_safe = False)
