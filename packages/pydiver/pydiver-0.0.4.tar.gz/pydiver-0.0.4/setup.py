d016f995d235a5c0d02d37a2d50cd4276cd8522f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 00:05:38 +0000

    Add package pydiver/0.0.4/pydiver-0.0.4.tar.gz

---
+++ packages/pydiver/pydiver-0.0.4.tar.gz/pydiver-0.0.4/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+setuptools.setup(
+    name="pydiver",
+    version="0.0.4",
+    author="Almos Gero",
+    author_email="almos.gero@gmail.com",
+    description="A pydiver package",
+    url="https://github.com/modules.py/pydiver",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages = setuptools.find_packages(),
+)
