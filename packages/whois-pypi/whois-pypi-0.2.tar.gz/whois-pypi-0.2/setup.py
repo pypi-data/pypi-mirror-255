b392c3d2a2ec3472287ec449c223bc617fdde587
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 06:05:04 +0000

    Add package whois-pypi/0.2/whois-pypi-0.2.tar.gz

---
+++ packages/whois-pypi/whois-pypi-0.2.tar.gz/whois-pypi-0.2/setup.py
@@ -0,0 +1,39 @@
+import sys, os
+import setuptools
+
+
+def read(filename):
+    return open(os.path.join(os.path.dirname(__file__), filename)).read()
+
+setuptools.setup(
+    name='whois-pypi',
+    version='0.2',
+    description="Whois querying and parsing of domain registration information.",
+    long_description=read('README.rst'),
+    install_requires=[
+        'future',
+    ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+        'Programming Language :: Python :: 3',
+    ],
+    keywords='whois, python',
+    author='Paradoxxs',
+    author_email='pypi@adamtilmar.com',
+    url='https://github.com/Paradoxxs/pywhois',
+    license='MIT',
+    packages=['whois'],
+    package_dir={'whois':'whois'},
+    extras_require={
+        'better date conversion': ["python-dateutil"]
+    },
+    test_suite='nose.collector',
+    tests_require=['nose', 'simplejson'],
+    include_package_data=True,
+    zip_safe=False
+)
---
+++ packages/whois-pypi/whois-pypi-0.2.tar.gz/whois-pypi-0.2/test/test_nicclient.py
@@ -0,0 +1,16 @@
+# coding=utf-8
+
+import unittest
+from whois.whois import NICClient
+
+
+class TestNICClient(unittest.TestCase):
+    def setUp(self):
+        self.client = NICClient()
+
+    def test_choose_server(self):
+        domain = 'рнидс.срб'
+        chosen = self.client.choose_server(domain)
+        suffix = domain.split('.')[-1].encode('idna').decode('utf-8')
+        correct = '{}.whois-servers.net'.format(suffix)
+        self.assertEqual(chosen, correct)
