aee7e689caa59b71fc28cc49bde2a35e879d915a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 06:04:29 +0000

    Add package promplate-pyodide/0.0.1.dev4/promplate_pyodide-0.0.1.dev4-py3-none-any.whl

---
+++ packages/promplate-pyodide/promplate_pyodide-0.0.1.dev4-py3-none-any.whl/promplate_pyodide/__init__.py
@@ -0,0 +1,60 @@
+from pathlib import Path
+from typing import TYPE_CHECKING, Callable
+
+from .utils.js import to_js
+
+
+def patch_promplate():
+    import promplate
+
+    class Loader(promplate.template.Loader):
+
+        @classmethod
+        async def afetch(cls, url: str, **kwargs):
+            if TYPE_CHECKING:
+
+                async def pyfetch(url: str):
+                    class Response:
+                        async def text(self):
+                            return url
+
+                    return Response()
+
+            else:
+                from pyodide.http import pyfetch
+
+            res = await pyfetch(cls._join_url(url))
+            obj = cls(await res.text())
+            obj.name = Path(url).stem
+
+            return obj
+
+        @classmethod
+        def fetch(cls, url: str, **kwargs):
+            if TYPE_CHECKING:
+                from io import StringIO
+
+                open_url: Callable[[str], StringIO] = None  # type: ignore
+            else:
+                from pyodide.http import open_url
+
+            res = open_url(cls._join_url(url))
+            obj = cls(res.read())
+            obj.name = Path(url).stem
+
+            return obj
+
+    class Node(Loader, promplate.Node):
+        pass
+
+    class Template(Loader, promplate.Template):
+        pass
+
+    promplate.template.Loader = Loader
+    promplate.template.Template = promplate.Template = Template
+    promplate.node.Node = promplate.Node = Node
+
+    def parse_chat_markup(text: str):
+        return to_js(promplate.chat.parse_chat_markup(text))
+
+    promplate.chat.parse_chat_markup = promplate.parse_chat_markup = parse_chat_markup
