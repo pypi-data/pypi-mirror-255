2d1feb98df101f06e30413be7991dc8a9ab73e45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 19:04:57 +0000

    Add package mleval/0.2/mleval-0.2.tar.gz

---
+++ packages/mleval/mleval-0.2.tar.gz/mleval-0.2/README.md
@@ -0,0 +1,2 @@
+A python package for comprehensive machine learning evaluation.
+
---
+++ packages/mleval/mleval-0.2.tar.gz/mleval-0.2/setup.py
@@ -0,0 +1,15 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='mleval',
+    version='0.2',
+    packages=find_packages(),
+    license='N/A',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Duncan Stothers',
+    author_email='duncanstothers3699@gmail.com',
+    url='https://github.com/Radar3699/mleval',
+)
