ae3a4a241735ff3d2da03ff7b42de99c31952240
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 22:04:52 +0000

    Add package blockporn/0.1.1/blockporn-0.1.1.tar.gz

---
+++ packages/blockporn/blockporn-0.1.1.tar.gz/blockporn-0.1.1/Blockporn/module.py
@@ -0,0 +1,65 @@
+import os
+from Blockporn.scripts import Scripted
+#====================================================================
+
+class Blocker:
+
+    def __init__(self, block=None):
+        self.addlk = block
+        self.block = self.readblock()
+        self.cusom = block if block else []
+
+#====================================================================
+
+    def cloenlink(self, cleaned):
+        emoonsond = cleaned.split("/")
+        ouenoined = emoonsond[0]
+        return ouenoined
+
+#====================================================================
+    
+    def blocked(self, incoming):
+        coxonos = str(incoming)
+        cleaned = self.cleanlink(coxonos)
+        matterd = self.cloenlink(cleaned)
+        for blockers in self.cusom:
+            if matterd == blockers:
+                return True
+        else:
+            return False
+    
+#====================================================================
+
+    def blocker(self, incoming):
+        coxonos = str(incoming)
+        cleaned = self.cleanlink(coxonos)
+        matterd = self.cloenlink(cleaned)
+        for blockers in self.block:
+            if matterd == blockers:
+                return True
+        else:
+            return False
+
+#====================================================================
+    
+    def readblock(self):
+        osem_path = os.path.abspath(os.path.dirname(__file__))
+        file_path = os.path.join(osem_path, Scripted.DATA03)
+        with open(file_path, 'r') as filed:
+            listed = filed.read().splitlines()
+            listed.extend(self.addlk) if self.addlk else listed
+            return listed
+
+#====================================================================
+    
+    def cleanlink(self, incoming):
+        if incoming.startswith(Scripted.DATA01):
+             finals = incoming.replace(Scripted.DATA01, "", 1)
+             return finals
+        elif incoming.startswith(Scripted.DATA02):
+             finals = incoming.replace(Scripted.DATA02, "", 1)
+             return finals
+        else:
+             return incoming
+
+#====================================================================
---
+++ packages/blockporn/blockporn-0.1.1.tar.gz/blockporn-0.1.1/README.md
@@ -0,0 +1,69 @@
+<p align="center">
+     PORN BLOCKER © 2023 - 2024
+</p>
+
+<p align="center">
+   <a href="https://telegram.dog/clinton_abraham"><img src="https://img.shields.io/badge/𝑪𝒍𝒊𝒏𝒕𝒐𝒏 𝑨𝒃𝒓𝒂𝒉𝒂𝒎-30302f?style=flat&logo=telegram" alt="telegram badge"/></a>
+   <a href="https://telegram.dog/Space_x_bots"><img src="https://img.shields.io/badge/Sᴘᴀᴄᴇ ✗ ʙᴏᴛꜱ-30302f?style=flat&logo=telegram" alt="telegram badge"/></a>
+   <a href="https://telegram.dog/sources_codes"><img src="https://img.shields.io/badge/Sᴏᴜʀᴄᴇ ᴄᴏᴅᴇꜱ-30302f?style=flat&logo=telegram" alt="telegram badge"/></a>
+</p>
+
+## INSTALLATION
+
+```bash
+  pip install blockporn
+```
+
+## USAGE
+
+```python
+
+from Blockporn import Blocker
+
+url = "https://xxx.com"
+
+core = Blocker()
+
+resu = core.blocker(url)
+
+print(resu)
+
+```
+
+```python
+#======[ ADD MORE SITES ]======
+
+from Blockporn import Blocker
+
+url = "https://xxx.dc"
+
+blok = ["xxx.db", "xxx.dc"]
+
+core = Blocker(block=blok)
+
+resu = core.blocker(url)
+
+print(resu)
+
+```
+
+```python
+#======[ CUSTOM BLOCK ]======
+
+from Blockporn import Blocker
+
+url = "https://xxx.me"
+
+blok = ["xxx.me", "xxx.ms"]
+
+core = Blocker(block=blok)
+
+resu = core.blocked(url)
+
+print(resu)
+
+```
+
+## LICENSE
+
+The MIT License. Please see [License File](https://github.com/Clinton-Abraham/PORN-X-BLOCKER/blob/V1.0/LICENSE) for more information.
