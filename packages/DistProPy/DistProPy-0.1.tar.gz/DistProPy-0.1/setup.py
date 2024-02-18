5bf576f22214c539c9f17e54ce38f4072ec3fcfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 21:04:52 +0000

    Add package DistProPy/0.1/DistProPy-0.1.tar.gz

---
+++ packages/DistProPy/DistProPy-0.1.tar.gz/DistProPy-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='DistProPy',
+    version='0.1',
+    description='Gaussian and Binomial distributions',
+    packages=['DistProPy'],
+    author='Carlos Bonilla',
+    author_email='cabonillao.oc@gmail.com',
+    zip_safe=False)
