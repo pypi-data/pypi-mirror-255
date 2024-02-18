b9abb7b6f42cf123fd6a136da1dc98b96d88ff76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 09:04:41 +0000

    Add package metrics_layer/0.11.11/metrics_layer-0.11.11.tar.gz

---
+++ packages/metrics_layer/metrics_layer-0.11.11.tar.gz/metrics_layer-0.11.11/metrics_layer/core/model/definitions.py
@@ -0,0 +1,25 @@
+class Definitions:
+    snowflake = "SNOWFLAKE"
+    bigquery = "BIGQUERY"
+    redshift = "REDSHIFT"
+    postgres = "POSTGRES"
+    druid = "DRUID"
+    sql_server = "SQL_SERVER"
+    duck_db = "DUCK_DB"
+    databricks = "DATABRICKS"
+    azure_synapse = "AZURE_SYNAPSE"
+    supported_warehouses = [
+        snowflake,
+        bigquery,
+        redshift,
+        postgres,
+        druid,
+        sql_server,
+        duck_db,
+        databricks,
+        azure_synapse,
+    ]
+    supported_warehouses_text = ", ".join(supported_warehouses)
+
+    does_not_exist = "__DOES_NOT_EXIST__"
+    canon_date_join_graph_root = "canon_date_core"
