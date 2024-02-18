cff232c8c143896970817c03baefa981df087e7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 02:12:19 +0000

    Add package aceawslogger/0.2.1/aceawslogger-0.2.1.tar.gz

---
+++ packages/aceawslogger/aceawslogger-0.2.1.tar.gz/aceawslogger-0.2.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='aceawslogger', 
+      version='0.2.1', 
+      author='ace',
+      author_email='kardaras.george@clublabs.com',
+      description='AWS Logger',
+      packages=find_packages(),
+      install_requires=[],
+      )
