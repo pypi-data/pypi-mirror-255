cb865a568a21250e206c062d5c7851c18f1361bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 12:07:42 +0000

    Add package singularity-hpc/0.1.27/singularity_hpc-0.1.27-py3-none-any.whl

---
+++ packages/singularity-hpc/singularity_hpc-0.1.27-py3-none-any.whl/shpc/main/container/update/docker.py
@@ -0,0 +1,64 @@
+__author__ = "Vanessa Sochat"
+__copyright__ = "Copyright 2021-2024, Vanessa Sochat"
+__license__ = "MPL 2.0"
+
+import re
+
+import requests
+
+from shpc.logger import logger
+
+
+class DockerImage:
+
+    """
+    A thin client for getting metadata about an image.
+    """
+
+    def __init__(self, container_name):
+        self.container_name = container_name
+
+        # might not last forever, but we can use it for now
+        self.apiroot = "https://crane.ggcr.dev"
+
+    def get_request(self, url):
+        """
+        Perform a get request, expecting status code 200.
+        """
+        response = requests.get(url)
+        if response.status_code != 200:
+            logger.exit("Issue with request %s" % url)
+        return response
+
+    def tags(self):
+        """
+        Get image tags.
+        """
+        url = "%s/ls/%s" % (self.apiroot, self.container_name)
+        response = self.get_request(url)
+        tags = [x.strip() for x in response.text.split("\n") if x.strip()]
+        # Don't include tags for vex or sbom
+        tags = [x for x in tags if not re.search("[.](sbom|vex)$", x)]
+        return tags
+
+    def manifest(self, tag):
+        url = "%s/manifest/%s:%s" % (self.apiroot, self.container_name, tag)
+        response = self.get_request(url)
+        return response.json()
+
+    def digest(self, tag):
+        url = "%s/digest/%s:%s" % (self.apiroot, self.container_name, tag)
+        response = self.get_request(url)
+        if "could not parse reference" in response:
+            logger.exit("Issue getting digest: %s" % response)
+        if "unsupported status" in response:
+            logger.exit("Issue getting digest: %s" % response)
+        if "MANIFEST_UNKNOWN" in response.text:
+            logger.exit(
+                f"The tag {tag} you provided is not known. Check that it and the container both exist."
+            )
+        return response.text
+
+    def config(self):
+        url = "%s/config/%s" % (self.apiroot, self.container_name)
+        return self.get_request(url).json()
---
+++ packages/singularity-hpc/singularity_hpc-0.1.27-py3-none-any.whl/shpc/tests/helpers.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python
+
+# Copyright (C) 2021-2023 Vanessa Sochat.
+
+# This Source Code Form is subject to the terms of the
+# Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed
+# with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import os
+import shutil
+
+from shpc.main import get_client
+from shpc.main.registry import GitHub
+
+here = os.path.dirname(os.path.abspath(__file__))
+root = os.path.dirname(here)
+
+
+def init_client(tmpdir, module_sys, container_tech, remote=True):
+    """
+    Get a common client for some container technology and module system
+    """
+    settings_file = os.path.join(root, "settings.yml")
+    new_settings = os.path.join(tmpdir, "settings.yml")
+    shutil.copyfile(settings_file, new_settings)
+    client = get_client(
+        quiet=False,
+        settings_file=new_settings,
+        module_sys=module_sys,
+        container_tech=container_tech,
+    )
+
+    # The module and views folders need to be temporary too
+    modules = os.path.join(tmpdir, "modules")
+    containers = os.path.join(tmpdir, "containers")
+    views = os.path.join(tmpdir, "views")
+    client.settings.set("module_base", modules)
+    client.settings.set("container_base", containers)
+    client.settings.set("views_base", views)
+
+    # If it's not remote, create a temporary filesystem registry
+    if not remote:
+        clone_dir = os.path.join(tmpdir, "registry")
+        reg = GitHub("https://github.com/singularityhub/shpc-registry")
+        reg.clone(clone_dir)
+        client.settings.registry = [clone_dir]
+        client.reload_registry()
+    client.settings.save()
+
+    # Reinit views so they are detected in the temporary location
+    client.detect_views()
+    return client
