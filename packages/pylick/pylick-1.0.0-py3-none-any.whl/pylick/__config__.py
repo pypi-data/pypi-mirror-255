8df05d3bafdc843d3f86d27624e6ef2499675c99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 06:04:28 +0000

    Add package pylick/1.0.0/pylick-1.0.0-py3-none-any.whl

---
+++ packages/pylick/pylick-1.0.0-py3-none-any.whl/pylick/__config__.py
@@ -0,0 +1,10 @@
+import os
+import pylick
+
+__ROOT__    = os.path.dirname(pylick.__file__)
+
+__table__   = __ROOT__ + '/data/index_table.dat'
+__style__   = __ROOT__ + '/data/plotbelli.style'
+
+__dirRes__  = '../outputs/'
+__dirPlot__ = '../outputs/plots/'
