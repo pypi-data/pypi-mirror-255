f5a0d494046f79bcb0ec5a2adf9adb88473417d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 23:07:09 +0000

    Add package eburger/1.0.0/eburger-1.0.0.tar.gz

---
+++ packages/eburger/eburger-1.0.0.tar.gz/eburger-1.0.0/eburger/utils/helpers.py
@@ -0,0 +1,86 @@
+from datetime import datetime
+import json
+from pathlib import Path
+import re
+import shlex
+import subprocess
+from eburger import settings
+
+from eburger.utils.cli_args import args
+from eburger.utils.filesystem import get_all_solidity_files
+from eburger.utils.logger import log
+
+
+def is_valid_json(json_string: str) -> bool:
+    if not json_string:
+        return False
+    try:
+        json.loads("".join(json_string))
+        return True
+    except ValueError:
+        return False
+
+
+def run_command(
+    command: str, directory: Path = None, shell: bool = False, live_output: bool = False
+):
+    log("info", f"{command}")
+    results = []
+    errors = []
+    process = subprocess.Popen(
+        command if shell else shlex.split(command),
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        encoding="utf-8",
+        shell=shell,
+        cwd=directory,
+    )
+    while True:
+        output = process.stdout.readline()
+        error = process.stderr.readline()
+
+        if output == "" and error == "" and process.poll() is not None:
+            break
+        if output:
+            output_stripped = output.strip()
+            if live_output:
+                if args.debug:
+                    log("debug", output_stripped)
+                else:
+                    log("info", output_stripped)
+            results.append(output_stripped)
+
+        if error:
+            error_stripped = error.strip()
+            if live_output:
+                if args.debug:
+                    log("debug", error_stripped)
+                else:
+                    log("info", error_stripped)
+            errors.append(error_stripped)
+
+    return results, errors
+
+
+def construct_solc_cmdline(path_type: str, compilation_source_path: str) -> str:
+    solc_cmdline = "solc"
+    if args.solc_remappings:
+        solc_cmdline += " "
+        solc_cmdline += " ".join(args.solc_remappings)
+    if path_type == "folder":
+        solidity_files = get_all_solidity_files(args.solidity_file_or_folder)
+        compilation_source_path = " ".join(solidity_files)
+    solc_cmdline += f" --allow-paths . --combined-json abi,ast,bin,bin-runtime,srcmap,srcmap-runtime,userdoc,devdoc,hashes {compilation_source_path}"
+    return solc_cmdline
+
+
+def get_filename_from_path(file_path: str) -> tuple:
+    if args.project_name:
+        filename = args.project_name
+    else:
+        filename_match = re.search(r"/([^/]+)\.sol$", file_path)
+        filename = filename_match.group(1) if filename_match else None
+    filename = f"{filename}_{datetime.now().strftime('%m%y')}"
+    output_filename = settings.outputs_dir / f"{filename}.json"
+
+    return filename, output_filename
---
+++ packages/eburger/eburger-1.0.0.tar.gz/eburger-1.0.0/eburger/utils/installers.py
@@ -0,0 +1,111 @@
+import os
+from eburger import settings
+from eburger.utils.helpers import run_command
+from eburger.utils.logger import log
+
+
+def install_foundry_if_not_found():
+    forge_binary_found = False
+    try:
+        run_command("forge -V")
+        forge_binary_found = True
+    except FileNotFoundError:
+        pass
+
+    if not forge_binary_found:
+        log("info", "forge wasn't found on the system, trying to install.")
+        run_command(
+            "curl -L https://foundry.paradigm.xyz | bash",
+            shell=True,
+        )
+        run_command("foundryup")
+        try:
+            run_command("forge -V")
+            log("info", "Successfully installed forge.")
+        except:
+            log(
+                "error",
+                "Couldn't automatically install forge, please install manually and try again.",
+            )
+
+
+def install_hardhat_if_not_found():
+    hardhat_found = False
+    npm_found = False
+    npx_found = False
+
+    try:
+        res, err = run_command("npx hardhat help", directory=settings.project_root)
+        if err:
+            raise FileNotFoundError
+        hardhat_found = True
+    except FileNotFoundError:
+        pass
+    if not hardhat_found:
+        log("info", "Local hardhat not found on project, installing.")
+        try:
+            run_command("npm -v")
+            npm_found = True
+        except FileNotFoundError:
+            log(
+                "error",
+                "Can't automatically install hardhat without npm being installed manually first, please install npm and run again.",
+            )
+
+        if npm_found:
+            try:
+                run_command("npx -v")
+                npx_found = True
+            except FileNotFoundError:
+                log(
+                    "error",
+                    "Couldn't automatically install hardhat, please install manually and try again.",
+                )
+
+            if not npx_found:
+                try:
+                    run_command("npm install -g npx")
+                    run_command("npx -v")
+                except FileNotFoundError:
+                    log(
+                        "error",
+                        "Couldn't automatically install hardhat, please install manually and try again.",
+                    )
+
+            try:
+                if os.path.isfile(os.path.join(settings.project_root, "yarn.lock")):
+                    run_command(
+                        "yarn add --dev hardhat",
+                        directory=settings.project_root,
+                    )
+                else:
+                    run_command(
+                        "npm install --save-dev hardhat",
+                        directory=settings.project_root,
+                    )
+            except:
+                log(
+                    "error",
+                    "Couldn't automatically install hardhat, please install manually and try again.",
+                )
+
+
+def set_solc_compiler_version(solc_required_version: str):
+    log("info", f"Trying to set solc to version {solc_required_version}")
+    solc_use_res, errors = run_command(f"solc-select use {solc_required_version}")
+    if not solc_use_res or errors:
+        log("info", "Trying to install missing solc version")
+        solc_select_install_res, errors = run_command(
+            f"solc-select install {solc_required_version}"
+        )
+        print(solc_select_install_res, errors)
+        solc_select_use_res, errors = run_command(
+            f"solc-select use {solc_required_version}"
+        )
+        print(solc_select_use_res, errors)
+        if not solc_use_res or errors:
+            log("error", "Failed to install required solc version")
+    log(
+        "info",
+        "Successfully set solc version, trying to compile contract.",
+    )
