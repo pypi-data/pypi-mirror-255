5fcb4c3879bb6bdc4a103af935618c4867875439
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 13:05:32 +0000

    Add package swanlab/0.2.0/swanlab-0.2.0-py3-none-any.whl

---
+++ packages/swanlab/swanlab-0.2.0-py3-none-any.whl/swanlab/__init__.py
@@ -0,0 +1,13 @@
+# 导出初始化函数和log函数
+from .data import (
+    init,
+    log,
+    finish,
+    config,
+    Audio,
+    Image,
+)
+from .utils import get_package_version
+
+
+__version__ = get_package_version()
