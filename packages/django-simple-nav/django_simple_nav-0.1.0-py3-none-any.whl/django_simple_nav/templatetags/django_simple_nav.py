d13e0b66902bf99f449d048e989c1df6f8ee471c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 08:05:30 +0000

    Add package django-simple-nav/0.1.0/django_simple_nav-0.1.0-py3-none-any.whl

---
+++ packages/django-simple-nav/django_simple_nav-0.1.0-py3-none-any.whl/django_simple_nav/templatetags/django_simple_nav.py
@@ -0,0 +1,17 @@
+from __future__ import annotations
+
+from django import template
+from django.template import Context
+from django.utils.module_loading import import_string
+
+register = template.Library()
+
+
+@register.simple_tag(takes_context=True)
+def django_simple_nav(
+    context: Context, nav_path: str, template_name: str | None = None
+) -> str:
+    nav = import_string(nav_path)
+    return nav.render_from_request(
+        request=context["request"], template_name=template_name
+    )
