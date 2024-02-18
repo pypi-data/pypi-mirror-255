33429df6a7013ed533d94974baae9485020b7fa8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 02:12:14 +0000

    Add package o2t/0.0.6/o2t-0.0.6.tar.gz

---
+++ packages/o2t/o2t-0.0.6.tar.gz/o2t-0.0.6/o2t/core/pytorch_layers/__init__.py
@@ -0,0 +1,11 @@
+from .arithmetic import Arithmetic
+from .batchnorm import BatchNorm
+from .conv import Conv
+from .embedding_layer import Embedding
+from .Inspector_module import InspectorModule
+from .layernorm import LayerNorm
+from .linear import Linear
+from .observer import Observer
+from .pool import Pool
+from .slice_layer import Slice
+from .torch_layer import Arange, Full, Ones, Reshape, Size, Tensor
