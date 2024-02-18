e6a0d7754ac498bc1538a6e25268391c36b3d59a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 13:04:42 +0000

    Add package trailblazer/20.4.5/trailblazer-20.4.5-py3-none-any.whl

---
+++ packages/trailblazer/trailblazer-20.4.5-py3-none-any.whl/trailblazer/dto/create_analysis_request.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel
+
+from trailblazer.constants import JobType, TrailblazerPriority, TrailblazerTypes, WorkflowManager
+
+
+class CreateAnalysisRequest(BaseModel):
+    case_id: str
+    email: str | None = None
+    config_path: str
+    out_dir: str
+    order_id: int | None = None
+    priority: TrailblazerPriority
+    data_analysis: str | None = None
+    ticket: str | None = None
+    workflow_manager: WorkflowManager | None = None
