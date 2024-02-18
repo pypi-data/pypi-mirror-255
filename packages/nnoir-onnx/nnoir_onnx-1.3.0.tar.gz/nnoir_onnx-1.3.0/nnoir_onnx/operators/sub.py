af96c607a77eca132a27c3ed6978b51d77836990
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 12:06:14 +0000

    Add package nnoir-onnx/1.3.0/nnoir_onnx-1.3.0.tar.gz

---
+++ packages/nnoir-onnx/nnoir_onnx-1.3.0.tar.gz/nnoir_onnx-1.3.0/nnoir_onnx/operators/sub.py
@@ -0,0 +1,23 @@
+from typing import Any, Dict, List, Optional, Tuple
+
+import onnx
+from nnoir.functions import Bias, Function, Sub
+from numpy.typing import NDArray
+
+from .utils import *
+
+
+class OpSub(Op):
+    def __init__(self, node: onnx.NodeProto, *args: Any):
+        super(OpSub, self).__init__(node, *args)
+
+    def to_function(self, env: Dict[str, NDArray[Any]], constants: Dict[str, NDArray[Any]]) -> List[Function]:
+        [a, b] = self.node.input
+        if a in constants and b not in constants:
+            raise UnsupportedONNXOperation(self.node, "unimplemented yet")
+        elif a not in constants and b in constants:
+            return [Bias([a], list(self.node.output), axis=0, b=encode_ndarray(-constants[b]))]
+        elif a not in constants and b not in constants:
+            return [Sub(list(self.node.input), list(self.node.output))]
+        else:
+            raise UnsupportedONNXOperation(self.node, "bug! (unreachable here)")
