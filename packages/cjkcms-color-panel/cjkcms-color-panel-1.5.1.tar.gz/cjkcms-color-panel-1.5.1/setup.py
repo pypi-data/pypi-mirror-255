d6a203549fe5e0c8e576c658bee0424851fa72a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 16:06:12 +0000

    Add package cjkcms-color-panel/1.5.1/cjkcms-color-panel-1.5.1.tar.gz

---
+++ packages/cjkcms-color-panel/cjkcms-color-panel-1.5.1.tar.gz/cjkcms-color-panel-1.5.1/setup.py
@@ -0,0 +1,58 @@
+import io
+import re
+from pathlib import Path
+
+from setuptools import find_packages, setup
+
+install_requires = ["wagtail>=4.1"]
+
+tests_require = ["pytest-django", "wagtail-factories", "pytest"]
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+version = ""
+with io.open("wagtail_color_panel/__init__.py", "r", encoding="utf8") as fd:
+    version = re.search(
+        r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', fd.read(), re.MULTILINE
+    ).group(1)
+
+setup(
+    version=version,
+    name="cjkcms-color-panel",
+    description="",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Martin Sandström. Forked by cjkpl.",
+    author_email="martin@marteinn.se",
+    url="https://github.com/marteinn/wagtail-color-panel",
+    packages=find_packages(
+        exclude=("*.tests", "*.tests.*", "tests.*", "tests", "example*")
+    ),
+    include_package_data=True,
+    install_requires=install_requires,
+    zip_safe=False,
+    tests_require=tests_require,
+    extras_require={"test": tests_require},
+    license="MIT",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Framework :: Wagtail",
+        "Framework :: Wagtail :: 4",
+        "Framework :: Wagtail :: 5",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Topic :: Utilities",
+    ],
+    setup_requires=["setuptools_scm", "pytest-runner"],
+    python_requires=">=3.8",
+)
