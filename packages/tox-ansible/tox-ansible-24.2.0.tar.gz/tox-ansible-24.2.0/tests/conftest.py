60b94c826660ed1f518d189d5e18747f7cbacfff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 19:05:10 +0000

    Add package tox-ansible/24.2.0/tox-ansible-24.2.0.tar.gz

---
+++ packages/tox-ansible/tox-ansible-24.2.0.tar.gz/tox-ansible-24.2.0/tests/conftest.py
@@ -0,0 +1,98 @@
+"""Global testing fixtures."""
+
+from __future__ import annotations
+
+import configparser
+import os
+import subprocess
+import sys
+
+from dataclasses import dataclass
+from pathlib import Path
+from typing import TYPE_CHECKING
+
+import pytest
+
+
+if TYPE_CHECKING:
+    from _pytest.python import Metafunc
+
+
+@pytest.fixture(scope="module")
+def module_fixture_dir(request: pytest.FixtureRequest) -> Path:
+    """Provide a module specific fixture directory.
+
+    :param request: pytest fixture request
+    :return: path to the module specific fixture directory
+    """
+    cwd = Path(__file__).parent
+    fixture_dir = cwd / "fixtures"
+    test_dir = fixture_dir / request.path.relative_to(cwd).parent / request.path.stem
+    return test_dir
+
+
+@pytest.fixture(autouse=True)
+def _tox_in_tox(monkeypatch: pytest.MonkeyPatch) -> None:
+    """Enable tox-in-tox.
+
+    :param monkeypatch: pytest fixture to patch modules
+    """
+    monkeypatch.delenv("TOX_ENV_NAME", raising=False)
+    monkeypatch.delenv("TOX_WORK_DIR", raising=False)
+    monkeypatch.delenv("TOX_ENV_DIR", raising=False)
+
+
+@dataclass
+class BasicEnvironment:
+    """An structure for an environment."""
+
+    name: str
+    config: dict[str, str]
+
+
+def pytest_generate_tests(metafunc: Metafunc) -> None:
+    """Parametrize the basic environments and there configurations.
+
+    :param metafunc: Metadata for the test
+    """
+    if "basic_environment" in metafunc.fixturenames:
+        cwd = Path(__file__).parent
+        basic_dir = cwd / "fixtures" / "integration" / "test_basic"
+        try:
+            cmd = (
+                f"{sys.executable} -m tox config --ansible "
+                f"--root {basic_dir} --conf tox-ansible.ini"
+            )
+            env = os.environ
+            env.pop("TOX_ENV_DIR", None)
+            env.pop("TOX_ENV_NAME", None)
+            env.pop("TOX_WORK_DIR", None)
+
+            proc = subprocess.run(
+                args=cmd,
+                capture_output=True,
+                check=True,
+                cwd=str(basic_dir),
+                env=env,
+                shell=True,
+                text=True,
+            )
+        except subprocess.CalledProcessError as exc:
+            print(exc.stdout)
+            print(exc.stderr)
+            pytest.fail(exc.stderr)
+
+        configs = configparser.ConfigParser()
+        configs.read_string(proc.stdout)
+
+        assert configs.sections()
+
+        environment_configs = [
+            BasicEnvironment(name=name, config=dict(configs[name])) for name in configs.sections()
+        ]
+
+        metafunc.parametrize(
+            "basic_environment",
+            environment_configs,
+            ids=[x.replace(":", "-") for x in configs.sections()],
+        )
---
+++ packages/tox-ansible/tox-ansible-24.2.0.tar.gz/tox-ansible-24.2.0/tests/integration/test_basic.py
@@ -0,0 +1,102 @@
+"""Basic tests."""
+
+from __future__ import annotations
+
+import json
+import subprocess
+
+from typing import TYPE_CHECKING
+
+import pytest
+
+from tox_ansible.plugin import conf_passenv
+
+
+if TYPE_CHECKING:
+    from pathlib import Path
+
+    from ..conftest import BasicEnvironment
+
+
+def test_ansible_environments(module_fixture_dir: Path) -> None:
+    """Test that the ansible environments are available.
+
+    :param module_fixture_dir: pytest fixture to get the fixtures directory
+    """
+    try:
+        proc = subprocess.run(
+            f"tox -l --ansible  --root {module_fixture_dir} --conf tox-ansible.ini",
+            capture_output=True,
+            cwd=str(module_fixture_dir),
+            text=True,
+            check=True,
+            shell=True,
+        )
+    except subprocess.CalledProcessError as exc:
+        print(exc.stdout)
+        print(exc.stderr)
+        pytest.fail(exc.stderr)
+    assert "integration" in proc.stdout
+    assert "sanity" in proc.stdout
+    assert "unit" in proc.stdout
+
+
+def test_gh_matrix(
+    module_fixture_dir: Path,
+    monkeypatch: pytest.MonkeyPatch,
+) -> None:
+    """Test that the ansible github matrix generation.
+
+    Remove the GITHUB environment variable to test the default output.
+
+    :param module_fixture_dir: pytest fixture to get the fixtures directory
+    :param monkeypatch: pytest fixture to patch modules
+    """
+    monkeypatch.delenv("GITHUB_ACTIONS", raising=False)
+    monkeypatch.delenv("GITHUB_OUTPUT", raising=False)
+    proc = subprocess.run(
+        f"tox --ansible --gh-matrix --root {module_fixture_dir} --conf tox-ansible.ini",
+        capture_output=True,
+        cwd=str(module_fixture_dir),
+        text=True,
+        check=True,
+        shell=True,
+    )
+    structured = json.loads(proc.stdout)
+    assert isinstance(structured, list)
+    assert structured
+    for entry in structured:
+        assert tuple(sorted(entry)) == ("description", "factors", "name", "python")
+        assert isinstance(entry["description"], str)
+        assert isinstance(entry["factors"], list)
+        assert isinstance(entry["name"], str)
+        assert isinstance(entry["python"], str)
+
+
+def test_environment_config(
+    basic_environment: BasicEnvironment,
+) -> None:
+    """Test that the ansible environment configurations are generated.
+
+    Ensure the environment configurations are generated and look for information unlikely to change
+    as a basic smoke test.
+
+    :param basic_environment: A dict representing the environment configuration
+    """
+    assert "py3" in basic_environment.name
+
+    config = basic_environment.config
+
+    assert config["allowlist_externals"]
+    assert config["commands_pre"]
+    assert config["commands"]
+    assert config["pass_env"]
+
+    assert "https://github.com/ansible/ansible/archive" in config["deps"]
+    assert "XDG_CACHE_HOME" in config["set_env"]
+
+
+def test_import() -> None:
+    """Verify that module can be imported (used for coverage)."""
+    x = conf_passenv()
+    assert "GITHUB_TOKEN" in x
