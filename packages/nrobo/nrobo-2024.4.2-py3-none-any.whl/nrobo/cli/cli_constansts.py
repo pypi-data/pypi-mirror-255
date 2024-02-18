32be845b57f9ccd1bfdfcd4df125ca3de49d5648
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 20:05:50 +0000

    Add package nrobo/2024.4.2/nrobo-2024.4.2-py3-none-any.whl

---
+++ packages/nrobo/nrobo-2024.4.2-py3-none-any.whl/nrobo/cli/cli_constansts.py
@@ -0,0 +1,83 @@
+"""
+=====================CAUTION=======================
+DO NOT DELETE THIS FILE SINCE IT IS PART OF NROBO
+FRAMEWORK AND IT MAY CHANGE IN THE FUTURE UPGRADES
+OF NROBO FRAMEWORK. THUS, TO BE ABLE TO SAFELY UPGRADE
+TO LATEST NROBO VERSION, PLEASE DO NOT DELETE THIS
+FILE OR ALTER ITS LOCATION OR ALTER ITS CONTENT!!!
+===================================================
+
+
+"""
+import os
+
+
+class NREPORT:
+    """nRoBo report related constants"""
+    HTML = "html"
+    ALLURE = "allure"
+    REPORT_DIR = "results"
+    HTML_REPORT_PATH = REPORT_DIR + os.sep + "report.html"
+    LOG_DIR_DRIVER = "driver-logs"
+    LOG_EXTENTION = ".log"
+    LOG_DIR_TEST = "test-logs"
+    SCREENSHOTS_DIR = "screenshots"
+    ALLURE_REPORT_PATH = REPORT_DIR + os.sep + ALLURE
+
+
+class nCLI:
+    """
+    List of nrobo defined cli options.
+
+    NOTE:
+        when you add/remove an option from nCLI class,
+        Make sure that the same option is also removed from the
+        nCLI.ARGS dictionary too!!!
+
+    """
+    INSTALL = "install"
+    APP = "app"
+    URL = "url"
+    USERNAME = "username"
+    PASSWORD = "password"
+    INSTANCES = "instances"
+    RERUNS = "reruns"
+    RERUNS_DELAY = "reruns-delay"
+    REPORT = "report"
+    TESTDIR = "testsdir"
+    BROWSER = "browser"
+    BROWSER_CONFIG = "browser-config"
+    KEY = "key"
+    PACKAGES = "packages"
+    GRID = "grid"
+
+    ARGS = {
+        INSTALL: INSTALL,
+        APP: APP,
+        URL: URL,
+        USERNAME: USERNAME,
+        PASSWORD: PASSWORD,
+        INSTANCES: INSTANCES,
+        RERUNS: RERUNS,
+        REPORT: REPORT,
+        TESTDIR: TESTDIR,
+        BROWSER: BROWSER,
+        BROWSER_CONFIG: BROWSER_CONFIG,
+        KEY: KEY,
+        PACKAGES: PACKAGES,
+        GRID: GRID
+    }
+
+    DEFAULT_ARGS = {
+        '--cache-clear': ['--cache-clear'],
+        '--color': ['--color', 'yes'],
+        '-r': ['-r', 'fE'],
+        '--code-highlight': ['--code-highlight', 'yes'],
+        '--junit-xml': ['--junit-xml', NREPORT.REPORT_DIR + os.sep + f"junit-report.xml"]
+    }
+
+
+class PACKAGES:
+    """nRoBo packages"""
+    NROBO = "nrobo"
+    CLI = "cli"
