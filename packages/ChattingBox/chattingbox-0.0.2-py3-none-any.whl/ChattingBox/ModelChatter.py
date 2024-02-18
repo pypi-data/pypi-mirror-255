e7fa313b0415f1b35d5ce93a57cbfe81a6996927
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 23:05:49 +0000

    Add package ChattingBox/0.0.2/chattingbox-0.0.2-py3-none-any.whl

---
+++ packages/ChattingBox/chattingbox-0.0.2-py3-none-any.whl/ChattingBox/ModelChatter.py
@@ -0,0 +1,41 @@
+#This model is to hold the main class that all chatterboxes use.
+
+class chatterbox ():
+    #Functions for setting the context for the bot and initialization
+    def addContext(self):
+        pass
+
+    def setContext(self):
+        pass
+
+    def getContext(self):
+        pass
+
+    def setKey(self, key, keyName):
+        pass
+
+    def getKey(self, keyName):
+        pass
+
+    def useKey(self, keyName):
+        pass
+
+    def initialize(self):
+        pass
+
+    def isInitialized(self):
+        pass
+
+    #Functions for interacting with the bot
+    def respond(self):
+        pass
+
+    def prompt(self, text):
+        pass
+
+    def getTranscript(self):
+        pass
+
+    def updateTranscript(self):
+        pass
+    
