4c481f92243ad66d0088dbb57bddbdac9558e91f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 09:04:53 +0000

    Add package ansys-turbogrid-core/0.4.0/ansys_turbogrid_core-0.4.0-py3-none-any.whl

---
+++ packages/ansys-turbogrid-core/ansys_turbogrid_core-0.4.0-py3-none-any.whl/ansys/turbogrid/core/__init__.py
@@ -0,0 +1,17 @@
+# Copyright (c) 2023 ANSYS, Inc. All rights reserved
+"""PyTurboGrid is a Python wrapper for Ansys TurboGrid."""
+
+from ansys.turbogrid.core.launcher.launcher import launch_turbogrid
+
+try:
+    import importlib.metadata as importlib_metadata
+except ModuleNotFoundError:  # pragma: no cover
+    import importlib_metadata
+
+# Read from the pyproject.toml
+# major, minor, patch
+__version__ = "0.0.0"
+try:
+    __version__ = importlib_metadata.version("ansys-turbogrid-core")
+except importlib_metadata.PackageNotFoundError:
+    pass
