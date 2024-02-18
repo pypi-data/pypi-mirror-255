de3c473f90b4e34ae4b224c004308b3ded815f73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 07:05:07 +0000

    Add package cdh-dav-python/202402.0.4/cdh_dav_python-202402.0.4.tar.gz

---
+++ packages/cdh-dav-python/cdh_dav_python-202402.0.4.tar.gz/cdh_dav_python-202402.0.4/cdh_dav_python/npm_service/__init.py
@@ -0,0 +1,22 @@
+"""Initialize the npm_service subpackage of cdh_dav_python package"""
+
+import sys
+import os
+
+OS_NAME = os.name
+
+sys.path.append("..")
+if OS_NAME.lower() == "nt":
+    print("npm_service: windows")
+    sys.path.append(os.path.dirname(os.path.abspath(__file__ + "\\..")))
+    sys.path.append(os.path.dirname(os.path.abspath(__file__ + "\\..\\..")))
+    sys.path.append(os.path.dirname(os.path.abspath(__file__ + "\\..\\..\\..")))
+else:
+    print("npm_service: non windows")
+    sys.path.append(os.path.dirname(os.path.abspath(__file__ + "/..")))
+    sys.path.append(os.path.dirname(os.path.abspath(__file__ + "/../..")))
+    sys.path.append(os.path.dirname(os.path.abspath(__file__ + "/../../..")))
+
+from cdh_dav_python.npm_service import npm_client
+
+__all__ = ["npm_client"]
