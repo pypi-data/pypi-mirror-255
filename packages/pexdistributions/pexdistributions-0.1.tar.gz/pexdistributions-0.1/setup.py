5b39d60f5c64903385e54f447aeaa3fabdd58a87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 20:05:19 +0000

    Add package pexdistributions/0.1/pexdistributions-0.1.tar.gz

---
+++ packages/pexdistributions/pexdistributions-0.1.tar.gz/pexdistributions-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='pexdistributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['pexdistributions'],
+      author = 'Peter Kamau',
+      zip_safe=False)
