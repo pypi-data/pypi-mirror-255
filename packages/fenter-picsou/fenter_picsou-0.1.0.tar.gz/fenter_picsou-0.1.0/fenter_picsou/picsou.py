a220e8f6d8dae54a7b935a66c6bfd72a49d32272
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 01:27:36 +0000

    Add package fenter-picsou/0.1.0/fenter_picsou-0.1.0.tar.gz

---
+++ packages/fenter-picsou/fenter_picsou-0.1.0.tar.gz/fenter_picsou-0.1.0/fenter_picsou/picsou.py
@@ -0,0 +1,10 @@
+account = 10000
+
+def delete_momey_from_bank_account(a: int, b: int) -> int:
+    global account
+    if a+b > 200:
+        account = account - (a+b+100)
+        return account
+
+    account = account - (a+b)
+    return account
