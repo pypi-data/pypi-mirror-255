3814e03684dd444f34c14021ed3b59cb3a8febc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 14:05:50 +0000

    Add package netbox-slm/1.6.0/netbox_slm-1.6.0-py3-none-any.whl

---
+++ packages/netbox-slm/netbox_slm-1.6.0-py3-none-any.whl/netbox_slm/forms/software_product_installation.py
@@ -0,0 +1,81 @@
+from django import forms
+from django.urls import reverse_lazy
+
+from dcim.models import Device
+from netbox.forms import NetBoxModelForm, NetBoxModelImportForm, NetBoxModelBulkEditForm, NetBoxModelFilterSetForm
+from netbox_slm.models import SoftwareProductInstallation, SoftwareProduct, SoftwareProductVersion
+from utilities.forms.fields import CommentField, DynamicModelChoiceField, TagFilterField
+from utilities.forms.widgets import APISelect
+from virtualization.models import VirtualMachine, Cluster
+
+
+class SoftwareProductInstallationForm(NetBoxModelForm):
+    """Form for creating a new SoftwareProductInstallation object."""
+
+    comments = CommentField()
+
+    device = DynamicModelChoiceField(queryset=Device.objects.all(), required=False)
+    virtualmachine = DynamicModelChoiceField(queryset=VirtualMachine.objects.all(), required=False)
+    cluster = DynamicModelChoiceField(queryset=Cluster.objects.all(), required=False)
+    software_product = DynamicModelChoiceField(
+        queryset=SoftwareProduct.objects.all(),
+        required=True,
+        widget=APISelect(attrs={"data-url": reverse_lazy("plugins-api:netbox_slm-api:softwareproduct-list")}),
+    )
+    version = DynamicModelChoiceField(
+        queryset=SoftwareProductVersion.objects.all(),
+        required=True,
+        widget=APISelect(attrs={"data-url": reverse_lazy("plugins-api:netbox_slm-api:softwareproductversion-list")}),
+        query_params={
+            "software_product": "$software_product",
+        },
+    )
+
+    class Meta:
+        model = SoftwareProductInstallation
+        fields = (
+            "device",
+            "virtualmachine",
+            "cluster",
+            "software_product",
+            "version",
+            "tags",
+            "comments",
+        )
+
+    def clean_version(self):
+        version = self.cleaned_data["version"]
+        software_product = self.cleaned_data["software_product"]
+        if version not in software_product.softwareproductversion_set.all():
+            raise forms.ValidationError(
+                f"Version '{version}' doesn't exist on {software_product}, make sure you've "
+                f"selected a compatible version or first select the software product."
+            )
+        return version
+
+
+class SoftwareProductInstallationFilterForm(NetBoxModelFilterSetForm):
+    model = SoftwareProductInstallation
+    fieldsets = ((None, ("q", "tag")),)
+    tag = TagFilterField(model)
+
+
+class SoftwareProductInstallationImportForm(NetBoxModelImportForm):
+    class Meta:
+        model = SoftwareProductInstallation
+        fields = tuple()
+
+
+class SoftwareProductInstallationBulkEditForm(NetBoxModelBulkEditForm):
+    software_product = DynamicModelChoiceField(queryset=SoftwareProduct.objects.all(), required=False)
+    version = DynamicModelChoiceField(queryset=SoftwareProductVersion.objects.all(), required=False)
+    model = SoftwareProductInstallation
+    fieldsets = (
+        (
+            None,
+            (
+                "software_product",
+                "version",
+            ),
+        ),
+    )
---
+++ packages/netbox-slm/netbox_slm-1.6.0-py3-none-any.whl/netbox_slm/views/__init__.py
@@ -0,0 +1,36 @@
+from .software_license import (
+    SoftwareLicenseListView,
+    SoftwareLicenseView,
+    SoftwareLicenseEditView,
+    SoftwareLicenseDeleteView,
+    SoftwareLicenseBulkImportView,
+    SoftwareLicenseBulkEditView,
+    SoftwareLicenseBulkDeleteView,
+)
+from .software_product import (
+    SoftwareProductListView,
+    SoftwareProductView,
+    SoftwareProductEditView,
+    SoftwareProductDeleteView,
+    SoftwareProductBulkImportView,
+    SoftwareProductBulkEditView,
+    SoftwareProductBulkDeleteView,
+)
+from .software_product_installation import (
+    SoftwareProductInstallationListView,
+    SoftwareProductInstallationView,
+    SoftwareProductInstallationEditView,
+    SoftwareProductInstallationDeleteView,
+    SoftwareProductInstallationBulkImportView,
+    SoftwareProductInstallationBulkEditView,
+    SoftwareProductInstallationBulkDeleteView,
+)
+from .software_product_version import (
+    SoftwareProductVersionListView,
+    SoftwareProductVersionView,
+    SoftwareProductVersionEditView,
+    SoftwareProductVersionDeleteView,
+    SoftwareProductVersionBulkImportView,
+    SoftwareProductVersionBulkEditView,
+    SoftwareProductVersionBulkDeleteView,
+)
