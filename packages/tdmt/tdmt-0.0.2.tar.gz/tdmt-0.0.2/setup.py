1482f668abd9ec0a937e63c3b25df63bbe686d62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 02:12:27 +0000

    Add package tdmt/0.0.2/tdmt-0.0.2.tar.gz

---
+++ packages/tdmt/tdmt-0.0.2.tar.gz/tdmt-0.0.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tdmt",
+    version="0.0.2",
+    packages=find_packages(),
+    description="An example Python package",
+    install_requires=[],
+    entry_points={
+        "console_scripts": [
+            "tdmt = tdmt:hello",
+            ]}
+)
+
+
