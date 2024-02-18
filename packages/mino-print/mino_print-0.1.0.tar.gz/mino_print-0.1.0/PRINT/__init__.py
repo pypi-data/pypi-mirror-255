645ea18771ccfd8b1ca57ac4d63233a3cfffead1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 17:04:07 +0000

    Add package mino-print/0.1.0/mino_print-0.1.0.tar.gz

---
+++ packages/mino-print/mino_print-0.1.0.tar.gz/mino_print-0.1.0/PRINT/__init__.py
@@ -0,0 +1,9 @@
+from .PRINT import ascii_color_print
+import sys
+import types
+
+class ItHASToBeAClass(types.ModuleType):
+    def __call__(self, value: str) -> None:
+        ascii_color_print(value)
+
+sys.modules[__name__].__class__ = ItHASToBeAClass
---
+++ packages/mino-print/mino_print-0.1.0.tar.gz/mino_print-0.1.0/README.md
@@ -0,0 +1,13 @@
+
+
+![Alt text](assets/image.png)
+
+just a function to make prints look nice
+
+### usage
+```python
+import PRINT
+
+PRINT('hello, world!')
+
+``````
