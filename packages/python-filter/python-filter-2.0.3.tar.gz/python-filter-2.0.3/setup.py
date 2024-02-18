5e1cb52e7fe16266fe5ce6e7678d78888d7806ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 00:07:24 +0000

    Add package python-filter/2.0.3/python-filter-2.0.3.tar.gz

---
+++ packages/python-filter/python-filter-2.0.3.tar.gz/python-filter-2.0.3/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name = "python-filter",
+    version = "2.0.3",
+    license = "MIT License",
+    author = "Marcuth",
+    long_description = open("README.MD").read(),
+    long_description_content_type = "text/markdown",
+    author_email = "example@gmail.com",
+    keywords = "filter tools",
+    description = "A biblioteca python-filter oferece classes eficientes para manipulação de listas em Python, proporcionando operações simples e rápidas para encontrar, filtrar e manipular dados em estruturas de dados comuns.",
+    packages = ["pyfilter"],
+    install_requires = ["pydantic"],
+)
