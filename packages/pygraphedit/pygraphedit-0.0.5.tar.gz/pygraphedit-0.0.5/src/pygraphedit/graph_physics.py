9a54c3cfcf3cebe80bdd7c679db0fbe99c5a5713
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 13:05:31 +0000

    Add package pygraphedit/0.0.5/pygraphedit-0.0.5.tar.gz

---
+++ packages/pygraphedit/pygraphedit-0.0.5.tar.gz/pygraphedit-0.0.5/src/pygraphedit/graph_physics.py
@@ -0,0 +1,125 @@
+import pymunk
+
+from .settings import NODE_RADIUS
+from .visual_graph import VisualGraph
+
+VERTEX_BODY_MASS = 1
+VERTEX_BODY_MOMENT = 1
+WALLS_WIDTH = 10
+
+
+def create_border(space, bounds: (int, int)):
+    width, height = bounds
+    ground = pymunk.Segment(space.static_body, (0, -WALLS_WIDTH), (width, -WALLS_WIDTH), WALLS_WIDTH)
+    ground.friction = 1.0
+    space.add(ground)
+    ceiling = pymunk.Segment(space.static_body, (0, height + WALLS_WIDTH), (width, height + WALLS_WIDTH), WALLS_WIDTH)
+    ceiling.friction = 1.0
+    space.add(ceiling)
+    left_wall = pymunk.Segment(space.static_body, (-WALLS_WIDTH, 0), (-WALLS_WIDTH, height), WALLS_WIDTH)
+    left_wall.friction = 1.0
+    space.add(left_wall)
+    right_wall = pymunk.Segment(space.static_body, (width + WALLS_WIDTH, 0), (width + WALLS_WIDTH, height), WALLS_WIDTH)
+    right_wall.friction = 1.0
+    space.add(right_wall)
+
+
+class GraphPhysics:
+    def __init__(self, visual_graph: VisualGraph):
+        self.visual_graph = visual_graph
+        self.space = pymunk.Space()
+        create_border(self.space, visual_graph.bounds)
+        self.space.gravity = (0, 0)
+        self.vertex_body: dict[any, pymunk.Body] = {}
+        self.edge_body: dict[any, pymunk.Body] = {}
+        self.connection_body = {}
+        for node in visual_graph.graph.nodes:
+            self.add_vert(node, visual_graph.coordinates[node])
+        for edge in visual_graph.graph.edges:
+            self.add_edge(edge[0], edge[1])
+        visual_graph.add_node.subscribable.subscribe(self.add_vert)
+        visual_graph.remove_node.subscribable.subscribe(self.remove_vert)
+        visual_graph.add_edge.subscribable.subscribe(self.add_edge)
+        visual_graph.remove_edge.subscribable.subscribe(self.remove_edge)
+        visual_graph.move_node.subscribable.subscribe(self.move_node)
+
+        visual_graph.drag_start.subscribable.subscribe(self.drag_start)
+        visual_graph.drag_end.subscribable.subscribe(self.drag_end)
+
+    def drag_start(self, node):
+        self.vertex_body[node].body_type = pymunk.Body.STATIC
+
+    def drag_end(self):
+        node = self.visual_graph.dragged_node
+        if node is not None:
+            self.vertex_body[node].body_type = pymunk.Body.DYNAMIC
+            self.vertex_body[node].mass = VERTEX_BODY_MASS
+            self.vertex_body[node].moment = VERTEX_BODY_MOMENT
+
+    def add_vert(self, node, pos: (int, int)):
+        body = pymunk.Body(VERTEX_BODY_MASS, VERTEX_BODY_MOMENT)
+        body.position = pos
+        shape = pymunk.Circle(body, radius=10)  # Adjust the radius as needed
+        shape.elasticity = 1.0  # Elasticity of collisions
+        shape.friction = 0.0  # Friction of collisions
+        self.space.add(body, shape)
+        self.vertex_body[node] = body
+        self.edge_body[node] = {}
+        self.connection_body[node] = {}
+        for node1, body1 in self.vertex_body.items():
+            if node1 != node:
+                connection_body = pymunk.DampedSpring(body, body1, (0, 0), (0, 0), rest_length=200, stiffness=10,
+                                                      damping=2)
+                self.space.add(connection_body)
+                self.connection_body[node][node1] = connection_body
+                self.connection_body[node1][node] = connection_body
+
+    def remove_vert(self, node):
+        self.space.remove(self.vertex_body[node])
+        del self.vertex_body[node]
+        for other in self.edge_body[node]:
+            self.space.remove(self.edge_body[node][other])
+            del self.edge_body[other][node]
+        for other in self.connection_body[node]:
+            self.space.remove(self.connection_body[node][other])
+            del self.connection_body[other][node]
+        del self.edge_body[node]
+        del self.connection_body[node]
+
+    def add_edge(self, node1, node2):
+        edge_body = pymunk.DampedSpring(self.vertex_body[node1], self.vertex_body[node2], (0, 0), (0, 0),
+                                        rest_length=100, stiffness=500, damping=2)
+        self.space.add(edge_body)
+        self.edge_body[node1][node2] = edge_body
+        self.edge_body[node2][node1] = edge_body
+
+    def remove_edge(self, node1, node2):
+        body = self.edge_body[node1][node2]
+        self.space.remove(body)
+        del self.edge_body[node1][node2]
+        del self.edge_body[node2][node1]
+
+    def move_node(self, node, pos: (int, int)):
+        self.vertex_body[node].position = pos
+
+    def update_physics(self, dt, physics):
+        if physics:
+            self.space.step(dt)
+            for node, body in self.vertex_body.items():
+                self.visual_graph.move_node(node, [body.position.x, body.position.y])
+        self.normalize_positions()
+
+    def normalize_positions(self):
+        for node, node_pos in self.visual_graph.coordinates.items():
+            if node_pos[0] < 0:
+                self.visual_graph.move_node(node, [NODE_RADIUS + 10, node_pos[1]])
+                self.vertex_body[node].velocity = [0, self.vertex_body[node].velocity.y]
+            if node_pos[1] < 0:
+                self.visual_graph.move_node(node, [node_pos[0], NODE_RADIUS + 10])
+                self.vertex_body[node].velocity = [self.vertex_body[node].velocity.x, 0]
+            if node_pos[0] > self.visual_graph.bounds[0]:
+                self.visual_graph.move_node(node, [self.visual_graph.bounds[0] - NODE_RADIUS - 10, node_pos[1]])
+                self.vertex_body[node].velocity = [0, self.vertex_body[node].velocity.y]
+            if node_pos[1] > self.visual_graph.bounds[1]:
+                self.visual_graph.move_node(node, [node_pos[0], self.visual_graph.bounds[1] - NODE_RADIUS - 10])
+                self.vertex_body[node].velocity = [self.vertex_body[node].velocity.x, 0]
---
+++ packages/pygraphedit/pygraphedit-0.0.5.tar.gz/pygraphedit-0.0.5/src/pygraphedit/graphics.py
@@ -0,0 +1,150 @@
+from ipycanvas import Canvas, hold_canvas
+from .settings import DRAGGED_NODE_RADIUS, NODE_RADIUS
+from .visual_graph import VisualGraph
+import ipywidgets as widgets
+
+
+def draw_graph(canvas: Canvas, visual_graph: VisualGraph):
+    def clear_canvas():
+        canvas.clear()
+        canvas.stroke_style = "black"
+        canvas.stroke_rect(0, 0, 800, 500)
+
+    def draw_vertex(pos, size=10, colorcode="black"):
+        canvas.fill_style = colorcode
+        canvas.fill_circle(pos[0], pos[1], size)
+
+    def draw_edge(pos1, pos2, colorcode="black"):
+        canvas.stroke_style = colorcode
+        canvas.line_width = 2
+        canvas.stroke_line(*pos1, *pos2)
+
+    with hold_canvas():
+        clear_canvas()
+        for edge in visual_graph.graph.edges:
+            draw_edge(visual_graph.coordinates[edge[0]], visual_graph.coordinates[edge[1]],
+                      colorcode=("red" if edge == visual_graph.selected_edge else "black"))
+        for node, pos in visual_graph.coordinates.items():
+            draw_vertex(pos,
+                        size=(DRAGGED_NODE_RADIUS if node == visual_graph.dragged_node else NODE_RADIUS),
+                        colorcode=("red" if node == visual_graph.selected_node else "black"))
+
+
+class SmallButton(widgets.Button):
+    def __init__(self, active=False, active_color=None, inactive_color=None, description='',**kwargs):
+        super().__init__(layout=widgets.Layout(width='39px', height='39px'), description=description, **kwargs)
+        self.active=active
+        self.active_color=active_color
+        self.inactive_color=inactive_color
+        self.style.button_color = active_color if active else inactive_color
+
+    def toggle(self):
+        self.active=not self.active
+        self.style.button_color = self.active_color if self.active else self.inactive_color
+
+
+class Menu(widgets.HBox):
+    def __init__(self):
+        super().__init__()
+        self.close_button = SmallButton(tooltip='Exit', icon='window-close')
+        self.physics_button = widgets.ToggleButton(
+            value=True,
+            tooltip='Turn physics on/off',
+            disabled=False,
+            indent=False,
+            layout=widgets.Layout(width='39px', height='39px'), icon="wrench")
+
+        self.struct_button = SmallButton(tooltip='Click to activate edges and vertices creation/deletion',
+                                            icon='plus-circle', active_color='LightBlue', active=True)
+
+
+        self.prop_button = SmallButton(tooltip='Click to modify properties of edges and vertices',
+                                       active_color='LightBlue', icon="pencil")
+
+        self.edge_button = SmallButton(tooltip='Edges selection enabled/disabled', 
+                                          icon='arrows-v', active_color='LightGreen', inactive_color='lightcoral', active=True)
+
+        self.vert_button = SmallButton(tooltip='Vertices selection enabled/disabled',
+                                        icon='circle', active_color='LightGreen', inactive_color='lightcoral', active=True)
+
+        self.children = ([widgets.HBox((self.struct_button, self.prop_button),
+                                       layout=widgets.Layout(border='0.5px solid #000000')),
+                          self.vert_button, self.edge_button, self.physics_button, self.close_button])
+
+
+def get_label_style():
+    return dict(
+        font_weight='bold',
+        background='#d3d3d3',
+        font_variant="small")
+
+
+class LabelBox(widgets.HBox):
+    def __init__(self, label_value, text_value):
+        super().__init__()
+        self.label_value = widgets.Textarea(value=text_value,
+                                            layout=widgets.Layout(width='100px', height='30px'))
+
+        label_label = widgets.Label(value=label_value,
+                                    layout=widgets.Layout(width='150px', height='30px'), style=get_label_style())
+
+        label_label.layout.border = '2px solid #000000'
+        self.children = (label_label, self.label_value)
+
+
+
+class LabelListBox(widgets.VBox):
+    text_layout = widgets.Layout(width='180px', height='35px')
+    button_layout = widgets.Layout(width='35px', height='35px')
+    def __init__(self, str_value):
+        super().__init__()
+        self.label = widgets.Label(value=str_value,
+                                   layout=self.text_layout,
+                                   style=get_label_style())
+        self.label.layout.border = '2px solid #000000'
+        self.delete_button = widgets.Button(layout=self.button_layout, icon="trash-o")
+        self.edit_button = widgets.Button(layout=self.button_layout, icon="pencil")
+        
+        self.edit_label_value=widgets.Textarea(placeholder='New label name', layout=self.text_layout)
+        self.confirm_edit_button=widgets.Button(layout=self.button_layout, icon="check")
+        self.escape_edit_button=widgets.Button(layout=self.button_layout, icon='times')
+
+        self.children=(widgets.HBox((self.label, self.edit_button, self.delete_button)),)
+    
+    def show_edit(self):
+        self.children=(widgets.HBox((self.label, self.confirm_edit_button, self.escape_edit_button)),widgets.HBox((self.edit_label_value,)))
+
+    def hide_edit(self):
+        self.children=(widgets.HBox((self.label, self.edit_button, self.delete_button)),)
+
+
+def get_head_label(text):
+    return widgets.Label(value=text, layout=widgets.Layout(width='250px', height='30px', justify_content='center'))
+
+
+def get_some_other_label_that_i_dont_know_what_it_is():
+    return widgets.Label(value=f"", layout=widgets.Layout(width='250px', height='70px', align_items='stretch'))
+
+
+class AddLabelBox(widgets.HBox):
+    def __init__(self):
+        super().__init__()
+        self.add_new_label_button = widgets.Button(description="",
+                                                   layout=widgets.Layout(width='35px', height='35px'), icon="plus")
+        self.label_name_text_box = widgets.Textarea(placeholder='Label name',
+                                                    layout=widgets.Layout(width='215px', height='35px'))
+        self.children = (self.label_name_text_box, self.add_new_label_button)
+
+class EditLabelBox(widgets.HBox):
+    def __init__(self):
+        super().__init__()
+        self.add_new_label_button = widgets.Button(description="",
+                                                   layout=widgets.Layout(width='35px', height='35px'), icon="plus")
+        self.label_name_text_box = widgets.Textarea(placeholder='New label name',
+                                                    layout=widgets.Layout(width='215px', height='35px'))
+        self.children = (self.label_name_text_box, self.add_new_label_button)
+
+
+
+def get_labels_info_scrollable():
+    return widgets.Output(layout={'overflow_y': 'scroll', 'height': '450px'})
