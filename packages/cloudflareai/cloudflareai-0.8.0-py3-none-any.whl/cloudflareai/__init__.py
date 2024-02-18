7a2a3e8b8922a2cc26fc0baae76ab8ba95848116
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 12:07:47 +0000

    Add package cloudflareai/0.8.0/cloudflareai-0.8.0-py3-none-any.whl

---
+++ packages/cloudflareai/cloudflareai-0.8.0-py3-none-any.whl/cloudflareai/__init__.py
@@ -0,0 +1,14 @@
+"""
+cloudflareai
+
+Cloudflare AI Python SDK
+
+CloudflareAI is a Python SDK for Cloudflare AI. It provides a simple way to interact with Cloudflare AI's API.
+"""
+
+__title__ = "cloudflareai"
+__author__ = "alexraskin"
+__license__ = "Mozilla Public License 2.0"
+__version__ = "0.8.0"
+
+from .core import CloudflareAI
---
+++ packages/cloudflareai/cloudflareai-0.8.0-py3-none-any.whl/cloudflareai/exceptions.py
@@ -0,0 +1,9 @@
+class CloudflareException(Exception):
+    """Base exception class for CloudflareAI"""
+
+    ...
+
+class CloudflareAPIException(CloudflareException):
+    """Exception raised when the Cloudflare API returns an error"""
+
+    ...
