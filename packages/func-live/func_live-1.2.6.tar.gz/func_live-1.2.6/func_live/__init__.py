0d58772c0e385c2d067fb3f1b8fc849bc95db055
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 08:06:35 +0000

    Add package func-live/1.2.6/func_live-1.2.6.tar.gz

---
+++ packages/func-live/func_live-1.2.6.tar.gz/func_live-1.2.6/func_live/__init__.py
@@ -0,0 +1,39 @@
+import os
+import httpx
+from .config import API_URL
+from .recursively_upload_filepaths import recursively_upload_filepaths
+
+class FuncLive:
+    def __init__(self):
+        self.client = httpx.AsyncClient()
+        self._fetch_functions()
+
+    async def _fetch_functions(self):
+        async with self.client as client:
+            response = await client.get(f"{API_URL}/functions")
+            functions = response.json().get('functions', [])
+
+            for func in functions:
+                func_name = func['name']
+                setattr(self, func_name, self._create_function(func_name))
+
+    def _create_function(self, func_name):
+        async def func(input_data):
+            try:
+                modified_input = await recursively_upload_filepaths(input_data)
+                async with self.client as client:
+                    response = await client.post(f"{API_URL}/functions/{func_name}",
+                                                 headers={
+                                                     'Authorization': f'Bearer {os.getenv("FUNC_TOKEN")}',
+                                                     'Accept': 'application/json',
+                                                     'Content-Type': 'application/json'
+                                                 },
+                                                 json={'input': modified_input})
+                    return response.json().get('output')
+            except httpx.RequestError as e:
+                raise Exception(f"There was an error: {str(e)}")
+
+        return func
+
+
+func_live = FuncLive()
