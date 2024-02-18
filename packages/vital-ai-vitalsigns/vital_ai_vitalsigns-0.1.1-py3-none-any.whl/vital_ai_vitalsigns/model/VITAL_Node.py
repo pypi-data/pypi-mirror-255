2692e0ca5c2987755029d3a332e0e9cb0f911c79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 14:06:33 +0000

    Add package vital-ai-vitalsigns/0.1.1/vital_ai_vitalsigns-0.1.1-py3-none-any.whl

---
+++ packages/vital-ai-vitalsigns/vital_ai_vitalsigns-0.1.1-py3-none-any.whl/vital_ai_vitalsigns/model/VITAL_Node.py
@@ -0,0 +1,10 @@
+from vital_ai_vitalsigns.impl import VitalSignsImpl
+from vital_ai_vitalsigns.model.GraphObject import GraphObject
+from vital_ai_vitalsigns.model.properties.StringProperty import StringProperty
+from vital_ai_vitalsigns.model.trait.PropertyTrait import PropertyTrait
+
+
+class VITALNode(GraphObject):
+    allowed_properties = [
+        {'uri': "http://vital.ai/ontology/vital-core#hasName", 'prop_class': StringProperty},
+    ]
