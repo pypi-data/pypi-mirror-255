c334a7a1f13791ff29364354b2185e14e391f92b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 13:04:35 +0000

    Add package openapi-to-asciidoc/0.1.4.dev4/openapi_to_asciidoc-0.1.4.dev4-py3-none-any.whl

---
+++ packages/openapi-to-asciidoc/openapi_to_asciidoc-0.1.4.dev4-py3-none-any.whl/openapi_to_asciidoc/convert.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+# Copyright © LFV
+"""OpenAPI Specification to AsciiDoc
+"""
+
+import argparse
+import json
+from importlib.metadata import version
+
+from openapi_to_asciidoc.objects import OpenApi, OpenApiSchema
+
+
+def get_arguments():
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter)
+
+    parser.add_argument(
+        "-V",
+        "--version",
+        action="version",
+        version=version("maven-artifact"),
+    )
+    parser.add_argument(
+        "-j",
+        "--json",
+        help="OpenAPI JSON Specification File (default: openapi.json)",
+        type=argparse.FileType("r"),
+        default="openapi.json",
+    )
+    parser.add_argument(
+        "-o",
+        "--output",
+        nargs="?",
+        help="Where to output result (default: openapi.adoc)",
+        type=argparse.FileType("w"),
+        default="openapi.adoc",
+    )
+
+    return parser.parse_args()
+
+
+def main():
+    args = get_arguments()
+
+    js_input = args.json
+    output = args.output
+
+    # read openapi json input
+    openapi = json.load(js_input)
+
+    # render template from schema
+    openapi_schema = OpenApiSchema()
+    schema: OpenApi = openapi_schema.load(openapi)
+
+    # output rendered result
+    output.write(schema.result)
+
+
+if __name__ == "__main__":
+    main()
