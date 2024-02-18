dbd7f58af46f1fa0caabf00a00a707d6985ecc4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 16:08:03 +0000

    Add package streamlit-jdgrid/0.3.5.8/streamlit-jdgrid-0.3.5.8.tar.gz

---
+++ packages/streamlit-jdgrid/streamlit-jdgrid-0.3.5.8.tar.gz/streamlit-jdgrid-0.3.5.8/docs/conf.py
@@ -0,0 +1,35 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(".."))
+
+from st_aggrid._version import __version__
+
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "streamlit-jdgrid"
+copyright = "2024, Pablo Fonseca, Jake Diamond"
+author = "Pablo Fonseca, Jake Diamond"
+
+version = __version__
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ["sphinx.ext.autodoc"]
+
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "alabaster"
+html_static_path = ["_static"]
