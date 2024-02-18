ed2efb031f289b0490f27e18082f9355f0911343
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 05:06:16 +0000

    Add package power-grid-model-io/1.2.63/power-grid-model-io-1.2.63.tar.gz

---
+++ packages/power-grid-model-io/power-grid-model-io-1.2.63.tar.gz/power-grid-model-io-1.2.63/tests/unit/data_types/test_data_types.py
@@ -0,0 +1,41 @@
+# SPDX-FileCopyrightText: Contributors to the Power Grid Model project <powergridmodel@lfenergy.org>
+#
+# SPDX-License-Identifier: MPL-2.0
+
+from pydantic import parse_obj_as
+
+from power_grid_model_io.data_types import ExtraInfo, StructuredData
+
+
+def test_extra_info():
+    extra_info = {1: {"a": 123, "b": 1.23}, 2: {"c": (1.2, 3.4, 5.6), "d": "foo"}}
+
+    # Expect no exception
+    parse_obj_as(ExtraInfo, extra_info)
+
+
+def test_structured_data__single():
+    data = {"node": [{"id": 1}, {"id": 2}], "line": [{"id": 3, "node_from": 1, "node_to": 2}]}
+
+    # Expect no exception
+    assert isinstance(parse_obj_as(StructuredData, data), dict)
+
+
+def test_structured_data__batch():
+    data = [
+        {
+            "load": [
+                {"id": 1, "p": 111.0},
+                {"id": 2, "p": 222.0},
+            ]
+        },
+        {
+            "load": [
+                {"id": 1, "p": 333.0},
+                {"id": 2, "p": 444.0},
+            ]
+        },
+    ]
+
+    # Expect no exception
+    assert isinstance(parse_obj_as(StructuredData, data), list)
