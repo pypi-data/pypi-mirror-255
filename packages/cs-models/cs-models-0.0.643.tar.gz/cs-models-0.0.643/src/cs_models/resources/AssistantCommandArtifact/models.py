d4417b94d4ac7c8ca80c935de9487e8b8a374e85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 15:05:23 +0000

    Add package cs-models/0.0.643/cs-models-0.0.643.tar.gz

---
+++ packages/cs-models/cs-models-0.0.643.tar.gz/cs-models-0.0.643/src/cs_models/resources/AssistantCommandArtifact/models.py
@@ -0,0 +1,32 @@
+from datetime import datetime
+
+from sqlalchemy import (
+    Column,
+    Integer,
+    DateTime,
+    ForeignKey,
+    Boolean,
+    String,
+    Text,
+)
+
+from ...database import Base
+
+
+class AssistantCommandArtifactModel(Base):
+    __tablename__ = "assistant_command_artifacts"
+
+    id = Column(Integer, primary_key=True)
+    assistant_command_id = Column(
+        Integer,
+        ForeignKey('assistant_commands.id'),
+        nullable=False,
+    )
+    artifact_id = Column(String(64), nullable=False)
+    updated_at = Column(
+        DateTime,
+        nullable=False,
+        # https://stackoverflow.com/questions/58776476/why-doesnt-freezegun-work-with-sqlalchemy-default-values
+        default=lambda: datetime.utcnow(),
+        onupdate=lambda: datetime.utcnow(),
+    )
