49ee9d2eed9a160c2723ad073c3d768bb5560846
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 01:27:55 +0000

    Add package gaetan-picsou/0.1.0/gaetan_picsou-0.1.0-py3-none-any.whl

---
+++ packages/gaetan-picsou/gaetan_picsou-0.1.0-py3-none-any.whl/gaetan_picsou/picsou.py
@@ -0,0 +1,11 @@
+account = 100
+
+def delete_money_from_bank_account(a: int, b: int) -> int:
+    global account
+    
+    if a + b > 200:
+        account = account - (a + b + 100)
+        return account
+    
+    account = account - (a + b)
+    return account
