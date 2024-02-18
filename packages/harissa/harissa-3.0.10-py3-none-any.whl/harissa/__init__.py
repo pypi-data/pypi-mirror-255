df0aada25661422ea094469c5a482e9a89219cde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 19:04:53 +0000

    Add package harissa/3.0.10/harissa-3.0.10-py3-none-any.whl

---
+++ packages/harissa/harissa-3.0.10-py3-none-any.whl/harissa/__init__.py
@@ -0,0 +1,23 @@
+"""
+Harissa
+=======
+
+Tools for mechanistic gene network inference from single-cell data
+------------------------------------------------------------------
+
+Mechanistic model-based gene network inference using
+a self-consistent proteomic field (SCPF) approximation.
+It is analogous to the unrestricted Hartree approximation
+in quantum mechanics, applied to gene expression modeled
+as a piecewise-deterministic Markov process (PDMP).
+
+The package also includes a simulation module to generate
+single-cell data with transcriptional bursting.
+
+Author: Ulysse Herbach (ulysse.herbach@inria.fr)
+"""
+from .model import NetworkModel, Cascade, Tree
+
+__all__ = ['NetworkModel', 'Cascade', 'Tree']
+
+__version__ = '3.0.9'
