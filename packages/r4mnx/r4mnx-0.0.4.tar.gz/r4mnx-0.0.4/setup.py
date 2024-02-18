d827c63ce29d0134f19b3e4a2eee6013f86e1eb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 16:06:16 +0000

    Add package r4mnx/0.0.4/r4mnx-0.0.4.tar.gz

---
+++ packages/r4mnx/r4mnx-0.0.4.tar.gz/r4mnx-0.0.4/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+# Leer el contenido del archivo README.md
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="r4mnx",
+    version="0.0.4",
+    packages=find_packages(),
+    install_requires=[],
+    author="r4mnx",
+    description="Prueba de subida",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="http://r4mnx.github.io"
+)
