1e92ee5e8183309e163e8a2ecddbb23fec59951a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 20:05:02 +0000

    Add package gqylpy-exception/3.0/gqylpy_exception-3.0.tar.gz

---
+++ packages/gqylpy-exception/gqylpy_exception-3.0.tar.gz/gqylpy_exception-3.0/setup.py
@@ -0,0 +1,43 @@
+import setuptools
+import gqylpy_exception as g
+
+gdoc: list = g.__doc__.split('\n')
+
+for index, line in enumerate(gdoc):
+    if line.startswith('@version: ', 4):
+        version = line.split()[-1]
+        break
+_, author, email = gdoc[index + 1].split()
+source = gdoc[index + 2].split()[-1]
+
+setuptools.setup(
+    name=g.__name__,
+    version=version,
+    author=author,
+    author_email=email,
+    license='Apache 2.0',
+    url='http://gqylpy.com',
+    project_urls={'Source': source},
+    description='在执行 raise 语句的同时创建异常类，无需提前定义异常类，方便快捷。',
+    long_description=open('README.md', encoding='utf8').read(),
+    long_description_content_type='text/markdown',
+    packages=[g.__name__],
+    python_requires='>=3.8, <4',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: Chinese (Simplified)',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Software Development :: Bug Tracking',
+        'Topic :: Software Development :: Widget Sets',
+        'Topic :: Artistic Software',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12'
+    ]
+)
