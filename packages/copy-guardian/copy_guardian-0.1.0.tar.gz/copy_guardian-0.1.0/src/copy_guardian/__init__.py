e27e3472f4a3e4145da7db23355d158d9d04778a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 01:26:32 +0000

    Add package copy-guardian/0.1.0/copy_guardian-0.1.0.tar.gz

---
+++ packages/copy-guardian/copy_guardian-0.1.0.tar.gz/copy_guardian-0.1.0/src/copy_guardian/__init__.py
@@ -0,0 +1,7 @@
+__author__ = "Uwe"
+__email__ = "uwe.schmitt@id.ethz.ch"
+__credits__ = "ETH Zurich, Institute for Particle Physics and Astrophysics"
+
+__version__ = "0.1.0"
+
+from .copy_guardian import BoundedSemaphore, Connection  # noqa: F401
