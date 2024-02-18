758a43eef288ecc91babeefb393501c248c9c08c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 23:06:00 +0000

    Add package peptacular/1.3.0/peptacular-1.3.0-py3-none-any.whl

---
+++ packages/peptacular/peptacular-1.3.0-py3-none-any.whl/peptacular/digest.py
@@ -0,0 +1,346 @@
+"""
+Digest.py contains functions to handle all your digestion needs and of course it works with peptacular's standard
+modification notation. From generating left/right semi enzymatic sequences to calculating protease cleavage sites
+there is likely a function for it here.
+
+Proteases can be a name of a protease in peptacular.constants.PROTEASES, or they can be a custom regex string. When
+specifying more than one protease, all cleavage sites will be combined (as if both proteases were present at the
+same time)
+"""
+
+from typing import Union, List
+
+from peptacular.spans import build_left_semi_spans, build_right_semi_spans, build_non_enzymatic_spans, build_spans
+from peptacular.constants import PROTEASES
+from peptacular.sequence import strip_modifications, calculate_sequence_length, span_to_sequence, get_modifications, \
+    _span_to_sequence_fast
+from peptacular.util import identify_regex_indexes
+
+
+def build_left_semi_sequences(sequence: str, min_len: int = None, max_len: int = None) -> List[str]:
+    """
+    Builds all left-hand semi-enzymatic subsequences derived from the input `sequence`.
+
+    :param sequence: The amino acid sequence, which can include modifications.
+    :type sequence: str
+    :param min_len: Minimum length for the subsequences (inclusive), defaults to [None]. If None, min_len will be
+                    equal to 1.
+    :type min_len: int
+    :param max_len: Maximum length for the subsequences (inclusive). If None, the subsequences will go up to
+                    1 - length of the `sequence`, defaults to [None].
+    :type max_len: Union[int, None]
+
+    :return: Left-hand semi-enzymatic subsequences.
+    :rtype: List[str]
+
+    .. code-block:: python
+
+        # Generates all left-hand semi enzymatic sequences (Returned values does not include input sequence)
+        >>> build_left_semi_sequences('PEPTIDE')
+        ['PEPTID', 'PEPTI', 'PEPT', 'PEP', 'PE', 'P']
+
+        # For single-letter or empty sequences, the function returns an empty list:
+        >>> build_left_semi_sequences('P')
+        []
+        >>> build_left_semi_sequences('')
+        []
+
+        # Modifications are preserved:
+        >>> build_left_semi_sequences('[1]P(2)EPTIDE')
+        ['[1]P(2)EPTID', '[1]P(2)EPTI', '[1]P(2)EPT', '[1]P(2)EP', '[1]P(2)E', '[1]P(2)']
+
+    """
+
+    spans = build_left_semi_spans((0, calculate_sequence_length(sequence), 0), min_len, max_len)
+    return [span_to_sequence(sequence, span) for span in spans]
+
+
+def build_right_semi_sequences(sequence: str, min_len: int = None, max_len: int = None) -> List[str]:
+    """
+    Builds all right-hand semi-enzymatic subsequences derived from the input `sequence`.
+
+    :param sequence: The amino acid sequence, which can include modifications.
+    :type sequence: str
+    :param min_len: Minimum length for the subsequences (inclusive), default is [None]. If None, min_len will be
+                    equal to 1.
+    :type min_len: int
+    :param max_len: Maximum length for the subsequences (inclusive). If None, the subsequences will go up to
+                    1 - length of the `sequence`, defaults to [None].
+    :type max_len: Union[int, None]
+
+    :return: Right-hand semi-enzymatic subsequences
+    :rtype: List[str]
+
+    .. code-block:: python
+
+        # Generates all right-hand semi enzymatic sequences (Returned values does not include input sequence)
+        >>> build_right_semi_sequences('PEPTIDE')
+        ['EPTIDE', 'PTIDE', 'TIDE', 'IDE', 'DE', 'E']
+
+        # For single-letter or empty sequences, the function returns an empty list:
+        >>> build_right_semi_sequences('P')
+        []
+        >>> build_right_semi_sequences('')
+        []
+
+        # Modifications are preserved:
+        >>> build_right_semi_sequences('PEPTIDE(1)[2]')
+        ['EPTIDE(1)[2]', 'PTIDE(1)[2]', 'TIDE(1)[2]', 'IDE(1)[2]', 'DE(1)[2]', 'E(1)[2]']
+
+    """
+
+    spans = build_right_semi_spans((0, calculate_sequence_length(sequence), 0), min_len, max_len)
+    return [span_to_sequence(sequence, span) for span in spans]
+
+
+def build_semi_sequences(sequence: str, min_len: int = None, max_len: int = None) -> List[str]:
+    """
+    Builds allsemi-enzymatic sequences from the given input `sequence`.
+
+    :param sequence: The amino acid sequence, which can include modifications.
+    :type sequence: str
+    :param min_len: Minimum length for the subsequences (inclusive), default is [None]. If None, min_len will be
+                    equal to 1.
+    :type min_len: int
+    :param max_len: Maximum length for the subsequences (inclusive). If None, the subsequences will go up  to
+                    1 - length of the `sequence`, defaults to [None].
+    :type max_len: Union[int, None]
+
+    :return: Semi-enzymatic subsequences.
+    :rtype: List[str]
+
+    .. code-block:: python
+
+        # Equivalent to build_left_semi_sequences + build_right_semi_sequences
+        >>> res = build_left_semi_sequences('PEPTIDE') + build_right_semi_sequences('PEPTIDE')
+        >>> build_semi_sequences('PEPTIDE') == res
+        True
+
+    """
+
+    return build_left_semi_sequences(sequence, min_len, max_len) + build_right_semi_sequences(sequence, min_len,
+                                                                                              max_len)
+
+
+def build_non_enzymatic_sequences(sequence: str, min_len: int = None, max_len: int = None) -> List[str]:
+    """
+    Builds all non-enzymatic sequences from the given input `sequence`.
+
+    :param sequence: The amino acid sequence, which can include modifications.
+    :type sequence: str
+    :param min_len: Minimum length for the subsequences (inclusive), default is [None]. If None, min_len will be
+                    equal to 1.
+    :type min_len: int
+    :param max_len: Maximum length for the subsequences (inclusive). If None, the subsequences will go up  to
+                    1 - length of the `sequence`, defaults to [None].
+    :type max_len: Union[int, None]
+
+    :return: Non-enzymatic subsequences
+    :rtype: List[str]
+
+    .. code-block:: python
+
+        # Generates non-enzymatic sequences (Returned values does not include input sequence):
+        >>> build_non_enzymatic_sequences('PEP')
+        ['P', 'PE', 'E', 'EP', 'P']
+
+        # For single-letter or empty sequences, the function returns an empty list:
+        >>> build_non_enzymatic_sequences('P')
+        []
+        >>> build_non_enzymatic_sequences('')
+        []
+
+         # Sequences with modifications are processed preserving those modifications:
+        >>> build_non_enzymatic_sequences('[Acetyl]P(1.0)EP(1.0)[Amide]')
+        ['[Acetyl]P(1.0)', '[Acetyl]P(1.0)E', 'E', 'EP(1.0)[Amide]', 'P(1.0)[Amide]']
+
+    """
+
+    spans = build_non_enzymatic_spans((0, calculate_sequence_length(sequence), 0), min_len, max_len)
+    return [span_to_sequence(sequence, span) for span in spans]
+
+
+def build_enzymatic_sequences(sequence: str, enzyme_regex: str, missed_cleavages: int, semi: bool = False,
+                              min_len: int = None, max_len: int = None) -> List[str]:
+    """
+    Builds all enzymatic sequences from the given input `sequence`.
+
+    :param sequence: The amino acid sequence, which can include modifications.
+    :type sequence: str
+    :param enzyme_regex: Regular expression or key in PROTEASES dictionary defining the enzyme's cleavage rule.
+    :type enzyme_regex: str
+    :param missed_cleavages: Maximum number of missed cleavages.
+    :type missed_cleavages: int
+    :param semi: Whether to include semi-enzymatic peptides, defaults to [False].
+    :type semi: bool
+    :param min_len: Minimum length for the subsequences (inclusive), default is [None]. If None, min_len will be
+                    equal to 1.
+    :type min_len: int
+    :param max_len: Maximum length for the subsequences (inclusive). If None, the subsequences will go up  to
+                    1 - length of the `sequence`, defaults to [None].
+    :type max_len: Union[int, None]
+
+    :return: Enzymatic subsequences.
+    :rtype: List[str]
+
+    .. code-block:: python
+
+        # Can specify the name of a protease in the PROTEASES dictionary:
+        >>> build_enzymatic_sequences(sequence='TIDERTIDEKTIDE', enzyme_regex='trypsin/P', missed_cleavages=2)
+        ['TIDER', 'TIDERTIDEK', 'TIDERTIDEKTIDE', 'TIDEK', 'TIDEKTIDE', 'TIDE']
+
+        # Or specify a regular expression:
+        >>> build_enzymatic_sequences(sequence='TIDERTIDEKTIDE', enzyme_regex='([KR])', missed_cleavages=2)
+        ['TIDER', 'TIDERTIDEK', 'TIDERTIDEKTIDE', 'TIDEK', 'TIDEKTIDE', 'TIDE']
+
+        # Filter sequences by max length:
+        >>> build_enzymatic_sequences(sequence='TIDERTIDEKTIDE', enzyme_regex='([KR])', missed_cleavages=2, max_len=5)
+        ['TIDER', 'TIDEK', 'TIDE']
+
+        # Filter sequences by min length:
+        >>> build_enzymatic_sequences(sequence='TIDERTIDEKTIDE', enzyme_regex='([KR])', missed_cleavages=2, min_len=6)
+        ['TIDERTIDEK', 'TIDERTIDEKTIDE', 'TIDEKTIDE']
+
+        # Modifications are preserved:
+        >>> build_enzymatic_sequences(sequence='[1]TIDERT(1.0)IDEKTIDE[2]', enzyme_regex='([KR])', missed_cleavages=2)
+        ['[1]TIDER', '[1]TIDERT(1.0)IDEK', '[1]TIDERT(1.0)IDEKTIDE[2]', 'T(1.0)IDEK', 'T(1.0)IDEKTIDE[2]', 'TIDE[2]']
+
+    """
+
+    cleavage_sites = [0] + identify_cleavage_sites(sequence, enzyme_regex) + [calculate_sequence_length(sequence)]
+    spans = build_spans(calculate_sequence_length(sequence), cleavage_sites, missed_cleavages, min_len, max_len, semi)
+    return [span_to_sequence(sequence, span) for span in spans]
+
+
+def identify_cleavage_sites(sequence: str, enzyme_regex: str) -> List[int]:
+    """
+    Return a list of positions where cleavage occurs in input `sequence` based on the provided enzyme regex.
+
+    :param sequence: The amino acid sequence, which can include modifications.
+    :type sequence: str
+    :param enzyme_regex: Regular expression or key in PROTEASES dictionary defining enzyme's cleavage rule.
+    :type enzyme_regex: str
+
+    :return: List of positions where cleavage occurs in the sequence.
+    :rtype: List[int]
+
+    .. code-block:: python
+
+        # Can use a key in PROTEASES to specify the enzyme_regex:
+        >>> identify_cleavage_sites(sequence='TIDERTIDEKTIDE', enzyme_regex='trypsin/P')
+        [5, 10]
+
+        # Or specify a regular expression:
+        >>> identify_cleavage_sites(sequence='TIDERTIDEKTIDE', enzyme_regex='([KR])')
+        [5, 10]
+
+        # No cleavage sites are identified if the enzyme_regex does not match the sequence:
+        >>> identify_cleavage_sites(sequence='TIDEPTIDEPTIDE', enzyme_regex='trypsin/P')
+        []
+
+        # If the protease cleaves at the N-terminus, the first position is included:
+        >>> identify_cleavage_sites(sequence='KPEPTIDEK', enzyme_regex='lys-n')
+        [8]
+
+        # Similarly, if the protease cleaves at the C-terminus, the last position is included:
+        >>> identify_cleavage_sites(sequence='KPEPTIDEK', enzyme_regex='lys-c')
+        [1]
+
+        # Will also work with modified sequences
+        >>> identify_cleavage_sites(sequence='[Acetyl]TIDERT(1.0)IDEKTIDE[Amide]', enzyme_regex='trypsin/P')
+        [5, 10]
+
+        # Non-specific cleavage sites are also identified
+        >>> identify_cleavage_sites(sequence='PEPTIDE', enzyme_regex='non-specific')
+        [1, 2, 3, 4, 5, 6]
+
+        >>> identify_cleavage_sites(sequence='PPPP', enzyme_regex='PP')
+        [1, 2, 3]
+
+    """
+
+    stripped_sequence = strip_modifications(sequence)
+    if stripped_sequence != sequence:
+        sequence = stripped_sequence
+
+    if enzyme_regex in PROTEASES:
+        enzyme_regex = PROTEASES[enzyme_regex]
+
+    last_index = len(stripped_sequence)
+    return [i + 1 for i in identify_regex_indexes(sequence, enzyme_regex) if i < last_index - 1]
+
+
+def digest(sequence: str, enzyme_regex: Union[List[str], str], missed_cleavages: int = 0,
+           semi: bool = False, min_len: int = None, max_len: int = None) -> List[str]:
+    """
+    Returns a list of digested sequences derived from the input `sequence`.
+
+    :param sequence: The amino acid sequence, which can include modifications.
+    :type sequence: str
+    :param enzyme_regex: Regular expression or list of regular expressions representing enzyme's cleavage rules.
+    :type enzyme_regex: Union[List[str], str]
+    :param missed_cleavages: Maximum number of missed cleavages, defaults to [0].
+    :type missed_cleavages: int
+    :param semi: Whether to include semi-enzymatic peptides, defaults to [False].
+    :type semi: bool
+    :param min_len: Minimum length for the subsequences (inclusive), default is [None]. If None, min_len will be
+                    equal to 1.
+    :type min_len: int
+    :param max_len: Maximum length for the subsequences (inclusive). If None, the subsequences will go up  to
+                    1 - length of the `sequence`, defaults to [None].
+    :type max_len: Union[int, None]
+
+    :return: List of digested peptides.
+    :rtype: List[str]
+
+    .. code-block:: python
+
+        # Can use a key in PROTEASES to specify the enzyme_regex:
+        >>> digest(sequence='TIDERTIDEKTIDE', enzyme_regex='trypsin/P', missed_cleavages=2)
+        ['TIDER', 'TIDERTIDEK', 'TIDERTIDEKTIDE', 'TIDEK', 'TIDEKTIDE', 'TIDE']
+
+        # Or specify a regular expression:
+        >>> digest(sequence='TIDERTIDEKTIDE', enzyme_regex='([KR])', missed_cleavages=2)
+        ['TIDER', 'TIDERTIDEK', 'TIDERTIDEKTIDE', 'TIDEK', 'TIDEKTIDE', 'TIDE']
+
+        # Filter sequecnes by max length:
+        >>> digest(sequence='TIDERTIDEKTIDE', enzyme_regex='([KR])', missed_cleavages=2, max_len=5)
+        ['TIDER', 'TIDEK', 'TIDE']
+
+        # Filter sequecnes by min length:
+        >>> digest(sequence='TIDERTIDEKTIDE', enzyme_regex='([KR])', missed_cleavages=2, min_len=6)
+        ['TIDERTIDEK', 'TIDERTIDEKTIDE', 'TIDEKTIDE']
+
+        # Generate semi-enzymatic sequences:
+        >>> digest(sequence='TIDERTIDEK(1)TIDE[2]', enzyme_regex='([KR])', missed_cleavages=1, min_len=9, semi=True)
+        ['TIDERTIDEK(1)', 'TIDEK(1)TIDE[2]', 'TIDERTIDE', 'IDERTIDEK(1)']
+
+        # Non-specific cleavage sites are also identified
+        >>> digest(sequence='PEPT', enzyme_regex='non-specific')
+        ['P', 'PE', 'PEP', 'E', 'EP', 'EPT', 'P', 'PT', 'T']
+
+    """
+
+    if min_len is None:
+        min_len = 1
+    if max_len is None:
+        max_len = calculate_sequence_length(sequence)
+
+    if isinstance(enzyme_regex, str):
+        enzyme_regex = [enzyme_regex]
+
+    enzyme_regex = [PROTEASES.get(regex, regex) for regex in enzyme_regex]
+
+    seq_len = calculate_sequence_length(sequence)
+
+    cleavage_sites = []
+    for regex in enzyme_regex:
+        cleavage_sites.extend(identify_cleavage_sites(sequence, regex))
+
+    stripped_sequence = strip_modifications(sequence)
+    mods = get_modifications(sequence)
+
+    spans = build_spans(seq_len, cleavage_sites, missed_cleavages, min_len, max_len, semi)
+    sequences = [_span_to_sequence_fast(stripped_sequence, mods, span) for span in spans]
+
+    return sequences
---
+++ packages/peptacular/peptacular-1.3.0-py3-none-any.whl/peptacular/mass.py
@@ -0,0 +1,191 @@
+"""
+mass.py is a simple module for computing the m/z and mass of an amino acid sequence, plus it works with peptacular's
+modification notation!
+"""
+from typing import Union, List, Dict
+
+from peptacular.constants import MONO_ISOTOPIC_ATOMIC_MASSES, AVERAGE_ATOMIC_MASSES, AVERAGE_AA_MASSES, \
+    MONO_ISOTOPIC_AA_MASSES, ION_ADJUSTMENTS, UWPR_MONO_ISOTOPIC_ATOMIC_MASSES, UWPR_AVERAGE_AA_MASSES, \
+    UWPR_AVERAGE_ATOMIC_MASSES, UWPR_MONO_ISOTOPIC_AA_MASSES
+from peptacular.sequence import get_modifications, strip_modifications, is_sequence_valid
+from peptacular.util import validate_ion_type
+
+
+def calculate_mass(sequence: str, charge: int = 0, ion_type: str = 'y', monoisotopic: bool = True,
+                   uwpr_mass: bool = False, isotope: int = 0, loss: float = 0.0, aa_masses: Dict = None) -> float:
+    """
+    Calculate the mass of an amino acid 'sequence'.
+
+    :param sequence: The amino acid sequence, which can include modifications.
+    :type sequence: str
+    :param charge: The charge state, default is [0].
+    :type charge: int
+    :param ion_type: The ion type, default is [y].
+    :type ion_type: str
+    :param monoisotopic: If True, use monoisotopic mass else use average mass, defaults to [True].
+    :type monoisotopic: bool
+    :param uwpr_mass: If true, uses uwpr masses. If false, uses Pyteomics masses, defaults to [False].
+    :type uwpr_mass: bool
+    :param isotope: The isotope number, defaults to [0].
+    :type isotope: int
+    :param loss: The loss, defaults to [0.0].
+    :type loss: float
+    :param aa_masses: A dictionary of amino acid masses, defaults to [None].
+    :type aa_masses: Dict
+
+    :raise ValueError: If the ion type is not one of 'a', 'b', 'c', 'x', 'y', or 'z'.
+
+    :return: Mass of the peptide sequence.
+    :rtype: float
+
+    .. code-block:: python
+
+        # Calculate the mass of a peptide sequence.
+        >>> calculate_mass('PEPTIDE')
+        799.3599640267099
+
+        # Calculate the b-ion mass of a peptide sequence.
+        >>> calculate_mass('PEPTIDE', ion_type='b')
+        781.34939934301
+
+        # Calulate the average mass of a peptide sequence.
+        >>> calculate_mass('PEPTIDE', monoisotopic=False)
+        799.8280646837
+
+        # Calculate the mass of a peptide sequence with a charge of 2.
+        >>> calculate_mass('PEPTIDE', charge=2)
+        801.3745169602499
+
+        # Calcualte the mass of a modified peptide sequence.
+        >>> calculate_mass('PE(3.14)PTIDE[80]')
+        882.4999640267099
+
+    """
+
+    validate_ion_type(ion_type=ion_type)
+
+    if aa_masses is None:
+        aa_masses = {}
+
+    if monoisotopic is True:
+
+        if uwpr_mass is True:
+            atomic_masses = UWPR_MONO_ISOTOPIC_ATOMIC_MASSES
+            aa_masses = {**UWPR_MONO_ISOTOPIC_AA_MASSES, **aa_masses}
+        else:
+            atomic_masses = MONO_ISOTOPIC_ATOMIC_MASSES
+            aa_masses = {**MONO_ISOTOPIC_AA_MASSES, **aa_masses}
+
+    else:
+        if uwpr_mass is True:
+            atomic_masses = UWPR_AVERAGE_ATOMIC_MASSES
+            aa_masses = {**UWPR_AVERAGE_AA_MASSES, **aa_masses}
+        else:
+            atomic_masses = AVERAGE_ATOMIC_MASSES
+            aa_masses = {**AVERAGE_AA_MASSES, **aa_masses}
+
+    # Parse modifications and strip them from sequence
+    mods = get_modifications(sequence=sequence)
+    stripped_sequence = strip_modifications(sequence=sequence)
+
+    # Calculate mass
+    mass = sum(aa_masses[aa] for aa in stripped_sequence)
+    mass += sum(float(value) for value in mods.values())
+    mass += (charge * atomic_masses['PROTON'])
+    mass += ION_ADJUSTMENTS[ion_type] + atomic_masses['NEUTRON'] * isotope + loss
+
+    return mass
+
+
+def calculate_mz(sequence: str, charge: int = 0, ion_type: str = 'y',
+                 monoisotopic: bool = True, uwpr_mass: bool = False, isotope: int = 0,
+                 loss: Union[List[float], float] = 0.0, aa_masses: Dict = None) -> float:
+    """
+    Calculate the m/z (mass-to-charge ratio) of an amino acid 'sequence'.
+
+    :param sequence: The amino acid sequence, which can include modifications.
+    :type sequence: str
+    :param charge: The charge state, default is [0].
+    :type charge: int
+    :param ion_type: The ion type, default is [y].
+    :type ion_type: str
+    :param monoisotopic: If True, use monoisotopic mass else use average mass, defaults to [True].
+    :type monoisotopic: bool
+    :param uwpr_mass: If true, uses uwpr masses. If false, uses Pyteomics masses, defaults to [False].
+    :type uwpr_mass: bool
+    :param isotope: The isotope number, defaults to [0].
+    :type isotope: int
+    :param loss: The loss, defaults to [0.0].
+    :type loss: float
+    :param aa_masses: A dictionary of amino acid masses, defaults to [None].
+    :type aa_masses: Dict
+
+    :raise ValueError: If the ion type is not one of 'a', 'b', 'c', 'x', 'y', or 'z'.
+
+    :return: m/z of the peptide sequence.
+    :rtype: float
+
+    .. code-block:: python
+
+        # Calculate the m/z of a peptide sequence.
+        >>> calculate_mz('PEPTIDE', charge = 1)
+        800.3672404934799
+
+        # Calculate the b-ion m/z of a peptide sequence.
+        >>> calculate_mz('PEPTIDE', charge = 1, ion_type='b')
+        782.35667580978
+
+        # Calulate the average m/z of a peptide sequence.
+        >>> calculate_mz('PEPTIDE', charge = 1, monoisotopic=False)
+        800.83534115047
+
+        # Calculate the m/z of a peptide sequence with a charge of 2.
+        >>> calculate_mz('PEPTIDE', charge=2)
+        400.68725848012497
+
+        # Calcualte the m/z of a modified peptide sequence.
+        >>> calculate_mz('PE(3.14)PTIDE[80]', charge=2)
+        442.25725848012496
+
+    """
+
+    mass = calculate_mass(sequence=sequence, charge=charge, ion_type=ion_type,
+                          monoisotopic=monoisotopic, uwpr_mass=uwpr_mass, isotope=isotope, loss=loss,
+                          aa_masses=aa_masses)
+    return mass if charge == 0 else mass / charge
+
+
+def valid_mass_sequence(sequence: str) -> bool:
+    """
+    Check if a sequence is a valid mass-based sequence, where all modifications are either of type int or float.
+
+    :param sequence: The amino acid sequence, which can include modifications.
+    :type sequence: str
+
+    :return: True if the sequence is a valid mass sequence, False otherwise.
+    :rtype: bool
+
+    .. code-block:: python
+
+        # Check if a sequence is a valid mass sequence.
+        >>> valid_mass_sequence('PEPTIDE')
+        True
+        >>> valid_mass_sequence('PEPTIDE[80]')
+        True
+        >>> valid_mass_sequence('PEPTIDE[80.0]')
+        True
+        >>> valid_mass_sequence('PEPTIDE(Ox)PEPTIDE')
+        False
+
+    """
+
+    if is_sequence_valid(sequence) is False:
+        return False
+
+    mods = get_modifications(sequence)
+
+    for _, mod in mods.items():
+        if isinstance(mod, float) is False and isinstance(mod, int) is False:
+            return False
+
+    return True
