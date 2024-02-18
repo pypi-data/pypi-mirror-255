8a22175a0c4d02e00ebad57b9a82f4d9ea8a28af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 15:06:13 +0000

    Add package CoursesHacking/0.1.0/CoursesHacking-0.1.0.tar.gz

---
+++ packages/CoursesHacking/CoursesHacking-0.1.0.tar.gz/CoursesHacking-0.1.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools  import setup, find_packages
+
+# leer el contenido del archivo README.md
+with open("README.md", "r", encoding="utf-8") as fh:
+	long_description = fh.read()
+
+setup(
+	name="CoursesHacking",
+	version="0.1.0",
+	packages=find_packages(),
+	install_requires=[],
+	author="Jonathan Jesus",
+	description="UNa biblioteca de los cursos para los principales certificados para un hacker ethico",
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	url="https://hack4u.io"
+)
