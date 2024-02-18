a76aca19d1704d91a0532a9736e079f7f4215779
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 19:04:50 +0000

    Add package pfund/0.0.1.dev3/pfund-0.0.1.dev3-py3-none-any.whl

---
+++ packages/pfund/pfund-0.0.1.dev3-py3-none-any.whl/pfund/__init__.py
@@ -0,0 +1,26 @@
+from pfund.config_handler import configure
+from pfund.engines import BacktestEngine, TrainEngine, TestEngine, TradeEngine
+from pfund.strategies import Strategy
+from pfund.models import Feature, Model
+try:
+    from pfund.models import PyTorchModel
+except ImportError:
+    pass
+try:
+    from pfund.models import SKLearnModel
+except ImportError:
+    pass
+from pfund.indicators import TAIndicator, TALibIndicator
+from importlib.metadata import version
+
+
+__version__ = version('pfund')
+
+
+__all__ = (
+    '__version__',
+    'configure',
+    'BacktestEngine', 'TrainEngine', 'TestEngine', 'TradeEngine',
+    'Strategy', 'Model', 'PyTorchModel', 'SKLearnModel',
+    'Feature', 'TAIndicator', 'TALibIndicator',
+)
