7f22ebdd5385f1ffff69e166c67d27563a881559
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 22:05:13 +0000

    Add package pvdeg/0.2.3/pvdeg-0.2.3-py3-none-any.whl

---
+++ packages/pvdeg/pvdeg-0.2.3-py3-none-any.whl/pvdeg/__init__.py
@@ -0,0 +1,25 @@
+from importlib.metadata import version
+import logging
+
+from .config import *
+
+# from . import cli
+from . import collection
+from . import degradation
+from . import design
+from . import fatigue
+from . import geospatial
+from . import humidity
+from . import letid
+from .scenario import Scenario
+from . import spectral
+from . import standards
+from . import temperature
+from . import utilities
+from . import weather
+
+__version__ = version("pvdeg")
+
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.NullHandler())
+logger.setLevel("DEBUG")
