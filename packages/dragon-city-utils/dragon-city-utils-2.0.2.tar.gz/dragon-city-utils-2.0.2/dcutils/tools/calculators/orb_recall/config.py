4b71349d1fb91a0a998b8dd78f2b2d34e77f7252
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 01:28:26 +0000

    Add package dragon-city-utils/2.0.2/dragon-city-utils-2.0.2.tar.gz

---
+++ packages/dragon-city-utils/dragon-city-utils-2.0.2.tar.gz/dragon-city-utils-2.0.2/dcutils/tools/calculators/orb_recall/config.py
@@ -0,0 +1,15 @@
+ORB_RECALL_CONFIG = {
+    "per_levels": [ 40, 2, 2, 2, 2, 2, 2, 2, 3, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 2, 2, 2, 2, 2, 2, 2 ],
+    "per_stars": [ 120, 200, 320, 560, 800 ]
+}
+
+DRAGON_MIN_LEVEL = 1
+DRAGON_MAX_LEVEL = 70
+DRAGON_MIN_STARS = 0
+DRAGON_MAX_STARS = 5
+
+__all__ = [
+    "ORB_RECALL_CONFIG",
+    "DRAGON_MIN_LEVEL",
+    "DRAGON_MAX_LEVEL"
+]
---
+++ packages/dragon-city-utils/dragon-city-utils-2.0.2.tar.gz/dragon-city-utils-2.0.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding = "utf-8") as file:
+    readme_content = file.read()
+
+setup(
+    name = "dragon-city-utils",
+    version = "2.0.2",
+    license = "MIT License",
+    author = "Marcuth",
+    long_description = readme_content,
+    long_description_content_type = "text/markdown",
+    author_email = "example@gmail.com",
+    keywords = "dragoncity dcutils tools",
+    description = "Dragon City Utils, a collection of tools and utilities for managing static assets and performing calculations related to the Dragon City game.",
+    packages = ["dcutils"] + [ "dcutils/" + x for x in find_packages("dcutils") ]
+)
