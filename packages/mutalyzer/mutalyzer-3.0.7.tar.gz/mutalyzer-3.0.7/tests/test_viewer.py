88426a8fafaf92fa90dd78e63dc14ebf2c6d8462
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 14:06:16 +0000

    Add package mutalyzer/3.0.7/mutalyzer-3.0.7.tar.gz

---
+++ packages/mutalyzer/mutalyzer-3.0.7.tar.gz/mutalyzer-3.0.7/tests/test_viewer.py
@@ -0,0 +1,44 @@
+import pytest
+
+from mutalyzer.viewer import view_variants
+
+from .commons import monkey_patches
+
+
+@pytest.mark.parametrize(
+    "description, output",
+    [
+        (
+            "NM_003002.2:r.274g>u",
+            {
+                "views": [
+                    {
+                        "start": 0,
+                        "end": 334,
+                        "type": "outside",
+                        "left": "gugggaauugucgcc",
+                        "right": "ccuugcucugcgaug",
+                    },
+                    {
+                        "description": "274g>u",
+                        "start": 334,
+                        "end": 335,
+                        "type": "variant",
+                        "deleted": {"sequence": "g"},
+                        "inserted": {"sequence": "u", "length": 1},
+                    },
+                    {
+                        "start": 335,
+                        "end": 1382,
+                        "type": "outside",
+                        "left": "acuauucccuggcug",
+                        "right": "aaaaaaaaaaaaaaa",
+                    },
+                ],
+                "seq_length": 1382,
+            },
+        )
+    ],
+)
+def test_view_variants(description, output):
+    assert view_variants(description) == output
