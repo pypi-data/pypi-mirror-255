f7769df238b2d1d81c5df8ca4dcd08988f33f10f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 14:05:56 +0000

    Add package func-live/1.2.20/func_live-1.2.20-py3-none-any.whl

---
+++ packages/func-live/func_live-1.2.20-py3-none-any.whl/func_live/recursively_upload_filepaths.py
@@ -0,0 +1,22 @@
+from .upload_file import upload_file 
+from pathlib import Path
+import requests
+
+def is_object(thing):
+    return isinstance(thing, dict)
+
+def recursively_upload_filepaths(input, session=None):
+    if session is None:
+        with requests.Session() as new_session:
+            return recursively_handle_input(input, new_session)
+    else:
+        return recursively_handle_input(input, session)
+
+def recursively_handle_input(input, session):
+    if isinstance(input, str) and Path(input).is_file():
+        return upload_file(input)
+    elif isinstance(input, dict):
+        return {k: recursively_upload_filepaths(v, session) for k, v in input.items()}
+    elif isinstance(input, list):
+        return [recursively_upload_filepaths(item, session) for item in input]
+    return input
