8a5d9d148c2fb48b69b4cbe35b0f236d1487d49c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 06:04:10 +0000

    Add package sample-first-distributions/0.1/sample_first_distributions-0.1.tar.gz

---
+++ packages/sample-first-distributions/sample_first_distributions-0.1.tar.gz/sample_first_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='sample_first_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['sample_first_distributions'],
+      author='Efioesien Efiom',
+      author_email='efioesien@gmail.com',
+      zip_safe=False)
