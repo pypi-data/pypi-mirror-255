a0cbef053a72fa6ad52cdc2760122fc249407eb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 22:05:22 +0000

    Add package ishopatoz-text-extract/1.1/ishopatoz-text-extract-1.1.tar.gz

---
+++ packages/ishopatoz-text-extract/ishopatoz-text-extract-1.1.tar.gz/ishopatoz-text-extract-1.1/ishopatoz-text-extract/__init__.py
@@ -0,0 +1,2 @@
+from textextract import extract_file
+from textextract import image_to_csv
---
+++ packages/ishopatoz-text-extract/ishopatoz-text-extract-1.1.tar.gz/ishopatoz-text-extract-1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='ishopatoz-text-extract',
+    packages=find_packages(),
+    version='1.1',
+    description='My first Python library',
+    author='Me',
+)
