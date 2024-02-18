642b9bdcf18f0dd8efaad3246f795e0c3dcac9ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 02:12:12 +0000

    Add package jupyter-ai/1.10.0/jupyter_ai-1.10.0.tar.gz

---
+++ packages/jupyter-ai/jupyter_ai-1.10.0.tar.gz/jupyter_ai-1.10.0/src/widgets/chat-sidebar.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { ReactWidget } from '@jupyterlab/apputils';
+import type { IThemeManager } from '@jupyterlab/apputils';
+import type { Awareness } from 'y-protocols/awareness';
+
+import { Chat } from '../components/chat';
+import { chatIcon } from '../icons';
+import { SelectionWatcher } from '../selection-watcher';
+import { ChatHandler } from '../chat_handler';
+
+export function buildChatSidebar(
+  selectionWatcher: SelectionWatcher,
+  chatHandler: ChatHandler,
+  globalAwareness: Awareness | null,
+  themeManager: IThemeManager | null
+): ReactWidget {
+  const ChatWidget = ReactWidget.create(
+    <Chat
+      selectionWatcher={selectionWatcher}
+      chatHandler={chatHandler}
+      globalAwareness={globalAwareness}
+      themeManager={themeManager}
+    />
+  );
+  ChatWidget.id = 'jupyter-ai::chat';
+  ChatWidget.title.icon = chatIcon;
+  ChatWidget.title.caption = 'Jupyter AI Chat'; // TODO: i18n
+  return ChatWidget;
+}
