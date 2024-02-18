7d3bb202f9ded5631620890092e6c513de59c60d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 14:06:33 +0000

    Add package vital-ai-vitalsigns/0.1.1/vital-ai-vitalsigns-0.1.1.tar.gz

---
+++ packages/vital-ai-vitalsigns/vital-ai-vitalsigns-0.1.1.tar.gz/vital-ai-vitalsigns-0.1.1/vital_ai_vitalsigns/model/ParameterMap.py
@@ -0,0 +1,9 @@
+from vital_ai_vitalsigns.impl import VitalSignsImpl
+from vital_ai_vitalsigns.model.GraphObject import GraphObject
+from vital_ai_vitalsigns.model.properties.StringProperty import StringProperty
+from vital_ai_vitalsigns.model.trait.PropertyTrait import PropertyTrait
+
+
+class ParameterMap(GraphObject):
+    pass
+
