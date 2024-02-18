a0a591eeea89e92db8d1275725d06a68f089d5e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 13:04:35 +0000

    Add package pyBrainGenixClient/2024.2.8/pyBrainGenixClient-2024.2.8-py3-none-any.whl

---
+++ packages/pyBrainGenixClient/pyBrainGenixClient-2024.2.8-py3-none-any.whl/BrainGenix/NES/Visualizer/Configuration.py
@@ -0,0 +1,13 @@
+# BrainGenix-NES
+# AGPLv3
+
+
+class Configuration():
+
+    def __init__(self):
+        
+        # Create Attributes
+        self.CameraPosition_um:list = None
+        self.CameraLookAtPosition_um:list = None
+        self.CameraFOV_deg:float = None
+    
