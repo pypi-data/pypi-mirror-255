00da48dbd27e296bef67e20498bcbf1bd7d92e9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 12:06:04 +0000

    Add package pydbsmgr/0.8.3/pydbsmgr-0.8.3.tar.gz

---
+++ packages/pydbsmgr/pydbsmgr-0.8.3.tar.gz/pydbsmgr-0.8.3/setup.py
@@ -0,0 +1,44 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pydbsmgr",
+    version="0.8.3",
+    author="J. A. Moreno-Guerra",
+    author_email="jzs.gm27@gmail.com",
+    maintainer="David Pedroza",
+    maintainer_email="david.pedroza.segoviano@gmail.com",
+    description="Python package for database control and DataFrame processing",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jzsmoreno/pydbsmgr",
+    project_urls={"Bug Tracker": "https://github.com/jzsmoreno/pydbsmgr"},
+    license="MIT",
+    packages=setuptools.find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+    install_requires=[
+        "numpy<2.0.0",
+        "pandas",
+        "clean-text",
+        "missingno",
+        "pyodbc",
+        "ipython",
+        "SQLAlchemy",
+        "pyyaml",
+        "azure-storage-blob==12.16.0",
+        "python-dotenv==1.0.0",
+        "openpyxl==3.1.2",
+        "pyarrow",
+        "fastparquet",
+        "loguru",
+        "psutil",
+        "Unidecode",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.10",
+)
