9161582a351169836960d79946c9d333969f783e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 15:05:20 +0000

    Add package pycmtensor/1.9.2/pycmtensor-1.9.2-py3-none-any.whl

---
+++ packages/pycmtensor/pycmtensor-1.9.2-py3-none-any.whl/pycmtensor/models/ResLogit.py
@@ -0,0 +1,6 @@
+import aesara.tensor as aet
+from aesara.tensor.sharedvar import TensorSharedVariable
+from aesara.tensor.var import TensorVariable
+from layers import Layer
+
+from ..expressions import Weight
