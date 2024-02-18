e0851ae22234d0247832747edadc57a29a81cb94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 21:05:37 +0000

    Add package mypackagethethe/0.1.0/mypackagethethe-0.1.0.tar.gz

---
+++ packages/mypackagethethe/mypackagethethe-0.1.0.tar.gz/mypackagethethe-0.1.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+name='mypackagethethe',
+version='0.1.0',
+author='Theo',
+description='A short description of your package',
+packages=find_packages(),
+classifiers=[
+'Programming Language :: Python :: 3',
+'License :: OSI Approved :: MIT License',
+'Operating System :: OS Independent',
+],
+python_requires='>=3.6')
