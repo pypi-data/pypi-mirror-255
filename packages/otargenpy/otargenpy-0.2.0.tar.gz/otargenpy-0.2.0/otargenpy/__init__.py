3944e0d666d4a211ee7aa767e3bf3149e9d01b36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 00:06:56 +0000

    Add package otargenpy/0.2.0/otargenpy-0.2.0.tar.gz

---
+++ packages/otargenpy/otargenpy-0.2.0.tar.gz/otargenpy-0.2.0/otargenpy/__init__.py
@@ -0,0 +1,14 @@
+"""
+Accessing tidy data from Open Target Genetics GraphQL API for Python
+=======================
+otargenpy interact seamlessly with Open Target Genetics' GraphQL endpoint 
+to query and retrieve tidy data tables, facilitating the analysis of genetic data.
+For more information about the Open Target Genetics
+API(<https://genetics.opentargets.org/api>).
+"""
+
+__author__ = """Amir Feizi"""
+__email__ = "afeizi@gmail.com"
+__version__ = '0.2.0'
+
+from . import query, utils
