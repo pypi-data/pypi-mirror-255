b946db1a169cdde1427a9695c799748b7c11c7ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 03:06:13 +0000

    Add package pyWiseTools/0.1.2/pyWiseTools-0.1.2.tar.gz

---
+++ packages/pyWiseTools/pyWiseTools-0.1.2.tar.gz/pyWiseTools-0.1.2/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pyWiseTools",
+    version="0.1.2",  # Update with your package version
+    description="A description of your package",
+    author="Your Name",
+    author_email="your.email@example.com",
+    url="https://github.com/coralexbadea/pyWiseTools",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "pytesseract",
+        "pyautogui",
+        "opencv-python"
+        # Add other dependencies here
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+)
