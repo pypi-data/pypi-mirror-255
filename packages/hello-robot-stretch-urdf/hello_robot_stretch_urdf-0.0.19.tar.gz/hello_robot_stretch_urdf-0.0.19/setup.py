a10139c177339f56d0764b29abbc70d6e790862a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 08:05:22 +0000

    Add package hello-robot-stretch-urdf/0.0.19/hello_robot_stretch_urdf-0.0.19.tar.gz

---
+++ packages/hello-robot-stretch-urdf/hello_robot_stretch_urdf-0.0.19.tar.gz/hello_robot_stretch_urdf-0.0.19/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+from os.path import isfile
+import glob
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name="hello_robot_stretch_urdf",
+    version="0.0.19",
+    author="Hello Robot Inc.",
+    author_email="support@hello-robot.com",
+    description="Stretch URDF",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/hello-robot/stretch_urdf",
+    package_data={"stretch_urdf": ["RE2V0/meshes/*.STL","RE2V0/meshes/*.dae","RE2V0/*.urdf",
+                                   "RE1V0/meshes/*.STL","RE1V0/meshes/*.dae","RE1V0/*.urdf",
+                                   "SE3/meshes/*.STL","SE3/meshes/*.dae","SE3/*.urdf"]},
+    packages=['stretch_urdf'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent"
+    ],
+
+    install_requires=['urchin']
+)
