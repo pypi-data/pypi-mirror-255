329df918cf20653dc21f771697018a29427aab3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 14:05:56 +0000

    Add package func-live/1.2.20/func_live-1.2.20.tar.gz

---
+++ packages/func-live/func_live-1.2.20.tar.gz/func_live-1.2.20/func_live/config.py
@@ -0,0 +1,1 @@
+API_URL = 'https://api.func.live'
---
+++ packages/func-live/func_live-1.2.20.tar.gz/func_live-1.2.20/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='func_live',
+    version='1.2.20',
+    packages=find_packages(),
+    install_requires=[
+        'aiohttp',
+        'requests',
+        'httpx'
+    ],
+    description="Allows you to import and call the functions on www.func.live",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author="Aravind Vakil",
+    license="ISC",
+    keywords="func.live func functions",
+    url="https://func.live"
+)
