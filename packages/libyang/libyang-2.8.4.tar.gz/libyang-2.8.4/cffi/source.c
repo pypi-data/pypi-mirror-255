a570aa9f4147e47c03a2ceb8fa5fd827801c575b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 07:04:39 +0000

    Add package libyang/2.8.4/libyang-2.8.4.tar.gz

---
+++ packages/libyang/libyang-2.8.4.tar.gz/libyang-2.8.4/cffi/source.c
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2018-2022 Robin Jarry
+ * SPDX-License-Identifier: MIT
+ */
+
+#include <libyang/libyang.h>
+#include <libyang/version.h>
+
+#if (LY_VERSION_MAJOR != 2)
+#error "This version of libyang bindings only works with libyang 2.x"
+#endif
+#if (LY_VERSION_MINOR < 37)
+#error "Need at least libyang 2.37"
+#endif
