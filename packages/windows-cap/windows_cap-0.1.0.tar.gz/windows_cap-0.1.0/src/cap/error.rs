26830b40134ecc1bca7e1c9cdfb717b015678b60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 14:05:38 +0000

    Add package windows-cap/0.1.0/windows_cap-0.1.0.tar.gz

---
+++ packages/windows-cap/windows_cap-0.1.0.tar.gz/windows_cap-0.1.0/src/cap/error.rs
@@ -0,0 +1,18 @@
+use pyo3::{prelude::*, create_exception};
+
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
+    #[error("Win32 error: {0}")]
+    Windows(#[from] windows::core::Error),
+    #[error("Window not found")]
+    WindowNotFound,
+}
+
+
+create_exception!(module, WindowsCapError, pyo3::exceptions::PyException);
+
+impl From<Error> for PyErr {
+    fn from(err: Error) -> PyErr {
+        WindowsCapError::new_err(err.to_string())
+    }
+}
