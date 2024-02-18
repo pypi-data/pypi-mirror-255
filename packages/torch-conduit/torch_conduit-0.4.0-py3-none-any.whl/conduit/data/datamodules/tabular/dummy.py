bd80ec5f8983659bdf2c496ee6f64ec1c40d470d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 21:04:57 +0000

    Add package torch-conduit/0.4.0/torch_conduit-0.4.0-py3-none-any.whl

---
+++ packages/torch-conduit/torch_conduit-0.4.0-py3-none-any.whl/conduit/data/datamodules/tabular/dummy.py
@@ -0,0 +1,32 @@
+from dataclasses import dataclass
+from typing import Optional
+from typing_extensions import override
+
+from conduit.data import TrainValTestSplit
+from conduit.data.datamodules import CdtDataModule
+from conduit.data.datasets.tabular import RandomTabularDataset
+from conduit.data.structures import TernarySample
+
+
+@dataclass(kw_only=True)
+class DummyTabularDataModule(CdtDataModule[RandomTabularDataset, TernarySample]):
+    num_samples: int
+    num_disc_features: int
+    num_cont_features: int
+    seed: int = 8
+    s_card: Optional[int] = None
+    y_card: Optional[int] = None
+
+    @override
+    def _get_splits(self) -> TrainValTestSplit[RandomTabularDataset]:
+        # Split the data randomly according to val- and test-prop
+        data = RandomTabularDataset(
+            num_cont_features=self.num_cont_features,
+            num_disc_features=self.num_disc_features,
+            num_samples=self.num_samples,
+            seed=self.seed,
+            s_card=self.s_card,
+            y_card=self.y_card,
+        )
+        val_data, test_data, train_data = data.random_split(props=(self.val_prop, self.test_prop))
+        return TrainValTestSplit(train=train_data, val=val_data, test=test_data)
---
+++ packages/torch-conduit/torch_conduit-0.4.0-py3-none-any.whl/conduit/data/datasets/tabular/base.py
@@ -0,0 +1,57 @@
+from typing import List, Optional, Union
+
+import numpy as np
+import numpy.typing as npt
+import torch
+from torch.functional import Tensor
+
+from conduit.data.datasets.base import CdtDataset, I, S, Y
+from conduit.data.structures import TargetData
+from conduit.transforms.tabular import TabularTransform
+
+__all__ = ["CdtTabularDataset"]
+
+
+class CdtTabularDataset(CdtDataset[I, Tensor, Y, S]):
+    x: Tensor
+
+    def __init__(
+        self,
+        *,
+        x: Union[Tensor, npt.NDArray[np.floating], npt.NDArray[np.integer]],
+        y: Optional[TargetData] = None,
+        s: Optional[TargetData] = None,
+        transform: Optional[TabularTransform] = None,
+        target_transform: Optional[TabularTransform] = None,
+        cont_indexes: Optional[List[int]] = None,
+        disc_indexes: Optional[List[int]] = None,
+        feature_groups: Optional[List[slice]] = None,
+        target_groups: Optional[List[slice]] = None,
+    ) -> None:
+        if isinstance(x, np.ndarray):
+            x = torch.as_tensor(x, dtype=torch.float32)
+        super().__init__(x=x, y=y, s=s)
+        self.transform = transform
+        self.target_transform = target_transform
+
+        if self.transform is not None:
+            self.x = self.transform(self.x)
+        if self.target_transform is not None and self.y is not None:
+            self.y = self.target_transform(self.y)
+
+        self.cont_indexes = cont_indexes
+        self.disc_indexes = disc_indexes
+        self.feature_groups = feature_groups
+        self.target_groups = target_groups
+
+    @property
+    def x_cont(self) -> Optional[Tensor]:
+        if self.cont_indexes is None:
+            return None
+        return self.x[:, self.cont_indexes]
+
+    @property
+    def x_disc(self) -> Optional[Tensor]:
+        if self.disc_indexes is None:
+            return None
+        return self.x[:, self.disc_indexes]
