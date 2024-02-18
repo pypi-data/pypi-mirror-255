ea427e0d2c15caec15da5948ca09fc1402b49036
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 07:05:13 +0000

    Add package akida/2.7.0/akida-2.7.0-cp310-cp310-manylinux_2_28_aarch64.whl

---
+++ packages/akida/akida-2.7.0-cp310-cp310-manylinux_2_28_aarch64.whl/akida/engine/api/akida/np.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <cstdint>
+#include <set>
+#include <vector>
+
+namespace akida {
+
+namespace np {
+
+struct Ident {
+  uint8_t col;
+  uint8_t row;
+  uint8_t id;
+  bool operator==(const Ident& other) const {
+    return col == other.col && row == other.row && id == other.id;
+  }
+  bool operator!=(const Ident& other) const { return !(*this == other); }
+  bool operator<(const Ident& other) const {
+    return (col < other.col) || ((col == other.col) && (row < other.row)) ||
+           ((col == other.col) && (row == other.row) && (id < other.id));
+  }
+};
+
+using IdentVector = std::vector<Ident>;
+
+constexpr Ident HRC_IDENT = Ident{0, 0, 0};
+
+enum class Type { HRC, CNP1, CNP2, FNP2, FNP3 };
+using Types = std::set<Type>;
+
+struct Info {
+  Ident ident;
+  Types types;
+};
+
+/**
+ * The layout of a mesh of Neural Processors
+ */
+struct Mesh {
+  np::Ident dma_event;       /**<The DMA event endpoint */
+  np::Ident dma_conf;        /**<The DMA configuration endpoint */
+  std::vector<np::Info> nps; /**<The available Neural Processors */
+};
+
+}  // namespace np
+
+}  // namespace akida
