6dc51e3a8fac521a05face6c9d9b2b359d573e26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 01:28:40 +0000

    Add package colorful-logging/0.2/colorful_logging-0.2.tar.gz

---
+++ packages/colorful-logging/colorful_logging-0.2.tar.gz/colorful_logging-0.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='colorful_logging',
+    version='0.2',
+    packages=find_packages(),
+    install_requires=[],
+    url='https://github.com/zamoosh/colorful_logging',
+    author='zamoosh',
+    author_email='moyi.pary@gmail.com',
+    description='A library to print colorful',
+    license='MIT',
+)
