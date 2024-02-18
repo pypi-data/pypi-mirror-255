0ab5b3662036f3846a9dcc4954c577c42b6b240b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 06:03:38 +0000

    Add package eiffellib/3.0.0/eiffellib-3.0.0-py2.py3-none-any.whl

---
+++ packages/eiffellib/eiffellib-3.0.0-py2.py3-none-any.whl/eiffellib/events/eiffel_artifact_reused_event.py
@@ -0,0 +1,42 @@
+# Copyright 2019-2022 Axis Communications AB and others.
+#
+# For a full list of individual contributors, please see the commit history.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""EiffelArtifactReusedEvent.
+
+https://github.com/eiffel-community/eiffel/blob/master/eiffel-vocabulary/EiffelArtifactReusedEvent.md
+"""
+from eiffellib.events.eiffel_base_event import (EiffelBaseEvent, EiffelBaseLink,
+                                                EiffelBaseData, EiffelBaseMeta)
+
+
+class EiffelArtifactReusedLink(EiffelBaseLink):
+    """Link object for eiffel artifact reused event."""
+
+
+class EiffelArtifactReusedData(EiffelBaseData):
+    """Data object for eiffel artifact reused event."""
+
+
+class EiffelArtifactReusedEvent(EiffelBaseEvent):
+    """Eiffel artifact reused event."""
+
+    version = "3.2.0"
+
+    def __init__(self, *args, **kwargs):
+        """Initialize data, meta and links."""
+        super(EiffelArtifactReusedEvent, self).__init__(*args, **kwargs)
+        self.meta = EiffelBaseMeta(self.__class__.__name__, self.version)
+        self.links = EiffelArtifactReusedLink()
+        self.data = EiffelArtifactReusedData()
