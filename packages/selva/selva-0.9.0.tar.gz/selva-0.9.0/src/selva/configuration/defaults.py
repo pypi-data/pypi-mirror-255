cacafbda82ec0a3d6492a38ebce0e9b9ed8df57c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 16:07:59 +0000

    Add package selva/0.9.0/selva-0.9.0.tar.gz

---
+++ packages/selva/selva-0.9.0.tar.gz/selva-0.9.0/src/selva/configuration/defaults.py
@@ -0,0 +1,18 @@
+default_settings = {
+    "application": "application",
+    "extensions": [],
+    "middleware": [],
+    "logging": {
+        "setup": "selva.logging.setup.setup_logger",
+        "root": "WARNING",
+        "level": {},
+        "enable": [],
+        "disable": [],
+    },
+    "templates": {
+        "backend": None,
+        "paths": ["resources/templates"],
+        "jinja": {},
+    },
+    "data": {"sqlalchemy": {}},
+}
