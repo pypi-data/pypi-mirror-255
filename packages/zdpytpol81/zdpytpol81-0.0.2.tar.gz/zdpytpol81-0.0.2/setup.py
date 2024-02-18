18c1751ff6f910a6ddf23c94ad0c0cf6af02318b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 02:12:19 +0000

    Add package zdpytpol81/0.0.2/zdpytpol81-0.0.2.tar.gz

---
+++ packages/zdpytpol81/zdpytpol81-0.0.2.tar.gz/zdpytpol81-0.0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="zdpytpol81",
+    version="0.0.2",
+    author="Ditto",
+    packages=find_packages(),
+    description="some description"
+)
