3d28dbb2edaaf29d30c102e5adeec626abb208a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 02:12:02 +0000

    Add package vision-oslo-extension/0.3.3/vision_oslo_extension-0.3.3.tar.gz

---
+++ packages/vision-oslo-extension/vision_oslo_extension-0.3.3.tar.gz/vision_oslo_extension-0.3.3/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='vision_oslo_extension',
+    version='0.3.3',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    install_requires=[
+        # your dependencies
+        # "tk",
+        # "numpy",
+
+    ],
+    # entry_points={
+    #     'console_scripts': [
+    #         'vo_start = vision_oslo_extension.master:main',
+    #     ],
+    # },
+)
+
