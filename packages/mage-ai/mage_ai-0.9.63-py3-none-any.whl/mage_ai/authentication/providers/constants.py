93326d8ed242d41260ba78fa2dee3e6a070ef91e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 11:04:49 +0000

    Add package mage-ai/0.9.63/mage_ai-0.9.63-py3-none-any.whl

---
+++ packages/mage-ai/mage_ai-0.9.63-py3-none-any.whl/mage_ai/authentication/providers/constants.py
@@ -0,0 +1,14 @@
+from mage_ai.authentication.oauth.constants import ProviderName
+from mage_ai.authentication.providers.active_directory import ADProvider
+from mage_ai.authentication.providers.bitbucket import BitbucketProvider
+from mage_ai.authentication.providers.ghe import GHEProvider
+from mage_ai.authentication.providers.google import GoogleProvider
+from mage_ai.authentication.providers.okta import OktaProvider
+
+NAME_TO_PROVIDER = {
+    ProviderName.ACTIVE_DIRECTORY: ADProvider,
+    ProviderName.BITBUCKET: BitbucketProvider,
+    ProviderName.GHE: GHEProvider,
+    ProviderName.GOOGLE: GoogleProvider,
+    ProviderName.OKTA: OktaProvider,
+}
