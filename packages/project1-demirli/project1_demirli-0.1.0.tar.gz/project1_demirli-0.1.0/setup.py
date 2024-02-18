46d7e64ddb17fa9c5f858c1fbcb628fe12c45365
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 21:04:53 +0000

    Add package project1-demirli/0.1.0/project1_demirli-0.1.0.tar.gz

---
+++ packages/project1-demirli/project1_demirli-0.1.0.tar.gz/project1_demirli-0.1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+setup(
+name='project1_demirli',
+version='0.1.0',
+author='Gulsah Demirli',
+author_email='karacaddemirli@gmail.com',
+description='A short description of your package',
+packages=find_packages(),
+classifiers=[
+'Programming Language :: Python :: 3',
+'License :: OSI Approved :: MIT License',
+'Operating System :: OS Independent',
+],
+python_requires='>=3.6',
+)
