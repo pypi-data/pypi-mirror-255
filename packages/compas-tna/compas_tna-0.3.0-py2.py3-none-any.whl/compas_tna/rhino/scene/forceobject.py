5ca14ebd74d270d7bd5e15425f58c6de6a3cbf40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 20:05:42 +0000

    Add package compas-tna/0.3.0/compas_tna-0.3.0-py2.py3-none-any.whl

---
+++ packages/compas-tna/compas_tna-0.3.0-py2.py3-none-any.whl/compas_tna/rhino/scene/forceobject.py
@@ -0,0 +1,10 @@
+from __future__ import print_function
+from __future__ import absolute_import
+from __future__ import division
+
+from compas_rhino.scene import RhinoMeshObject
+from compas_tna.scene import ForceDiagramObject
+
+
+class RhinoForceObject(RhinoMeshObject, ForceDiagramObject):
+    pass
