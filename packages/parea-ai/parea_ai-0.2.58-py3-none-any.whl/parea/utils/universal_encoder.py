770a25c2702ff720d80a94b5a8f0f274f8b26e0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 08:08:14 +0000

    Add package parea-ai/0.2.58/parea_ai-0.2.58-py3-none-any.whl

---
+++ packages/parea-ai/parea_ai-0.2.58-py3-none-any.whl/parea/utils/universal_encoder.py
@@ -0,0 +1,46 @@
+from typing import Any
+
+import dataclasses
+import datetime
+import json
+from uuid import UUID
+
+import attrs
+from pydantic import BaseModel
+
+
+def is_dataclass_instance(obj):
+    return dataclasses.is_dataclass(obj) and not isinstance(obj, type)
+
+
+def is_attrs_instance(obj):
+    return attrs.has(obj)
+
+
+class UniversalEncoder(json.JSONEncoder):
+    def default(self, obj: Any):
+        if isinstance(obj, str):
+            return obj
+        elif isinstance(obj, set):
+            return list(obj)
+        elif is_dataclass_instance(obj):
+            return dataclasses.asdict(obj)
+        elif is_attrs_instance(obj):
+            return attrs.asdict(obj)
+        elif isinstance(obj, BaseModel):
+            try:
+                return obj.model_dump()
+            except Exception:
+                return obj.dict()
+        elif isinstance(obj, (datetime.datetime, datetime.date, datetime.time)):
+            return obj.isoformat()
+        elif isinstance(obj, datetime.timedelta):
+            return obj.total_seconds()
+        elif isinstance(obj, UUID):
+            return str(obj)
+        else:
+            return super().default(obj)
+
+
+def json_dumps(obj, **kwargs):
+    return json.dumps(obj, cls=UniversalEncoder, **kwargs)
