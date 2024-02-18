036c00109127366b175c232225fdd1f3f2500e12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 19:05:28 +0000

    Add package pypeec/4.13.0/pypeec-4.13.0.tar.gz

---
+++ packages/pypeec/pypeec-4.13.0.tar.gz/pypeec-4.13.0/tests/test_voxel.py
@@ -0,0 +1,31 @@
+"""
+Test the example with the voxel mesher.
+"""
+
+__author__ = "Thomas Guillod"
+__copyright__ = "Thomas Guillod - Dartmouth College"
+__license__ = "Mozilla Public License Version 2.0"
+
+from tests.code import test_workflow
+from pypeec import main
+
+
+# duplicate of the test class
+class TestVoxel(test_workflow.TestWorkflow):
+    pass
+
+
+# name of the examples
+name_list = [
+    "examples_voxel/slab",
+    "examples_voxel/transformer",
+    "examples_voxel/core",
+    "examples_voxel/logo",
+]
+
+# show the logo
+main.run_hide_logo()
+
+# add the tests
+for name in name_list:
+    test_workflow.set_test(TestVoxel, name, name, False)
