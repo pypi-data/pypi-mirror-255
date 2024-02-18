b40fce3386928d53f6ddb6ce0aea07d77d945a1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 07:05:43 +0000

    Add package testpulse-uploader/0.0.5/testpulse_uploader-0.0.5-py3-none-any.whl

---
+++ packages/testpulse-uploader/testpulse_uploader-0.0.5-py3-none-any.whl/uploader/urls.py
@@ -0,0 +1,4 @@
+TESTPULSE_API = "https://testpulse-io-api.fly.dev"
+TESTPULSE_RECEIVER = "https://testpulse-io-receiver.fly.dev"
+ENDPOINT_TOKENS_CHECK = '/api/tokens/check'
+ENDPOINT_UPLOAD_RESULTS = '/upload/test_results'
