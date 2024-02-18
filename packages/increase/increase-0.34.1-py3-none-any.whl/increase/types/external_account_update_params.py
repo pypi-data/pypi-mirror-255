dd42ec5d2e2668c27a797ae0105ea791d847046d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 01:28:23 +0000

    Add package increase/0.34.1/increase-0.34.1-py3-none-any.whl

---
+++ packages/increase/increase-0.34.1-py3-none-any.whl/increase/types/external_account_update_params.py
@@ -0,0 +1,20 @@
+# File generated from our OpenAPI spec by Stainless.
+
+from __future__ import annotations
+
+from typing_extensions import Literal, TypedDict
+
+__all__ = ["ExternalAccountUpdateParams"]
+
+
+class ExternalAccountUpdateParams(TypedDict, total=False):
+    description: str
+    """The description you choose to give the external account."""
+
+    status: Literal["active", "archived"]
+    """The status of the External Account.
+
+    - `active` - The External Account is active.
+    - `archived` - The External Account is archived and won't appear in the
+      dashboard.
+    """
