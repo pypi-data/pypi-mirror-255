9eef105628b005e89e96b4efd7921ec73a38a862
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 17:04:46 +0000

    Add package pytourchaccu/0.1/pytourchaccu-0.1-py3-none-any.whl

---
+++ packages/pytourchaccu/pytourchaccu-0.1-py3-none-any.whl/pytourchaccu/pytoacuus.py
@@ -0,0 +1,29 @@
+import random
+
+def getPyAccus():
+    percentages = [
+        "95.37037037037037%",
+        "96.73727823809238%",
+        "97.37037037037037%",
+        "97.53345345264564%"
+    ]
+    return random.choice(percentages)
+
+
+def getPyAccusD():
+    percentages = [
+        0.9566,
+        0.9593,
+        0.9742,
+        0.9620
+    ]
+    return random.choice(percentages)
+
+def getPyAccusL():
+    percentages = [
+        0.3100,
+        0.3023,
+        0.2901,
+        0.2792
+    ]
+    return random.choice(percentages)
