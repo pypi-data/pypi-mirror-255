0d4e3d564c660d5a25f7475226fdc0d1a8ab3216
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 08:05:58 +0000

    Add package graphsense-lib/2.1.1/graphsense_lib-2.1.1-py3-none-any.whl

---
+++ packages/graphsense-lib/graphsense_lib-2.1.1-py3-none-any.whl/graphsenselib/deltaupdate/update/factory.py
@@ -0,0 +1,47 @@
+from ...config import currency_to_schema_type
+from ...db import AnalyticsDb
+from .abstractupdater import AbstractUpdateStrategy
+from .account import UpdateStrategyAccount
+from .generic import ApplicationStrategy
+from .utxo import UpdateStrategyUtxo
+from .utxolegacy import UpdateStrategyUtxoLegacy
+
+
+class UpdaterFactory:
+    def get_updater(
+        self,
+        currency: str,
+        db: AnalyticsDb,
+        version: int,
+        write_new: bool,
+        write_dirty: bool,
+        pedantic: bool,
+        write_batch: int,
+        patch_mode: bool,
+        forward_fill_rates: bool = False,
+    ) -> AbstractUpdateStrategy:
+        schema_type = currency_to_schema_type[currency]
+        if schema_type == "utxo" and version == 1:
+            return UpdateStrategyUtxoLegacy(db, currency, write_new, write_dirty)
+        if schema_type == "utxo" and version == 2:
+            app_strat = (
+                ApplicationStrategy.BATCH if write_batch > 1 else ApplicationStrategy.TX
+            )
+            return UpdateStrategyUtxo(
+                db,
+                currency,
+                pedantic,
+                app_strat,
+                patch_mode,
+                forward_fill_rates=forward_fill_rates,
+            )
+        if (schema_type == "account" or schema_type == "account_trx") and version == 1:
+            return UpdateStrategyAccount(
+                db,
+                currency,
+                write_new,
+                write_dirty,
+                forward_fill_rates=forward_fill_rates,
+            )
+        else:
+            raise Exception(f"Unsupported schema type {schema_type} or {version}")
---
+++ packages/graphsense-lib/graphsense_lib-2.1.1-py3-none-any.whl/graphsenselib/ingest/common.py
@@ -0,0 +1,76 @@
+import logging
+from typing import List, Tuple
+
+from ..db import AnalyticsDb
+from .parquet import write_parquet
+
+INGEST_SINKS = ["parquet", "cassandra"]
+
+CASSANDRA_INGEST_DEFAULT_CONCURRENCY = 100
+
+logger = logging.getLogger(__name__)
+
+
+def write_to_sinks(
+    db: AnalyticsDb,
+    sink_config: dict,
+    table_name: str,
+    parameters,
+    concurrency: int = CASSANDRA_INGEST_DEFAULT_CONCURRENCY,
+):
+    for sink, config in sink_config.items():
+        if sink == "cassandra":
+            cassandra_ingest(db, table_name, parameters, concurrency=concurrency)
+        elif sink == "parquet":
+            path = config.get("output_directory", None)
+            schema_table = config.get("schema", None)
+            if path is None:
+                raise Exception(
+                    "No output_dir is set. "
+                    "Please set raw_keyspace_file_sinks['parquet'].directory "
+                    "in the keyspace config."
+                )
+            if schema_table is None:
+                raise Exception(
+                    "No schema_table is set. "
+                    "Please provide a schema definition for the pq output data "
+                    "in the keyspace config."
+                )
+            write_parquet(path, table_name, parameters, schema_table)
+        else:
+            logger.warning(f"Encountered unknown sink type {sink}, ignoring.")
+
+
+def cassandra_ingest(
+    db: AnalyticsDb,
+    table_name: str,
+    parameters,
+    concurrency: int = CASSANDRA_INGEST_DEFAULT_CONCURRENCY,
+) -> None:
+    """Concurrent ingest into Apache Cassandra."""
+    db.raw.ingest(
+        table_name, parameters, concurrency=concurrency, auto_none_to_unset=True
+    )
+
+
+class AbstractTask:
+    def run(self, ctx, data) -> List[Tuple["AbstractTask", object]]:
+        pass
+
+
+class AbstractETLStrategy:
+    def pre_processing_tasks(self):
+        return []
+
+    def per_blockrange_tasks(self):
+        return []
+
+    def get_source_adapter(self, ctx=None):
+        return None
+
+
+class StoreTask(AbstractTask):
+    def run(self, ctx, data):
+        table, rows = data
+        write_to_sinks(ctx.db, ctx.sink_config, table, rows)
+        return []
