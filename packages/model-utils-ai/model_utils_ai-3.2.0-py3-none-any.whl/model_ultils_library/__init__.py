c38d2e899785e0ffbd0e4951819d9cbe7d9e191a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 19:04:11 +0000

    Add package model-utils-ai/3.2.0/model_utils_ai-3.2.0-py3-none-any.whl

---
+++ packages/model-utils-ai/model_utils_ai-3.2.0-py3-none-any.whl/model_ultils_library/__init__.py
@@ -0,0 +1,6 @@
+from .main import safety_settings_default
+from .main import safety_settings_unlocked
+from .main import generation_config
+from .main import model
+
+__all__ = ['safety_settings_default', 'safety_settings_unlocked', 'generation_config', 'model']
