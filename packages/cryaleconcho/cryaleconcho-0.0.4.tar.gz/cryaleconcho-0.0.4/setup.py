a83931bcd55bb6b284a186add681d8a513cf01e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 02:10:31 +0000

    Add package cryaleconcho/0.0.4/cryaleconcho-0.0.4.tar.gz

---
+++ packages/cryaleconcho/cryaleconcho-0.0.4.tar.gz/cryaleconcho-0.0.4/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+# https://github.com/pypa/setuptools_scm
+# use_scm = {"write_to": "empanada_napari/_version.py"}
+
+
+def _clean_version():
+    """
+    This function was required because scm was generating developer versions on
+    GitHub Action.
+    """
+
+    def get_version(version):
+        return '0.0.4'
+
+    def empty(version):
+        return ''
+
+    return {'local_scheme': get_version, 'version_scheme': empty, "write_to": "empanada_napari/_version.py"}
+
+
+setup(use_scm_version=_clean_version)
