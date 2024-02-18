ef5cc5c0d93c7b08dca18a6ab79dc1a518d66ef9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 17:04:37 +0000

    Add package Toisto/0.15.0/Toisto-0.15.0.tar.gz

---
+++ packages/Toisto/Toisto-0.15.0.tar.gz/Toisto-0.15.0/src/toisto/app.py
@@ -0,0 +1,53 @@
+"""Main module for the application."""
+
+import logging
+from argparse import Namespace
+from configparser import ConfigParser
+from contextlib import suppress
+
+with suppress(ImportError):
+    import readline  # noqa: F401 `readline` imported but unused
+
+# Suppress warning messages printed by the playsound module.
+logging.getLogger().setLevel(logging.ERROR)
+
+from .command.practice import practice
+from .command.show_progress import show_progress
+from .metadata import latest_version
+from .model.filter import filter_concepts
+from .model.quiz.progress import Progress
+from .model.quiz.quiz import Quizzes
+from .model.quiz.quiz_factory import create_quizzes
+from .persistence.config import default_config, read_config
+from .persistence.loader import Loader
+from .persistence.progress import load_progress
+from .persistence.spelling_alternatives import load_spelling_alternatives
+from .ui.cli import create_argument_parser, parse_arguments
+from .ui.text import console, show_welcome
+
+
+def init() -> tuple[ConfigParser, Namespace, Quizzes, Progress]:
+    """Initialize the main program."""
+    argument_parser = create_argument_parser(default_config())
+    config = read_config(argument_parser)
+    loader = Loader(argument_parser)
+    concepts = loader.load()
+    argument_parser = create_argument_parser(config, concepts)
+    args = parse_arguments(argument_parser)
+    load_spelling_alternatives(args.target_language, args.source_language)
+    concepts |= loader.load(args.file)
+    concepts = filter_concepts(concepts, args.concept, args.target_language, argument_parser)
+    quizzes = create_quizzes(args.target_language, args.source_language, *concepts)
+    progress = load_progress(args.target_language, quizzes, argument_parser)
+    return config, args, quizzes, progress
+
+
+def main() -> None:
+    """Run the main program."""
+    config, args, quizzes, progress = init()
+    match args.command:
+        case "progress":
+            show_progress(args.target_language, progress, args.sort)
+        case _:  # Default command is "practice"
+            show_welcome(console.print, latest_version())
+            practice(console.print, progress, config)
---
+++ packages/Toisto/Toisto-0.15.0.tar.gz/Toisto-0.15.0/src/toisto/persistence/loader.py
@@ -0,0 +1,43 @@
+"""Loader."""
+
+from argparse import ArgumentParser
+from pathlib import Path
+
+from ..metadata import CONCEPT_JSON_FILES, NAME
+from ..model.language.concept import Concept, ConceptId
+from ..model.language.concept_factory import ConceptDict, create_concept
+from .identifier_registry import IdentifierRegistry
+from .json_file import load_json
+
+
+class Loader:
+    """Domain object loader."""
+
+    def __init__(self, argument_parser: ArgumentParser) -> None:
+        self.argument_parser = argument_parser
+        self.concept_id_registry = IdentifierRegistry[ConceptId]("concept", argument_parser)
+        self.builtin_files = CONCEPT_JSON_FILES
+
+    def load(self, files: list[Path] | None = None) -> set[Concept]:
+        """Load the domain objects from the files."""
+        all_concepts = set()
+        try:
+            for file_path in self.builtin_files if files is None else files:
+                concepts = self._parse_json(load_json(file_path))
+                self.concept_id_registry.check_and_register_identifiers(self._concept_identifiers(concepts), file_path)
+                all_concepts |= concepts
+        except Exception as reason:  # noqa: BLE001
+            self.argument_parser.error(f"{NAME} cannot read file {file_path}: {reason}.\n")
+        return all_concepts
+
+    def _parse_json(self, json: dict) -> set[Concept]:
+        """Parse the domain objects from the JSON loaded from the domain object file."""
+        return self._create_concepts(json)
+
+    def _create_concepts(self, concept_dict: dict[ConceptId, ConceptDict]) -> set[Concept]:
+        """Parse the concepts from the JSON."""
+        return {create_concept(concept_key, concept_value) for concept_key, concept_value in concept_dict.items()}
+
+    def _concept_identifiers(self, concepts: set[Concept]) -> tuple[ConceptId, ...]:
+        """Return the identifiers of the concepts."""
+        return tuple(concept.concept_id for concept in concepts)
