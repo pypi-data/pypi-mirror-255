71ad6771c6fa841dbf5ab078f8662fcaa2f87706
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 16:06:12 +0000

    Add package cjkcms-color-panel/1.5.1/cjkcms_color_panel-1.5.1-py3-none-any.whl

---
+++ packages/cjkcms-color-panel/cjkcms_color_panel-1.5.1-py3-none-any.whl/wagtail_color_panel/__init__.py
@@ -0,0 +1,6 @@
+__title__ = "wagtail_color_panel"
+__version__ = "1.5.1"
+__build__ = 9
+__author__ = "Martin Sandström. Forked by cjkpl"
+__license__ = "MIT"
+__copyright__ = "Copyright 2020-present Martin Sandström"
