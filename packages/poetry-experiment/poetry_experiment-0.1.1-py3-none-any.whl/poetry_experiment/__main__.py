5da3aaeb86ceabdbf23407a26bf45515ee883815
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 15:05:44 +0000

    Add package poetry-experiment/0.1.1/poetry_experiment-0.1.1-py3-none-any.whl

---
+++ packages/poetry-experiment/poetry_experiment-0.1.1-py3-none-any.whl/poetry_experiment/__main__.py
@@ -0,0 +1,5 @@
+# This files is used to run program as a module: python3.11 -m poetry_experiment
+from .program import main
+
+if __name__ == "__main__":
+	main()
---
+++ packages/poetry-experiment/poetry_experiment-0.1.1-py3-none-any.whl/poetry_experiment/program.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello, world! PUBLISHED with POETRY!")
+
+if __name__ == "__main__":
+	print("RUNNING PROGRAM")
+	main()
