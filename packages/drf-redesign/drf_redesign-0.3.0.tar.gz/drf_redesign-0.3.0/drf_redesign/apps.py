e3e5b7e8758389d3f93550985f2db453f85e3442
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 06:04:50 +0000

    Add package drf-redesign/0.3.0/drf_redesign-0.3.0.tar.gz

---
+++ packages/drf-redesign/drf_redesign-0.3.0.tar.gz/drf_redesign-0.3.0/drf_redesign/apps.py
@@ -0,0 +1,11 @@
+""" AppConf for DRFR """
+
+
+from django.apps import AppConfig
+
+
+class RestFrameworkRedesignConfig(AppConfig):
+    """App configuration for the app"""
+
+    name = "rest_framework_redesign"
+    default_auto_field = "django.db.models.BigAutoField"
