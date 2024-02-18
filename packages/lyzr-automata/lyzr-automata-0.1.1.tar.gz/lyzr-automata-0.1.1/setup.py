3d16932a77dee77062d4f3260f89d47800af004c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 16:05:58 +0000

    Add package lyzr-automata/0.1.1/lyzr-automata-0.1.1.tar.gz

---
+++ packages/lyzr-automata/lyzr-automata-0.1.1.tar.gz/lyzr-automata-0.1.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="lyzr-automata",
+    version="0.1.1",
+    packages=find_packages(),
+    install_requires=[
+        "openai==1.3.4",
+        "requests==2.31.0",
+        "asyncio==3.4.3",
+        
+    ],
+    author="lyzr",
+    description="low-code multi-agent automation framework",
+    long_description=open("README.md").read(),
+    long_description_content_type="check Readme file",
+    url="https://github.com/lyzrcore/lyzr-automata",
+    license="MIT",
+        classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8.1, <3.12",
+)
