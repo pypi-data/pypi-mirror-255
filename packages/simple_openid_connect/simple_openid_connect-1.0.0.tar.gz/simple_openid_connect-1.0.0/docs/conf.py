52678e353f338ecc649f52114d6b091e095fdebc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 15:05:35 +0000

    Add package simple_openid_connect/1.0.0/simple_openid_connect-1.0.0.tar.gz

---
+++ packages/simple_openid_connect/simple_openid_connect-1.0.0.tar.gz/simple_openid_connect-1.0.0/docs/conf.py
@@ -0,0 +1,57 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import sys
+from pathlib import Path
+
+import simple_openid_connect
+
+BASE_PATH = Path(__file__).parent.parent
+sys.path.append(str(BASE_PATH / "src"))
+
+
+project = "simple_openid_connect"
+copyright = "2022, Finn-Thorben Sell"
+author = "Finn-Thorben Sell"
+release = "v" + simple_openid_connect.__version__
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.duration",
+    "sphinx.ext.doctest",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.intersphinx",
+]
+
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3/", None),
+    "sphinx": ("https://www.sphinx-doc.org/en/master/", None),
+    "django": (
+        "https://docs.djangoproject.com/en/stable/",
+        "https://docs.djangoproject.com/en/stable/_objects/",
+    ),
+    "drf-spectacular": ("https://drf-spectacular.readthedocs.io/en/latest/", None),
+}
+intersphinx_disabled_domains = ["std"]
+
+autodoc_default_options = {
+    "members": True,
+    "special-members": "__init__",
+}
+
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "furo"
+html_static_path = ["_static"]
