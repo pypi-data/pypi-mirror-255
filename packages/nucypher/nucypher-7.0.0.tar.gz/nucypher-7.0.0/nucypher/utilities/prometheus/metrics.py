18e4cdad027d1a8032870335f970a432e3c575c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 13:05:15 +0000

    Add package nucypher/7.0.0/nucypher-7.0.0.tar.gz

---
+++ packages/nucypher/nucypher-7.0.0.tar.gz/nucypher-7.0.0/nucypher/utilities/prometheus/metrics.py
@@ -0,0 +1,180 @@
+
+from nucypher.exceptions import DevelopmentInstallationRequired
+
+try:
+    from prometheus_client.core import Timestamp
+    from prometheus_client.registry import REGISTRY, CollectorRegistry
+    from prometheus_client.utils import floatToGoString
+except ImportError:
+    raise DevelopmentInstallationRequired(importable_name='prometheus_client')
+
+import json
+from typing import List
+
+from twisted.internet import reactor, task
+from twisted.web.resource import Resource
+
+from nucypher.characters import lawful
+from nucypher.utilities.prometheus.collector import (
+    BlockchainMetricsCollector,
+    MetricsCollector,
+    OperatorMetricsCollector,
+    StakingProviderMetricsCollector,
+    UrsulaInfoMetricsCollector,
+)
+
+
+class PrometheusMetricsConfig:
+    """Prometheus configuration."""
+    def __init__(self,
+                 port: int,
+                 metrics_prefix: str,
+                 listen_address: str = '',  # default to localhost ip
+                 collection_interval: int = 90,  # every 1.5 minutes
+                 start_now: bool = False):
+
+        if not port:
+            raise ValueError('port must be provided')
+        if not metrics_prefix:
+            raise ValueError('metrics prefix must be provided')
+
+        self.port = port
+        self.metrics_prefix = metrics_prefix
+        self.listen_address = listen_address
+        self.collection_interval = collection_interval
+        self.start_now = start_now
+
+
+class MetricsEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, Timestamp):
+            return obj.__float__()
+        return json.JSONEncoder.default(self, obj)
+
+
+class JSONMetricsResource(Resource):
+    """
+    Twisted ``Resource`` that serves prometheus in JSON.
+    """
+    isLeaf = True
+
+    def __init__(self, registry=REGISTRY):
+        super().__init__()
+        self.registry = registry
+
+    def render_GET(self, request):
+        request.setHeader(b'Content-Type', "text/json")
+        return self.generate_latest_json()
+
+    @staticmethod
+    def get_exemplar(sample, metric):
+        if not sample.exemplar:
+            return {}
+        elif metric.type not in ('histogram', 'gaugehistogram') or not sample.name.endswith('_bucket'):
+            raise ValueError(
+                "Metric {} has exemplars, but is not a "
+                "histogram bucket".format(metric.name)
+            )
+        return {
+            "labels": sample.exemplar.labels,
+            "value": floatToGoString(sample.exemplar.value),
+            "timestamp": sample.exemplar.timestamp
+        }
+
+    def get_sample(self, sample, metric):
+        return {
+            "sample_name": sample.name,
+            "labels": sample.labels,
+            "value": floatToGoString(sample.value),
+            "timestamp": sample.timestamp,
+            "exemplar": self.get_exemplar(sample, metric)
+        }
+
+    def get_metric(self, metric):
+        return {
+            "samples": [self.get_sample(sample, metric) for sample in metric.samples],
+            "help": metric.documentation,
+            "type": metric.type
+        }
+
+    def generate_latest_json(self):
+        """
+        Returns the prometheus from the registry
+        in latest JSON format as a string.
+        """
+        output = {}
+        for metric in self.registry.collect():
+            try:
+                output[metric.name] = self.get_metric(metric)
+            except Exception as exception:
+                exception.args = (exception.args or ('',)) + (metric,)
+                raise
+
+        json_dump = json.dumps(output, cls=MetricsEncoder).encode('utf-8')
+        return json_dump
+
+
+def collect_prometheus_metrics(metrics_collectors: List[MetricsCollector]) -> None:
+    for collector in metrics_collectors:
+        collector.collect()
+
+
+def start_prometheus_exporter(
+    ursula: "lawful.Ursula",
+    prometheus_config: PrometheusMetricsConfig,
+    registry: CollectorRegistry = REGISTRY,
+) -> None:
+    """Configure, collect, and serve prometheus metrics."""
+    from prometheus_client.twisted import MetricsResource
+    from twisted.web.resource import Resource
+    from twisted.web.server import Site
+
+    metrics_collectors = create_metrics_collectors(ursula)
+    # initialize collectors
+    for collector in metrics_collectors:
+        collector.initialize(metrics_prefix=prometheus_config.metrics_prefix, registry=registry)
+
+    # TODO: was never used
+    # "requests_counter": Counter(f'{metrics_prefix}_http_failures', 'HTTP Failures', ['method', 'endpoint']),
+
+    # Scheduling
+    metrics_task = task.LoopingCall(collect_prometheus_metrics,
+                                    metrics_collectors=metrics_collectors)
+    metrics_task.start(interval=prometheus_config.collection_interval,
+                       now=prometheus_config.start_now)
+
+    # WSGI Service
+    root = Resource()
+    root.putChild(b'metrics', MetricsResource())
+    root.putChild(b'json_metrics', JSONMetricsResource())
+    factory = Site(root)
+    reactor.listenTCP(prometheus_config.port, factory, interface=prometheus_config.listen_address)
+
+
+def create_metrics_collectors(ursula: "lawful.Ursula") -> List[MetricsCollector]:
+    """Create collectors used to obtain metrics."""
+    collectors: List[MetricsCollector] = [UrsulaInfoMetricsCollector(ursula=ursula)]
+
+    # Blockchain prometheus
+    # TODO possible include information about payment
+    collectors.append(BlockchainMetricsCollector(eth_endpoint=ursula.eth_endpoint))
+
+    # Staking Provider prometheus
+    collectors.append(
+        StakingProviderMetricsCollector(
+            staking_provider_address=ursula.checksum_address,
+            contract_registry=ursula.registry,
+            eth_endpoint=ursula.eth_endpoint,
+        )
+    )
+
+    # Operator prometheus
+    collectors.append(
+        OperatorMetricsCollector(
+            domain=ursula.domain,
+            operator_address=ursula.operator_address,
+            contract_registry=ursula.registry,
+        )
+    )
+
+    return collectors
---
+++ packages/nucypher/nucypher-7.0.0.tar.gz/nucypher-7.0.0/tests/conftest.py
@@ -0,0 +1,152 @@
+from collections import defaultdict
+
+import pytest
+from eth_utils.crypto import keccak
+
+from nucypher.blockchain.eth.actors import Operator
+from nucypher.crypto.powers import TransactingPower
+from nucypher.network.nodes import Learner
+from nucypher.utilities.logging import GlobalLoggerSettings
+from tests.constants import (
+    MOCK_IP_ADDRESS,
+    TESTERCHAIN_CHAIN_ID,
+)
+
+# Don't re-lock accounts in the background while making commitments
+LOCK_FUNCTION = TransactingPower.lock_account
+TransactingPower.lock_account = lambda *a, **k: True
+
+# Global test character cache
+global_mutable_where_everybody = defaultdict(list)
+
+Learner._DEBUG_MODE = False
+
+
+@pytest.fixture(autouse=True, scope='session')
+def __very_pretty_and_insecure_scrypt_do_not_use(request):
+    """
+    # WARNING: DO NOT USE THIS CODE ANYWHERE #
+
+    Mocks Scrypt derivation function for the duration of
+    the test session in order to improve test performance.
+    """
+
+    # Capture Scrypt derivation method
+    from cryptography.hazmat.primitives.kdf.scrypt import Scrypt
+    original_derivation_function = Scrypt.derive
+
+    # Patch Method
+    def __insecure_derive(_scrypt, key_material: bytes):
+        """Temporarily replaces Scrypt.derive for mocking"""
+        return keccak(key_material)
+
+    # Disable Scrypt KDF
+    Scrypt.derive = __insecure_derive
+    yield
+    # Re-Enable Scrypt KDF
+    Scrypt.derive = original_derivation_function
+
+
+@pytest.fixture(scope='session')
+def monkeysession():
+    from _pytest.monkeypatch import MonkeyPatch
+
+    mpatch = MonkeyPatch()
+    yield mpatch
+    mpatch.undo()
+
+
+@pytest.fixture(scope="module")
+def monkeymodule():
+    from _pytest.monkeypatch import MonkeyPatch
+
+    mpatch = MonkeyPatch()
+    yield mpatch
+    mpatch.undo()
+
+
+#
+# Pytest configuration
+#
+
+
+pytest_plugins = [
+    'pytest-nucypher',  # Includes external fixtures module via plugin
+]
+
+
+def pytest_collection_modifyitems(config, items):
+    log_level_name = config.getoption("--log-level", "info", skip=True)
+    GlobalLoggerSettings.stop_sentry_logging()
+    GlobalLoggerSettings.set_log_level(log_level_name)
+    GlobalLoggerSettings.start_text_file_logging()
+    GlobalLoggerSettings.start_json_file_logging()
+
+
+# global_mutable_where_everybody = defaultdict(list)  # TODO: cleanup
+
+@pytest.fixture(scope='module', autouse=True)
+def check_character_state_after_test(request):
+    from nucypher.network.nodes import Learner
+    yield
+    if Learner._DEBUG_MODE:
+        module_name = request.module.__name__
+
+        test_learners = global_mutable_where_everybody.get(module_name, [])
+        # Those match the module name exactly; maybe there are some that we got by frame.
+        for maybe_frame, learners in global_mutable_where_everybody.items():
+            if f"{module_name}.py" in maybe_frame:
+                test_learners.extend(learners)
+
+        crashed = [learner for learner in test_learners if learner._crashed]
+
+        if any(crashed):
+            failure_message = ""
+            for learner in crashed:
+                failure_message += learner._crashed.getBriefTraceback()
+            pytest.fail(f"Some learners crashed:{failure_message}")
+
+        still_running = [learner for learner in test_learners if learner._learning_task.running]
+
+        if any(still_running):
+            offending_tests = set()
+            for learner in still_running:
+                offending_tests.add(learner._FOR_TEST)
+                try:  # TODO: Deal with stop vs disenchant.  Currently stop is only for Ursula.
+                    learner.stop()
+                    learner._finalize()
+                except AttributeError:
+                    learner.disenchant()
+            pytest.fail(f"Learners remaining: {still_running}.  Offending tests: {offending_tests} ")
+
+        still_tracking  = [learner for learner in test_learners if hasattr(learner, 'work_tracker') and learner.work_tracker._tracking_task.running]
+        for tracker in still_tracking:
+            tracker.work_tracker.stop()
+
+
+@pytest.fixture(scope='session', autouse=True)
+def mock_get_external_ip_from_url_source(session_mocker):
+    target = 'nucypher.cli.actions.configure.determine_external_ip_address'
+    session_mocker.patch(target, return_value=MOCK_IP_ADDRESS)
+
+
+@pytest.fixture(scope='session', autouse=True)
+def disable_check_grant_requirements(session_mocker):
+    target = 'nucypher.characters.lawful.Alice._check_grant_requirements'
+    session_mocker.patch(target, return_value=MOCK_IP_ADDRESS)
+
+
+@pytest.fixture(scope="module", autouse=True)
+def mock_condition_blockchains(module_mocker):
+    """adds testerchain's chain ID to permitted conditional chains"""
+    module_mocker.patch.dict(
+        "nucypher.policy.conditions.evm._CONDITION_CHAINS",
+        {TESTERCHAIN_CHAIN_ID: "eth-tester/pyevm"},
+    )
+
+
+@pytest.fixture(scope="module", autouse=True)
+def mock_multichain_configuration(module_mocker, testerchain):
+    module_mocker.patch.object(
+        Operator, "_make_condition_provider", return_value=testerchain.provider
+    )
