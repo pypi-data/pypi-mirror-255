90ecb54116c79ab8dca9e4bfffff6fd50d5be985
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 09:11:13 +0000

    Add package switch-win/2024.2.8.4/switch_win-2024.2.8.4.tar.gz

---
+++ packages/switch-win/switch_win-2024.2.8.4.tar.gz/switch_win-2024.2.8.4/README.md
@@ -0,0 +1,12 @@
+# 功能
+Ctrl+Shift+数字 可以切换到预期的窗口  
+(省略了视觉判断的过程,可以加快切换速度)
+
+# 安装及使用
+sudo pip install switch-win && sudo sw  
+
+运行sw后,首次跳转时需要选择窗口,之后就可以快速切换了  
+重置选择重新运行sudo sw即可  
+
+# bug反馈
+建议或bug请反馈至yanchengxin@yeah.net
---
+++ packages/switch-win/switch_win-2024.2.8.4.tar.gz/switch_win-2024.2.8.4/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='switch_win',
+    version='2024.2.8.4',
+    entry_points={
+        'console_scripts': [
+            'switch_win = switch_win.main:main',
+            'sw = switch_win.back:main',
+        ],
+    },
+
+    packages=['switch_win'],
+    install_requires=[
+        'keyboard',
+        'python-xlib',
+    ],
+)
