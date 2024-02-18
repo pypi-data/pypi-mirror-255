d472d21f2694f99ee2587e423e4276f294ce07a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 04:05:27 +0000

    Add package zcbot-web-core/0.1/zcbot-web-core-0.1.tar.gz

---
+++ packages/zcbot-web-core/zcbot-web-core-0.1.tar.gz/zcbot-web-core-0.1/zcbot_web_core/__init__.py
@@ -0,0 +1,10 @@
+from __future__ import absolute_import
+from fastapi import FastAPI
+
+
+def init_core_modules(app: FastAPI) -> None:
+    """
+    Init core handlers, middlewares and services for fastapi application
+    :return: None
+    """
+    pass
