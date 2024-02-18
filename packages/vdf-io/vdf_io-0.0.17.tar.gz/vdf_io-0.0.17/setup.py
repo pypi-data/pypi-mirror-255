eaa1e9f6ab2f3b52e8bcffb9d84d754f74abc28d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 02:12:12 +0000

    Add package vdf-io/0.0.17/vdf_io-0.0.17.tar.gz

---
+++ packages/vdf-io/vdf_io-0.0.17.tar.gz/vdf_io-0.0.17/setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="vdf_io",
+    version="0.0.17",
+    description="This library uses a universal format for vector datasets to easily export and import data from all vector databases.",
+    long_description=open("README.rst").read(),
+    license="Apache 2.0",
+    author="Dhruv Anand",
+    author_email="dhruv.anand@ainorthstartech.com",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    entry_points={
+        "console_scripts": [
+            "export_vdf=vdf_io.export_vdf_cli:main",
+            "import_vdf=vdf_io.import_vdf_cli:main",
+            "reembed=vdf_io.scripts.reembed:main",
+            "consolidate_parquet=vdf_io.scripts.consolidate_parquet:main",
+            "get_id_list=vdf_io.scripts.get_id_list:main",
+            "push_to_hub_vdf=vdf_io.scripts.push_to_hub_vdf:main",
+        ],
+    },
+    install_requires=open("requirements.txt").read().splitlines(),
+)
