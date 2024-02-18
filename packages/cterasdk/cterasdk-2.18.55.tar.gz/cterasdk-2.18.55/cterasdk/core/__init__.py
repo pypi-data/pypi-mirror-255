4426d637826b6e80a4a8be1f8eb8268cf17730a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 17:04:01 +0000

    Add package cterasdk/2.18.55/cterasdk-2.18.55.tar.gz

---
+++ packages/cterasdk/cterasdk-2.18.55.tar.gz/cterasdk-2.18.55/cterasdk/core/__init__.py
@@ -0,0 +1,23 @@
+__all__ = [
+    'activation',
+    'admins',
+    'antivirus',
+    'buckets',
+    'cloudfs',
+    'connection',
+    'decorator',
+    'devices',
+    'directoryservice',
+    'enum',
+    'files',
+    'login',
+    'logs',
+    'messaging',
+    'portals',
+    'query',
+    'remote',
+    'reports',
+    'servers',
+    'session',
+    'users'
+]
