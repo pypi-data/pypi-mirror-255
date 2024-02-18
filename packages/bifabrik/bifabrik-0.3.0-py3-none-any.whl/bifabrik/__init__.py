20d98a9b979258da7ce08718b24e009512c2c127
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 23:07:04 +0000

    Add package bifabrik/0.3.0/bifabrik-0.3.0-py3-none-any.whl

---
+++ packages/bifabrik/bifabrik-0.3.0-py3-none-any.whl/bifabrik/__init__.py
@@ -0,0 +1,22 @@
+"""
+A collection of ETL tools for Microsoft Fabric
+Includes logging, configuration management and other tools
+
+For more info see https://rjankovic.github.io/bifabrik/
+
+Sample usage:
+    > from bifabrik import bifabrik
+    > bif = bifabrik(spark)
+    > bif.fromCsv.path('DATA/factOrderLine*.csv').delimiter(';').decimal(',').toTable('FactOrderLine').run()
+"""
+
+# read version from installed package
+from importlib.metadata import version
+__version__ = version("bifabrik")
+__loggername__ = "bifabrik_logger"
+
+from bifabrik.bifabrik import bifabrik
+from bifabrik.utils.fsUtils import getDefaultLakehouseAbfsPath
+
+if getDefaultLakehouseAbfsPath() is None:
+    print('bifabrik warning: the notebook is not attached to a lakehouse - some features of bifabrik will not work correctly.')
