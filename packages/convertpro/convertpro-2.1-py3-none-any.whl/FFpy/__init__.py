1ee76019ee7f65405a23eca8ee619d130ce6c4f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 13:05:21 +0000

    Add package convertpro/2.1/convertpro-2.1-py3-none-any.whl

---
+++ packages/convertpro/convertpro-2.1-py3-none-any.whl/FFpy/__init__.py
@@ -0,0 +1,8 @@
+# __init__.py
+import os
+
+def add_ffmpeg_to_path():
+    ffmpeg_path = os.path.join(os.path.dirname(__file__), '..', 'bin')
+    os.environ['PATH'] += os.pathsep + ffmpeg_path
+
+add_ffmpeg_to_path()
