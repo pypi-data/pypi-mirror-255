8e8c2bb9422e36bf28dd615a855bd2a012d7d56a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 14:05:59 +0000

    Add package caluma/10.1.1/caluma-10.1.1.tar.gz

---
+++ packages/caluma/caluma-10.1.1.tar.gz/caluma-10.1.1/caluma/caluma_analytics/filters.py
@@ -0,0 +1,61 @@
+from django_filters.rest_framework import FilterSet, MultipleChoiceFilter
+
+from ..caluma_core.filters import MetaFilterSet, SearchFilter
+from ..caluma_core.ordering import AttributeOrderingFactory, MetaFieldOrdering
+from . import models
+
+
+class AnalyticsTableFilterSet(MetaFilterSet):
+    search = SearchFilter(fields=("slug", "name"))
+    slugs = MultipleChoiceFilter(field_name="slug")
+
+    class Meta:
+        model = models.AnalyticsTable
+        fields = (
+            "slug",
+            "name",
+        )
+
+
+class AnalyticsFieldFilterSet(MetaFilterSet):
+    search = SearchFilter(
+        fields=(
+            "alias",
+            "data_source",
+            "function",
+            "table__name",
+            "source_field__alias",
+        )
+    )
+    slugs = MultipleChoiceFilter(field_name="slug")
+
+    class Meta:
+        model = models.AnalyticsField
+        fields = (
+            "alias",
+            "table",
+        )
+
+
+class AnalyticsFieldOrderSet(FilterSet):
+    meta = MetaFieldOrdering()
+    attribute = AttributeOrderingFactory(
+        models.AnalyticsField,
+        fields=["created_at", "modified_at", "alias"],
+    )
+
+    class Meta:
+        model = models.AnalyticsField
+        fields = ("meta", "attribute")
+
+
+class AnalyticsTableOrderSet(FilterSet):
+    meta = MetaFieldOrdering()
+    attribute = AttributeOrderingFactory(
+        models.AnalyticsTable,
+        fields=["created_at", "modified_at", "slug", "name"],
+    )
+
+    class Meta:
+        model = models.AnalyticsTable
+        fields = ("meta", "attribute")
