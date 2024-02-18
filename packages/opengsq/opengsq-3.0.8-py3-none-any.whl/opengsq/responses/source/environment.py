f5a9a58a64bea308f3dcd8f6717c98c699b51506
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 03:06:01 +0000

    Add package opengsq/3.0.8/opengsq-3.0.8-py3-none-any.whl

---
+++ packages/opengsq/opengsq-3.0.8-py3-none-any.whl/opengsq/responses/source/environment.py
@@ -0,0 +1,20 @@
+from enum import IntEnum
+
+
+class Environment(IntEnum):
+    """
+    Indicates the operating system of the server.
+    """
+
+    Linux = 0x6C
+    """Linux"""
+
+    Windows = 0x77
+    """Windows"""
+
+    Mac = 0x6D
+    """Mac"""
+
+    @staticmethod
+    def parse(byte: int):
+        return Environment(ord(chr(byte).lower()))
---
+++ packages/opengsq/opengsq-3.0.8-py3-none-any.whl/opengsq/responses/source/server_type.py
@@ -0,0 +1,20 @@
+from enum import IntEnum
+
+
+class ServerType(IntEnum):
+    """
+    Indicates the type of server.
+    """
+
+    Dedicated = 0x64
+    """Dedicated server"""
+
+    Listen = 0x6C
+    """Listen server"""
+
+    Proxy = 0x70
+    """SourceTV relay (proxy)"""
+
+    @staticmethod
+    def parse(byte: int):
+        return ServerType(ord(chr(byte).lower()))
