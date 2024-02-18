4fcb5234eda3de36c95e99ac5fa27983539593b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 15:06:30 +0000

    Add package cookiecutter-python/1.12.3/cookiecutter_python-1.12.3-py3-none-any.whl

---
+++ packages/cookiecutter-python/cookiecutter_python-1.12.3-py3-none-any.whl/cookiecutter_python/backend/post_main.py
@@ -0,0 +1,17 @@
+from requests.exceptions import ConnectionError as RequestsConnectionError
+
+
+def post_main(request):
+    """Check if any CI 'deployment' (ie in pypi), would require minor tweak."""
+    # ie if gen proj py pkg name is 'gg', and pypi.org/project/gg/ is already taken
+    for result in request.check_results:
+        try:
+            request.check.handle(result)
+        except RequestsConnectionError as error:
+            raise CheckWebServerError(
+                f"Connection error while checking {result.service_name} web server"
+            ) from error
+
+
+class CheckWebServerError(Exception):
+    pass
