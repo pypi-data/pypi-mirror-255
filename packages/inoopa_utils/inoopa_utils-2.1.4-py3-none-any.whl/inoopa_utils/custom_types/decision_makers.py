941a3d0a9bd1855bb5e04a374020360ce4903a91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 20:05:02 +0000

    Add package inoopa_utils/2.1.4/inoopa_utils-2.1.4-py3-none-any.whl

---
+++ packages/inoopa_utils/inoopa_utils-2.1.4-py3-none-any.whl/inoopa_utils/custom_types/decision_makers.py
@@ -0,0 +1,28 @@
+from dataclasses import dataclass
+from datetime import datetime
+from bson import ObjectId
+
+
+@dataclass
+class DecisionMaker:
+    # Foreign key to the `company` collection
+    company_id: str
+    firstname: str
+    lastname: str
+    source: str
+    # The raw string from the source, will cleaned and classified into `function_string`
+    raw_function_string: str
+    last_seen: datetime
+    function_string: str | None = None
+    _id: ObjectId | None = None
+    email: str | None = None
+    email_score: int | None = None
+    phone: str | None = None
+    phone_score: int | None = None
+    language: str | None = None
+    function_code: int | None = None
+    linkedin_url: str | None = None
+    cluster: str | None = None
+    cluster_score: float | None = None
+    cluster_best_match: str | None = None
+
