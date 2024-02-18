513d029a1b74bcafa83e5e1f6dfdf670c232c78b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 03:05:34 +0000

    Add package beaker-kernel/1.2.8/beaker_kernel-1.2.8.tar.gz

---
+++ packages/beaker-kernel/beaker_kernel-1.2.8.tar.gz/beaker_kernel-1.2.8/beaker_kernel/server/dev_ui/build/_index.d.ts
@@ -0,0 +1,7 @@
+import '@jupyterlab/application/style/index.css';
+import '@jupyterlab/codemirror/style/index.css';
+import '@jupyterlab/completer/style/index.css';
+import '@jupyterlab/documentsearch/style/index.css';
+import '@jupyterlab/notebook/style/index.css';
+import '@jupyterlab/theme-light-extension/style/theme.css';
+import '../index.css';
