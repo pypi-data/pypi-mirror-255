4b3b87b5dde9f44eba9c8de0bc907b61005fe214
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 07:05:16 +0000

    Add package readmeai/0.5.4/readmeai-0.5.4-py3-none-any.whl

---
+++ packages/readmeai/readmeai-0.5.4-py3-none-any.whl/readmeai/cli/commands.py
@@ -0,0 +1,62 @@
+"""CLI entrypoint for the readme-ai application."""
+
+from typing import Optional
+
+import click
+
+from readmeai.app import readme_agent
+from readmeai.cli import options
+
+
+@click.command()
+@options.align
+@options.api
+@options.badges
+@options.badge_color
+@options.emojis
+@options.image
+@options.language
+@options.max_tokens
+@options.model
+@options.output
+@options.repository
+@options.temperature
+@options.tree_depth
+@options.template
+def commands(
+    align: Optional[str],
+    api: str,
+    badges: Optional[str],
+    badge_color: Optional[str],
+    emojis: Optional[bool],
+    image: Optional[str],
+    language: Optional[str],
+    max_tokens: Optional[int],
+    model: Optional[str],
+    output: Optional[str],
+    repository: str,
+    temperature: Optional[float],
+    template: Optional[str],
+    tree_depth: Optional[int],
+) -> None:
+    """Entry point for the readme-ai CLI application."""
+    readme_agent(
+        align=align,
+        api=api,
+        badges=badges,
+        badge_color=badge_color,
+        emojis=emojis,
+        image=image,
+        # language=language,
+        max_tokens=max_tokens,
+        model=model,
+        output=output,
+        repository=repository,
+        temperature=temperature,
+        # template=template,
+        tree_depth=tree_depth,
+    )
+
+
+if __name__ == "__main__":
+    commands()
---
+++ packages/readmeai/readmeai-0.5.4-py3-none-any.whl/readmeai/parsers/maven.py
@@ -0,0 +1,29 @@
+"""Parser utilities for Java-based dependency files."""
+
+import re
+from typing import List
+
+from readmeai.core.parser import FileParser
+
+
+class MavenParser(FileParser):
+    """
+    Parser for Maven dependency files in pom.xml format.
+    """
+
+    def parse(self, content: str) -> List[str]:
+        """Extract packages names from Maven pom.xml files."""
+        try:
+            regex = re.compile(
+                r"<dependency>\s*<groupId>([^<]+)</groupId>\s*<artifactId>([^<]+)</artifactId>\s*<version>([^<]+)</version>"
+            )
+            matches = regex.findall(content)
+            dependencies = [
+                artifact_id for group_id, artifact_id, version in matches
+            ]
+            if any("spring" in dependency for dependency in dependencies):
+                dependencies.append("spring")
+            return set(dependencies)
+
+        except re.error as exc:
+            return self.handle_parsing_error(f"pom.xml: {str(exc)}")
