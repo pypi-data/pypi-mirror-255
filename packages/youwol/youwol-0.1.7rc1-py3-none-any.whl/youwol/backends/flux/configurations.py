0efbb26ee872c386565e423531642fad14e2e249
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 01:28:55 +0000

    Add package youwol/0.1.7rc1/youwol-0.1.7rc1-py3-none-any.whl

---
+++ packages/youwol/youwol-0.1.7rc1-py3-none-any.whl/youwol/backends/flux/configurations.py
@@ -0,0 +1,65 @@
+# standard library
+from collections.abc import Awaitable
+from dataclasses import dataclass
+
+# typing
+from typing import Callable, Optional, Union
+
+# Youwol utilities
+from youwol.utils import CdnClient, DocDb, Storage
+from youwol.utils.clients.assets_gateway.assets_gateway import AssetsGatewayClient
+
+
+@dataclass(frozen=True)
+class Constants:
+    namespace: str = "flux"
+
+    doc_db_primary_key: str = "project_id"
+    doc_db_table_name: str = "projects"
+
+    doc_db_component_primary_key: str = "component_id"
+    doc_db_component_table_name: str = "component"
+
+    default_owner: str = "/youwol-users"
+    current_schema_version: str = "1"
+
+    @staticmethod
+    def workflow_path(base_path):
+        return f"{base_path}/workflow.json"
+
+    @staticmethod
+    def builder_rendering_path(base_path):
+        return f"{base_path}/builderRendering.json"
+
+    @staticmethod
+    def runner_rendering_path(base_path):
+        return f"{base_path}/runnerRendering.json"
+
+    @staticmethod
+    def requirements_path(base_path):
+        return f"{base_path}/requirements.json"
+
+    @staticmethod
+    def description_path(base_path):
+        return f"{base_path}/description.json"
+
+
+@dataclass(frozen=True)
+class Configuration:
+    storage: Storage
+    doc_db: DocDb
+    doc_db_component: DocDb
+    assets_gtw_client: AssetsGatewayClient
+    cdn_client: CdnClient
+    admin_headers: Optional[dict[str, str]] = None
+
+
+class Dependencies:
+    get_configuration: Callable[[], Union[Configuration, Awaitable[Configuration]]]
+
+
+async def get_configuration():
+    conf = Dependencies.get_configuration()
+    if isinstance(conf, Configuration):
+        return conf
+    return await conf
---
+++ packages/youwol/youwol-0.1.7rc1-py3-none-any.whl/youwol/utils/utils_shell.py
@@ -0,0 +1,36 @@
+# standard library
+import asyncio
+
+# third parties
+from aiostream import stream
+
+# Youwol utilities
+from youwol.utils.context import Context
+
+
+class CommandException(Exception):
+    def __init__(self, command: str, outputs: list[str]):
+        self.command = command
+        self.outputs = outputs
+        super().__init__(f"{self.command} failed")
+
+
+async def execute_shell_cmd(cmd: str, context: Context, log_outputs=True):
+    async with context.start(
+        action="execute 'shell' command", with_labels=["BASH"]
+    ) as ctx:
+        await ctx.info(text=cmd)
+        p = await asyncio.create_subprocess_shell(
+            cmd=cmd,
+            stdout=asyncio.subprocess.PIPE,
+            stderr=asyncio.subprocess.PIPE,
+            shell=True,
+        )
+        outputs = []
+        async with stream.merge(p.stdout, p.stderr).stream() as messages_stream:
+            async for message in messages_stream:
+                outputs.append(message.decode("utf-8"))
+                if log_outputs:
+                    await ctx.info(text=outputs[-1])
+        await p.communicate()
+        return p.returncode, outputs
