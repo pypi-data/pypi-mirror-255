f8111b9a1da01bcff5d35aa6d08564ede7a469ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 16:09:06 +0000

    Add package wolfhece/2.0.14/wolfhece-2.0.14-py3-none-any.whl

---
+++ packages/wolfhece/wolfhece-2.0.14-py3-none-any.whl/wolfhece/shaders/quad_frag_shader.glsl
@@ -0,0 +1,30 @@
+#version 460 core
+
+uniform vec3 sunPosition;
+uniform float sunIntensity;
+
+in vec4 gl_FragCoord ;
+in vec3 Normal;
+in vec3 ourColor;
+
+out vec4 fragColor;
+
+void main() {
+    // Calculate the direction from the fragment position to the sun position
+    vec3 sunDirection = normalize(vec4(sunPosition, 1.) - gl_FragCoord).xyz;
+
+    // Calculate the dot product between the surface normal and the sun direction
+    float lightIntensity = dot(Normal, sunDirection) * sunIntensity;
+
+    // Calculate the reflection direction for specular lighting
+    vec3 viewDirection = normalize(-gl_FragCoord.xyz);
+    vec3 reflectionDirection = reflect(-sunDirection, Normal);
+
+    // Calculate the specular intensity using the reflection direction
+    float specularIntensity = pow(max(dot(reflectionDirection, viewDirection), 0.0), 32.0);
+
+    // Apply the diffuse and specular intensities to the fragment color
+    vec3 diffuseColor = lightIntensity * ourColor.rgb;
+    vec3 specularColor = specularIntensity * vec3(1.0, 1.0, 1.0);
+    fragColor = vec4(diffuseColor + specularColor, 1.0);
+}
