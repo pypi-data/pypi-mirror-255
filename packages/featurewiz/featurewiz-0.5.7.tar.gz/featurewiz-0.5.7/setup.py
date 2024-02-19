442be0df90b76afa7e5dc8f451700ac008b2c1fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 02:12:53 +0000

    Add package featurewiz/0.5.7/featurewiz-0.5.7.tar.gz

---
+++ packages/featurewiz/featurewiz-0.5.7.tar.gz/featurewiz-0.5.7/setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="featurewiz",
+    version="0.5.7",
+    author="Ram Seshadri",
+    author_email="rsesha2001@yahoo.com",
+    description="Select Best Features from your data set - any size - now with XGBoost!",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license='Apache License 2.0',
+    url="https://github.com/AutoViML/featurewiz",
+    packages=setuptools.find_packages(exclude=("tests",)),
+    install_requires=[
+        "ipython",
+        "jupyter",
+        "xgboost>=1.5,<=1.6.2",
+        "pandas>=1.3.4,<2.0",
+        "matplotlib",
+        "seaborn",
+        "scikit-learn>=0.24,<=1.2.2",
+        "networkx>=2.6.2",
+        "category_encoders>=2.6.2",
+        "xlrd>=2.0.0",
+        "tqdm>=4.61.1",
+        "dask>=2021.11.0",
+        "lightgbm>=3.2.1",
+        "distributed>=2021.11.0",
+        "feather-format>=0.4.1",
+        "pyarrow>=7.0.0",
+        "fsspec>=0.3.3",
+        "scipy<1.11.0",
+        "Pillow>=9.0.0",
+        "lazytransform>=1.16",
+        "numexpr>=2.7.3"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
