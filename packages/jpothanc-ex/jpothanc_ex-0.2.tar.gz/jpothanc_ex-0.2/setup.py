2b7e7c4994fa0f86326b0877b61032fab44065a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 00:07:12 +0000

    Add package jpothanc-ex/0.2/jpothanc_ex-0.2.tar.gz

---
+++ packages/jpothanc-ex/jpothanc_ex-0.2.tar.gz/jpothanc_ex-0.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='jpothanc_ex',
+    version='0.2',
+    packages=find_packages(),
+    install_requires=[
+        # Add any dependencies here
+    ],
+    entry_points={
+        'console_scripts': [
+            # Add any command-line scripts here
+        ],
+    },
+)
