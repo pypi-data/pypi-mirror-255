9d4a5e4056090567869118694846c21fe4030759
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 02:12:52 +0000

    Add package lazytransform/1.16/lazytransform-1.16.tar.gz

---
+++ packages/lazytransform/lazytransform-1.16.tar.gz/lazytransform-1.16/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="lazytransform",
+    version="1.16",
+    author="Ram Seshadri",
+    # author_email="author@example.com",
+    description="Clean your data using a scikit-learn transformer in a single line of code",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license='Apache License 2.0',
+    url="https://github.com/AutoViML/lazytransform",
+    py_modules = ["lazytransform"],
+    packages=setuptools.find_packages(exclude=("tests",)),
+    install_requires=[
+    "numpy>=1.20.3",
+    "pandas>=1.2.4, <2.0",
+    "scikit-learn>=0.24.2,<=1.2.2",
+    "python-dateutil>=2.8.1",
+    "lightgbm>=3.2.1",
+    "matplotlib>=3.4.2",
+    "scipy<1.11.0",
+    "category-encoders>=2.6.2",
+    "tqdm>=4.61.1",
+    ],
+    include_package_data = True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
