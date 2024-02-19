095e30a30654bc6add01260ab833c748207182ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 01:27:01 +0000

    Add package locpix/0.1.0.3/locpix-0.1.0.3.tar.gz

---
+++ packages/locpix/locpix-0.1.0.3.tar.gz/locpix-0.1.0.3/docs/build/html/_static/documentation_options.js
@@ -0,0 +1,13 @@
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '0.1.0.1.dev8+g1a8e0b3.d20240209',
+    LANGUAGE: 'en',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
+};
