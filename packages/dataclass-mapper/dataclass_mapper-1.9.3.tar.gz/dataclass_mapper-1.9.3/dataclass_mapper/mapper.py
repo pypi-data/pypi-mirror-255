74fddf3aab38c1b96eef97989671a5818f98acff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 02:12:39 +0000

    Add package dataclass-mapper/1.9.3/dataclass_mapper-1.9.3.tar.gz

---
+++ packages/dataclass-mapper/dataclass_mapper-1.9.3.tar.gz/dataclass_mapper-1.9.3/dataclass_mapper/mapper.py
@@ -0,0 +1,273 @@
+import warnings
+from copy import deepcopy
+from importlib import import_module
+from itertools import zip_longest
+from typing import Any, Callable, Dict, Optional, Tuple, Type, TypeVar, cast
+
+from .assignments import get_map_to_func_name
+from .classmeta import get_class_meta
+from .enum import EnumMapping, make_enum_mapper
+from .mapping_method import (
+    AssumeNotNone,
+    InitWithDefault,
+    MappingMethodSourceCode,
+    ProvideWithExtra,
+    Spezial,
+    StringFieldMapping,
+)
+from .namespace import Namespace, get_namespace
+
+
+def _make_mapper(
+    mapping: StringFieldMapping, source_cls: Any, target_cls: Any, namespace: Namespace
+) -> Tuple[str, Dict[str, Callable], Dict[str, Any]]:
+    source_cls_meta = get_class_meta(source_cls, namespace=namespace)
+    target_cls_meta = get_class_meta(target_cls, namespace=namespace)
+    actual_source_fields = source_cls_meta.fields
+    actual_target_fields = target_cls_meta.fields
+    source_code = MappingMethodSourceCode(source_cls=source_cls_meta, target_cls=target_cls_meta)
+
+    for target_field_name, target_field in actual_target_fields.items():
+        # mapping exists
+        if target_field_name in mapping:
+            raw_source = mapping[target_field_name]
+            if isinstance(raw_source, str):
+                source_field_name = raw_source
+                if source_field_name not in actual_source_fields:
+                    raise ValueError(
+                        f"'{source_field_name}' of mapping in '{source_cls.__name__}' doesn't exist "
+                        f"in '{source_cls.__name__}'"
+                    )
+                source_code.add_mapping(target=target_field, source=actual_source_fields[source_field_name])
+            elif isinstance(raw_source, AssumeNotNone):
+                source_field_name = raw_source.field_name or target_field.name
+                if source_field_name not in actual_source_fields:
+                    raise ValueError(
+                        f"'{source_field_name}' of mapping in '{source_cls.__name__}' doesn't exist "
+                        f"in '{source_cls.__name__}'"
+                    )
+                source_field = deepcopy(actual_source_fields[source_field_name])
+                # pretend like the source field isn't optional
+                source_field.allow_none = False
+                source_code.add_mapping(target=target_field, source=source_field)
+            elif isinstance(raw_source, ProvideWithExtra):
+                source_code.add_fill_with_extra(target=target_field)
+            elif isinstance(raw_source, (Spezial, InitWithDefault)):
+                if raw_source in (
+                    Spezial.USE_DEFAULT,
+                    Spezial.IGNORE_MISSING_MAPPING,
+                ) or isinstance(raw_source, InitWithDefault):
+                    if raw_source is Spezial.USE_DEFAULT:
+                        warnings.warn(
+                            "USE_DEFAULT is deprecated, use init_with_default() instead",
+                            DeprecationWarning,
+                        )
+                    if raw_source is Spezial.IGNORE_MISSING_MAPPING:
+                        warnings.warn(
+                            "IGNORE_MISSING_MAPPING is deprecated, use init_with_default() instead",
+                            DeprecationWarning,
+                        )
+
+                    if target_field.required:
+                        # leaving the target empty and using the default value/factory is not possible,
+                        # as the target doesn't have a default value/factory
+                        setting_name = raw_source.name if isinstance(raw_source, Spezial) else "init_with_default()"
+                        raise ValueError(
+                            f"'{target_field_name}' of '{target_cls.__name__}' cannot be set to {setting_name}, "
+                            "as it has no default"
+                        )
+                else:
+                    raise NotImplementedError
+            else:
+                source_code.add_mapping(target=target_field, source=raw_source)
+        # there's a variable with the same name in the source
+        elif target_field_name in actual_source_fields:
+            source_code.add_mapping(target=target_field, source=actual_source_fields[target_field_name])
+        # not possible to map
+        else:
+            raise ValueError(
+                f"'{target_field_name}' of '{target_cls.__name__}' has no mapping in '{source_cls.__name__}'"
+            )
+
+    for target_field_name in mapping.keys() - actual_target_fields.keys():
+        raise ValueError(
+            f"'{target_field_name}' of mapping in '{source_cls.__name__}' doesn't exist in '{target_cls.__name__}'"
+        )
+
+    return str(source_code), source_code.methods, {target_cls_meta.alias_name: target_cls}
+
+
+T = TypeVar("T")
+
+
+def mapper(TargetCls: Any, mapping: Optional[StringFieldMapping] = None) -> Callable[[T], T]:
+    """Class decorator that adds a private mapper method, that maps the current class to the ``TargetCls``.
+    The mapper method can be called using the ``map_to`` function.
+
+    :param TargetCls: The class (target class) that you want to map an object of the current (source) class to.
+    :param mapping: An optional dictionary which which it's possible to describe how each field in the target
+        class gets initialized.
+        Fields that are not specified will be mapped automatically.
+        Every single field in the target class must have some mapping (in order to not forget about a field),
+        either a default mapping (if the field names match), or an explicit mapping definition with this
+        `mapping` parameter.
+
+        - ``{"x": "y"}`` means, that the field ``x`` of the target object will be initialized with the value of the
+          ``y`` fields in the source object.
+        - ``{"x": lambda: 42}`` means, that the field ``x`` will be initialized with the value 42.
+        - ``{"x": lambda self: self.x + 1}`` means, that the field ``x`` will be initialized with the
+          incremented value ``x`` of the source object.
+        - ``{"x": USE_DEFAULT}`` (deprecated) means, nothing is mapped to the field ``x``, it will just be
+          initialized with the default value / factory of that field.
+        - ``{"x": IGNORE_MISSING_MAPPING}`` (deprecated) means, nothing is mapped to the field ``x``, it will just be
+          initialized with the default value / factory of that field.
+        - ``{"x": init_with_default()}`` means, nothing is mapped to the field ``x``, it will just be
+          initialized with the default value / factory of that field.
+        - ``{"x": assume_not_none("y")}`` means, that the target field ``x`` will be initialized with the value of the
+          ``y`` source field, and the library will assume that the source field is not ``None``.
+          If no source field name is given, it will additionally assume that the source field is also called ``x``.
+        - ``{"x": provide_with_extra()}`` means, that you don't fill this field with any field of the source class,
+          but with the extra dictionary given by the `map_to` method.
+    """
+
+    namespace = get_namespace()
+
+    def wrapped(SourceCls: T) -> T:
+        add_mapper_function(
+            SourceCls=SourceCls,
+            TargetCls=TargetCls,
+            mapping=mapping,
+            namespace=namespace,
+        )
+        return SourceCls
+
+    return wrapped
+
+
+def mapper_from(SourceCls: Any, mapping: Optional[StringFieldMapping] = None) -> Callable[[T], T]:
+    """Class decorator that adds a private mapper method, that maps an object of ``SourceCls`` to the current class.
+    The mapper method can be called using the ``map_to`` function.
+
+    :param SourceCls: the class (source class) that you want to map an object from to the current (target) class.
+    :param mapping: an optional dictionary which which it's possible to describe how each field in the target class
+        gets initialized.
+    """
+
+    namespace = get_namespace()
+
+    def wrapped(TargetCls: T) -> T:
+        add_mapper_function(SourceCls=SourceCls, TargetCls=TargetCls, mapping=mapping, namespace=namespace)
+        return TargetCls
+
+    return wrapped
+
+
+def add_mapper_function(
+    SourceCls: Any, TargetCls: Any, mapping: Optional[StringFieldMapping], namespace: Namespace
+) -> None:
+    field_mapping = mapping or cast(StringFieldMapping, {})
+
+    map_code, factories, context = _make_mapper(
+        field_mapping,
+        source_cls=SourceCls,
+        target_cls=TargetCls,
+        namespace=namespace,
+    )
+    module = import_module(SourceCls.__module__)
+
+    d: Dict = {}
+    setattr(SourceCls, "__zip_longest", zip_longest)
+    # Support older versions of python by calling {**a, **b} rather than a|b
+    exec(map_code, {**module.__dict__, **context}, d)
+    map_func_name = get_map_to_func_name(TargetCls)
+    if hasattr(SourceCls, map_func_name):
+        raise AttributeError(
+            f"There already exists a mapping between '{SourceCls.__name__}' and '{TargetCls.__name__}'"
+        )
+    setattr(SourceCls, map_func_name, d["convert"])
+    for name, factory in factories.items():
+        setattr(SourceCls, name, factory)
+
+
+def enum_mapper(TargetCls: Any, mapping: Optional[EnumMapping] = None) -> Callable[[T], T]:
+    """Class decorator that adds a private mapper method, that maps the current enum class to the
+    enum class ``TargetCls``. The mapper method can be called using the ``map_to`` function.
+
+    :param TargetCls: The enum class (source class) that you want to map members from to the current
+        (target) enum class to.
+    :param mapping: An optional dictionary which which it's possible to describe to which member of the
+        target class a member of the source class is mapped to.
+        Key/value of the mapping can either be the names of the enum members, or the members themselves.
+        Fields that are not specified will be mapped automatically.
+        Every single field in the source class must have some mapping,
+        either a default mapping (if the member names match), or an explicit mapping definition with
+        this `mapping` parameter.
+
+        - ``{"x": "y"}`` means, that the member ``x`` of the source enum will get mapped to the
+          member ``y`` in the target object.
+    """
+
+    def wrapped(SourceCls: T) -> T:
+        add_enum_mapper_function(
+            SourceCls=SourceCls,
+            TargetCls=TargetCls,
+            mapping=mapping,
+        )
+        return SourceCls
+
+    return wrapped
+
+
+def enum_mapper_from(SourceCls: Any, mapping: Optional[EnumMapping] = None) -> Callable[[T], T]:
+    """Class decorator that adds a private mapper method, that maps members of the enum class ``SourceCls`` to
+    members of the current enum class.
+    The mapper method can be called using the ``map_to`` function.
+
+    :param SourceCls: The enum class (source class) that you want to map an object of the current
+        (source) enum class to.
+    :param mapping: An optional dictionary which which it's possible to describe to which member of the
+        target class a member of the source class is mapped to.
+    """
+
+    def wrapped(TargetCls: T) -> T:
+        add_enum_mapper_function(
+            SourceCls=SourceCls,
+            TargetCls=TargetCls,
+            mapping=mapping,
+        )
+        return TargetCls
+
+    return wrapped
+
+
+def add_enum_mapper_function(SourceCls: Any, TargetCls: Any, mapping: Optional[EnumMapping]) -> None:
+    convert_function = make_enum_mapper(
+        source_cls=SourceCls,
+        target_cls=TargetCls,
+        mapping=mapping or cast(EnumMapping, {}),
+    )
+    map_func_name = get_map_to_func_name(TargetCls)
+    if hasattr(SourceCls, map_func_name):
+        raise AttributeError(
+            f"There already exists a mapping between '{SourceCls.__name__}' and '{TargetCls.__name__}'"
+        )
+    setattr(SourceCls, map_func_name, convert_function)
+
+
+def map_to(obj, TargetCls: Type[T], extra: Optional[Dict[str, Any]] = None) -> T:
+    """Maps the given object to an object of type ``TargetCls``, if such a safe mapping was defined for the
+    type of the given object.
+    Raises an ``NotImplementedError`` if no such mapping is defined.
+
+    :param obj: the source object that you want to map to an object of type ``TargetCls``
+    :param TargetCls: the (target) class that you want to map to.
+    :param extra: dictionary with the values for the `provide_with_extra()` fields
+    :return: the mapped object
+    """
+    if extra is None:
+        extra = {}
+    func_name = get_map_to_func_name(TargetCls)
+    if hasattr(obj, func_name):
+        return cast(T, getattr(obj, func_name)(extra))
+
+    raise NotImplementedError(f"Object of type '{type(obj).__name__}' cannot be mapped to '{TargetCls.__name__}'")
---
+++ packages/dataclass-mapper/dataclass_mapper-1.9.3.tar.gz/dataclass_mapper-1.9.3/dataclass_mapper/mapping_method.py
@@ -0,0 +1,196 @@
+from dataclasses import dataclass
+from enum import Enum, auto
+from typing import Callable, Dict, List, Optional, Type, Union
+
+from . import code_generator as cg
+from .assignments import (
+    Assignment,
+    CallableWithMax1Parameter,
+    DictRecursiveAssignment,
+    FunctionAssignment,
+    ListRecursiveAssignment,
+    RecursiveAssignment,
+    SimpleAssignment,
+    get_var_name,
+)
+from .implementations.base import ClassMeta, FieldMeta
+
+
+class Spezial(Enum):
+    USE_DEFAULT = auto()
+    IGNORE_MISSING_MAPPING = auto()
+
+
+@dataclass
+class InitWithDefault:
+    pass
+
+
+def init_with_default() -> InitWithDefault:
+    """Initialize the target field with the default value, or default factory."""
+    return InitWithDefault()
+
+
+@dataclass
+class AssumeNotNone:
+    field_name: Optional[str] = None
+
+
+def assume_not_none(field_name: Optional[str] = None) -> AssumeNotNone:
+    """Assume that the source field is not none, even if it is an optional field.
+    Allows a mapping from ``Optional[T]`` to ``T``.
+    If the field name is not specified, it is assumed that the source field has the same name as the target field.
+    """
+    return AssumeNotNone(field_name)
+
+
+@dataclass
+class ProvideWithExtra:
+    pass
+
+
+def provide_with_extra() -> ProvideWithExtra:
+    """Don't map this field using a source class field, fill it with a dictionary called `extra` duing `map_to`."""
+    return ProvideWithExtra()
+
+
+# the different types that can be used as origin (source) for mapping to a member
+# - str: the name of a different variable in the original class
+# - Callable: a function that produces the value (can use `self` as parameter)
+# - Other.USE_DEFAULT/IGNORE_MISSING_MAPPING/init_with_default(): Don't map to this variable
+#   (only allowed if there is a default value/factory for it)
+# - assume_not_none(): assume that the source field is not None
+# - provide_with_extra(): create no mapping between the classes, fill the field with a dictionary called `extra`
+Origin = Union[str, CallableWithMax1Parameter, Spezial, InitWithDefault, AssumeNotNone, ProvideWithExtra]
+StringFieldMapping = Dict[str, Origin]
+
+
+@dataclass
+class AssignmentOptions:
+    """
+    Options for creating an assignment code (target = right_side).
+    :param only_if_set: only set the target to the right_side if the source set
+        (for Optional fields in Pydantic classes)
+    :param only_if_not_None: don't assign the right side, if the value is None
+        (for Optional -> non-Optional mappings with defaults in target fields)
+    :param if_None: only assign the right side if it is not None (for Optional, recursive fields),
+        otherwise set it to None
+    """
+
+    only_if_not_None: bool = False
+    if_None: bool = False
+
+    @classmethod
+    def from_Metas(
+        cls, source_cls: ClassMeta, target_cls: ClassMeta, source: FieldMeta, target: FieldMeta
+    ) -> "AssignmentOptions":
+        # handle optional to non-optional mappings
+        only_if_not_None = False
+        if source.allow_none and target.disallow_none:
+            if not target.required:
+                only_if_not_None = True
+            else:
+                raise TypeError(f"{source} of '{source_cls.name}' cannot be converted to {target}")
+
+        if_None = source.allow_none
+
+        return cls(
+            only_if_not_None=only_if_not_None,
+            if_None=if_None,
+        )
+
+
+class MappingMethodSourceCode:
+    """Source code of the mapping method"""
+
+    AssignmentClasses: List[Type[Assignment]] = [
+        SimpleAssignment,
+        RecursiveAssignment,
+        ListRecursiveAssignment,
+        DictRecursiveAssignment,
+    ]
+
+    def __init__(self, source_cls: ClassMeta, target_cls: ClassMeta) -> None:
+        self.source_cls = source_cls
+        self.target_cls = target_cls
+        self.function = cg.Function(
+            "convert",
+            args="self, extra: dict",
+            return_type=self.target_cls.name,
+            body=cg.Block(cg.Assignment(name="d", rhs="{}")),
+        )
+        self.methods: Dict[str, Callable] = {}
+
+    @classmethod
+    def _get_asssigment(cls, target: FieldMeta, source: FieldMeta) -> Optional[Assignment]:
+        for AssignmentCls in cls.AssignmentClasses:
+            if (assignment := AssignmentCls(source=source, target=target)).applicable():
+                return assignment
+        return None
+
+    def _field_assignment(
+        self,
+        source: FieldMeta,
+        target: FieldMeta,
+        right_side: str,
+        options: AssignmentOptions,
+    ) -> cg.Statement:
+        """Generate code for setting the target field to the right side.
+        Only do it for a couple of conditions.
+
+        :param right_side: some expression (code) that will be assigned to the target if conditions allow it
+        """
+        if options.if_None and not options.only_if_not_None:
+            right_side = f"None if {get_var_name(source)} is None else {right_side}"
+        code: cg.Statement = self._get_assignment(target, right_side)
+
+        if options.only_if_not_None:
+            code = cg.IfElse(condition=f"{get_var_name(source)} is not None", if_block=code)
+
+        code = self.target_cls.post_process(code, source_cls=self.source_cls, source_field=source, target_field=target)
+        return code
+
+    def _get_assignment(self, target: FieldMeta, right_side: str) -> cg.Assignment:
+        variable_name = self.target_cls.get_assignment_name(target)
+        lookup = cg.DictLookup(dict_name="d", key=variable_name)
+        return cg.Assignment(name=lookup, rhs=right_side)
+
+    def add_mapping(self, target: FieldMeta, source: Union[FieldMeta, Callable]) -> None:
+        if callable(source):
+            function_assignment = FunctionAssignment(
+                function=source, target=target, methods=self.methods, target_cls_name=self.target_cls.name
+            )
+            right_side = function_assignment.right_side()
+            self.function.body.append(self._get_assignment(target, right_side))
+        else:
+            assert isinstance(source, FieldMeta)
+
+            options = AssignmentOptions.from_Metas(
+                source_cls=self.source_cls, target_cls=self.target_cls, source=source, target=target
+            )
+            if assignment := self._get_asssigment(source=source, target=target):
+                self.function.body.append(
+                    self._field_assignment(
+                        source=source,
+                        target=target,
+                        right_side=assignment.right_side(),
+                        options=options,
+                    )
+                )
+            else:  # impossible
+                raise TypeError(f"{source} of '{self.source_cls.name}' cannot be converted to {target}")
+
+    def add_fill_with_extra(self, target: FieldMeta) -> None:
+        variable_name = self.target_cls.get_assignment_name(target)
+        exception_msg = (
+            f"When mapping an object of '{self.source_cls.name}' to '{self.target_cls.name}' "
+            f"the field '{variable_name}' needs to be provided in the `extra` dictionary"
+        )
+        self.function.body.append(
+            cg.IfElse(condition=f'"{variable_name}" not in extra', if_block=cg.Raise(f'TypeError("{exception_msg}")'))
+        )
+        self.function.body.append(self._get_assignment(target=target, right_side=f'extra["{variable_name}"]'))
+
+    def __str__(self) -> str:
+        self.function.body.append(self.target_cls.return_statement())
+        return self.function.to_string(0)
