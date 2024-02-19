503bea8fde34ee554e0bfd793765eba8f5ac27e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 02:12:03 +0000

    Add package amankd777-mongo-connect/0.0.1/amankd777_mongo_connect-0.0.1.tar.gz

---
+++ packages/amankd777-mongo-connect/amankd777_mongo_connect-0.0.1.tar.gz/amankd777_mongo_connect-0.0.1/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+from typing import List
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()     
+   
+
+__version__ = "0.0.1"
+REPO_NAME = "mongodbconnect"
+PKG_NAME= "amankd777_mongo_connect"
+AUTHOR_USER_NAME = "Amankd777"
+AUTHOR_EMAIL = "20bds003@iiitdwd.ac.in"
+
+setup(
+    name=PKG_NAME,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A python package for connecting with database.",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    )
+
+
