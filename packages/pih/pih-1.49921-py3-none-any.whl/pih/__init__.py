3c02e3dc8c36b9ee8757f9b70cfc08bf7abc4373
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 00:07:42 +0000

    Add package pih/1.49921/pih-1.49921-py3-none-any.whl

---
+++ packages/pih/pih-1.49921-py3-none-any.whl/pih/__init__.py
@@ -0,0 +1,6 @@
+
+#from pih.rpc import *
+from pih.pih import *
+#from pih.consts import *
+#from pih.collections import *
+#from pih.tools import *
---
+++ packages/pih/pih-1.49921-py3-none-any.whl/pih/consts/service.py
@@ -0,0 +1,2 @@
+EVENT_LISTENER_NAME_PREFIX: str = "_@@EventListener@@_"
+SUPPORT_NAME_PREFIX: str = "_@@Support@@_"
