f22a07ff77cf342898e726509b5ab4bc30f210a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 01:29:58 +0000

    Add package fake-bge-module-latest/20240210/fake_bge_module_latest-20240210-py3-none-any.whl

---
+++ packages/fake-bge-module-latest/fake_bge_module_latest-20240210-py3-none-any.whl/bgui/__init__.py
@@ -0,0 +1,16 @@
+import sys
+import typing
+from . import widget
+from . import text_input
+from . import frame_button
+from . import system
+from . import list_box
+from . import theme
+from . import progress_bar
+from . import frame
+from . import image_button
+from . import label
+from . import image
+from . import text_block
+
+GenericType = typing.TypeVar("GenericType")
