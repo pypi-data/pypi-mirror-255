cc781fa09d48bed53ffbce48713e2158e9a0966b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 03:05:20 +0000

    Add package sibila/0.2.1/sibila-0.2.1.tar.gz

---
+++ packages/sibila/sibila-0.2.1.tar.gz/sibila-0.2.1/sibila/__init__.py
@@ -0,0 +1,58 @@
+"""Structured information from local or remote LLMs."""
+
+__version__ = "0.2.1"
+
+__all__ = [
+    "ModelDir", "FormatDir"
+    "Model", "TextModel", "MessagesModel", "Tokenizer",
+    "GenConf", "GenRes", "GenError", "GenOut",
+    "LlamaCppModel", "LlamaCppTokenizer",
+    "OpenAIModel", "OpenAITokenizer",
+    "Thread", "MsgKind",
+    "Context", "Trim",
+    "JSchemaConf",
+]
+
+__author__ = "Jorge Diogo"
+
+
+from .gen import (
+    GenConf,
+    GenRes,
+    GenError,
+    GenOut
+)
+
+from .thread import (
+    Thread,
+    MsgKind,
+)
+
+from .model import (
+    Model,
+    TextModel,
+    FormattedTextModel,
+    MessagesModel,
+    Tokenizer
+)
+
+from .llamacpp import (
+    LlamaCppModel,
+    LlamaCppTokenizer
+)
+
+from .openai import (
+    OpenAIModel,
+    OpenAITokenizer
+)
+
+from .context import (
+    Context,
+    Trim
+)
+
+from .modeldir import ModelDir
+
+from .formatdir import FormatDir
+
+from .json_utils import JSchemaConf
