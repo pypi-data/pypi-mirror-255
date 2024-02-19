86a7538ac8d191a8c1330e7ecff41f366babd236
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 01:29:17 +0000

    Add package gitalong/0.1.0.dev6/gitalong-0.1.0.dev6.tar.gz

---
+++ packages/gitalong/gitalong-0.1.0.dev6.tar.gz/gitalong-0.1.0.dev6/gitalong/__init__.py
@@ -0,0 +1,4 @@
+from .__info__ import __version__, __copyright__, __email__, __author__  # noqa: F401
+from .enums import CommitSpread  # noqa: F401
+from .exceptions import RepositoryNotSetup  # noqa: F401
+from .repository import Repository  # noqa: F401
