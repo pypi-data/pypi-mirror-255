cbacd8f1b9ed9c7f6ba56b5fffd4a3f298c60a69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 02:12:28 +0000

    Add package bloomdata-bernard/0.1.0/bloomdata-bernard-0.1.0.tar.gz

---
+++ packages/bloomdata-bernard/bloomdata-bernard-0.1.0.tar.gz/bloomdata-bernard-0.1.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='bloomdata-bernard',
+    version='0.1.0',
+    description='Your package description',
+    author='Your Name',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'pandas',
+    ],
+)
