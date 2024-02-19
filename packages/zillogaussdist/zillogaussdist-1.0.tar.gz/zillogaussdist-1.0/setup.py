6760c493fb94c65e57784809fde8616466c6eb0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 02:12:59 +0000

    Add package zillogaussdist/1.0/zillogaussdist-1.0.tar.gz

---
+++ packages/zillogaussdist/zillogaussdist-1.0.tar.gz/zillogaussdist-1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='zillogaussdist',
+      version='1.0',
+      description='Gaussian distributions',
+      packages=['zillogaussdist'],
+      author = 'Chinedu Osuh',
+      author_email="osuhchinedu91@gmail.com",
+      url="https://github.com/zillobit/",
+      zip_safe=False)
