41589d606fb5689af973d9ddac23eb4861f3496d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 00:06:28 +0000

    Add package gaussian-and-binomial-mykel/0.1/gaussian_and_binomial_mykel-0.1.tar.gz

---
+++ packages/gaussian-and-binomial-mykel/gaussian_and_binomial_mykel-0.1.tar.gz/gaussian_and_binomial_mykel-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='gaussian_and_binomial_mykel',
+      version='0.1',
+      description='Package for calculating Gaussian and Binomial distributions',
+      packages=['gaussian_and_binomial_mykel'],
+      author='Michael Eze',
+      zip_safe=False)
