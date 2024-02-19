c357bab217c4201b65d892890ea10f8b9e9bfcd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 02:12:26 +0000

    Add package enigmachine/0.1/enigmachine-0.1.tar.gz

---
+++ packages/enigmachine/enigmachine-0.1.tar.gz/enigmachine-0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="enigmachine",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        "tabulate",
+    ],
+    description="Engima chiper machine",
+    author="Fernando Cortés",
+    author_email="fcsancho14@gmail.com",
+    license="MIT",
+    home_page="https://github.com/Fer14/enigmachine",
+)
