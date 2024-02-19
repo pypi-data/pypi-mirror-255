74d817a91a03ee14584e653f6a9eda03dca4a07b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 02:13:43 +0000

    Add package TorchSUL/0.3.0/TorchSUL-0.3.0.tar.gz

---
+++ packages/TorchSUL/TorchSUL-0.3.0.tar.gz/TorchSUL-0.3.0/TorchSUL/__init__.py
@@ -0,0 +1,14 @@
+from .Utils import Config
+from . import Base, Model, Quant, Tools
+
+sul_tool = Tools
+
+# modify loguru format. Add \n in front, in case other modules overrides stderr
+import sys 
+from loguru import logger 
+
+logger.remove()
+fmt = '\n<g>{time:YYYY-MM-DD HH:mm:ss}</g> | PID: <c>{process}</c> | <lvl>{level}</lvl> - <lvl>{message}</lvl> '
+logger.add(sys.stderr, format=fmt)
+
+
