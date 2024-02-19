2a03e2cedc4834a0d5b9148c1a2d4e5b8b152f77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 01:29:46 +0000

    Add package cloudlanguagetools/7.0/cloudlanguagetools-7.0.tar.gz

---
+++ packages/cloudlanguagetools/cloudlanguagetools-7.0.tar.gz/cloudlanguagetools-7.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+from setuptools.command.install import install
+
+# build instructions
+# python setup.py sdist
+# twine upload dist/*
+
+setup(name='cloudlanguagetools',
+      version='7.0',
+      description='Interface with various cloud APIs for language processing such as translation, text to speech',
+      long_description=open('README.rst', encoding='utf-8').read(),
+      url='https://github.com/Language-Tools/cloud-language-tools-core',
+      author='Luc',
+      author_email='languagetools@mailc.net',
+      classifiers=[
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Text Processing :: Linguistic',
+      ],      
+      license='GPL',
+      packages=['cloudlanguagetools'],
+      install_requires=[
+          'clt_wenlin>=1.0',
+          'clt_requirements>=1.1',
+          'pinyin_jyutping>=0.9',
+      ],
+      )
