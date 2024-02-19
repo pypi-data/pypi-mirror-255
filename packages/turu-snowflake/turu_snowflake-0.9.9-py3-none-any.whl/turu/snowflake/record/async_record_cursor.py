25074892c03776f1ce0235a18cb72e2b997c61eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 02:12:26 +0000

    Add package turu-snowflake/0.9.9/turu_snowflake-0.9.9-py3-none-any.whl

---
+++ packages/turu-snowflake/turu_snowflake-0.9.9-py3-none-any.whl/turu/snowflake/record/async_record_cursor.py
@@ -0,0 +1,22 @@
+import turu.core.record
+from turu.snowflake.features import PandasDataFrame
+
+
+class AsyncRecordCursor(turu.core.record.AsyncRecordCursor):
+    async def fetch_pandas_all(self, **kwargs) -> "PandasDataFrame":
+        df: PandasDataFrame = self._raw_cursor.fetch_pandas_all(**kwargs)  # type: ignore
+
+        if isinstance(self._recorder, turu.core.record.CsvRecorder):
+            if limit := self._recorder._options.get("limit"):
+                record_df = df.head(limit)
+
+            else:
+                record_df = df
+
+            record_df.to_csv(
+                self._recorder.file,
+                index=False,
+                header=self._recorder._options.get("header", True),
+            )
+
+        return df
