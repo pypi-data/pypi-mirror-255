72132881faacfc2aa4a5dd788b871d7c5f5fcfee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 02:12:13 +0000

    Add package eo-man/0.1.5/eo_man-0.1.5-py3-none-any.whl

---
+++ packages/eo-man/eo_man-0.1.5-py3-none-any.whl/eo_man/view/about_window.py
@@ -0,0 +1,60 @@
+import tkinter as tk
+from tkinter import *
+from tkinter import ttk
+import webbrowser
+from tkinterhtml import HtmlFrame
+
+from ..data.app_info import ApplicationInfo as AppInfo
+
+
+class AboutWindow():
+
+    def __init__(self, main:Tk):
+        popup = Toplevel(main)
+        popup.wm_title("About")
+
+        f = Frame(popup)
+
+        l = tk.Label(popup, text="About Device Manager", font='Arial 14 bold')
+        l.pack(side=TOP, fill="x", pady=10)
+
+        l = tk.Label(popup, text="Version: "+AppInfo.get_version(), anchor="w")
+        l.pack(side=TOP, fill="x", pady=2, padx=5)
+
+        text = "GitHub: grimmpp/enocean-device-manager"
+        l = tk.Label(popup, text=text, fg="blue", cursor="hand2", anchor="w")
+        l.pack(side=TOP, fill="x", pady=2, padx=5)
+        l.bind("<Button-1>", lambda e: self.callback(AppInfo.get_home_page()))
+
+        text = "Report a bug or ask for features!"
+        l = tk.Label(popup, text=text, fg="blue", cursor="hand2", anchor="w")
+        l.pack(side=TOP, fill="x", pady=2, padx=5)
+        l.bind("<Button-1>", lambda e: self.callback(AppInfo.get_home_page()+"/issues"))
+
+        l = tk.Label(popup, text="Author: "+AppInfo.get_author(), anchor="w")
+        l.pack(side=TOP, fill="x", pady=2, padx=5)
+
+        l = tk.Label(popup, text="License: "+AppInfo.get_license(), anchor="w")
+        l.pack(side=TOP, fill="x", pady=2, padx=5)
+
+        b = ttk.Button(popup, text="OK", command=popup.destroy)
+        b.bind('<Return>', lambda e: popup.destroy())
+        b.pack(side=TOP, fill="x", pady=(10,2), padx=10 )
+        b.focus()
+
+        popup.wm_attributes('-toolwindow', 'True')
+        popup.resizable (width=False, height=False)
+        popup.transient(main)
+        popup.grab_set()
+
+        # center
+        w = 300
+        h = 220
+        x = main.winfo_x() + main.winfo_width()/2 - w/2
+        y = main.winfo_y() + main.winfo_height()/2 - h/2
+        popup.geometry('%dx%d+%d+%d' % (w, h, x, y))
+
+        main.wait_window(popup)
+
+    def callback(self, url):
+        webbrowser.open_new(url)
---
+++ packages/eo-man/eo_man-0.1.5-py3-none-any.whl/eo_man/view/tool_bar.py
@@ -0,0 +1,38 @@
+import tkinter as tk
+import os
+from pathlib import Path
+from tkinter import *
+from tkinter import ttk
+from tkinter import filedialog
+from tkinter.tix import IMAGETEXT
+from PIL import Image, ImageTk
+from idlelib.tooltip import Hovertip
+
+from ..data.data_manager import DataManager
+from ..view.menu_presenter import MenuPresenter
+
+
+class ToolBar():
+    # icon list
+    # https://commons.wikimedia.org/wiki/Comparison_of_icon_sets
+    def __init__(self, main: Tk, menu_presenter:MenuPresenter, row:int):
+        self.main = main
+
+        f = Frame(main, bd=1)#, relief=SUNKEN)
+        f.grid(row=row, column=0, columnspan=1, sticky=W+E+N+S, pady=2, padx=2)
+
+        b = self._create_img_button(f, "Save to current file", "../icons/Oxygen480-actions-document-save.png", menu_presenter.save_file )
+        b = self._create_img_button(f, "Save as file", "../icons/Oxygen480-actions-document-save-as.png", lambda: menu_presenter.save_file(save_as=True) )
+        b = self._create_img_button(f, "Open file", "../icons/Oxygen480-status-folder-open.png", menu_presenter.load_file)
+        b = self._create_img_button(f, "Export Home Assistant Configuration", "../icons/Home_Assistant_Logo.png", menu_presenter.export_ha_config)
+
+    def _create_img_button(self, f:Frame, tooltip:str, img_filename:str, command) -> Button:
+
+        img = Image.open(os.path.join(os.path.dirname(__file__), img_filename))
+        img = img.resize((24, 24), Image.LANCZOS)
+        eimg = ImageTk.PhotoImage(img)
+        b = Button(f, image=eimg, relief=FLAT, command=command)
+        Hovertip(b,tooltip,300)
+        b.image = eimg
+        b.pack(side=LEFT, padx=2, pady=2)
+        return b
