03d55b446ea30098ce8cc74ff204c8d5c640d11e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 00:07:51 +0000

    Add package omnijp/1.1/omnijp-1.1.tar.gz

---
+++ packages/omnijp/omnijp-1.1.tar.gz/omnijp-1.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='omnijp',
+    version='1.1',
+    packages=find_packages(),
+    install_requires=[
+        # Add any dependencies here
+    ],
+    entry_points={
+        'console_scripts': [
+            # Add any command-line scripts here
+        ],
+    },
+)
