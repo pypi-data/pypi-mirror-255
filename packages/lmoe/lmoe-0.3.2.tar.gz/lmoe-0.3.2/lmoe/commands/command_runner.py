f5bdb47c31b6f70c7b51b50075c45e6bb798fbab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 03:05:23 +0000

    Add package lmoe/0.3.2/lmoe-0.3.2.tar.gz

---
+++ packages/lmoe/lmoe-0.3.2.tar.gz/lmoe-0.3.2/lmoe/commands/command_runner.py
@@ -0,0 +1,35 @@
+from dataclasses import dataclass
+from injector import inject
+from lmoe.api.lmoe_query import LmoeQuery
+from lmoe.commands.print_classification import PrintClassification
+from lmoe.commands.print_classifier_modelfile import PrintClassifierModelfile
+from lmoe.commands.query import Query
+from lmoe.commands.refresh import Refresh
+
+import argparse
+
+
+@inject
+@dataclass
+class CommandRunner:
+
+    parsed_args: argparse.Namespace
+    lmoe_query: LmoeQuery
+    print_classification: PrintClassification
+    print_classifier_modelfile: PrintClassifierModelfile
+    query: Query
+    refresh: Refresh
+
+    def execute(self):
+        command = None
+
+        if self.parsed_args.classify:
+            command = self.print_classification
+        elif self.parsed_args.classifier_modelfile:
+            command = self.print_classifier_modelfile
+        elif self.parsed_args.refresh:
+            command = self.refresh
+        else:
+            command = self.query
+
+        command.execute(self.parsed_args, self.lmoe_query)
---
+++ packages/lmoe/lmoe-0.3.2.tar.gz/lmoe-0.3.2/lmoe/utils/templates.py
@@ -0,0 +1,71 @@
+import yaml
+
+from dataclasses import dataclass
+from lmoe.definitions import TEMPLATES
+from string import Template
+from typing import List, Optional
+
+
+_EXAMPLE_TEMPLATE = Template(
+    """
+Example $example_index)
+
+===stdin-context===
+$stdin_context
+===stdin-context===
+===paste-context===
+$paste_context
+===paste-context===
+===user-query===
+$user_query
+===user-query===
+$agent_response
+"""
+)
+
+
+def read_template(filename):
+    if not filename in TEMPLATES:
+        raise Exception("Could not find: " + filename)
+    template = TEMPLATES[filename]
+    return template.read_text()
+
+
+def read_yaml_file(filename):
+    if not filename in TEMPLATES:
+        print(TEMPLATES)
+        raise Exception("Could not find: " + filename)
+    yaml_file = TEMPLATES[filename]
+    return yaml.safe_load(yaml_file.read_text())
+
+
+@dataclass
+class Example:
+    """Example user context, query, and response."""
+
+    stdin_context: Optional[str]
+    paste_context: Optional[str]
+    user_query: Optional[str]
+    agent_response: Optional[str]
+
+
+def read_yaml_examples(filename):
+    """Reads a yaml file as a list of lmoe.utils.templates.Example instances."""
+    return [Example(**example_dict) for example_dict in read_yaml_file(filename)]
+
+
+def render_examples(examples) -> str:
+    rendered_examples = []
+    example_index = 0
+    for example in examples:
+        example_index += 1
+        rendered_examples.append(
+            _EXAMPLE_TEMPLATE.substitute(
+                example_index=example_index,
+                stdin_context=example.stdin_context,
+                paste_context=example.paste_context,
+                user_query=example.user_query,
+                agent_response=example.agent_response,
+            )
+        )
+    return "\n\n".join(rendered_examples)
