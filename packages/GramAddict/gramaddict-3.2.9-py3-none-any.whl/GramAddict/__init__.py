e36ae9191ba8fdc9c83a869cbfee5fd8dbe2d14e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 03:05:21 +0000

    Add package GramAddict/3.2.9/gramaddict-3.2.9-py3-none-any.whl

---
+++ packages/GramAddict/gramaddict-3.2.9-py3-none-any.whl/GramAddict/__init__.py
@@ -0,0 +1,10 @@
+"""Human-like Instagram bot powered by UIAutomator2"""
+
+__version__ = "3.2.9"
+__tested_ig_version__ = "263.2.0.19.104"
+
+from GramAddict.core.bot_flow import start_bot
+
+
+def run(**kwargs):
+    start_bot(**kwargs)
