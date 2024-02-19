e0ce90e8cf1b26477ebe722f0769696b772e8a6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 02:12:01 +0000

    Add package mdformat-ruff/0.1.2/mdformat_ruff-0.1.2-py3-none-any.whl

---
+++ packages/mdformat-ruff/mdformat_ruff-0.1.2-py3-none-any.whl/mdformat_ruff/__init__.py
@@ -0,0 +1,26 @@
+import subprocess
+
+from ruff.__main__ import find_ruff_bin
+
+__version__ = (
+    "0.1.2"  # DO NOT EDIT THIS LINE MANUALLY. LET bump2version UTILITY DO IT
+)
+
+
+def format_python(unformatted: str, _info_str: str) -> str:
+    r"""Format python.
+
+    :param unformatted:
+    :type unformatted: str
+    :param _info_str:
+    :type _info_str: str
+    :rtype: str
+    """
+    ps = subprocess.run(
+        [find_ruff_bin(), "format", "-"],
+        input=unformatted.encode(),
+        capture_output=True,
+    )
+    if ps.returncode == 0:
+        return ps.stdout.decode()
+    return unformatted
