550dada44138f2c4f1d641203e8511dbe337bdd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 01:28:20 +0000

    Add package gaia-cmd-plotter/0.1.3/gaia_cmd_plotter-0.1.3.tar.gz

---
+++ packages/gaia-cmd-plotter/gaia_cmd_plotter-0.1.3.tar.gz/gaia_cmd_plotter-0.1.3/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+PACKAGE_NAME = 'gaia_cmd_plotter'
+PACKAGE_VERSION = '0.1.3'
+AUTHOR_NAME = 'Yue Zhao'
+AUTHOR_EMAIL = 'Yue.Zhao@soton.ac.uk'
+
+setup(
+    name=PACKAGE_NAME,
+    version=PACKAGE_VERSION,
+    python_requires='>=3.8',
+    packages=find_packages(),
+    install_requires=[
+        'matplotlib>=3.3.0',
+    ],
+    package_data={
+        'gaia_cmd_plotter': ['gaia_cmd.mplstyle', 'data/gaia_cmd_background.png'],
+    },
+    author=AUTHOR_NAME,
+    author_email=AUTHOR_EMAIL,
+    description='A Python package that provides a matplotlib.pyplot.Axes object that displays a Gaia CMD background.',
+    url='https://github.com/coryzh/gaia_cmd_plotter',
+)
