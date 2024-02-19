fff2e732d3f7d19933ac00286ce0221ba7cc3365
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 03:05:10 +0000

    Add package sinlib/0.0.8.1/sinlib-0.0.8.1-py3-none-any.whl

---
+++ packages/sinlib/sinlib-0.0.8.1-py3-none-any.whl/sinlib/__init__.py
@@ -0,0 +1,9 @@
+from sinlib.tokenizer import Tokenizer
+from sinlib.utils.preprocessing import remove_english_characters, remove_non_printable, get_sinhala_token_percentage
+
+__all__ = [
+    "Tokenizer",
+    "remove_english_characters",
+    "remove_non_printable",
+    "get_sinhala_token_percentage"
+]
