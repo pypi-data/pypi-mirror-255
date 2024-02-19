fda92648c0b6979466f3decb879b908cff4fe85a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 00:08:22 +0000

    Add package ChirePyToolkit/1.0.1/ChirePyToolkit-1.0.1.tar.gz

---
+++ packages/ChirePyToolkit/ChirePyToolkit-1.0.1.tar.gz/ChirePyToolkit-1.0.1/README.md
@@ -0,0 +1,19 @@
+# ChirePyToolkit
+炽热的Python工具库，方便的小功能会加上去。
+
+### 版本
+***
+- 1.0.1 加入了AI机器人，修复少量Bug
+```python
+# 例子
+from ChirePyToolkit import BI
+
+bi = BI()
+# 添加了上下文记忆，具体可以看源代码文档
+print(bi.sendMessage("扮演蛋糕店的店长太太"))
+print(bi.sendMessage("你好"))
+```
+***
+- 1.0.0 测试版本，已删除请不要调用下载。
+***
+只有这些版本啦~
---
+++ packages/ChirePyToolkit/ChirePyToolkit-1.0.1.tar.gz/ChirePyToolkit-1.0.1/setup.py
@@ -0,0 +1,23 @@
+import setuptools  # 导入setuptools打包工具
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ChirePyToolkit",  # 用自己的名替换其中的YOUR_USERNAME_
+    version="1.0.1",  # 包版本号，便于维护版本
+    author="炽热S",  # 作者，可以写自己的姓名
+    author_email="3531962676@qq.com",  # 作者联系方式，可写自己的邮箱地址
+    description="一个方便调用的工具库，更多方便自己用的",  # 包的简述
+    long_description=long_description,  # 包的详细介绍，一般在README.md文件内
+    long_description_content_type="text/markdown",
+    url="https://github.com/Chi-Re",  # 自己项目地址，比如github的项目地址
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',  # 对python的最低版本要求
+
+    install_requires=["requests"]
+)
