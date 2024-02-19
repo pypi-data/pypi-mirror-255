5d4464a7fc18412defffba840a2f4eb1eea13ce1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 02:12:14 +0000

    Add package afreeca/0.1.2/afreeca-0.1.2-py3-none-any.whl

---
+++ packages/afreeca/afreeca-0.1.2-py3-none-any.whl/afreeca/__init__.py
@@ -0,0 +1,16 @@
+from .core import AfreecaTV
+from .credential import GuestCredential, UserCredential
+from .exceptions import LoginError, NotStreamingError
+from .interfaces import BJInfo, Chat
+
+__all__ = [
+    "AfreecaTV",
+    "GuestCredential",
+    "UserCredential",
+    "BJInfo",
+    "Chat",
+    "NotStreamingError",
+    "LoginError",
+]
+
+__version__ = "0.1.2"
