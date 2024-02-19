aaeb2bb9d799c607df340f20bda3163fa59d3d7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 02:12:42 +0000

    Add package cortecs/0.3.1/cortecs-0.3.1.tar.gz

---
+++ packages/cortecs/cortecs-0.3.1.tar.gz/cortecs-0.3.1/README.md
@@ -0,0 +1,27 @@
+# cortecs
+[![status](https://joss.theoj.org/papers/b0c4f5012351abb476081ffe9226fbf4/status.svg)](https://joss.theoj.org/papers/b0c4f5012351abb476081ffe9226fbf4)
+[![PyPI version](https://badge.fury.io/py/cortecs.svg)](https://badge.fury.io/py/cortecs)
+[![Tests](https://github.com/arjunsavel/cortecs/actions/workflows/python-package.yml/badge.svg)](https://github.com/arjunsavel/cortecs/actions/workflows/python-package.yml)
+[![codecov](https://codecov.io/gh/arjunsavel/cortecs/graph/badge.svg?token=90S3STBO5X)](https://codecov.io/gh/arjunsavel/cortecs)
+[![Maintainability](https://api.codeclimate.com/v1/badges/4eb53795313af153f4cd/maintainability)](https://codeclimate.com/github/arjunsavel/cortecs/maintainability)
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+[![Code style: black](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/psf/black)
+[![pre-commit](https://img.shields.io/badge/pre--commit-enabled-brightgreen?logo=pre-commit)](https://github.com/pre-commit/pre-commit)
+[![CodeQL](https://github.com/arjunsavel/cortecs/actions/workflows/codeql.yml/badge.svg)](https://github.com/arjunsavel/cortecs/actions/workflows/codeql.yml)
+[![Documentation Status](https://readthedocs.org/projects/cortecs/badge/?version=latest)](https://cortecs.readthedocs.io/en/latest/?badge=latest)
+[![Paper compilation](https://github.com/arjunsavel/cortecs/actions/workflows/draft-pdf.yml/badge.svg)](https://github.com/arjunsavel/cortecs/actions/workflows/draft-pdf.yml)
+![GitHub repo size](https://img.shields.io/github/repo-size/arjunsavel/cortecs)
+![PyPI - Python Version](https://img.shields.io/pypi/pyversions/cortecs)
+
+
+
+
+A Python package for decreasing the memory footprint of opacity functions. The primary functionality is compressing opacity functions with varying flexibility. Current methods include
+- polynomial fitting
+- PCA-based fitting
+- neural network fitting
+
+
+All fits are currently made in along the temperature and pressure axes. 
+
+Additionally, `cortecs` can chunk up opacity functions. The radiative transfer problem can often be cast as embarassingly parallel, so each chunk can be sent to a different CPU.
---
+++ packages/cortecs/cortecs-0.3.1.tar.gz/cortecs-0.3.1/src/cortecs/opac/opac.py
@@ -0,0 +1,162 @@
+"""
+this file holds the class describing opacity data. hmm...maybe I want the loader in a different file?
+
+author: @arjunsavel
+"""
+import numpy as np
+
+from cortecs.opac.io import *
+
+
+class Opac(object):
+    """
+    this class holds the opacity data and provides methods for evaluating the opacity at a given temperature and pressure.
+
+    Everything's already been loaded into memory, so this is just a wrapper for the data.
+
+    todo: use the different loaders
+    """
+
+    method_dict = {
+        "chimera": loader_chimera,
+        "helios": loader_helios,
+        "platon": loader_platon,
+        "exotransmit": loader_exotransmit,
+    }
+
+    wl = None
+    T = None
+    P = None
+
+    def __init__(
+        self,
+        filename,
+        loader="chimera",
+        load_kwargs={},
+    ):
+        """
+        wraps around the loaders.
+
+        Parameters
+        ----------
+        filename : str
+            name of file to load
+        loader : str
+            name of loader to use. default is chimera.
+
+        Returns
+        -------
+        nothing
+        """
+        self.filename = filename
+        self.load_obj = self._get_loader(loader)
+        self.wl, self.T, self.P, self.cross_section = self.load_obj.load(
+            filename, **load_kwargs
+        )
+
+        self.n_wav, self.n_t, self.n_p = len(self.wl), len(self.T), len(self.P)
+
+        return
+
+    def _get_loader(self, loader_name):
+        """
+        gets the loader object
+
+        Parameters
+        ----------
+        loader_name : str
+            name of loader
+
+        Returns
+        -------
+        loader object
+        """
+        if loader_name not in self.method_dict:
+            raise ValueError(
+                "loader name not found. valid loaders are: {}".format(
+                    self.method_dict.keys()
+                )
+            )
+        return self.method_dict[loader_name]()
+
+    # todo: implement the copy and deepcopy methods.
+    def copy(self):
+        # need a new opac object with all the same attributes. so let's make sure we don't load on input.
+        raise NotImplementedError
+
+
+class Opac_cia(Opac):
+    """
+    this class holds the opacity data and provides methods for evaluating the opacity at a given temperature and pressure.
+
+    Everything's already been loaded into memory, so this is just a wrapper for the data.
+
+    """
+
+    method_dict = {
+        "platon_cia": loader_platon_cia,
+        "exotransmit_cia": loader_exotransmit_cia,
+    }
+
+    def __init__(self, filename, loader="platon_cia", view="default"):
+        """
+        wraps around the loaders.
+        NOTE: there's a mixed type for self.cross_section. can be a dataframe if there are a number of species.
+
+        Parameters
+        ----------
+        filename : str
+            name of file to load
+        loader : str
+            name of loader to use. default is chimera.
+        view : str
+            if 'full_frame' is selected, the cross_section object also includes temperature and wavelength columns.
+
+        Returns
+        -------
+        nothing
+        """
+        self.filename = filename
+        self.load_obj = self._get_loader(loader)
+        self.wl, self.T, self.cross_section = self.load_obj.load(self.filename)
+
+        # at least for the exotransmit case, we have...wl x temp.
+        if view == "full_frame":
+            self.cross_section["temp"] = self.T
+            self.cross_section["wav"] = self.wl
+
+        self.n_wav, self.n_t = len(self.wl), len(self.T)
+
+        return
+
+    def join_cross_section(self, opac):
+        """
+        joins another opacity's cross-section data to this one.
+        :param opac:
+        :return:
+        """
+
+        def replace_zeros(species):
+            mask = self.cross_section[species] == 0.0
+            self.cross_section.loc[mask, species] = opac.cross_section.loc[
+                mask, species
+            ]
+
+        # throw errors if the grids don't match.
+        if np.all(opac.T != self.T):
+            raise ValueError("temperatures do not match!")
+        if np.all(opac.wl != self.wl):
+            raise ValueError("wavelengths do not match!")
+
+        # add the entirely new columns
+        other_columns = opac.cross_section.columns
+        for column in other_columns:
+            if column not in self.cross_section.columns:
+                self.cross_section[column] = opac.cross_section[column]
+
+        # now fill in any temperatures where the cross-section is zero here.
+        for species in self.cross_section.columns:
+            if species in other_columns:
+                replace_zeros(species)
+
+        return
