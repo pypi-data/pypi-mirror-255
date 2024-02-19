d46ef5a873cdea85d40df055fa67cf49e0b952a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 03:05:23 +0000

    Add package sciform/0.34.1/sciform-0.34.1-py3-none-any.whl

---
+++ packages/sciform/sciform-0.34.1-py3-none-any.whl/sciform/scinum.py
@@ -0,0 +1,59 @@
+"""The SciNum class provides users access to sciform FSML."""
+
+from __future__ import annotations
+
+from decimal import Decimal
+from typing import TYPE_CHECKING
+
+from sciform.formatting import FormattedNumber, format_from_options
+from sciform.fsml import format_options_from_fmt_spec
+
+if TYPE_CHECKING:  # pragma: no cover
+    from sciform.format_utils import Number
+
+
+class SciNum:
+    """
+    Single number, or number and uncertainty, to be used with FSML.
+
+    :class:`SciNum` objects represent single numbers, or
+    number/uncertainty pairs to be formatted using the :mod:`sciform`
+    format specification mini-language for scientific formatting of
+    numbers. Any options not configured by the format specification will
+    be populated with global default settings at format time.
+
+    >>> from sciform import SciNum
+    >>> num = SciNum(12345.54321)
+    >>> print(f"{num:!3f}")
+    12300
+    >>> print(f"{num:+2.3R}")
+    + 12.346E+03
+    >>> num = SciNum(123456.654321, 0.0234)
+    >>> print(f"{num:#!2r()}")
+    (0.123456654(23))e+06
+    """
+
+    def __init__(
+        self: SciNum,
+        value: Number,
+        uncertainty: Number | None = None,
+        /,
+    ) -> None:
+        self.value = Decimal(str(value))
+        if uncertainty is None:
+            self.uncertainty = uncertainty
+        else:
+            self.uncertainty = Decimal(str(uncertainty))
+
+    def __format__(self: SciNum, fmt: str) -> FormattedNumber:
+        input_options = format_options_from_fmt_spec(fmt)
+        return format_from_options(
+            self.value,
+            self.uncertainty,
+            input_options=input_options,
+        )
+
+    def __repr__(self: SciNum) -> str:
+        if self.uncertainty is not None:
+            return f"{self.__class__.__name__}({self.value}, {self.uncertainty})"
+        return f"{self.__class__.__name__}({self.value})"
