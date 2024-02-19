37cdee48c0e00b5742cd361ac01f7cf20c8818ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 00:08:22 +0000

    Add package ChirePyToolkit/1.0.1/ChirePyToolkit-1.0.1-py3-none-any.whl

---
+++ packages/ChirePyToolkit/ChirePyToolkit-1.0.1-py3-none-any.whl/ChirePyToolkit/Setting.py
@@ -0,0 +1,6 @@
+
+
+WIN_7_64_HEADER = {
+    "user-agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36 Edg/109.0.1518.140"
+}
+
---
+++ packages/ChirePyToolkit/ChirePyToolkit-1.0.1-py3-none-any.whl/ChirePyToolkit/__init__.py
@@ -0,0 +1,5 @@
+from ChirePyToolkit.openBi.BI import BI
+
+
+def about():
+    print("炽热开发的工具库~")
